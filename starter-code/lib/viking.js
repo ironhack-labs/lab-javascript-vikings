bcde3aef2a1f5049d8de5efcf1313f19a93154ab
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-01-25 14:24:41 +0100

    notDone. passed 12 tests.

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,11 +19,41 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier () {
+  this.health = 300;
+  this.strength = 150;
+  this.attack = function (){
+    return this.strength;
+  };
+  this.receiveDamage = function (damage){
+    this.health -= damage;
+  };
+}
 
-function Viking () {}
+function Viking (name) {
+  Soldier.call(this);
+  this.name = name;
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+    // this.super();
+    return this.health <= 0 ?
+      (name + ' has died in act of combat') :
+      (name + ' has received ' + damage + ' points of damage');
+  };
+  this.battleCry = function (){
+    return "Odin Owns You All!";
+  };
+}
 
-function Saxon () {}
+Viking.prototype = Object.create(Soldier.prototype);
+
+
+function Saxon () {
+  Soldier.call(this);
+  
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
 
 function War () {}
 
