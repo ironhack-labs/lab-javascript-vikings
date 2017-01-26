fda74b14b33e34eaf00f5bc57d448ede5cddec46
Suazolc <suazolc@gmail.com>
2017-01-26 09:28:29 +0100

    done

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,9 +19,35 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function(){
+    return this.strength;
+  };
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+  };
+}
 
-function Viking () {}
+
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if (this.health < 0){
+      return this.name + " has died in act of combat";
+    }
+    return this.name + " has received " + damage + " points of damage";
+  };
+  this.battleCry = function(){
+    console.log("Odin Owns You All!");
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 function Saxon () {}
 
