7bb14027e6633168a64202c854d5dc9020484748
Alice Guan <aliguan726@gmail.com>
2017-02-18 08:18:27 -0500

    28 passing, 2 failing tests

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,13 +19,88 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier () {
+  this.health = 300;
+  this.strength = 150;
+  this.attack = function () {
+  };
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+  };
+}
 
-function Viking () {}
+Viking.prototype = Object.create(Soldier.prototype);
 
-function Saxon () {}
+function Viking (name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+  this.attack = function (strength) {
+  };
+  this.receiveDamage = function(damage) {
+    console.log(name + ' has received ' + damage + ' points of damage.');
+    this.health = health - damage;
+    if(this.health <= 0) {
+      return name + ' has died in act of combat';
+    }
+    return this.health;
+  };
+  this.battleCry = function () {
+    return "Odin Owns You All!";
+  };
 
-function War () {}
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+function Saxon (health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function (strength) {
+  };
+  this.receiveDamage = function(damage) {
+    console.log( 'A Saxon has received ' + damage + ' points of damage.');
+    this.health = health - damage;
+    console.log(this.health);
+    if(this.health <= 0) {
+      return 'A Saxon has died in combat';
+    }
+    return this.health;
+  };
+}
+
+var oberinth = new Viking('Oberinth Hnkui', 600, 170);
+var ragnhild = new Viking('Ragnhild Rogle', 200, 270);
+var solveig = new Viking('Solveig Danhsk', 1000, 70);
+var yngvild = new Viking('Yngvild', 500, 100);
+var vikingspool = [oberinth, ragnhild, solveig, yngvild];
+
+var createSaxon = new Saxon(60, 25);
+
+function War () {
+  this.vikingsArmy = [];
+  this.addViking = function(viking) {
+    var number = Math.round(Math.random()*3);
+    this.vikingsArmy.push(vikingspool[number]);
+    return this.vikingsArmy;
+  };
+
+  this.saxonsArmy = [];
+  this.addSaxon = function(saxon) {
+    this.saxonsArmy.push(createSaxon);
+  };
+
+  this.saxonAttack = function() {
+    return viking.name + ' has received ' + saxon.strength + ' points of damage';
+  };
+
+  this.vikingAttack = function() {
+    this.saxonsArmy.shift();
+    return 'A Saxon has died in combat';
+  };
+
+
+}
 
 module.exports = {
   Soldier: Soldier,
