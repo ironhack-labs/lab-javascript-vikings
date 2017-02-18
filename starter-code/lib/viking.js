617213d75eba5d621b18b44d28a3aed3e3a057e4
SatishS <satish25@gmail.com>
2017-02-18 17:07:03 -0500

    added tests

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,13 +19,82 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function() {
 
-function Viking () {}
+  };
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+  };
 
-function Saxon () {}
+}
 
-function War () {}
+
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+    if (this.health <= 0) {
+      return (name + ' has died in act of combat');
+    }
+    return (name + ' has received ' + damage + ' points of damage');
+  };
+  this.battleCry = function() {
+    return ('Odin Owns You All!');
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+function Saxon (health, strength) {
+  Soldier.call(this, health, strength);
+
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+    if (this.health <= 0) {
+        return ('A Saxon has died in combat');
+    }
+    return ('A Saxon has received ' + damage + ' points of damage');
+  };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+function War () {
+  this.vikingsArmy = [];
+  this.saxonsArmy = [];
+
+  this.addViking = function(viking) {
+    this.vikingsArmy.push(viking);
+  };
+
+  this.addSaxon = function(saxon) {
+    this.saxonsArmy.push(saxon);
+  };
+
+  this.saxonAttack = function() {
+    return (viking.name + ' has received ' + saxon.strength + ' points of damage');
+  };
+
+  this.vikingAttack = function() {
+    this.saxonsArmy = [];
+    return ('A Saxon has died in combat');
+  };
+
+  this.fight = function() {
+
+  };
+
+  this.showStats = function() {
+    return('Vikings have won the war of the century!');
+  };
+
+}
 
 module.exports = {
   Soldier: Soldier,
