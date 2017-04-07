4f057b9d7809414b16089dad5126005c724ac788
edsonjmv <edsonjmv@gmail.com>
2017-04-07 17:07:25 +0200

    Vikings ex

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,13 +19,48 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier (health, strength, attack, receiveDamage) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function(){
+    return strength;
+  };
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+    return this.name + ' has received ' + damage + ' points of damage';
+  };
+}
 
-function Viking () {}
+Viking.prototype = Object.create(Soldier.prototype);
 
-function Saxon () {}
 
-function War () {}
+function Viking (name, health, strength, attack, receiveDamage, battleCry) {
+  Soldier.call(this, health, strength, attack, receiveDamage);
+  this.name = name;
+  this.battleCry = function(){
+    return ("Odin Owns You All!");
+  };
+}
+
+
+if (this.health <= 0) {
+  return (this.name + " has died in act of combat");
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+function Saxon (health, strength, attack, receiveDamage) {
+  Soldier.call(this, health, strength, attack);
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+    return  'A Saxon has received ' + damage + ' points of damage';
+  };
+}
+
+function War (vikingsArmy, saxonsArmy, addViking) {
+  this.vikingsArmy = [];
+  this.saxonsArmy = [];
+}
 
 module.exports = {
   Soldier: Soldier,
