ac5aa85c8c6f893918a10ba8a66795fe44def47c
Rafael Melón <info@rafaelmelon.es>
2017-04-07 17:14:47 +0200

    Primer commit

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,9 +19,32 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier (health,strength) {
+  this.health = health;
+  this.strength = strength;
+}
 
-function Viking () {}
+Soldier.prototype.attack = function (){
+  return strength;
+};
+Soldier.prototype.receiveDamage = function(damage){
+  this.health = this.health - damage;
+};
+
+function Viking (name,health,strength) {
+  Soldier.call(this, health,strength);
+  this.name = name;
+  this.battleCry = function(){
+    console.log("Odin Owns You All!!");
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+//var viking1 = new Viking("Pepe",100,100);
+//console.log(viking1.receiveDamage(45));
+
 
 function Saxon () {}
 
