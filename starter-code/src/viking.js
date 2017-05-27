c3efdda7c3904dc557e0551d8baea93aa2d2b7fe
polgasull <pol89.gn@gmail.com>
2017-05-27 10:49:16 +0200

    done but battle didn't finish

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -19,7 +19,7 @@ function Viking(name, health, strength) {
     if (this.health > 0) {
       console.log(name + " has received " + damage + " points of damage");
     }
-    else if (this.health <= 0) {
+    else {
       console.log(name + " has died in act of combat");
     }
   };
@@ -36,7 +36,7 @@ function Saxon(name, health, strength) {
     if (this.health > 0) {
       console.log("A Saxon has received " + damage + " points of damage");
     }
-    else if (this.health <= 0) {
+    else {
       console.log("A Saxon has died in act of combat");
     }
   };
@@ -46,18 +46,19 @@ function Saxon(name, health, strength) {
 function War (newVikings, newSaxons) {
   this.vikingArmy = [];
   this.saxonArmy = [];
-  this.addViking = function(Viking) {
-    this.vikingArmy.push(Viking);
+  this.addViking = function(viking) {
+    this.vikingArmy.push(viking);
   };
-  this.addSaxon = function(Saxon) {
-    this.saxonArmy.push(Saxon);
+  this.addSaxon = function(saxon) {
+    this.saxonArmy.push(saxon);
   };
   console.log(this.vikingArmy);
 
   this.vikingAttack = function() {
     var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
-    var doDamage = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
+
+    this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
     if (this.saxonArmy[randomSaxon].health <= 0) {
       console.log("I'm a dead Saxon");
       this.saxonArmy.splice(randomSaxon, 1);
@@ -67,7 +68,8 @@ function War (newVikings, newSaxons) {
   this.saxonAttack = function() {
     var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
-    var doDamage = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+
+    this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
     if (this.vikingArmy[randomViking].health <= 0) {
       console.log("I'm a dead Viking");
       this.vikingArmy.splice(randomViking, 1);
@@ -117,9 +119,8 @@ console.log(newVikings, newSaxons);
 
 // This function must activate the game
 function play() {
-
   console.log("The Battle between Vikings and Saxons is about to start");
-while (startBattle.vikingArmy.length !==0 && startBattle.saxonArmy.length !== 0) {
+while (startBattle.vikingArmy.length !== 0 && startBattle.saxonArmy.length !== 0) {
     startBattle.vikingAttack();
     startBattle.saxonAttack();
   }
