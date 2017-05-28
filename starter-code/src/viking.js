75aa9f22b2f1cdfaa3320567d2225dd4773897ac
Manuel Valle <vallerem17@gmail.com>
2017-05-28 16:50:16 +0200

    Functional

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -7,8 +7,8 @@ function Soldier (health,strength) {
   };
   this.receiveDamage = function(damage){
     this.health -= this.damage;
-    if(this.health< 0) {
-      this.health=0;
+    if(this.health < 0) {
+      this.health = 0;
   }
 };
 }
@@ -22,7 +22,7 @@ function Viking (name, health, strength) {
     if (this.health > 0){
       return this.name + " has received " + damage +  " points of damage";
     }
-    else if (this.health <= 0){
+    else {
       return this.name + " has died in act of combat";
     }
   };
@@ -42,7 +42,7 @@ function Saxon (health, strength) {
     if (this.health > 0){
       return "A saxon has received " + damage +  "points of damage";
     }
-    else if (this.health <= 0){
+    else {
       return "A saxon has died in combat";
     }
   };
@@ -61,8 +61,9 @@ function War (vikingArmy, saxonArmy) {
   this.vikingAttack = function(){
      var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
      var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
-     var injuredSaxon = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
-     if(this.saxonArmy[randomSaxon].health <= 0){
+     var injuredSaxon = this.saxonArmy[randomSaxon]
+                        .receiveDamage(this.vikingArmy[randomViking].strength);
+     if (this.saxonArmy[randomSaxon].health <= 0){
        this.saxonArmy.splice(randomSaxon, 1);
      }
      return injuredSaxon;
@@ -73,16 +74,16 @@ function War (vikingArmy, saxonArmy) {
      var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
      var injuredViking = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
      if (this.vikingArmy[randomViking].health <= 0){
-       this.vikingArmy.splice(randomViking, 1);
+         this.vikingArmy.splice(randomViking, 1);
      }
      return injuredViking;
    };
 
   this.showStatus = function(){
-    if (this.saxonArmy.length <= 0){
+    if (this.saxonArmy.length === 0){
       return "Vikings have won the war of the century!";
     }
-    else if (this.vikingArmy.length <= 0){
+    else if (this.vikingArmy.length === 0){
       return "Saxons have fought for their lives and survive another day...";
     }
     else {
@@ -129,22 +130,24 @@ var saxonObject = [
 var newWar = new War(vikingObject, saxonObject);
 
 
+//Display the war + status 
 
+while (newWar.vikingArmy.length !== 0 && newWar.saxonArmy.length !== 0){
 
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
-console.log(newWar.vikingAttack());
-console.log(newWar.saxonAttack());
+    console.log(newWar.vikingArmy);
+    console.log(newWar.saxonArmy);
 
+  if(newWar.vikingArmy.length !== 0 && newWar.saxonArmy.length !== 0){
+    console.log(newWar.vikingAttack());
 
-console.log(newWar.showStatus());
+  }
+
+  if(newWar.vikingArmy.length !== 0 && newWar.saxonArmy.length !== 0){
+    console.log(newWar.saxonAttack());
+  }
+
+  console.log("***************************");
+
+  console.log(newWar.showStatus());
+
+}
