fea1f21c58ea1c2a3fa4c5d244c1f470b7f0c5c2
polgasull <pol89.gn@gmail.com>
2017-05-28 21:18:35 +0200

    battle and attacks is working but the array is not working

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -2,11 +2,12 @@
 function Soldier (health, strength) {
   this.health = health;
   this.strength = strength;
+
   this.attack = function() {
     return this.strength;
   };
   this.receiveDamage = function(damage) {
-    this.health -= this.damage;
+    this.health -= damage;
   };
 }
 
@@ -14,8 +15,9 @@ function Soldier (health, strength) {
 function Viking(name, health, strength) {
   Soldier.call(this, health, strength);
   this.name = name;
+
   this.receiveDamage = function(damage) {
-    this.health -= this.damage;
+    this.health -= damage;
     if (this.health > 0) {
       console.log(name + " has received " + damage + " points of damage");
     }
@@ -32,7 +34,7 @@ function Viking(name, health, strength) {
 function Saxon(name, health, strength) {
   Soldier.call(this, health, strength);
   this.receiveDamage = function(damage){
-    this.health -= this.damage;
+    this.health -= damage;
     if (this.health > 0) {
       console.log("A Saxon has received " + damage + " points of damage");
     }
@@ -46,20 +48,16 @@ function Saxon(name, health, strength) {
 function War (newVikings, newSaxons) {
   this.vikingArmy = [];
   this.saxonArmy = [];
-  this.addViking = function(viking) {
-    this.vikingArmy.push(viking);
-  };
-  this.addSaxon = function(saxon) {
-    this.saxonArmy.push(saxon);
-  };
+
   console.log(this.vikingArmy);
+  console.log(this.saxonArmy);
 
   this.vikingAttack = function() {
     var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
 
     this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
-    console.log(this.saxonAmy[randomSaxon]);
+    console.log(this.saxonArmy[randomSaxon]);
     if (this.saxonArmy[randomSaxon].health <= 0) {
       console.log("I'm a dead Saxon");
       this.saxonArmy.splice(randomSaxon, 1);
@@ -78,7 +76,6 @@ function War (newVikings, newSaxons) {
   };
 
   this.showStatus = function() {
-    console.log(this.saxonArmy, this.vikingArmy);
     if (this.saxonArmy.length <= 0) {
       console.log("Vikings have won the war of the century!");
     }
@@ -92,31 +89,30 @@ function War (newVikings, newSaxons) {
 }
 
 // I'm adding the newSaxons inside WAR constructor
+
 var startBattle = new War(newVikings, newSaxons);
 
 // Variables that then I gonna use for names
 
-var vikingsName = ["Ragnar", "Bjorn", "Rollo", "Ubbe", "Ivar"];
+var vikingsName = ["Ragnar", "Bjorn", "Rollo", "Ubbe", "Ivar", "PolViking"];
 var saxonsName = ["Saxon1", "Saxon2", "Saxon3", "Saxon4", "Saxon5"];
 
 // this variable is iterating with map, but the health, and strength is inside because I want warriors with
 // his own and independent stats, now it's working!
 
-var newVikings = vikingsName.map(function(vikName) {
+  var newVikings = vikingsName.forEach(function(vikName) {
   var randomVikingHealth = Math.floor(Math.random() * 10) + 1;
   var randomVikingStrength = Math.floor(Math.random() * 20) + 1;
     console.log(vikName, "  Con ganas de aplastar cráneo Sajón");
-    new Viking(vikName, randomVikingHealth, randomVikingStrength);
+    startBattle.vikingArmy.push(new Viking(vikName, randomVikingHealth, randomVikingStrength));
 });
 
-var newSaxons = saxonsName.map(function(saxName) {
+var newSaxons = saxonsName.forEach(function(saxName) {
   var randomSaxonHealth = Math.floor(Math.random() * 20) + 1;
   var randomSaxonStrength = Math.floor(Math.random() * 10) + 1;
   console.log(saxName, "  Con ganas de matar Vikingos");
-  new Saxon(saxName, randomSaxonHealth, randomSaxonStrength);
+  startBattle.saxonArmy.push(new Saxon(saxName, randomSaxonHealth, randomSaxonStrength));
 });
-
-console.log(newVikings, newSaxons);
 
 // This function must activate the game
 
