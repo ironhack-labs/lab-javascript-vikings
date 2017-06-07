22129b0540696dc75fb5471d93bf72902836e2f0
jsrj <rj.javadev@icloud.com>
2017-06-07 15:15:04 -0400

    MIA Robert Jones and Nick Hoats

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,43 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function () {
+  return this.strength;
+  };
+  this.receiveDamage = function (theDamage) {
+  this.health -= theDamage;
+  };
+}
 
+
+Viking.prototype = Object.create(Soldier.prototype);
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.receiveDamage = function (theDamage) {
+  this.health -= theDamage;
+  if (this.health > 0) {
+    return name + ' has received ' + theDamage + ' points of damage';
+  }
+  else {
+    return (name + " has died in act of combat");
+  }
 
+  };
+  this.battleCry = function () {
+    return "Odin Owns You All!";
+  };
+}
+
+Saxon.prototype = Object.create(Viking.prototype);
+Saxon.prototype.name = "A Saxon";
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+  Soldier.call(this, health, strength);
+}
+
 
 // War
 function War () {}
