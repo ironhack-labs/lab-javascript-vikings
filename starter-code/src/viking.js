8d3467d4078b03f78cc5b9adc71bbe43b24cfed4
Mario Martin <mariomarsan7@gmail.com>
2017-06-16 13:11:08 +0200

    Soldier and Viking testing Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,37 @@
-// Soldier
-function Soldier () {}
+// Soldier constructor
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+};
 
 // Viking
-function Viking () {}
+Viking.prototype = Object.create(Soldier.prototype); // Inheritance Soldier's prototype
+
+// Constructor of viking
+function Viking(name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+  // Soldier.call(this, health, strength);
+}
+Viking.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  console.log(this.health);
+  if (this.health > 0)  {
+    return this.name + " has received " + damage + " points of damage";
+  }
+};
+
 
 // Saxon
-function Saxon () {}
+function Saxon() {}
 
 // War
-function War () {}
+function War() {}
