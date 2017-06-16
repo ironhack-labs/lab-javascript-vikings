720f3faea11792ed4d74749151f2f9fc54386faf
Mario Martin <mariomarsan7@gmail.com>
2017-06-16 16:29:46 +0200

    Implement War Object testing

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -11,27 +11,73 @@ Soldier.prototype.receiveDamage = function(damage) {
   this.health -= damage;
 };
 
-// Viking
-Viking.prototype = Object.create(Soldier.prototype); // Inheritance Soldier's prototype
-
 // Constructor of viking
 function Viking(name, health, strength) {
   this.name = name;
   this.health = health;
   this.strength = strength;
-  // Soldier.call(this, health, strength);
 }
+
+// Viking inherit from Soldier
+Viking.prototype = Object.create(Soldier.prototype);
+
+// Overrride method
 Viking.prototype.receiveDamage = function(damage) {
   this.health -= damage;
   console.log(this.health);
-  if (this.health > 0)  {
+  if (this.health > 0) {
     return this.name + " has received " + damage + " points of damage";
+  } else {
+    return this.name + " has died in act of combat";
+  }
+};
+
+Viking.prototype.battleCry = function() {
+  return "Odin Owns You All!";
+};
+
+
+// Saxon inherit from Soldier
+function Saxon(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+// Saxon inherit from Soldier
+Saxon.prototype = Object.create(Soldier.prototype);
+
+// Overrride method
+Saxon.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  console.log(this.health);
+  if (this.health > 0) {
+    return "A Saxon has received " + damage + " points of damage";
+  } else {
+    return "A Saxon has died in combat";
   }
 };
 
 
-// Saxon
-function Saxon() {}
-
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(viking) {
+  this.vikingArmy.push(viking);
+};
+
+War.prototype.addSaxon = function(saxon) {
+  this.saxonArmy.push(saxon);
+};
+
+War.prototype.vikingAttack = function() {
+
+  this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+
+  this.saxonArmy = this.saxonArmy.filter(function(obj) {
+    return obj.health > 0;
+  });
+
+};
