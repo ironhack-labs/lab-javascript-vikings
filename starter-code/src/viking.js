f5cc08636a0fbef7b8a67251037c6c7e021960d6
Juan Macias <yo@soyjuanmacias.com>
2017-06-16 13:12:05 +0200

    Los bárbaros ganaron

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,30 @@
 // Soldier
-function Soldier () {}
+function Soldier (healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+}
+Soldier.prototype.attack = function attack () {
+  return this.strength;
+};
+Soldier.prototype.receiveDamage = function receiveDamage (theDamage) {
+  this.health = healthArg - theDamage;
+};
 
 // Viking
-function Viking () {}
+// Prototype
+Viking.prototype = Object.create(Soldier.prototype);
+function Viking (name, healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+  this.name = name;
+}
+Viking.prototype.receiveDamage = function receiveDamage (theDamage) {
+  this.health = healthArg - theDamage;
+  /*if (this.health > 0){
+    console.log(name + " has received " + theDamage + " points of damage" );
+  }else {
+    console.log(name + " has died in act of combat");
+  }*/
+};
 
 // Saxon
 function Saxon () {}
