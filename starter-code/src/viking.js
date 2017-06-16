a9b27184ab686521e966f5f79d5a6849ad89316f
Arnau Guadall <arnau.guadall@gmail.com>
2017-06-16 15:51:30 +0200

    Initial commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,33 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
+
+
+function attack(){
+
+}
+
+
+
+
 
 // Viking
-function Viking () {}
+function Viking () {
+
+
+}
 
 // Saxon
-function Saxon () {}
+function Saxon () {
+
+
+}
 
 // War
-function War () {}
+function War () {
+
+
+}
+
