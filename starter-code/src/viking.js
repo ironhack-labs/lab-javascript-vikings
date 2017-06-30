b01c50e00cddc37d7d4ccedac58adc57a263dd5d
Salvatore Dziekan <sdziekan@gmail.com>
2017-06-30 13:00:03 +0200

    Salvatore and Vincent viking exercise

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,31 @@
 // Soldier
-function Soldier () {}
+function Soldier (healthArg, strengthArg) {
+  function attack () {
+    return Soldier.strengthArg
+  }
+  function receiveDamage (damage) {
+    soldier.healthArg -= damage
+  }
+}
 
 // Viking
-function Viking () {}
+function Viking (name)(
+  Soldier.call(this, name);
+
+  battleCry: function(){
+    return "Odin owns you all"
+  }
+  receiveDamage.call(this,the damage);
+  if (Viking.healthArg >0){
+    return (Viking.name + "is alive")
+  }else {
+    return (Viking.name+"is dead" )
+  }
+
+  Viking.prototype=Object.create(Soldier.prototype);
+
+
+) {}
 
 // Saxon
 function Saxon () {}
