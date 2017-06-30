adc4068a8f39a8cd466eccd8f102cc978fcfa912
Bénédicte Meignan <benedictemeignan@Benedictes-MacBook-Air.local>
2017-06-30 13:01:27 +0200

    Paris - benmeign + macslymo - 25% done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,21 @@
 // Soldier
-function Soldier () {}
+function Soldier (healthArg,strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function () {
+    return strengthArg;
+  };
+  this.receiveDamage = function(theDamage){
+    return this.health -= theDamage;
+  };
+}
+
+Soldier.prototype =
 
 // Viking
-function Viking () {}
+function Viking () {
+
+}
 
 // Saxon
 function Saxon () {}
