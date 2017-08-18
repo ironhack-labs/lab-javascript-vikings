4a802eec0754eb12b704ea53b7ac228f57e616d7
raquel500c <raquel500c@gmail.com>
2017-08-18 15:45:20 +0200

    Viking constructor function

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,24 @@
 // Soldier
-function Soldier () {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function() {
+    return strengthArg;
+  }
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+  }
+}
+
+
 
 // Viking
-function Viking () {}
+function Viking() {
 
+}
+Viking.prototype=Object.create(Soldier.prototype);
 // Saxon
-function Saxon () {}
+function Saxon() {}
 
 // War
-function War () {}
+function War() {}
