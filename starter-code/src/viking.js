a5b7b099072b9d41447cf2d57f1339affbe970b0
Cleaveit <lele@crackberry.local>
2017-08-18 16:24:26 +0200

    first commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,75 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+	this.health     = health;
+	this.strength   = strength;
+}
 
+Soldier.prototype.attack = function() {
+	return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+	this.health     = this.health - damage;
+}
+
+	
+	
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+	this.name = name;
+	Soldier.call(this, health, strength)
+}
 
+//Inheritance from soldier to viking
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.battleCry = function(){
+	return "Odin Owns You All!";
+}
+
+Viking.prototype.receiveDamage = function(damage){
+	this.health = this.health - damage;
+	if(this.health<1){
+		return this.name + " has died in act of combat";
+	} else {
+		return this.name + " has received "+ damage +" points of damage";
+	}
+}
+var viking = new Viking('Marco', 30, 10)
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+	Soldier.call(this, health, strength)
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+Saxon.prototype.receiveDamage = function(damage){
+	this.health = this.health - damage;
+	if(this.health<1){
+		return "A Saxon has died in combat";
+	} else {
+		return "A Saxon has received " + damage + " points of damage";
+	}
+}
+
+var saxon = new Saxon(25, 5)
+
+
 
 // War
 function War () {}
+	
+
+
+
+
+
+// Modify the Soldier constructor function and add 2 methods to its prototype: attack(), and receiveDamage().
+
+// constructor function
+
+// should receive 2 arguments (health & strength)
+// should receive the health property as its 1st argument
+// should receive the strength property as its 2nd argument
