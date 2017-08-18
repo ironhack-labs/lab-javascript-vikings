bb521001b58c3671f2b1a959db2b1b9911d07ef1
paolo <pablocardini5@gmail.com>
2017-08-18 15:42:55 +0200

    done first part

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,26 @@
 // Soldier
-function Soldier () {}
+function Soldier (health,strength) {
+  this.health=health
+  this.strength=strength
+  this.attack = function(){
+   return this.strength;
+  }
+  this.receiveDamage = function(damage){
+    this.health -= damage
+
+
+  }
+}
+
+
 
 // Viking
-function Viking () {}
+Viking.prototype=Object.create(Soldier.prototype)
+function Viking (name,health,strength) {
+  this.name= name 
+
+
+}
 
 // Saxon
 function Saxon () {}
