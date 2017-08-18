49e1f5c523819704e12b63199881ee5f5540f45c
trigoporres <santiagotrigoporres@gmail.com>
2017-08-18 15:38:20 +0200

    MAD - Santiago Francisco

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,44 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health ;
+  this.strength = strength;
+}
+
+  Soldier.prototype.attack = function() {
+    return this.strength;
+  };
+
+  Soldier.prototype.receiveDamage = function (damage){
+    this.health -= damage;
+  };
 
 // Viking
-function Viking () {}
+Viking.prototype = Object.create(Soldier.prototype);
+// Viking.prototype.constructor = Viking;
+
+function Viking (name, health, strength) {
+  this.name = name;
+  Soldier.call(this, health, strength);
+}
+
+Viking.prototype.receiveDamage = function (damage) {
+  var vikHealth = Soldier.call(this, receiveDamage);
+  if (vikHealth > health) {
+    return name + "has received " + damage + " points of damage";
+  } else {
+    return name + " has died in act of combat";
+  }
+};
+
+Viking.prototype.battleCry = function battleCry () {
+  return "Odin Owns You All";
+};
+
 
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+  Soldier.call(this, health, strength);
+}
 
 // War
 function War () {}
