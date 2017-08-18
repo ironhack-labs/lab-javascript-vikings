da4be93a61e46d2d0f5a2e8aabb88b4b00e4ada6
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-08-19 00:46:17 +0200

    All iterations done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -42,11 +42,11 @@ Saxon.prototype.constructor = Saxon;
 
 Saxon.prototype.receiveDamage = function (damage){
   this.health -= damage;
+  var result = "A Saxon has died in combat";
   if (this.health > 0){
-    return "A Saxon has received "+damage+" points of damage";
-  }else{
-    return "A Saxon has died in combat";
+    result = "A Saxon has received "+damage+" points of damage";
   }
+  return result;
 };
 
 
@@ -54,15 +54,49 @@ Saxon.prototype.receiveDamage = function (damage){
 function War () {
   this.vikingArmy = [];
   this.saxonArmy = [];
-  this.addViking = function(viking) {
-    Viking.call(this);
-    this.vikingArmy.push(viking);
-  };
-
-  this.addSaxon = function(saxon){
-    this.saxonArmy.push(saxon);
-  };
-  this.vikingAttack = function(){
-
-  };
 }
+
+War.prototype.addViking = function (viking) {
+  this.vikingArmy.push(viking);
+};
+
+War.prototype.addSaxon = function (saxon) {
+  this.saxonArmy.push(saxon);
+};
+
+War.prototype.vikingAttack = function(){
+    var rand1 = parseInt(Math.random()*((this.saxonArmy).length));
+    var rand2 = parseInt(Math.random()*((this.vikingArmy).length));
+    this.saxonArmy[rand1].health -= this.vikingArmy[rand2].strength;
+
+    var result = this.saxonArmy[rand1].name + " has received " + this.vikingArmy[rand2].strength + " points of damage";
+    if(this.saxonArmy[rand1].health<=0){
+      this.saxonArmy.splice(rand1,1);
+      result = "A Saxon has died in combat";
+      return result;
+    }
+};
+
+War.prototype.saxonAttack = function(){
+    var rand1 = parseInt(Math.random()*((this.saxonArmy).length));
+    var rand2 = parseInt(Math.random()*((this.vikingArmy).length));
+    this.vikingArmy[rand1].health -= this.saxonArmy[rand2].strength;
+
+    var result = this.vikingArmy[rand1].name + " has received " + this.saxonArmy[rand2].strength + " points of damage";
+    if(this.vikingArmy[rand1].health<=0){
+      this.vikingArmy.splice(rand1, 1);
+    }
+    return result;
+};
+
+War.prototype.showStatus = function(){
+    var result;
+    if (this.saxonArmy.length === 0){
+      result = "Vikings have won the war of the century!";
+    }else if (this.vikingArmy.length === 0) {
+      result= "Saxons have fought for their lives and survive another day...";
+    }else{
+      result= "Vikings and Saxons are still in the thick of battle.";
+    }
+    return result;
+};
