0f19c845186c59694e567cd9af184c20b04523a6
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-18 13:28:40 +0200

    Viking Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -15,8 +15,26 @@ Soldier.prototype.receiveDamage = function (damage) {
 }
 
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+  this.name = name
+  this.health = health
+  this.strength = strength
+}
 
+Viking.prototype = Object.create(Soldier.prototype)
+Viking.prototype.constructor = Viking
+
+Viking.prototype.receiveDamage = function (damage) {
+  var total = this.health -= damage
+  if(total <= 0) {
+    return this.name + ' has died in act of combat'
+  } else {
+    return this.name + ' has received ' + damage + ' points of damage'
+  }
+}
+Viking.prototype.battleCry = function () {
+  return 'Odin Owns You All!'
+};
 // Saxon
 function Saxon () {}
 
