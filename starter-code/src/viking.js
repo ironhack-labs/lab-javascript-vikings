82217a23ac33c7bb36e4799cd26ad96377c2dbd3
raquel500c <raquel500c@gmail.com>
2017-08-20 10:21:27 +0200

    Viking completo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -2,28 +2,78 @@
 function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
-  this.attack = function() {
-    return strength;
+  //attack method
+  Soldier.prototype.attack = function() {
+    return this.strength;
   };
-  this.receiveDamage = function(damage) {
+  //receiveDamage method
+  Soldier.prototype.receiveDamage = function(damage) {
     this.health -= damage;
   };
 }
 
-
-
 // Viking
 function Viking(name, health, strength) {
   this.name = name;
-Vicking.prototype.receiveDamage= funcion(damage){
-  this.health-=damage;
-  if(this.health>0){
+  //Call to parent constructor
+  Soldier.call(this, health, strength);
+}
 
+// prototype inherited from Soldier
+Viking.prototype = Object.create(Soldier.prototype);
+//assigns the own Viking constructor
+Viking.prototype.constructor = Viking;
 
-Viking.prototype=Object.create(Soldier.prototype);
-viking.prototype.constructor=Viking;
+//receiveDamage Method overwritten--> inherited from Soldier
+Viking.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return (this.name + " has received " + damage + " points of damage");
+  } else {
+    return (this.name + " has died in act of combat");
+  }
+};
+
+//battleCry method
+Viking.prototype.battleCry = function battleCry() {
+  return "Odin Owns You All!";
+};
+
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  //Call to parent constructor
+  Soldier.call(this, health, strength);
+}
+
+// create Saxon prototype inherited from Soldier
+Saxon.prototype = Object.create(Soldier.prototype);
+//fix Saxon constructor
+Saxon.prototype.constructor = Saxon;
+
+//attack Method  not need to change anything--> inherited from Soldier
+
+//receiveDamage Method overwritten--> inherited from Soldier
+Saxon.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return ("A Saxon has received " + damage + " points of damage");
+  } else {
+    return ("A Saxon has died in combat");
+  }
+};
 
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+//addViking Method
+War.prototype.addViking = function(Viking) {
+  this.vikingArmy.push(Viking);
+};
+
+//addSaxon Method
+War.prototype.addSaxon = function(Saxon) {
+  this.saxonArmy.push(saxon);
+};
