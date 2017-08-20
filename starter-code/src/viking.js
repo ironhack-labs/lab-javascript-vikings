bb149d1d0f5f7c8d31447f3d0c02206373c51f68
trigoporres <santiagotrigoporres@gmail.com>
2017-08-20 20:06:28 +0200

    Guerra terminada

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -68,3 +68,40 @@ War.prototype.addViking = function (Viking) {
 War.prototype.addSaxon = function (Saxon) {
     this.saxonArmy = [Saxon];
 };
+
+
+
+War.prototype.vikingAttack = function (){
+  var randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length));
+  var randomViking = Math.floor(Math.random() * (this.vikingArmy.length));
+  var damage = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
+  if (this.saxonArmy[randomSaxon].health <= 0){
+    this.saxonArmy.splice(randomSaxon, 1);
+  }
+  return damage;
+};
+
+War.prototype.saxonAttack = function (){
+  var randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length));
+  var randomViking = Math.floor(Math.random() * (this.vikingArmy.length));
+
+  var damage = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+  if (this.vikingArmy[randomViking].health <= 0){
+    this.vikingArmy.splice(randomViking, 1);
+  }
+  return damage;
+};
+
+War.prototype.showStatus = function (){
+  if (this.vikingArmy.length == 0) {
+    return "Saxons have fought for their lives and survive another day...";
+  }
+
+  else if (this.saxonArmy.length == 0){
+    return "Vikings have won the war of the century!";
+  }
+
+  return "Vikings and Saxons are still in the thick of battle.";
+
+
+};
