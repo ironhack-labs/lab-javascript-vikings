9ead03b6bb15cecd46efbdadc5d75a8c9f918f67
Frances Morales Velilla <frances0688@gmail.com>
2017-08-21 09:25:59 +0200

    done-no bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,54 @@
 // Soldier
-function Soldier () {}
+function Soldier(soldierHealth, soldierStrength) {
+	this.health = soldierHealth;
+	this.strength = soldierStrength;
+}
+
+Soldier.prototype.attack = function() {
+	return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+	this.health -= damage;
+};
 
 // Viking
-function Viking () {}
+function Viking(vikingName, vikingHealth, vikingStrength) {
+	Soldier.call(this, vikingHealth, vikingStrength);
+	this.name = vikingName;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.receiveDamage = function(damage) {
+	this.health -= damage;
+	if (this.health > 0) {
+		return this.name + " has received " + damage + " points of damage";
+	} else {
+		return this.name + " has died in act of combat";
+	}
+};
+Viking.prototype.battleCry = function() {
+	return "Odin Owns You All!";
+};
 
 // Saxon
-function Saxon () {}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+function Saxon(saxonHealth, saxonStrength) {
+	Soldier.call(this, saxonHealth, saxonStrength);
+}
+
+Saxon.prototype.receiveDamage = function(damage) {
+	this.health -= damage;
+	if (this.health > 0) {
+		return this.name + " has received " + damage + " points of damage";
+	} else {
+		return this.name + " has died in act of combat";
+	}
+};
 
 // War
-function War () {}
+function War() {}
