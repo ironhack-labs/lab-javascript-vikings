3ee1dab383500e3a608a8acc0ef6cf0917bb10b9
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-10-20 14:19:42 +0200

    MAD Laura - Angelina

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,38 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage){
+  this.health = this.health - damage;
+}
+
+// Soldier.receiveDamage(undefined);
 
 // Viking
-function Viking () {}
+function Viking () {
+  var viking;
+  var name     = "Harald";
+  var strength = 150;
+  var health   = 300;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+var myViking = new Viking(name, health, strength);
+
+
+
+// var soldier = new Viking(name, he)
+
+// Viking.prototype = .create
+
+// Soldier.attack()
 
 // Saxon
 function Saxon () {}
