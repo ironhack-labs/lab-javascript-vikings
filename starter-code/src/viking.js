0fe271264944c54cbf2f4b7298e1e2a35dcc954a
FranRom <maildefranz@gmail.com>
2017-10-20 14:28:00 +0200

    Working in  progress

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,23 @@
-// Soldier
-function Soldier () {}
+function Soldier (healthArg, strengthArg) {
+  this.health= healthArg;
+  this.strength= strengthArg;
+  this.attack= function (){
+   return this.strength;
+  };
+  this.receiveDamage= function (damage){
+  return this.health - damage;
+
+  };
+}
+
+var mySoldier = new Soldier(100, "Strong");
+
+console.log(mySoldier.health);
+
+mySoldier.attack();
+mySoldier.receiveDamage(75);
+
+function attack() {}
 
 // Viking
 function Viking () {}
