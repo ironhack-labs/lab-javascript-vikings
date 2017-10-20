cd56f9229842731bfd6747c5286fb31577cc39bb
FranRom <maildefranz@gmail.com>
2017-10-20 18:21:36 +0200

    Done, except Bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,15 +1,64 @@
-function Soldier (healthArg, strengthArg) {
-  this.health= healthArg;
-  this.strength= strengthArg;
-  this.attack= function (){
-   return this.strength;
-  };
-  this.receiveDamage= function (damage){
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.receiveDamage = function (damage){
     this.health = this.health - damage;
   };
 }
 
+Soldier.prototype.attack = function (){
+   return this.strength;
+  };
+
+
+  // Viking
+
+function Viking (name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+
+  this.receiveDamage = function (damage){
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    }
+    else {
+      return this.name + " has died in act of combat";
+    }
+  };
+  this.battleCry = function (){
+    return  "Odin Owns You All!";
+  };
+ }
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+//Saxon
+
+function Saxon (health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.receiveDamage = function (damage){
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    }
+    else {
+      return "A Saxon has died in combat";
+    }
+  };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+
+//INIT
+
 var mySoldier = new Soldier(100, "Strong");
+var newViking = new Viking(Rathnok, 150, "Superstrong");
 
 console.log(mySoldier.health);
 
@@ -18,13 +67,10 @@ mySoldier.receiveDamage(75);
 
 
 
-function attack() {}
 
-// Viking
-function Viking () {}
 
 // Saxon
-function Saxon () {}
+
 
 // War
 function War () {}
