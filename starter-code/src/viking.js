29f438faee7d088c63630e7dbbcea62b0380006c
gemaderus <hi@gemaderus.com>
2017-10-20 14:21:15 +0200

    Finished soldier.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(attack) {
+  this.health -= attack;
+};
+
 
 // Viking
-function Viking () {}
+function Viking (name, health, strength) {
+  Soldier.call(this, name, health, strength);
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.name = function() {
+  this.name = name;
+  this.health = health;
+};
+
 
 // Saxon
-function Saxon () {}
+function Saxon () {
+
+}
 
 // War
-function War () {}
+function War () {
+
+}
