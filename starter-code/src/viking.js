730e75ef3bf3f997238223b7fbf3c8d5a8a28c31
Clementina <mataclemy@gmail.com>
2017-10-20 14:19:40 +0200

    viking damage

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,35 @@
 // Soldier
-function Soldier () {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health>0){
+    return this.name + " has received " + damage + " point of damage "
+  }
+
+};
 
 // Viking
-function Viking () {}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+function Viking(name,health,strength) {
+this.name = name;
+this.health = health;
+this.strength = strength;
+
+
+
+}
 
 // Saxon
-function Saxon () {}
+function Saxon() {}
 
 // War
-function War () {}
+function War() {}
