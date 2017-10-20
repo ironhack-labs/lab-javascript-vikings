6016fb7aea40f46f0c6341cf8cef4e9e937645b5
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-20 18:05:04 +0200

    Soldier Viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,23 +1,40 @@
 // Soldier
-function Soldier (health,strength) {
+function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
 }
 
-Soldier.prototype.attack = function (){
+Soldier.prototype.attack = function() {
   return this.strength;
 };
 
-Soldier.prototype.receiveDamage = function(damage){
- this.health -= damage;
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
 };
 
 
 // Viking
-function Viking (name, health, strength) {}
+
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+attack();
+
+Viking.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (Viking > 0) {
+    return (name + " has received" + damage + "points of damage");
+  } else {
+    return (name + " has died in act of combat");
+  }
+};
 
 // Saxon
-function Saxon () {}
+function Saxon() {}
 
 // War
-function War () {}
+function War() {}
