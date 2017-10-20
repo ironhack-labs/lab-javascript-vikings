15ecedbd20e930461b4a0c3efb4c2bf7c0c731da
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-20 14:24:04 +0200

    Soldier finished

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,28 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+ this.health = health;
+ this.strength = strength;
+ }
+
+Soldier.prototype.attack = function (){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function (damage) {
+  this.health -= damage;
+}
 
 // Viking
-function Viking () {}
+function Viking (health, strength, name) {
+  Soldier.call(this,health,strength);
+  this.name = "Harald";
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+  var myViking = new Viking (300, 150, "Harald")
+// var myViking = new Soldier (150,300)
+
 
 // Saxon
 function Saxon () {}
