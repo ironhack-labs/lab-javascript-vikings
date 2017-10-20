ff75da0fb776ccf1358529620ab293bfa132f55d
sdediego <sergio.dediego@outlook.com>
2017-10-20 14:25:18 +0200

    Iteracion 1. Creating war scenario object

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,72 @@
 // Soldier
-function Soldier () {}
+function Soldier (health,strength) {
+    this.health =health;
+    this.strength = strength;
+    this.attack = function (){
+        return this.strength;
+    };
+    this.receiveDamage = function (damage) {
+        this.health -= damage;
+    };
+}
+
 
 // Viking
-function Viking () {}
+function Viking (name, health,strength) {
+    Soldier.call(this,health,strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.attack = function () {
+        return this.strength;
+    };
+    this.receiveDamage = function (damage){
+        this.health -= damage;
+        if (this.health!=0){
+        return this.name+ " has received " + damage +" points of damage";}
+        if (this.health<=0) {
+            return this.name + " has died in act of combat";
+        }
+    };
+    this.battleCry = function (){
+        return "Odin Owns You All!";
+    };
+
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon () {}
+function Saxon (health, strength) {
+    Soldier.call(this,health, strength);
+    this.health = health;
+    this.strength = strength;
+    this.attack = function (){
+        return this.strength;
+    };
+    this.receiveDamage = function (damage){
+        this.health -= damage;
+        if (this.health!=0){
+        return "A Saxon has received " + damage +" points of damage";}
+        if (this.health<=0) {
+            return "A Saxon has died in combat";
+        }
+    };
+}
 
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 // War
-function War () {}
+function War () {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    this.addViking = function (viking){
+        this.vikingArmy.push(viking);
+    };
+    this.addSaxon = function (saxon){
+        this.saxonArmy.push(saxon);
+    };
+
+}
