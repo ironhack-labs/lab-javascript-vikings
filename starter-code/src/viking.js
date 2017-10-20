6ac31c3c0d53565a5073b9553b75c46d224e9aa7
Asier Cisneros <ascisneros14@gmail.com>
2017-10-20 14:18:48 +0200

    Viking receive damage

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-function Soldier () {}
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(theDamage) {
+  this.health -= theDamage;
+};
 
 // Viking
-function Viking () {}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+function Viking (name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+}
+
+Viking.prototype.attack = function() {
+  return this.strength;
+};
+
+Viking.prototype.receiveDamage = function(theDamage) {
+  this.health -= theDamage;
+};
+
 
 // Saxon
 function Saxon () {}
