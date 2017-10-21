95e53bfcb66c3fcb4220b78e468ae36041117160
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-10-21 15:47:07 +0200

    Vikings-Done -AlbertoRivera

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -44,10 +44,38 @@ function Saxon (health,strength) {
       if(this.health>0){
         return "Has received "+ damage + " points of damage";
       }else{
-        return "Has died in act of combat";
+        return "A Saxon has died in combat";
       }
   };
 }
 
-// War
-function War () {}
+function War () {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+  this.addViking = function(viking){
+    this.vikingArmy.push(viking);
+  };
+  this.addSaxon = function (saxon){
+    this.saxonArmy.push(saxon);
+  };
+  this.vikingAttack = function(){
+
+    var result = this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+    if(this.saxonArmy[0].health <= 0) this.saxonArmy.splice(0, 1);
+    return result;
+  };
+  this.saxonAttack = function () {
+    var result = this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+    if(this.vikingArmy[0].health <= 0) this.vikingArmy.splice(0, 1);
+    return result;
+  };
+  this.showStatus = function () {
+    if (this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survive another day...";
+    } else {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  };
+}
