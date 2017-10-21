a6aa5a7673a41ea6df7926c47c38d26878fdedaa
gemaderus <hi@gemaderus.com>
2017-10-21 21:35:44 +0200

    Latest changes.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -15,21 +15,59 @@ Soldier.prototype.receiveDamage = function(attack) {
 
 // Viking
 function Viking (name, health, strength) {
-  Soldier.call(this, name, health, strength);
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
 }
 
 Viking.prototype = Object.create(Soldier.prototype);
 
 Viking.prototype.name = function() {
-  this.name = name;
+  Soldier.call(this, name, health, strength);
+};
+
+Viking.prototype.attack = function() {
+  return this.strength;
+};
+
+Viking.prototype.receiveDamage = function(attack) {
+  this.health -= attack;
+
+  if(this.health > 0) {
+    return (this.name + " has received " + attack + " points of damage");
+    } else {
+      return (this.name + " has died in act of combat");
+    }
+};
+
+Viking.prototype.battleCry = function() {
+  return ("Odin Owns You All!");
+};
+
+// Saxon
+function Saxon (health, strength) {
   this.health = health;
+  this.strength = strength;
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.attack = function() {
+  return this.strength;
+};
+
+Saxon.prototype.receiveDamage = function(attack) {
+  this.health -= attack;
+
+  if(this.health > 0) {
+    return ("A Saxon has received " + attack + " points of damage");
+    } else {
+      return ("A Saxon has died in combat");
+    }
 };
 
 
-// Saxon
-function Saxon () {
 
-}
 
 // War
 function War () {
