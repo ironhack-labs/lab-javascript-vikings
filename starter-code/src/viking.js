5bcfb9559b4abcc444e612073d93be857147b39b
Mario Velasco Alonso <mario1velasco@gmail.com>
2017-11-05 16:38:28 +0100

    Feat: Almost all done, just need to finesh the last 3 methods

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,23 +1,53 @@
 // Soldier
-function Soldier (health, strength) {
+function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
 }
-Soldier.prototype.attack = function () {
+Soldier.prototype.attack = function() {
   return this.strength;
 };
-Soldier.prototype.receiveDamage = function (damage){
-   this.health = this.health - damage;
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health = this.health - damage;
 };
 // Viking
-function Viking (name, health, strength) {
+function Viking(name, health, strength) {
   this.name = name;
-  this.health = health;
-  this.strength = strength;
+  // this.health = health;
+  // this.strength = strength;
+  Soldier.call(this, health, strength);
 }
-Viking.prototype = Soldier.prototype;
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.receiveDamage = function(damage) {
+  this.health = this.health - damage;
+  return (this.health) ? this.name + " has received " + damage + " points of damage" : this.name + " has died in act of combat";
+};
+Viking.prototype.battleCry = function() {
+  return "Odin Owns You All!";
+};
 // Saxon
-function Saxon () {}
-
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.receiveDamage = function(damage) {
+  this.health = this.health - damage;
+  return (this.health) ? "A Saxon has received " + damage + " points of damage" : "A Saxon has died in combat";
+};
 // War
-function War () {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+War.prototype.addViking = function (viking) {
+  if(viking instanceof Viking){
+    this.vikingArmy.push(viking);
+  }
+};
+War.prototype.addSaxon = function (saxon) {
+  if(saxon instanceof Saxon){
+    this.saxonArmy.push(saxon);
+  }
+};
+War.prototype.vikingAttack = function () {
+  
+};
