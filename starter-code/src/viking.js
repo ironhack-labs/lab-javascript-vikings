ede3dbed2888791854aa9dfb81a76283a3813c2c
Your Actual Name, In Quotes <Paloma@MacBook-Pro-de-Paloma.local>
2017-11-07 21:39:16 +0100

    With Changes

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,21 @@
 // Soldier
-function Soldier () {}
-
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function () {
+  return this.strength;
+};
+Soldier.prototype.receiveDamage = function (damage){
+   this.health = this.health - damage;
+};
 // Viking
-function Viking () {}
-
+function Viking (name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+}
+Viking.prototype = Soldier.prototype;
 // Saxon
 function Saxon () {}
 
