0d53e0296f61d2b57a528493343b010f372511cc
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-11-11 14:14:53 +0100

    feat:  Viking constructor ok

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -24,11 +24,28 @@ Soldier.prototype.receiveDamage = function(damage) {
 function Viking(name, health, strength) {
     this.name = name;
     Soldier.call(this, health, strength )
-}
-Viking.prototype = Soldier.prototype;
+} 
 //
-// Definimos el método "attack" del objeto Viking
-
+//heredamos sus Métodos  
+Viking.prototype = Object.create(Soldier.prototype);
+//
+// El método "attack" del objeto Viking ya no hay que declararlo pq lo hereda de SOLDIER
+// y tb habría heredado el método "receiveDamage" aunque lo queremos modificar:
+Viking.prototype.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+        return this.name + " has received " + this.damage + "  points of damage"
+    }
+    else {
+        return this.name + " has died in act of combat" 
+    }
+}
+//
+// definimos el método battleCry
+Viking.prototype.battleCry = function(){
+    return "Odin Owns You All!";
+    }
+    
 
 // Saxon
 function Saxon () {}
