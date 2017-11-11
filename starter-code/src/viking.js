bfb51dbc8ab873a512f46529d917ce06dd2ade8f
Juancho Arregui <juan.arregui.lopez@gmail.com>
2017-11-11 13:55:35 +0100

    feat: soldier ok

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,34 @@
-// Soldier
-function Soldier () {}
+//SOLDIER
+//
+// Función constructora del objeto Soldier
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
+//
+// Definimos el método "attack" del objeto Soldier
+Soldier.prototype.attack = function(){
+return this.strength;
+}
+//
+// Definimos el método "receiveDamage" del objeto Soldier
+Soldier.prototype.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+}
 
-// Viking
-function Viking () {}
+
+
+// VIKING
+//
+// Función constructora del objeto Viking
+function Viking(name, health, strength) {
+    this.name = name;
+    Soldier.call(this, health, strength )
+}
+Viking.prototype = Soldier.prototype;
+//
+// Definimos el método "attack" del objeto Viking
+
 
 // Saxon
 function Saxon () {}
