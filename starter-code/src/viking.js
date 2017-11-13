568741a298c13490912a870ee4a160a51a9e2b24
rafalopezgit <rafael.lopezgm@gmail.com>
2017-11-13 01:18:20 +0100

    Hecho, ya comparado con las respuestas que subisteis, incluyo una duda comentada en fila 53

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,56 @@
-// Soldier
-function Soldier () {}
+Soldier
+function Soldier (health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage){
+   this.health -= damage;
+}
 
 // Viking
-function Viking () {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.receiveDamage = function(damage){
+    Soldier.prototype.receiveDamage.call(this, damage);
+   if (this.health > 0) {
+     return this.name + " has received " + damage + " points of damage";}
+     else {
+      return this.name + " has died in act of combat";
+}}
+
+Viking.prototype.battleCry = function() {
+    return "Odin Owns You All!";
+}
+
 
 // Saxon
-function Saxon () {}
+
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.receiveDamage = function(damage) {
+  Soldier.prototype.receiveDamage.call(this, damage);
+
+  if (this.health > 0)
+  {return "A Saxon has received" + damage + "points of damage";
+    } else {
+      return "A Saxon has died in combat";
+    }
+}
+// ¿Por qué se pone el "call" en la fila 45 igual que en la 25?"
 
 // War
 function War () {}
