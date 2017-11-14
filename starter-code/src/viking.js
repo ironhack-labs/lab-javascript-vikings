49d291fc873d9d2c098a92be00cc05de9f018a86
Cristina Bertrand <cristina@11870.com>
2017-11-14 17:21:42 +0100

    FEAT: full exercise with war, aunque no tengo muy clara las funciones de ataque de saxon y vikings en war

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -70,21 +70,23 @@ War.prototype.addSaxon = function(saxon) {
   this.saxonArmy.push(saxon);
 };
 
-Saxon.prototype.receiveDamage =  function(damage) {
-    Soldier.prototype.receiveDamage.call(this, damage);
-    if (this.health > 0) {
-        return "A Saxon has received " + damage + " points of damage";
-    } else {
-        return "A Saxon has died in combat";
-    }
+//Vicking attack
+
+//Obtener un vikingo aleatorio
+//Obtener un saxon aleatorio
+
+War.prototype.random = function() {
+    return this.vikingArmy[Math.floor(Math.random() * this.length)];
 };
+
+War.prototype.random = function() {
+    return this.saxonArmy[Math.floor(Math.random() * this.length)];
+};
+
+//Eliminar el saxon si este ha muerto
 
 Soldier.prototype.isAlive = function() {
     return this.health > 0;
-};
-
-Array.prototype.random = function() {
-    return this[Math.floor(Math.random() * this.length)];
 };
 
 War.prototype.deleteSaxon = function (saxon) {
@@ -96,6 +98,9 @@ War.prototype.deleteSaxon = function (saxon) {
     }
 };
 
+//Que el vikingo ataque al saxon
+//Devolver el resultado del ataque del vikingo al saxon
+
 War.prototype.vikingAttack = function() {
     saxon = this.saxonArmy.random();
     viking = this.vikingArmy.random();
@@ -105,4 +110,29 @@ War.prototype.vikingAttack = function() {
         this.deleteSaxon(saxon);
     }
     return status;
+};
+
+//SAXON Attack
+
+war.prototype.saxonAttack = function () {
+    saxon = this.saxonArmy.random();
+    viking = this.vikingArmy.random();
+
+    status = viking.receiveDamage(saxon.attack());
+    if (!viking.isAlive()) {
+        this.deleteViking(viking);
+    }
+    return status;
+};
+
+//status
+
+War.prototype.showStatus = function() {
+     if (this.saxonArmy.length === 0) {
+         return "Vikings have won the war of the century!";
+     } else if (this.vikingArmy.length === 0) {
+         return "Saxons have fought for their lives and survive another day...";
+     } else if (this.saxonArmy.length === 1 && this.vikingArmy.length === 1) {
+         return "Vikings and Saxons are still in the thick of battle.";
+     }
 };
