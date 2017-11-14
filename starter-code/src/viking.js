e422957d57ff7f7fab5556f06bfdf881e05ba2df
Cristina Bertrand <cristina@11870.com>
2017-11-14 13:37:42 +0100

    FEAT: Soldier, vikings, saxons y la parte de war de añadir vikings and saxon bien. La función de viking attackwar necesito explicación porque me hice un lío, la dejé pero tiene 3 errores

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,83 @@
 // Soldier
-function Soldier () {}
-
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+Soldier.prototype.receiveDamage = function (damage) {
+  this.health -= damage;
+};
 // Viking
-function Viking () {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+}
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.attack = function() {
+  return this.strength;
+};
+
+Viking.prototype.receiveDamage = function(damage) {
+  Soldier.prototype.receiveDamage.call(this, damage);
+  if (this.health > 0) {
+    return this.name + " has received " + damage + " points of damage";
+  }
+  else {
+    return this.name + " has died in act of combat";
+  }
+};
+
+Viking.prototype.battleCry = function() {
+  return "Odin Owns You All!";
+};
 
 // Saxon
-function Saxon () {}
+function Saxon(health,strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.attack = function() {
+  return this.strength;
+};
+
+Saxon.prototype.receiveDamage = function(damage) {
+  Soldier.prototype.receiveDamage.call (this, damage);
+  if (this.health > 0) {
+    return "A Saxon has received " + damage + " points of damage";
+  }
+  else {
+    return "A Saxon has died in combat";
+  }
+};
 
 // War
-function War () {}
+function War () {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(viking){
+  this.vikingArmy.push(viking);
+};
+
+War.prototype.addSaxon = function(saxon) {
+  this.saxonArmy.push(saxon);
+};
+
+War.prototype.vikingAttack = function() {
+  War.prototype.saxonreceiveDamage =function(damage) {
+    return saxon.health += vinking.strength;
+  };
+  var oldHealth = saxon.health;
+  if (saxon.health  > 0){
+    return saxon.health -= viking.strength;
+  } elseif (saxon.health = viking.strength); {
+    return "A Saxon has died in combat";
+  }
+};
