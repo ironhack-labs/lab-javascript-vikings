f080aea01004464f677879804d31485fb1af26a1
Kathia Rieser <kath.medulla@gmail.com>
2018-01-12 15:27:49 +0100

    done estructura de datos

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,88 @@
+
+
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  
+  this.health = healthArg;
+  this.strength = strengthArg;
+
+}
+
+Soldier.prototype.attack = function (){
+
+  return this.strength;
+  
+};
+
+Soldier.prototype.receiveDamage = function (damageArg){
+
+   this.health -=  damageArg;
+   
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+
+  Soldier.call(this, health, strength);
+  this.name = name;
+  
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.receiveDamage = function (damageArg){
+
+  this.health -=  damageArg;
+  
+  var mensaje;
+  
+  if(this.health <= 0){
+
+    mensaje = this.name + " has died in act of combat";
+
+  }else{
+
+    mensaje = this.name + " has received "+ damageArg +" points of damage";
+    
+  }
+  
+  return mensaje;
+  
+}
+
+Viking.prototype.battleCry = function(){
+
+  return "Odin Owns You All!";
+}
+
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.receiveDamage = function(damageArg){
+
+  this.health -=  damageArg;
+
+  var mensaje;
+  
+  if(this.health <= 0){
+
+    mensaje = "A Saxon has died in combat";
+
+  }else{
+
+    mensaje = "A Saxon has received "+ damageArg +" points of damage";
+    
+  }
+  
+  return mensaje;
+
+}
 
 // War
 function War() {}
