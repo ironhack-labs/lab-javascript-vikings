f140206b3e85e694512fd3f3e0de2777cd8d5a21
jesusdelrioc <jesusdelrioc@gmail.com>
2018-01-12 13:55:43 +0100

    vikingos

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,31 @@
 // Soldier
-function Soldier() {}
+function Soldier(health ,strength) {
+    this.health= health;
+    this.strength= strength;
+    this.attack= function(){
+        return this.strength
+    };
+    this.receiveDamage = function(damage){
+         this.health = this.health - damage;
+    }
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health , strength) {
+    this.name= name;
+    this.health= health;
+    this.strength= strength;
+}
+    
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+
+Soldier.prototype.attack = function(){
+    return this.strength
+};
+
+
 
 // Saxon
 function Saxon() {}
