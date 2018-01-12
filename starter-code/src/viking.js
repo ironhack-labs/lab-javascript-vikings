67648d6035d830bf6414e1636895f7036df884ae
Ana Recuero <anarecsa@gmail.com>
2018-01-12 18:22:08 +0100

    war with vikingAttack

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,73 @@
 // Soldier
-function Soldier() {}
+function Soldier(health ,strength) {
+    this.health= health;
+    this.strength= strength;
+    this.receiveDamage = function(damage){
+         this.health = this.health - damage;
+    }
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health , strength) {
+    this.name= name;
+    this.health= health;
+    this.strength= strength;
+    this.battleCry= function(){
+    return "Odin Owns You All!"
+    };
+    this.receiveDamage = function(damage){
+        this.health = this.health - damage;
+        if (this.health > 0){return this.name + " has received " + damage + " points of damage";}
+        else {return this.name + " has died in act of combat";}
+    };
+    
+}
+    
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Soldier.prototype.attack = function(){
+    return this.strength
+};
 
 // Saxon
-function Saxon() {}
+function Saxon( health , strength) {
+    this.health = health;
+    this.strength= strength;
+    this.receiveDamage = function(damage){
+        this.health = this.health - damage;
+        if (this.health > 0){return  "A Saxon has received "  + damage + " points of damage";}
+        else {return "A Saxon has died in combat";}
+    };
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
-function War() {}
+function War() {
+  var vikingArmy = [];
+  var saxonArmy = [];
+  this.vikingArmy = vikingArmy;
+  this.saxonArmy = saxonArmy;
+
+  this.addViking = function(viking){
+    vikingArmy.push(viking);
+  };
+  this.addSaxon = function(saxon){
+    saxonArmy.push(saxon);
+  };
+  this.vikingAttack = function(){
+    var randomSaxon = Math.floor(Math.random() * saxonArmy.length);
+    var randomViking = vikingArmy[Math.floor(Math.random() * vikingArmy.length)];
+    saxonArmy[randomSaxon].receiveDamage(randomViking.strength);
+    if (saxonArmy[randomSaxon].health < 1){
+      saxonArmy.splice(randomSaxon, 1);
+    }
+  };
+  this.showStatus = function(){
+
+  };
+  this.saxonAttack = function(){
+
+  };
+}
