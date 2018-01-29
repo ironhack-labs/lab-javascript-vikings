5a1b6a50e486f5ac4946b124a8d5b7637e62aeb2
Ana Raballo <anitaraballo@gmail.com>
2018-01-29 14:23:15 -0500

    <Initial commit>

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+};
+
+
+Soldier.prototype.attack = function(){
+    return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage){
+    this.health -= damage
+}
 
 // Viking
-function Viking() {}
+function Viking() {
+    this.Soldier = name;
+    Soldier.call(this, health, strength);
+}
+
 
 // Saxon
 function Saxon() {}
