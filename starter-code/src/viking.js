1d436b2ac162c263f6da021d7d36776bfcd3a850
Jonathan Sanchez <jonathansanchez@Jonathans-MacBook-Pro.local>
2018-01-30 09:10:10 -0500

    first commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,26 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+}
+
+Soldier.prototype.attack = function () {
+    return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function (damage) {
+    this.health -= damage;
+};
 
 // Viking
-function Viking() {}
+function Viking(nameArg, healthArg, strengthArg) {
+    Soldier.call(this, healthArg, strengthArg);
+    this.name = nameArg;
+}
+
+Viking.prototype.receiveDamage = function (damage) {
+    if (nameArg;
+};
 
 // Saxon
 function Saxon() {}
