814b281c514eece0e08700ebf022a729b9137a9b
fredrik62 <hogstrom10@gmail.com>
2018-03-23 16:04:19 +0100

    bcn-dauv-fredrik

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,70 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+}
+
+Soldier.prototype.attack = function() {
+    return this.strengthArg;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+   return Soldier.healthArg - damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, healthArg, strengthArg);
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+
+
+ Viking.prototype.receiveDamage = function(damage) {
+     var currentHealth; 
+     currentHealth = Viking.health - damage;
+
+     if (currentHealth > 0) {
+         console.log(name, " has recieved", damage, "points of damage");
+     } else {
+         console.log(name, " has died in act of combat");
+     }
+ }
+
+
+// Solider.call(attack);
+// g.prototype.battleCry = function () {
+
+// console.log("odin owns you all");
+
+// }
+
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+
+
+     Soldier.call(this, healthArg, strengthArg);
+
+     Solider.call(attack);
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+    Saxon.prototype.receiveDamage = function(damage) {
+
+        var currentHealth; 
+        currentHealth = Saxon.health - damage;
+    
+        if (currentHealth > 0) {
+         console.log(name, " has recieved", damage, "points of damage");
+        } else {
+            console.log(name, " has died in act of combat");
+        }
+
+}
 
 // War
 function War() {}
