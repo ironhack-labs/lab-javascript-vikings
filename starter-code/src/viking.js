4b698e8ae3ce1f5441a81fc350b76bf0a7767fa8
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-03-23 13:45:08 +0100

    23 tests pasados, terminado vikingo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,32 @@
 // Soldier
-function Soldier(healthArg, strengthArg) {}
+function Soldier(healthArg, strengthArg) {
   this.health = healthArg;
   this.strength = strengthArg;
+  }
+
+Soldier.prototype.attack = function() {
+    return this.strength ;
+};
+Soldier.prototype.receiveDamage  = function (damage) {
+    this.health -= damage;
+};
+
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  Soldier.call(this, health, strength);
+}
+Viking.prototype.receiveDamage  = function (damage) {
+  this.health -= damage;
+
+  if(this.health > 0){
+    return this.name + " has received " +damage+ " points of damage";
+  } else {
+    return this.name + " has died in act of combat"
+  }
+};
+Viking.prototype = Object.create(Soldier.prototype);
+
 
 // Saxon
 function Saxon() {}
