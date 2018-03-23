db059578fe87bdd00c21365dfa30c9519376f4f4
muflonex <muflonex@gmail.com>
2018-03-23 14:12:18 +0100

    Aquí andando por sajones. Hasta ahora solo mi propia torpeza con corchetes sigue siendo mi enemigo interno principal ;P Beneficio mucho de haber hecho lo de rover con constructores, aunque la parte de esto no lo tenía por falta de saber como se comportan las heredencias de funciones. Me ha ayudado mucho la lección de hoy en entender la cosa :)

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,42 @@
 // Soldier
-function Soldier() {}
- 
-// Viking
-function Viking() {}
+function Soldier(health , strength) {
+ this.health = health;
+ this.strength = strength;
+}
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+Soldier.prototype.receiveDamage = function (damage){
+console.log(this)
+  if(this.hasOwnProperty('name')){
+    this.health = this.health - damage
+    if((this.health - damage)<0){
+      return this.name + " has died in act of combat"
+    }else{  
+      return this.name + " has received " + damage + " points of damage"
+    }
+  }else{
+    this.health = this.health - damage
+  }
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
+// Viking
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength)
+  this.name = name;
+}
+Viking.prototype.battleCry = function(){
+  return "Odin Owns You All!"
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength)
+}
 
 // War
 function War() {}
+
