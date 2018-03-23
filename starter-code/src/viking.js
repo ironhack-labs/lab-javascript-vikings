d32e353e2d00cc9e0fa85926d840ea93415d2cf6
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-03-23 14:15:19 +0100

    War saxonAttack method created

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -85,3 +85,20 @@ War.prototype.vikingAttack = function() {
     return "A Saxon has died in combat";
   }
 }
+
+War.prototype.saxonAttack = function() {
+  //random viking
+  var viking = this.vikingArmy[ Math.floor(Math.random()*(this.vikingArmy.length)) ];
+
+  //random saxon
+  var saxon = this.saxonArmy[ Math.floor(Math.random()*(this.saxonArmy.length)) ];
+
+  viking.receiveDamage( saxon.strength );
+
+  // remove dead vikings
+  this.vikingArmy = this.vikingArmy.filter( function(e) {
+    return e.health > 0;
+  });
+
+  return viking.name + " has received " + saxon.strength + " points of damage";
+}
