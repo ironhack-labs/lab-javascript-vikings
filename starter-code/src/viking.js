a542441aed682673f26ffded6fea863ab8a86e87
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-03-23 17:21:53 +0100

    54 test pasad0s, bonus guerra

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -16,20 +16,65 @@ function Viking(name, health, strength) {
   this.name = name;
   Soldier.call(this, health, strength);
 }
-Viking.prototype.receiveDamage  = function (damage) {
-  this.health -= damage;
-
-  if(this.health > 0){
-    return this.name + " has received " +damage+ " points of damage";
-  } else {
-    return this.name + " has died in act of combat"
-  }
-};
 Viking.prototype = Object.create(Soldier.prototype);
 
+Viking.prototype.receiveDamage  = function (damage) {
+  this.health -=damage;
+
+  if(this.health ==  0){
+    return this.name + " has died in act of combat";   
+  } else {
+    return this.name + " has received " +damage+ " points of damage";
+  }
+};
+
+Viking.prototype.battleCry  = function () {
+    return "Odin Owns You All!"
+};
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.receiveDamage  = function (damage) {
+  this.health -=damage;
+
+  if(this.health ==  0){
+    return "A Saxon has died in combat";
+  } else {
+    return "A Saxon has received " +damage+ " points of damage";
+  }      
+};
+
 
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(viking) {
+    this.vikingArmy.push(viking);
+}
+
+War.prototype.addSaxon = function(saxon) {
+  this.saxonArmy.push(saxon);
+}
+
+War.prototype.vikingAttack = function() {
+  var s = Math.floor(Math.random() * this.vikingArmy.length);
+  var v = Math.floor(Math.random() * this.saxonArmy.length);
+  //console.log(s.health);
+  //s.health = Saxon.prototype.receiveDamage(v.strength);
+}
+
+War.prototype.saxonAttack = function() {
+  
+}
+War.prototype.showStatus = function() {
+  
+}
