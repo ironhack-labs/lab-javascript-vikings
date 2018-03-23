6b1d2a4ae81bb906b6d72e0e216c803549779839
A-Tr <alvarotrancon@gmail.com>
2018-03-23 13:15:00 +0100

    avanzando con los vikingos

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -2,10 +2,21 @@
 function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
+  this.attack = function() {
+    return this.strength;
+  };
+  this.receiveDamage = function(attack) {
+    this.health -= attack;
+  };
 }
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
 function Saxon() {}
