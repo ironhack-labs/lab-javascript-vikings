783fedb43c0e08b01196bc68a250d11aafa3aa92
Pablo García <pablo.ramon.garcia.fernandez@gmail.com>
2018-03-23 18:01:40 +0100

    voy por el 37

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -6,7 +6,7 @@ function Soldier(healthArg, strengthArg) {
   this.attack = function () { return this.strength; };
   this.receiveDamage = function (damage) { this.health = this.health - damage; }; }
 
-// Viking
+// Viking constructor
 function Viking(nameArg, healthArg, strengthArg) {
   Soldier.call(this, healthArg, strengthArg);
   this.name = nameArg;
@@ -15,17 +15,51 @@ function Viking(nameArg, healthArg, strengthArg) {
     if (this.health > 0) {
       return this.name + " has received " + damage + " points of damage"; }
     else {
-      return this.name + " has received " + damage + " points of damage"; }
-    if (this.health < 1) {
       return this.name + " has died in act of combat";
     }
   };
+  this.battleCry = function () {
+    return "Odin Owns You All!";
+  };
 }
+// Unimos Viquingos al objeto Soldados, y apuntamos al constructor de Viquingos
 Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 
-// Saxon
-function Saxon() {}
+// Saxon constructor
+function Saxon(healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+  this.receiveDamage = function (damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage"; }
+    else {
+      return this.name + " has died in act of combat";
+    }
+  }
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
 
 // War
 function War() {}
+
+
+
+/* 
+
+// recibirZascas
+function receiveDamage(damage) {
+  this.health = this.health - damage;
+
+  if(this.name) {
+
+  }
+  if (this.health > 0) {
+    return this.name + " has received " + damage + " points of damage"; }
+  else {
+    return this.name + " has died in act of combat";
+  }
+}
+ */
