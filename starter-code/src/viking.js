aecd8edca28426654cb6b64d93be579d0defd3fc
aberouch <a.aberouch@gmail.com>
2018-03-23 13:44:32 +0100

    Done. Falta Bonus.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,54 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function(){
+      return this.strength;
+  }
+  this.receiveDamage = function(damage){
+        this.health =  this.health - damage;
+        }
+  
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this,health,strength);
+  this.name = name;
+  
+  this.receiveDamage = function(damage){
+    this.health = this.health - damage;
+    if (this.health > 0){
+       return (this.name + " has received " + damage + " points of damage");
+      }
+    else{
+      return (this.name + " has died in act of combat");
+    }
+  }
+
+  this.battleCry = function(){
+    return "Odin Owns You All!";
+  }
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
 
+  Soldier.call(this,health,strength);
+  this.receiveDamage = function(damage){
+    this.health = this.health - damage;
+
+    if (this.health > 0){
+        return ("A Saxon has received " + damage + " points of damage");
+    }
+    else{
+      return ("A Saxon has died in combat");
+    }
+  
+  }
+}
+Saxon.prototype = Object.create(Soldier.prototype);
 // War
 function War() {}
