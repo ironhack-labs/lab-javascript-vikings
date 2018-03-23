8389a275896a1df079268bfdb1a1542d1bef332e
lloren27 <llorenteyuste@hotmail.com>
2018-03-23 14:21:18 +0100

    Soldado Listo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,19 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health=health;
+    this.strength=strength;
+    
+
+ this.attack = function (){
+    return strength;
+    };
+
+Soldier.prototype.receiveDamage = function(damage){
+    
+    this.health -= damage; 
+};
+
+}
 
 // Viking
 function Viking() {}
