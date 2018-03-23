ef41406744de4b138a046a267f31e9672be7e781
aacecandev <aacecan.dev@gmail.com>
2018-03-23 13:34:54 +0100

    Primer commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,21 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.attack = function(){
+    return this.strength;
+  }
+
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.n
+}
 
 // Saxon
 function Saxon() {}
