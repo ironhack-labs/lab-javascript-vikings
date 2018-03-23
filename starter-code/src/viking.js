93f86ba9f7badaa260a98b627db69dec97292a6c
Pablo García <pablo.ramon.garcia.fernandez@gmail.com>
2018-03-23 14:24:59 +0100

    estoy haciendo la ultima del vikingo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,28 @@
-// Soldier
-function Soldier() {}
-
+// Soldier constructor
+function Soldier(healthArg, strengthArg) {
+  // should receive the health property as its 1st argument
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function () { return this.strength; };
+  this.receiveDamage = function (damage) { this.health = this.health - damage; }; }
 
 // Viking
-function Viking() {}
+function Viking(nameArg, healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+  this.name = nameArg;
+  this.receiveDamage = function (damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage"; }
+    else {
+      return this.name + " has received " + damage + " points of damage"; }
+    if (this.health < 1) {
+      return this.name + " has died in act of combat";
+    }
+  };
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
 function Saxon() {}
