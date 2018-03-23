657272aafddee93d2a8a30198862b80049c18a39
A-Tr <alvarotrancon@gmail.com>
2018-03-23 13:47:01 +0100

    creados los constructores de los soldados

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -5,21 +5,45 @@ function Soldier(health, strength) {
   this.attack = function() {
     return this.strength;
   };
-  this.receiveDamage = function(attack) {
-    this.health -= attack;
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
   };
 }
 
 // Viking
 function Viking(name, health, strength) {
-
+  this.name = name;
+  Soldier.call(this, health, strength);
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return name + " has received " + damage + " points of damage";
+    } else {
+      return name + " has died in act of combat";
+    }
+  };
+  this.battleCry = function() {
+    return "Odin Owns You All!";
+  };
 }
 
 Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else {
+      return "A Saxon has died in act of combat";
+    }
+  };
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
 function War() {}
