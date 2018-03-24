6eecea79971c4345ccfac5d06d9ec31343782d73
FernandoGalende <fernandogalende@gmail.com>
2018-03-24 21:33:15 +0100

    comprovando si el vikingo esta vivo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,13 +1,35 @@
 // Soldier
 function Soldier(healthArg, strengthArg) {
-  this.health = healthArg;
-  this.strength = strengthArg;
+	this.health = healthArg;
+	this.strength = strengthArg;
 }
-function attack(){
 
-}
+Soldier.prototype.attack = function() {
+	return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+	this.health = this.health - damage;
+	if (this.health > 0) {
+		return this.name + " has received "+ damage +" points of damage";
+	} else {
+		return this.name + " has died in act of combat";
+	}
+
+	return this.name;
+};
+
 // Viking
-function Viking() {}
+function Viking(name, healthArg, strengthArg) {
+	Soldier.call(this, healthArg, strengthArg);
+	this.name = name;
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.battleCry = function() {
+	return "Odin Owns You All!";
+};
 
 // Saxon
 function Saxon() {}
