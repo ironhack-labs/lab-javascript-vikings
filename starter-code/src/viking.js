a11faa4fa1f9b48859210a5fb632bb547017b937
Gabriel Cebrián <kunry3@hotmail.com>
2018-03-25 03:05:16 +0200

    Bonus parte completada, errores en funciones Attack

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -3,8 +3,8 @@ function Soldier(health, strength) {
 
     this.health = health;
     this.strength = strength;
-     
-    
+
+
 
 }
 Soldier.prototype.attack = function () {
@@ -17,22 +17,20 @@ Soldier.prototype.receiveDamage = function(damage) {
 // Viking
 function Viking(name, health, strength) {
     Soldier.call(this,health,strength);
-    this.name = name;    
-    
-    this.battleCry = function(){
-        return "Odin Owns You All!";
-    }
-    
+    this.name = name;
 }
 Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 Viking.prototype.receiveDamage = function(damage) {
     this.health -=  damage;
-    if (this.health === 0){
+    if (this.health <= 0){
         return this.name + " has died in act of combat";
     } else {
         return this.name + " has received " + damage + " points of damage";
     }
+}
+Viking.prototype.battleCry = function(){
+    return "Odin Owns You All!";
 }
 
 // Saxon
@@ -44,7 +42,7 @@ Saxon.prototype.constructor = Saxon;
 
 Saxon.prototype.receiveDamage = function(damage) {
     this.health -=  damage;
-    if (this.health === 0){
+    if (this.health <= 0){
         return "A Saxon has died in combat";
     } else {
         return "A Saxon has received " + damage + " points of damage";
@@ -55,20 +53,33 @@ function War() {
     this.vikingArmy = [];
     this.saxonArmy = [];
 
-    
+
 }
 
 
-War.prototype.addViking = function(Viking){
-    for(let i = 1; i < Viking.length ;i++)
-        this.vikingArmy.push(Viking[i]);
-    return this.vikingArmy;
+War.prototype.addViking = function(Viking){    
+    this.vikingArmy.push(Viking);
 }
 War.prototype.addSaxon = function(Saxon){
-    return this.saxonArmy;
+    this.saxonArmy.push(Saxon);
 }
 War.prototype.vikingAttack = function(){
-    
+    var numRanViking = Math.floor(Math.random()*this.vikingArmy.length);
+    var numRanSaxon = Math.floor(Math.random()*this.saxonArmy.length);
+    this.saxonArmy[numRanSaxon].receiveDamage(this.vikingArmy[numRanViking].strength);
+    if (this.saxonArmy[numRanSaxon].health <= 0){
+        this.saxonArmy.splice(numRanSaxon,1);
+    }
+    return this.saxonArmy[numRanSaxon].receiveDamage(this.vikingArmy[numRanViking].strength);
+
 }
-War.prototype.saxonAttack = function(){}
+War.prototype.saxonAttack = function(){
+    var numRanViking = Math.floor(Math.random()*this.vikingArmy.length);
+    var numRanSaxon = Math.floor(Math.random()*this.saxonArmy.length);
+    this.vikingArmy[numRanViking].receiveDamage(saxonArmy[numRanSaxon].strength);
+    if (this.vikingArmy[numRanViking].health <= 0){
+        this.vikingArmy.splice(numRanViking,1);
+    }
+    return this.vikingArmy[numRanViking].receiveDamage(saxonArmy[numRanSaxon].strength);
+}
 War.prototype.showStatus = function(){}
