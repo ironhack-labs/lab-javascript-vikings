f95c64d5c09aab4b60f464058512d5f94f2c56cd
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-04-10 11:30:16 -0400

    Add Viking const methods pass tests

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,23 +1,45 @@
 // Soldier
+
+// Soldier Constructor
 function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
 
+  // attack method
   Soldier.prototype.attack = function() {
     return this.strength;
   }
 
+  // receiveDamage method
   Soldier.prototype.receiveDamage = function(damage) {
     this.health -= damage;
   }
 }
 
-
-
-
-
 // Viking
-function Viking() {}
+
+// Viking Constructor. Inherits Soldier props and defines a name prop
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+
+  // receiveDamage method
+  Viking.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+    if(this.health > 0) {
+      return this.name + ' has received ' + damage + ' points of damage'
+    } else {
+      return this.name + ' has died in act of combat'
+    }
+  }
+
+  // battleCry method
+  Viking.prototype.battleCry = function() {
+    return 'Odin Owns You All!'
+  }
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
 function Saxon() {}
