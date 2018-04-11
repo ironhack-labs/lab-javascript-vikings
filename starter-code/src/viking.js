d68e719b30f16cb2635a09ff6734c3cf1a9ee41a
Brian Jaimes <jaimes.brian001@gmail.com>
2018-04-11 19:17:59 -0400

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,39 @@
-// Soldier
-function Soldier() {}
+ // Soldier
+ function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+}
+Soldier.prototype.attack = function (){
+return this.strength;
+}
+Soldier.prototype.receiveDamage = function (damage){
+this.health -=  damage;
+};
+
+
+
 
 // Viking
-function Viking() {}
 
-// Saxon
-function Saxon() {}
-
-// War
-function War() {}
+function Viking(name,healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+  this.name = name;
+  console.log(this.name);
+  }
+  Viking.prototype = Object.create(Soldier.prototype);
+  Viking.prototype.constructor = Viking;
+  //var viking = new Viking (name, healthArg, strengthArg);
+  
+  var viking = new Viking (name, health, strength);
+  console.log(viking);
+  
+  
+  Viking.prototype.receiveDamage = function (damage) {
+    viking.health -= damage;
+    if (viking.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    }
+    else {
+      return this.name + " has died in act of combat";
+    }
+  }
