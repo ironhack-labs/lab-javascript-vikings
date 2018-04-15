57145b31845d37d009185fe4bba7cf26d401871e
Teresa Plaz <teresaplaz@gmail.com>
2018-04-15 12:37:29 -0400

    war

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -6,20 +6,47 @@ function Soldier(health,strength) {
     return this.strength;
   };
   this.receiveDamage = function(damage) {
-      this.health = this.health - damage;
+      this.health -= damage;
   };
 }
 
 // Viking
+
+Viking.prototype = Object.create(Soldier.prototype);
+
 function Viking(name,health,strength) {
   Soldier.call(this,health,strength);
+  this.name = name;
   this.receiveDamage = function(damage) {
-    this.health = this.health - damage;};
-  this.battleCry = function(){};
+    this.health -= damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    }
+    else if (this.health <= 0) {
+      return this.name + " has died in act of combat";
+    }
+  };
+  this.battleCry = function(){
+    return "Odin Owns You All!";
+  };
 }
 
 // Saxon
-function Saxon() {}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+function Saxon(health,strength) {
+  Soldier.call(this,health,strength);
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    }
+    else if (this.health <= 0) {
+      return "A Saxon has died in combat";
+    }
+};
+}
 
 // War
 function War() {}
