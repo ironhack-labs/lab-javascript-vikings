a0f5dfad04c3007ea7fdc4fd256c5324a4662f29
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-04-17 15:48:09 -0500

    DONE

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -91,6 +91,12 @@ War.prototype.saxonAttack = function () {
 };
 
 War.prototype.showStatus = function () {
-
+  if (this.saxonArmy.length === 0) {
+    return 'Vikings have won the war of the century!';
+  } else if (this.vikingArmy.length === 0) {
+    return 'Saxons have fought for their lives and survive another day...';
+  } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+    return 'Vikings and Saxons are still in the thick of battle.';
+  }
 };
 
