988775f38ccda5d6d160fcffad8b123ada87aa7a
Alekz7 <ing.aragon.ipn@gmail.com>
2018-05-13 18:30:20 -0500

    MEXPT-ALEJANDRO_ARAGON_ALVAREZ

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,57 @@
 // Soldier
-function Soldier() {}
+function Soldier(health,strength) {
+    this.health = health;
+    this.strength = strength;       
+}
+Soldier.prototype.attack = function () {
+    return this.strength;
+}
+Soldier.prototype.receiveDamage = function (damageArg) {
+    this.health = this.health - 50;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    var viking;
+    this.name       = name;
+    this.health     = health;
+    this.strength   = strength;    
+}
+Viking.prototype.attack = function () {
+    return this.strength;
+}
+Viking.prototype.receiveDamage = function (damageArg) {
+    this.health = this.health - damageArg;
+    if (this.health <= 0) return this.name + " has died in act of combat";
+    else return this.name + " has received " + damageArg + " points of damage";
+}
+Viking.prototype.battleCry = function(){
+    return "Odin Owns You All!";
+}
 
-// Saxon
-function Saxon() {}
+// Saxon    
+function Saxon(health, strength) {
+    var viking;    
+    this.health     = health;
+    this.strength   = strength;    
+}
+Saxon.prototype.attack = function () {
+    return this.strength;
+}
+Saxon.prototype.receiveDamage = function (damageArg) {
+    this.health = this.health - damageArg;
+    if (this.health <= 0) return "A Saxon has died in combat";
+    else return "A Saxon has received " + damageArg + " points of damage";    
+}
 
 // War
-function War() {}
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+}
+War.prototype.showStatus = function () {
+}
+War.prototype.addViking = function () {
+}
+War.prototype.addSaxon = function () {
+}
