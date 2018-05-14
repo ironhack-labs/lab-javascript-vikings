f7ddc23ac8e22fe626500de05e988dae82f5fa23
Ravi Kalia <ravkalia@gmail.com>
2018-05-14 13:15:08 -0500

    vikings and saxons done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,13 +4,13 @@ function Soldier(health, strength) {
   this.strength = strength;
 }
 
-Soldier.prototype.attack = function(){
+Soldier.prototype.attack = function () {
   return this.strength;
-}
+};
 
-Soldier.prototype.receiveDamage = function(damage){
+Soldier.prototype.receiveDamage = function (damage) {
   this.health -= damage;
-}
+};
 // Viking
 function Viking(name, health, strength) {
   Soldier.call(this, health, strength);
@@ -19,9 +19,34 @@ function Viking(name, health, strength) {
 
 Viking.prototype = Object.create(Soldier.prototype);
 
+Viking.prototype.receiveDamage = function (damage) {
+  Soldier.receiveDamage.call(this, damage);
+  const msg = this.health > 0
+    ? `${this.name} has received ${this.damage} points of damage`
+    : '{this.name} has died in act of combat';
+  return msg;
+};
+
+Viking.prototype.battleCry = function () {
+  return ('Odin Owns You All!');
+};
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+
+Saxon.prototype.receiveDamage = function (damage) {
+  this.health -= damage;
+  const msg = this.health > 0
+    ? `A Saxon has received ${this.damage} points of damage`
+    : 'A Saxon has died in combat';
+  return msg;
+};
+
+Saxon.prototype = Object.create(Soldier.prototype);
 
 // War
-function War() {}
+function War() {
+
+}
