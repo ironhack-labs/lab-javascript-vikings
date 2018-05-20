e81db539133041e25a6d38ab3b03f45a9cbd2827
Alma Alonso <alma.aa93@gmail.com>
2018-05-20 14:25:04 +0200

    done right

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -2,55 +2,61 @@
 function Soldier(health, strength) {
     this.health = health;
     this.strength = strength;
-    this.attack = function(){
-        return this.strength;
-    }
-    this.receiveDamage = function(damage){
-        this.health -= damage;
-    }
+
+}
+
+Soldier.prototype.attack = function(){
+    return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage){
+     this.health -= damage;
 }
 // Viking
 
 Viking.prototype = Object.create(Soldier.prototype);
+// Viking.prototype.constructor = Viking;cual sería la diferencia de usar esto o no?
 
 function Viking(name, health, strength) {
     Soldier.call(this,health,strength);
     this.name = name;
+}
 
-    this.receiveDamage = function(damage){
-        this.health -= damage;
-        if (this.health > 0){
-            return this.name + " has received " + damage + " points of damage";
-        } else {
-            return this.name + " has died in act of combat";
-        }
-    }
 
-    this.battleCry = function(){
-        return "Odin Owns You All!"
+Viking.prototype.receiveDamage = function (damage){
+    Soldier.prototype.receiveDamage.call(this, damage);
+    if (this.health > 0){
+        return this.name + " has received " + damage + " points of damage";
+    } else {
+        return this.name + " has died in act of combat";
     }
+}
+
+Viking.prototype.battleCry = function(){
+    return "Odin Owns You All!"
 }
 
 // Saxon
 Saxon.prototype = Object.create(Soldier.prototype);
+// Saxon.prototype.constructor = Soldier; cual sería la diferencia de usar esto o no?
 
 function Saxon(health, strength) {
-    Soldier.call(this, health,strength);
-    this.attack = function(){
-        return this.strength;
-    }
-    this.receiveDamage = function(damage){
-        this.health -= damage;
-        if (this.health > 0){
-            return "A Saxon has received " + damage + " points of damage";
-        } else {
-            return "A Saxon has died in combat";
-        }
+    Soldier.call(this, health, strength);
+}
+
+Saxon.prototype.receiveDamage = function(damage){
+    Soldier.prototype.receiveDamage.call(this, damage);
+    if (this.health > 0){
+        return "A Saxon has received " + damage + " points of damage";
+    } else {
+        return "A Saxon has died in combat";
     }
 }
 
-// War
-function War() {
+
+ War
+
+ function War() {
     this.vikingArmy = [];
     this.saxonArmy = [];
 
