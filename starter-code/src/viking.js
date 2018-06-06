a582fb7045da737b6e44ecfe67654d51b0ce90ca
Paul de Barbeyrac <pauldebarbeyrac@gmail.com>
2018-06-06 12:45:10 +0200

    up to attack, done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,37 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+}
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+};
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+
+
+
+
+function attack() {
+  return this.strength
+}
+
+attack.prototype = Object.create(Soldier.prototype);
+
+
 
 // Saxon
 function Saxon() {}
