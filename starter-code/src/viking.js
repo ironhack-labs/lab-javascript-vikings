20fcb0d17738290007700adb246184302b3727d5
Fernando <fmen9400@gmail.com.com>
2018-06-15 13:19:31 -0500

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,43 @@
 // Soldier
-function Soldier() {}
+function Soldier(health,strength) {
+    this.strength=strength;
+    this.health=health;
+    this.attack=function(){
+        return strength;
+    };
+    this.receiveDamage=function(damage){
+        health=health-damage;
+    };
+}
 
 // Viking
-function Viking() {}
+function Viking(name,health,strength) {
+    Soldier.call(this,health,strength);
+    this.name=name;
+    Soldier.prototype.receiveDamage=function(damage){
+        health=health-damage;
+        if(health>0){console.log(name+"has received DAMAGE points of damage")}
+        else{console.log("NAME has died in act of combat")};
+        return health;
+    };
+    this.battleCry=function(){console.log("Odin Owns You All!")};
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health,strength) {
+    Soldier.call(this,health,strength);
+    Soldier.prototype.receiveDamage=function(damage){
+        health=health-damage;
+        if(health>0){console.log("A Saxon has received DAMAGE points of damage")}
+        else{console.log("A Saxon has died in combat")};
+        return health;
+    };
+
+}
+var luis=new Viking("luis",100,50);
+console.log(luis);
+
+
 
 // War
 function War() {}
