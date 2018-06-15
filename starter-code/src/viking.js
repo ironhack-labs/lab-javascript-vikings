b880055a3bbed8a0b20f1ba877be2e8999837d48
Sarai Vasquez <svmarinez@hotmail.com>
2018-06-15 19:02:42 +0200

    Error found

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -2,16 +2,14 @@
 function Soldier(health, strength) {
     this.health = health;
     this.strength = strength;
-    // Viking
-
-
-    this.attack = function() {
+}
+    Soldier.prototype.attack = function() {
         return this.strength;
     }
-    this.receiveDamage = function(damage) {
+    Soldier.prototype.receiveDamage = function(damage) {
         this.health -= damage;
-    }
-}
+		}
+
 
 function Viking(name, health, strength) {
     Soldier.call(this, health, strength);
