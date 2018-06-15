cfedd268a82080871d81455d20af721e901c13bc
Ricardo Manzano <noteplay101@hotmail.com>
2018-06-15 13:59:58 -0500

    PP MEXRicardo Aaron  Felipe Patrick

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,44 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
 
-// Viking
-function Viking() {}
+    this.attack = function(){
+        return strength;
+    }
+    this.receiveDamage = function(damage){
+        this.health -= damage;
+    }
+}
 
-// Saxon
-function Saxon() {}
+//Viking
 
-// War
-function War() {}
+function Viking (name, health, strength) {
+
+    Soldier.call(this,health,strength);
+    this.name = name;
+
+    this.receiveDamage = function(damage){
+        if (this.health - damage) { return this.name + "has recieved" + damage + "points of damage"
+    }else if (this.health === 0) {return this.name + "has died in act of combat"}
+    }
+
+    this.battleCry = function(){
+    return "Odin Owns You All"
+    }
+
+}
+
+//Saxon
+function Saxon (health,strength){
+    Soldier.call(this,health,strength);
+
+
+    this.receiveDamage = function(damage){
+    if (this.health - damage) { return "A Saxon has recieved " + damage + " points of damage"
+    }else if (this.health === 0) {"A Saxon died in act of combat"}
+}
+}
+
+// // War
+// function War() {}
