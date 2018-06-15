a9c9d841d94358790c9ab4f118cabb5b9e5b7696
MateoReparaz <mateoreparaz1989@gmail.com>
2018-06-15 15:16:37 +0200

    Todo hasta Bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,58 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function() {
+    return this.strength;
+  };
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+  };
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  Soldier.call(this, health, strength);
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    } else if (this.health <= 0) {
+      return this.name + " has died in act of combat";
+    }
+  };
+  this.battleCry = function() {
+    return "Odin Owns You All!";
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+    this.receiveDamage = function(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+          return "A Saxon has received " + damage + " points of damage";
+        } else if (this.health <= 0) {
+          return "A Saxon has died in combat";
+        }
+      };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
-function War() {}
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    this.addViking = function (viking){
+        this.vikingArmy.push(viking);
+    }
+    this.addSaxon = 
+}
+
