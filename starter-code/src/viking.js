6bd7d772423fb9e0bb5bbaf5caf83385567d8139
TomyTM <franciscotomasapariciobustos@gmail.com>
2018-06-17 21:15:53 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -57,6 +57,7 @@ Saxon.prototype.receiveDamage = function (damage){
     else {return "A Saxon has died in combat"}
 }
 
+
 // War
 function War() {
     this.vikingArmy =[]
@@ -70,18 +71,33 @@ function War() {
     } 
     this.vikingAttack = function (){
         var aleatorioSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
-        var aleatorioVikingo = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
-            aleatorioVikingo.strength
-            if ( aleatorioSaxon.health <= aleatorioVikingo.strength){
-                saxonArmy.splice(aleatorioSaxon)
+        var aleatorioVikingo = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]; 
+        this.saxonArmy=this.saxonArmy.filter(function(e){
+            if(aleatorioSaxon.health>aleatorioVikingo.strength){
+                return e
             }
+        })
+        return aleatorioSaxon.receiveDamage(aleatorioVikingo.strength)  
+         
     } 
     this.saxonAttack = function (){
-        
+        var aleatorioSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)]
+        var aleatorioVikingo = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]
+        this.vikingArmy=this.vikingArmy.filter(function(e){
+            if(aleatorioVikingo.health>aleatorioSaxon.strength){
+                 return e
+            }
+        })
+        return aleatorioVikingo.receiveDamage(aleatorioSaxon.strength)  
     } 
     this.showStatus = function (){
-        
+        if(this.saxonArmy.length==0){
+            return "Vikings have won the war of the century!"
+        }else if(this.vikingArmy.length==0){
+            return "Saxons have fought for their lives and survive another day..."
+        }else if(this.saxonArmy.length>0 && this.vikingArmy.length>0){
+            return "Vikings and Saxons are still in the thick of battle."
+        } 
     }    
 }
 
-// Saxon.prototype.receiveDamage.call(this, str)
