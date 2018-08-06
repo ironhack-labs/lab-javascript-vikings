4e027a7c7221de57d2d03b8569a97aa30c9f9ee5
Markus Heldrup <markusheldrup@hotmail.com>
2018-08-06 15:37:50 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,42 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
+
+Soldier.prototype.attack = function() {
+    return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+   this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
+
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+
+Viking.prototype.receiveDamage = function (damage) {
+    this.damage = damage;
+    this.health -= damage;
+    
+    if (this.health > 0) {
+        return this.name + " has received " + this.damage + " points of damage";
+    } else return this.name + " has died in act of combat";
+}
+
+
+Viking.prototype.battleCry = function () {
+    return "Odin Owns You All!";
+}
+
 
 // Saxon
 function Saxon() {}
