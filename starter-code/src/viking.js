0c59794134d7e524d8651a0022539ce760352032
Schaum <schaumgarten@gmail.com>
2018-08-10 22:17:28 -0500

    commit iteraciones 1-3 bonus incompleto

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,74 @@
 // Soldier
-function Soldier() {}
+function Soldier(health,strength) {
+    this.health = health;
+    this.strength = strength;
+}
+
+Soldier.prototype.attack = function(){
+    return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function (damage){
+    this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health,strength) {
+    this.name = name;
+    Soldier.call(this, health, strength);
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+   if (this.health > 0){
+        return this.name + " has received " + damage + " points of damage";
+    } else {
+       return this.name + " has died in act of combat";
+   }
+}
+
+Viking.prototype.battleCry = function (){
+    return "Odin Owns You All!";
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+   if (this.health > 0){
+        return "A Saxon has received " + damage + " points of damage";
+    } 
+       return "A Saxon has died in combat";
+   }
+
 
 // War
-function War() {}
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(Viking){
+    this.vikingArmy.push(Viking);
+}
+
+War.prototype.addSaxon = function(Saxon){
+    this.saxonArmy.push(Saxon);
+
+War.prototype.vikingAttack = function(){
+   var r = Math.floor(Math.random()*this.vikingArmy.length);
+   var s = Math.floor(Math.random()*this.saxonArmy.length);
+   this.saxonArmy[s].receiveDamage(this.vikingArmy[r].strength);
+   if (this.saxonArmy[s].health <= 0){
+    this.saxonArmy.splice(s,1);
+    }
+    }
+
+}
