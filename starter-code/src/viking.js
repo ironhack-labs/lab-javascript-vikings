3341864cc6cb69f3a4233e01220e2c3087d5c973
Andrei Fitai <fitai.andrei@gmail.com>
2018-08-12 13:50:02 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,25 +4,23 @@ function Soldier(health, strength) {
   this.strength = strength;
 }
 
-Soldier.prototype.attack = function(){
+Soldier.prototype.attack = function () {
   return this.strength;
 };
 
-Soldier.prototype.receiveDamage = function(dmg){
-  this.health -= dmg
-  console.log("initial"this.health - dmg)
-  if (this instanceof Viking){
-    if ( this.health - dmg <= 0 )
+Soldier.prototype.receiveDamage = function (dmg) {
+  this.health -= dmg;
+  if (this instanceof Viking) {
+    if (this.health <= 0)
       return this.name + ' has died in act of combat';
     else
       return this.name + ' has received ' + dmg + ' points of damage';
   }
-  if (this instanceof Saxon){
-    console.log(this.health)
-    if( this.health - dmg <= 0)
+  if (this instanceof Saxon) {
+    if (this.health <= 0)
       return "A Saxon has died in combat";
     else
-      return "A Saxon has received " + dmg + " points of damage"
+      return "A Saxon has received " + dmg + " points of damage";
   }
 }
 
@@ -34,37 +32,53 @@ function Viking(name, health, strength) {
   this.name = name;
 }
 
-Viking.prototype.battleCry = function (){
+Viking.prototype.battleCry = function () {
   return "Odin Owns You All!"
 }
 
 Saxon.prototype = Object.create(Soldier.prototype)
-
-function Saxon(health,strength) {
-  Soldier.call(this, health,strength)
+// Saxon
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength)
 }
 
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
 
-// Viking
-// should inherit from Soldier
-// constructor function
-// should receive 3 arguments (name, health & strength)
-// should receive the name property as its 1st argument
-// should receive the health property as its 2nd argument
-// should receive the strength property as its 3rd argument
-// attack() method
-// should be a function
-// should receive 0 arguments
-// should return the strength property of the Viking
-// receiveDamage() method
-// should be a function
-// should receive 1 argument (the damage)
-// should remove the received damage from the health property
-// should return "NAME has received DAMAGE points of damage", if the Viking is still alive
-// should return "NAME has died in act of combat", if the Viking dies
-// battleCry() method
-// should be a function
-// should receive 0 arguments
-// should return "Odin Owns You All!"
+War.prototype.addViking = function (viking) {
+  this.vikingArmy.push(viking);
+}
+
+War.prototype.addSaxon = function (saxon) {
+  this.saxonArmy.push(saxon);
+}
+
+
+War.prototype.vikingAttack = function () {
+  var lastSax = this.saxonArmy[this.saxonArmy.length - 1]
+  var lastVik = this.vikingArmy[this.vikingArmy.length - 1]
+  var result = lastSax.receiveDamage(lastVik.strength)
+  if (lastSax.health <= 0)
+    this.saxonArmy.pop();
+  return result;
+}
+
+War.prototype.saxonAttack = function () {
+  var lastSax = this.saxonArmy[this.saxonArmy.length - 1]
+  var lastVik = this.vikingArmy[this.vikingArmy.length - 1]
+  var result = lastVik.receiveDamage(lastSax.strength);
+  if (lastVik.health <= 0)
+    this.vikingArmy.pop()
+  return result;
+}
+
+War.prototype.showStatus = function () {
+  if (this.saxonArmy.length == 0)
+    return "Vikings have won the war of the century!"
+  if (this.vikingArmy.length == 0)
+    return "Saxons have fought for their lives and survive another day..."
+  return "Vikings and Saxons are still in the thick of battle."
+}
