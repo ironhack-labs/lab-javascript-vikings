0ee08fc70c536778470d0cea85810626b0b44d1b
Ilham Moutaharik <ilham@prescrypto.com>
2018-08-24 13:07:11 -0500

    Code done in ES6

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,58 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor (health, strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health -= damage
+  }
+}
+
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+  constructor (name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage){
+    this.health -= damage
+    if(this.health > 0){
+      return this. name + " has received " + damage + " points of damage"
+    }
+    else if (this.health <= 0){
+      return this.name + " has died in act of combat"
+    }
+  }
+
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
+
 
 // Saxon
-function Saxon() {}
+class Saxon extends Soldier {
+  constructor(health, strength){
+    super(health, strength)
+  }
+  receiveDamage(damage){
+    this.health -= damage
+    if(this.health > 0){
+      return "A Saxon has received " + damage + " points of damage"
+    }
+    else if (this.health <= 0){
+      return "A Saxon has died in combat"
+    }
+  }
+}
 
 // War
-function War() {}
+class War() {
+  this.vikingArmy = []
+  this.saxonArmy = []
+}
