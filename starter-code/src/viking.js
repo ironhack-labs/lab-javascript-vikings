d9a40fc8bf7747d36fb4b27a0c18d427d1eedb9e
Luther <luther0211@hotmail.com>
2018-08-24 15:55:07 -0500

    DONE

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,47 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor (health, strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+  return this.strength;
+}
 
-// Viking
-function Viking() {}
+receiveDamage(damage){
+   this.health -= damage
+}
+}
 
-// Saxon
-function Saxon() {}
 
-// War
-function War() {}
+
+class Viking extends Soldier{
+  constructor(name, health, strength){ 
+  super(health, strength)
+  this.name = name
+  }
+  receiveDamage(damage){
+    this.health -= damage
+    if(health > 0) console.log(name + " has received " + damage + " points of damage.")
+    if(health <= 0) console.log(name + " has died in act of combat")
+  }
+
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
+
+class Saxon extends Soldier{
+  constructor(health, strength){
+    super(health, strength)
+  }
+
+  receiveDamage(damage){
+    this.health -= damage
+    if(this.health > 0){  console.log("A Saxon has received " + damage + " points of damage")}
+    if(this.health <= 0) console.log("A Saxon has died in combat")
+  }
+
+}
+ var sax = new Saxon (100,90);
+ sax.receiveDamage(5)
