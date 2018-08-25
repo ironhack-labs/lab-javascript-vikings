b567957df242c8ba26e027fc3483f4eb6c78043f
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-08-25 21:04:07 +0200

    test

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,38 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+}
 
-// Viking
-function Viking() {}
+Soldier.prototype.attack = function(){
+    return this.strength;
+    //should return the strength property of the Soldier
+}
+Soldier.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+    //should remove the received damage from the health property
+}
+
+// Vikings
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
+}
+Viking.prototype = Object.create(Soldier.prototype);
+
+/*Viking.prototype.receiveDamage() = function(damage){
+    this.health -= damage;
+    if (health>0){
+        console.log(this.name + " has received " + damage + " points of damage");
+   // } else{
+        console.log(this.name + " has died in act of combat");
+   // }
+}*/
+//Viking.prototype.battleCry() = function(){
+   // console.log("Odin Owns You All!");
+//}
+
+
 
 // Saxon
 function Saxon() {}
