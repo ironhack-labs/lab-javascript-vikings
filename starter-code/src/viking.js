2dc0e041df34ea9d5b9e4e15019725de3e0d57f7
zalo Riera <nadalriera84@gmail.com>
2018-08-26 20:33:38 +0200

    cambios

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-function Soldier() {}
 
-// Viking
-function Viking() {}
+  function Soldier() {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function() {
+        return this.strength;
+    };
+    this.receiveDamage = function(damage) {
+        this.health -= damage;
+    };
+  }
+  // Viking
 
-// Saxon
-function Saxon() {}
+  Viking.prototype = Object.create(Soldier.prototype);
 
-// War
-function War() {}
+  function Viking(name,health,strength) {
+    Soldier.call(this,health,strength);
+    this.name = name;
+    this.receiveDamage = function(damage) {
+      this.health -= damage;
+      if (this.health > 0) {
+        return this.name + " has received " + damage + " points of damage";
+      }
+      else if (this.health <= 0) {
+        return this.name + " has died in act of combat";
+      }
+    };
+    this.battleCry = function(){
+      return "Odin Owns You All!";
+    };
+  }
+  
+  // Saxon
+  
+  Saxon.prototype = Object.create(Soldier.prototype);
+  
+  function Saxon(health,strength) {
+    Soldier.call(this,health,strength);
+    this.receiveDamage = function(damage) {
+      this.health -= damage;
+      if (this.health > 0) {
+        return "A Saxon has received " + damage + " points of damage";
+      }
+      else if (this.health <= 0) {
+        return "A Saxon has died in combat";
+      }
+  };
