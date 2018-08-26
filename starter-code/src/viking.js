aab7a629ccb67f37702b013a97a77cc4cfdf7db9
Chris Lopez <pagarizabal2@gmail.com>
2018-08-26 13:00:00 +0200

    rewrote exercise using .prototype instead of class and extends

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,74 +1,72 @@
 // Soldier
-class Soldier {
-  constructor (health, strength) {
-    this.health = health;
-    this.strength = strength;
-  }
-  attack() {
-    return this.strength
-  }
-  receiveDamage(damage){
-    this.health -= damage
-  }
-}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+};
+
+Soldier.prototype.attack = function(){
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+  return;
+};
 
 // Viking
-class Viking extends Soldier {
-  constructor (name, health, strength) {
-    super(health, strength);
-    this.name = name;
-  }
-  attack() {
-    return this.strength;
-  }
-  receiveDamage(damage) {
-    this.health -= damage;
-    if (this.health > 0) {
-      return `${this.name} has received ${damage} points of damage`    
-    } else {
-      return `${this.name} has died in act of combat`
-    }
-  }
-  battleCry() {
-    return "Odin Owns You All!";
-  }
+function Viking(name, health, strength) {
+  this.name = name;
+  Soldier.call(this, health, strength);
+};
+//assign prototype from soldier and viking constructor
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
+Viking.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return this.name + " has received " + damage + " points of damage";
+  } else {
+    return this.name + " has died in act of combat";
+  };
+};
 
-}
+Viking.prototype.battleCry = function() {
+  return "Odin Owns You All!";
+};
 
 // Saxon
-class Saxon extends Soldier {
-  constructor(health, strength) {
-    super(health, strength);
-  }
-  attack() {
-    return this.strength;
-  }
-  receiveDamage(damage) {
-    this.health = this.health - damage;
-    if (this.health > 0) {
-      return `A Saxon has received ${damage} points of damage` ;
-    } else {
-      return `A Saxon has died in combat`;
-    }
-  }
-}
+function Saxon(health, strength) {
+  Soldier.call(this,health,strength);
+  };
+//we assign solder prototype and Saxon constructor
+Saxon.prototype= Object.create(Soldier.prototype);
+Saxon.prototype.constructor= Saxon;
+
+Saxon.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+  if (this.health > 0) {
+    //alive
+    return "A Saxon has received " + damage + " points of damage";
+  } else {
+    //dead
+    return "A Saxon has died in combat";
+  };
+};
 
 // War
-class War {
-  constructor() {
-  this.vikingArmy = [];
-  this.saxonArmy = [];
-  }
-  addViking(Viking) {
-    this.vikingArmy.push(Viking);
-  }
-  addSaxon(Saxon) {
-    this.saxonArmy.push(Saxon);
-  }
-  vikingAttack() {
-    var rngSoldier = Math.floor((Math.random() * this.rngSoldier.length));
-    var rngViking = Math.floor((Math.random() * this.saxonArmy));
-    //...
-  }
+function War() {
+  //empty arrays
+  this.vikingArmy=[];
+  this.saxonArmy=[];
 }
+
+War.prototype.addViking = function(Viking){
+  //push to new arrays
+  this.vikingArmy.push(Viking);
+};
+//push to new arrays
+War.prototype.addSaxon = function(Saxon){
+  this.saxonArmy.push(Saxon);
+};
+
