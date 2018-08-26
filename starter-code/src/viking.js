d1aa6a6aad6bdbd3811a109db09bb9cf6d93e1ca
Esperanza Amaya <eamayag@gmail.com>
2018-08-26 19:10:18 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -34,7 +34,6 @@ function Viking(nameArg, healthArg, strengthArg) {
   }
   }
 
-
 Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 
@@ -52,25 +51,42 @@ Saxon.prototype.constructor = Saxon;
 function War() {
   this.vikingArmy = [];
   this.saxonArmy = [];
-  this.addViking = function({Viking}) {    
-    vikingArmy.push({Viking});
+  this.addViking = function(Viking) {    //no tengo claro este punto, si voy a pasar una  callback function, debería pasar argumentos a la función Viking, no? 
+    this.vikingArmy.push(Viking);
   }
-  this.addSaxon = function(saxon) {
-    saxonArmy.push(saxon);
+  this.addSaxon = function(Saxon) {
+    this.saxonArmy.push(Saxon);  // lo mismo que en addViking, no entiendo que no haya que dar argumenos a la función saxon al llamarla
   }
-  this.vikingAttack = function() {
-    
-  }
-  this.saxonAttack = function() {
+  this.vikingAttack = function() { //Jasmine me admite esta misma función en la siguiente iteración, la de saxonAttack, pero no en viking, no sé por qué
+    var randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length - 1));
+    var randomViking = Math.floor(Math.random() * (this.vikingArmy.length - 1));
+    var saxonDamage = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);  
+    if (this.saxonArmy[randomSaxon].health <= 0) {
+      this.saxonArmy.splice(randomSaxon, 1);
+    }
+    return saxonDamage;
+  } 
 
+  this.saxonAttack = function() {
+    var randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length - 1));
+    var randomViking = Math.floor(Math.random() * (this.vikingArmy.length - 1));
+    var vikingDamage = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);  
+    if (this.vikingArmy[randomViking].health <= 0) {
+      this.vikingArmy.splice(randomViking, 1);
+    }
+    return vikingDamage;
   }
-  this.showStatus = function() {
+  this.showStatus = function() {    //he probado primero con ifs anidados, pero no sé cómo contemplar el caso de que ambos sean cero, de hecho esa es la situación actual, y no entiendo cómo la está pasando Jasmine. Si ambos son cero, utilizado así siempre saldrá como ganador el que hayamos puesto primero en los ifs, y no es verdad, pero no estaría llegando al final del ciclo.
+    if (this.saxonArmy.length == 0 && this.vikingArmy.length == 0) {
+      return "Both died";
+    }
     if (this.saxonArmy.length == 0) {
       return "Vikings have won the war of the century!";
-    } else if (this.vikingArmy.length == 0) {
-      return "Saxons have fought for their lives and survive another day...";
-    } else {
-      return "Vikings and Saxons are still in the thick of battle"
     }
+    if (this.vikingArmy.length == 0) {
+      return "Saxons have fought for their lives and survive another day...";
+    }
+    return "Vikings and Saxons are still in the thick of battle.";
+     
   }
 }
