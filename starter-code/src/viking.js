e606f953564a898db976d4031925b650ff38a4ef
Celia Arnelas <37745928+Carnelas@users.noreply.github.com>
2018-08-26 16:34:08 +0200

    Version pacífica
    Todavía no he preparado la parte de la guerra. Quiero terminar la tarea de Slack y después, si me da tiempo, ponerme con ello.
    Me he sentido bastante cómoda con el código de este proyecto, una vez he encontrado el método adecuado para comenzar ha sido relativamente sencillo.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-function Soldier() {}
-
-// Viking
-function Viking() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+ }
+ Soldier.prototype.attack = function() {
+  return this.strength;
+ };
+ Soldier.prototype.receiveDamage = function(damageArg) {
+  this.health -= damageArg;
+ };
+ 
+ // Viking
+ function Viking(nameArg, healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+  this.name = nameArg;
+ }
+ Viking.prototype = Object.create(Soldier.prototype);
+ Viking.prototype.constructor = Viking;
+ Viking.prototype.receiveDamage = function(damageArg) {
+  this.health -= damageArg;
+  if (this.health > 0) {
+    return this.name + " has received " + damageArg + " points of damage";
+  } else {
+    return this.name + " has died in act of combat";
+  }
+ };
+ Viking.prototype.battleCry = function() {
+  return "Odin Owns You All!";
+ };
 
 // Saxon
-function Saxon() {}
+function Saxon(healthArg, strengthArg) {
+  Soldier.call(this, healthArg, strengthArg);
+ }
+ Saxon.prototype = Object.create(Soldier.prototype);
+ Saxon.prototype.constructor = Saxon;
+ Saxon.prototype.receiveDamage = function(damageArg) {
+  this.health -= damageArg;
+  if (this.health > 0) {
+    return "A Saxon has received " + damageArg + " points of damage";
+  } else {
+    return "A Saxon has died in combat";
+  }
+ };
+
 
 // War
 function War() {}
