e5f3b75a85978df0e219b7576bae9bfdc499d92f
wergilia <weronika.zdrojewska@gmail.com>
2018-08-27 00:07:53 +0200

    Soldier attack() function uncorrect

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,16 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    expect(Soldier.length).toEqual(2);
+this.health = healthArg;
+this.strength = strengthArg;
+this.attack = function(){
+    return soldier.strength;
+};
+this.damage = function(damage){
+    return soldier.health - damage;
+};
+};
+
 
 // Viking
 function Viking() {}
