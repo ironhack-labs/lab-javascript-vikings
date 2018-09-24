a292761c8884a6792f477143a9e0cc0172dce5c7
Ester Diaz <ester.diaz.sola@gmail.com>
2018-09-24 17:15:02 +0200

    En proceso

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+//Soldier methods
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+Soldier.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+}
+
+// inherit methods from base Soldier
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+  if (this.health > 0){
+    return this.name + ' has received ' + damage + ' points of damage';
+  }
+  if (this.health <= 0){
+    return this.name + ' has died in act of combat';
+  }
+}
 
 // Saxon
 function Saxon() {}
