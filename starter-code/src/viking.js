1d0d9aa8a10a111f27a841851bd700a6c5d06ae9
iban Dasca <ibandasca@gmail.com>
2018-09-24 19:25:36 +0200

    Vicking & Saxon exercice

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -55,14 +55,10 @@ Saxon.prototype.receiveDamage = function(damage){
 }
 
 // War
-
-
-
 function War() {
     this.vikingArmy = [];
     this.saxonArmy = [];
 }
-//
 
 War.prototype.addViking = function(Viking){
     this.vikingArmy.push(Viking);
@@ -71,3 +67,32 @@ War.prototype.addViking = function(Viking){
 War.prototype.addSaxon = function(Saxon){
     this.saxonArmy.push(Saxon);
 }
+
+War.prototype.vikingAttack = function(){
+    var numberSaxon = Math.floor(Math.random(this.saxonArmy.length));
+    var numberViking = Math.floor(Math.random(this.vikingArmy.length));
+    var resultado = this.saxonArmy[numberSaxon].receiveDamage(this.vikingArmy[numberViking].strength);
+ 
+    if(this.saxonArmy[numberSaxon].health <= 0){
+        this.saxonArmy.splice(numberSaxon, 1);
+    }
+    return resultado;
+ }
+ // --- Same pero comprimido ---
+ War.prototype.saxonAttack = function (){
+   var resultado=  this.vikingArmy[Math.floor(Math.random(this.vikingArmy.length))].receiveDamage(this.saxonArmy[Math.floor(Math.random(this.saxonArmy.length))].strength);
+   if(this.vikingArmy[Math.floor(Math.random(this.vikingArmy.length))].health <=0){
+       this.vikingArmy.splice(Math.floor(Math.random(this.vikingArmy.length)), 1);
+   }
+   return resultado;
+ }
+ War.prototype.showStatus = function (){
+    if (!this.saxonArmy.length){
+        return "Vikings have won the war of the century!";
+    }
+    else if(!this.vikingArmy.length){
+    return "Saxons have fought for their lives and survive another day...";
+    } else {
+        return "Vikings and Saxons are still in the thick of battle.";
+    }
+ }
