481d2f6dc734bb0773dfb5bb5d98d9d44d4ffe1a
enamorau <msadki@me.com>
2018-10-02 21:24:24 +0200

    Avec Béatrice

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,10 +1,63 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.attack = function() {
+    return this.strength;
+  };
+
+  this.receiveDamage = function(theDamage) {
+    this.health = this.health - theDamage;
+  };
+}
 
 // Viking
-function Viking() {}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+
+  this.receiveDamage = function(theDamage) {
+    this.health = this.health - theDamage;
+
+    if (this.health > 0) {
+      return this.name + " has received " + theDamage + " points of damage";
+    } else {
+      return this.name + " has died in act of combat";
+    }
+  };
+
+  this.battleCry = function() {
+    return "Odin Owns You All!";
+  };
+}
 
 // Saxon
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+function Saxon(health, strength) {
+  
+  Soldier.call(this, health, strength);
+/*
+  receiveDamage = function(theDamage) {
+    this.health -= theDamage;
+
+    if (this.health > 0) {
+      return (
+        "A " + this.name + " has received " + theDamage + " points of damage"
+      );
+    } else {
+      return "A" + this.name + " has died in combat";
+    }
+  };*/
+}
+
 function Saxon() {}
 
 // War
