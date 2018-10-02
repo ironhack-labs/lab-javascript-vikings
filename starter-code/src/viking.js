48142338abe1d7c47d17b048ce3fc89ae648c285
Sandra Dupont <ssduptcom@gmail.com>
2018-10-02 21:34:07 +0200

    Soldier,Viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function() {
+        return this.strength;
+    };
+    this.receiveDamage = function(damage) {
+        this.health = health - damage;
+    };
+}
 
 // Viking
-function Viking() {}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+function Viking(name, health, strength) {
+    this.name = name;
+    // Soldier.call(this, health, strength, attack);
+    Soldier.call(this, health, strength);
+
+    this.receiveDamage = function(damage) {
+        this.health = health - damage;
+        if (this.health > 0) {
+            return name + " has received " + damage + " points of damage";
+        }
+        else {
+            return name + " has died in act of combat";
+        }
+    };
+}
 
 // Saxon
 function Saxon() {}
