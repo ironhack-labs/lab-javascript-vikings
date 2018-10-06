6b03723e2cb1515535fa42416601bd196e6cfa75
Gabriel Cueva <gabrielcueva_10@hotmail.com>
2018-10-06 18:43:16 +0200

    Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,58 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.attack = function() {
+    return strength;
+  };
+
+  this.receiveDamage = function(damage) {
+    vida = this.health;
+    this.health = vida - damage;
+  };
+}
 
 // Viking
-function Viking() {}
+
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength, this.attack);
+  this.name = name;
+
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+
+    if (this.health > 0) {
+      return name + " has received " + damage + " points of damage";
+    } else {
+      return name + " has died in act of combat";
+    }
+  };
+
+  this.battleCry = function() {
+    return "Odin Owns You All!";
+  };
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength, this.attack);
+
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else {
+      return "A Saxon has died in combat";
+    }
+  };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
 function War() {}
