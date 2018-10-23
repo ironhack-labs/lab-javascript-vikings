7aae7a6ff4a4a078c5d33c8ace69f6b947c375fb
Juan Ignacio Benito <juanignaciobenito@Juans-MacBook-Air.local>
2018-10-23 22:01:54 +0200

    not the bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,65 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage;
+    }
+
+
+}
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name;
+    }
+
+    receiveDamage(damage){
+        this.health = this.health - damage; 
+        if(this.health>damage){
+            return this.name + " has received " + damage + " points of damage"
+        }else if(this.health<=damage){
+            return this.name + " has died in act of combat"
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+    attack(){
+        return this.strength;
+    }
+}
 
 // Saxon
-function Saxon() {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength)
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        var result;
+        if(this.health>damage){
+            result = "A Saxon has received " + damage + " points of damage"
+        }else if(this.health<=damage){
+            result = "A Saxon has died in combat"
+        }
+        this.health = this.health - damage; 
+        return result;
+    }
+}
 
-// War
-function War() {}
+class War{
+    constructor(){
+        vikingArmy = new Viking()
+    }
+    
+}
