a28e7899d6a910b46959566bc1ca469caf6176b8
nirtzur <nirtzur89@gmail.com>
2018-10-23 21:44:13 +0200

    end of class

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+    constructor (health , strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health-=damage;
+    }
+}
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+    constructor (name , health , strength){
+        super (health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage){
+        this.health-=damage;
+        if (this.health > 0) {
+            return this.name + " has received " + damage + " points of damage"
+        }
+        else {
+            return this.name + " has died in act of combat"
+        }
+    }
+    
+}
 
 // Saxon
-function Saxon() {}
+class Saxon {}
 
 // War
-function War() {}
+class War {}
