fc9959e8bcb50318b7f605a569c66760d4260da3
Luise Schneider <luise.schneider@berlin.de>
2018-10-23 21:36:52 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,52 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor(health, strength) {
+    (this.health = health),
+      (this.strength = strength),
+      (this.attack = function attack() {
+        return strength;
+      }),
+      (this.receiveDamage = function receiveDamage(damage) {
+        this.health -= damage;
+      });
+  }
+}
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    (this.name = name),
+      (this.receiveDamage = function receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+          return name + " has received " + damage + " points of damage";
+        }
+        if (this.health <= 0) {
+          return name + " has died in act of combat";
+        }
+      }),
+      (this.battleCry = function battleCry() {
+        return "Odin Owns You All!";
+      });
+  }
+}
 
 // Saxon
-function Saxon() {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    this.receiveDamage = function receiveDamage(damage) {
+      this.health -= damage;
+      if (this.health > 0) {
+        return "A Saxon has received " + damage + " points of damage";
+      }
+      if (this.health <= 0) {
+        return "A Saxon has died in combat";
+      }
+    };
+  }
+}
 
 // War
-function War() {}
+class War {}
