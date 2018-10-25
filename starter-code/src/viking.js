7f156df52947dc0284e9c5c79d6e4dc855285c0d
John Doe <perrot.nic@gmail.com>
2018-10-25 14:53:05 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,64 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+    constructor(health, strength){
+        console.log("Creating a new soldier....")
+        //"this" is the generic name you use to REFER TO THE OBJECT
+        this.health = health;
+        this.strength = strength;
+    }
+   attack(){
+       return this.strength; 
+   }
+   receiveDamage(damage){
+       this.health = this.health - damage;
+    }
+};
 
 // Viking
-function Viking() {}
 
-// Saxon
-function Saxon() {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super(health, strength)
+/*super equivaut à :
+        this.health = health;
+        this.strength = strength;*/
+        this.name = name;
+    };
+
+    receiveDamage(damage){
+        super.receiveDamage(damage);
+        if (this.health > 0){
+            return this.name + " has received " + damage + " points of damage";
+        } 
+         else  
+        {
+            return this.name + " has died in act of combat";
+        }; 
+        
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+};
+//Saxon
+
+class Saxon extends Soldier {
+    constructor (health, strength) {
+        super(health, strength)
+    };
+
+    receiveDamage(damage){
+        if (this.health > 0){
+            return "A Saxon has received " + damage + " points of damage";
+        } 
+         else  
+        {
+            return  + "A Saxon has died in combat";
+        }; 
+        
+    }
+};
+//function Saxon() {}
 
 // War
-function War() {}
+//function War() {}
