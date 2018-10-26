f274f485b8349e6d0757343ffea5969752a8cbe5
Simon G Negrete <simongnegrete@MacBook-Pro-de-Simon.local>
2018-10-26 17:28:20 +0200

    A medias =D

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,39 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function(){
+        return this.strength;
+    }
+    Soldier.prototype.receiveDamage = function(damage){
+        this.health -=50;
+    }
+
+}
+
+
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) { 
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  Viking.prototype = Object.create(Soldier.prototype);
+
+  Viking.prototype.attack = function(){
+      return this.strength;
+  };
+  Viking.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+   // if function (this.health === 0) {
+   // return name + "has received" + damage + "points of damage";
+  };
+
+
+
+
+
 
 // Saxon
 function Saxon() {}
