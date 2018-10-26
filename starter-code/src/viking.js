05d0d76f30899317ff91593de1503248e0ea8a1a
chloehd <chloehipeau@gmail.com>
2018-10-26 18:53:09 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,51 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+  }
+  attack () {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
+
 
 // Viking
-function Viking() {}
+
+class Viking extends Soldier {
+  constructor(name, healthArg, strengthArg) {
+    super(healthArg, strengthArg);
+    this.name = name;
+  }
+
+receiveDamage(damage) {
+  this.health -= damage;
+
+  if (this.health > 0) {
+    return this.name + " has received " + damage + " points of damage";
+  }
+  
+  else {
+    return this.name + " has died in act of combat";
+  }
+}
+
+battleCry() {
+  return "Odin Owns You All!";
+}
+
+}
+
+
+
 
 // Saxon
 function Saxon() {}
+
+
 
 // War
 function War() {}
