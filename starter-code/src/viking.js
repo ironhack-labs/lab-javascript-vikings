470e255308e18467173918906714e2b8dc30fc2b
Yared Santiago <yaredmyers@gmail.com>
2018-10-26 17:27:43 +0200

    primera iteracion a falta de bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+ this.health -= damage;
+};
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
 
 // Viking
-function Viking() {}
+function Viking(name, healthVik, strengthVik) {
+this.name = name;
+Soldier.call(this, healthVik, strengthVik)
+this.battleCry = function() {
+  return "Odin Owns You All!";
+}
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.receiveDamage = function(damage) {
+  this.health -= damage
+  if (this.health === 0) {
+    return this.name + " has died in act of combat";
+  } else {
+    return this.name + " has received " + damage + " points of damage";
+  }
+}
+
 
 // Saxon
-function Saxon() {}
+function Saxon(healthSax, strengthSax) {
+  Soldier.call(this, healthSax, strengthSax)
+  
+}
+
+ Saxon.prototype = Object.create(Soldier.prototype);
+ Saxon.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health === 0) {
+    return "A Saxon has died in combat";
+  } else {
+    return "A Saxon has received " + damage  + " points of damage";
+  }
+
+  this.health -= damage;
+ };
+
+ 
+
 
 // War
 function War() {}
