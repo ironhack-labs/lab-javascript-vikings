f5436d300efcf1c5f06514b4060aa6c5db1826dc
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-10-26 14:58:56 +0200

    commit2

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,59 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+
+  this.health = health;
+  this.strength = strength;
+  this.attack = function(){
+    return this.strength;
+  }
+  this.receiveDamage = function(damage){
+    this.health =  health - damage;
+  }
+
+}
+
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  
+  Soldier.call(this, health, strength)
+  this.name = name;
+  this.receiveDamage = function(damage){
+ 
+    this.health = health - damage;
+
+    if(this.health === 'alive'){
+      return `${this.name} has received ${damage} points of damage`;
+    }else{
+      return `${this.name} has died in act of combat`;
+    }
+
+    
+
+  }
+  this.battleCry() = function(){
+  
+  
+  }
+
+
+
+}
+
+ 
+
+
+
+
+
+
 
 // Saxon
 function Saxon() {}
 
 // War
 function War() {}
+
+
+
+
