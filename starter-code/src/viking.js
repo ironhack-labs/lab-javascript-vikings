fbe37035fe970ebc46ce0bd7ef7b65754a709544
Ross Gravitte <Rbgravitte@gmail.com>
2018-10-26 17:59:11 -0400

    Still working on the War class

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-function Soldier() {}
-
+//Two Arguments - health and strength
+class Soldier{
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+  
+}
 // Viking
-function Viking() {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+  super(health, strength)
+      this.name = name;
+      this.alive = true;
+  }
+    receiveDamage(damage){
+      this.health -= damage;
+      if(this.alive === true){
+        return this.name + " has received " + damage + " points of damage ";
+      }else{
+        return this.name + " has died in act of combat";
+      }
+    }
+    battleCry(){
+      return "Odin owns you all!";
+    }
+}
 
 // Saxon
-function Saxon() {}
+class Saxon extends Soldier{
+  constructor(health, strength){
+    super(health, strength)
+    this.alive = true;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.alive === true){
+      return "A Saxon has received" + damage + " points of damage";
+    }else{
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
-function War() {}
+class War{
+  constructor(){
+    vikingArmy = [];
+    saxonArmy = [];
+  }
+  addViking(viking){
+    
+  }
+}
