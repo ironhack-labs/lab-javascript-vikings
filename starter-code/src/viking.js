218fa58508a4d037d3dc9b091fd0174e127d6f16
Sandra Luengo <luchosandra@gmail.com>
2018-10-27 18:04:03 +0200

    Iteration 4: War construction done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -31,7 +31,7 @@ Viking.prototype.constructor = Viking;
 Viking.prototype.receiveDamage= function(damage){
 
     this.health=this.health-damage;
-    if(this.health) return `${this.name} has received ${damage} points of damage`
+    if(this.health>0) return `${this.name} has received ${damage} points of damage`
     return `${this.name} has died in act of combat`;
 }
 
@@ -47,9 +47,35 @@ Saxon.prototype.constructor = Saxon;
 Saxon.prototype.receiveDamage= function(damage){
 
     this.health=this.health-damage;
-    if(this.health) return `A Saxon has received ${damage} points of damage`
+    if(this.health>0) return `A Saxon has received ${damage} points of damage`
     return `A Saxon has died in combat`;
 }
 
 // War
-function War() {}
+function War() {
+    War.prototype.vikingArmy=[];
+    War.prototype.saxonArmy=[];
+}
+War.prototype.addViking= function(vikingObjet){
+  War.prototype.vikingArmy.push(vikingObjet);
+}
+War.prototype.addSaxon= function(saxonObjet){
+    War.prototype.saxonArmy.push(saxonObjet);
+}
+War.prototype.vikingAttack= function(){
+  var vickingRandom=War.prototype.vikingArmy[Math.floor( Math.random()*War.prototype.vikingArmy.length)];
+  var saxonRandom=War.prototype.saxonArmy[Math.floor(Math.random()*War.prototype.saxonArmy.length)];
+  var resultOfRexiveDamage=saxonRandom.receiveDamage(vickingRandom.strength);
+  if(saxonRandom.health<=0){
+    War.prototype.saxonArmy.pop(saxonRandom);
+    return resultOfRexiveDamage;
+  }
+  return resultOfRexiveDamage;
+
+}
+War.prototype.saxonAttack= function(){
+  
+}
+War.prototype.showStatus= function(){
+  
+}
