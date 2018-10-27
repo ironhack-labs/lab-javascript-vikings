f0529df6f4b5b3e2162d34552f7d18a1a154346e
aterron84 <aterron84@gmail.com>
2018-10-27 17:06:17 +0200

    Soldier, Viking and Saxon complete

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+}
+    Soldier.prototype.attack = function(){
+        return this.strength;
+    };
+    Soldier.prototype.receiveDamage = function(damage){
+         this.health -= damage;
+    }
 
-// Viking
-function Viking() {}
+//Viking
+function Viking(nameViking, healthArg, strengthArg) {
+    Soldier.call(this, healthArg, strengthArg );
+    this.name = nameViking;
+}
+     Viking.prototype = Object.create(Soldier.prototype);
+      Viking.prototype.receiveDamage = function(damage){
+          this.health -= damage;
+            if (this.health === 0) {
+                return `${this.name} has died in act of combat`;
+                  } else {
+                    return `${this.name} has received ${damage} points of damage`;
+              }
+            }
 
-// Saxon
-function Saxon() {}
+    Soldier.prototype.battleCry = function(){
+        return "Odin Owns You All!";
+    }
+
+// Saxonhe
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+}
+    Saxon.prototype = Object.create(Soldier.prototype);
+    Saxon.prototype.receiveDamage = function(damage){
+        this.health -= damage;
+        if (this.health >0) {
+            return `A Saxon has received ${damage} points of damage`;           
+        } else {
+            return `A Saxon has died in combat`;
+        }
+
+    }
+
 
 // War
 function War() {}
