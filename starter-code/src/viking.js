af50ebd13d4e6121a68b58acf58f12064dae5bd1
Rodrigo.Munoz <rodrigomunozga@gmail.com>
2018-10-27 15:22:26 +0200

    commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -5,7 +5,7 @@ function Soldier(health, strength) {
 }
 
 
-//Crea funciones dentro de Soldier _proto_ para que las subclases Viking y Saxon lo compartan cuando hereden el _proto_ de la clase padre Soldier
+
 Soldier.prototype.attack = function() {
   return this.strength;
 };
@@ -14,12 +14,6 @@ Soldier.prototype.receiveDamage = function(damage) {
 }
 
 
-
-
-
-
-
-  //'copia' el _proto_ de Soldier en el proto de viking
 Viking.prototype = Object.create(Soldier.prototype);
 
 // Viking
@@ -28,18 +22,16 @@ function Viking(name, health, strength) {
   this.health = health;
   this.strength = strength;
 
-  
+  this.receiveDamage = function(damage) {
+    this.health = health - damage;
+    console.log(this.health);
 
-  // this.receiveDamage = function(damage) {
-  //   this.health = health - damage;
-  //   console.log(this.health);
-
-  //   if (damage < health) {
-  //     return `${this.name} has received ${damage} points of damage`;
-  //   } else {
-  //     return `${this.name} has died in act of combat`;
-  //   }
-  // };
+    if (damage < health) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  };
   this.battleCry = function(){
     return `Odin Owns You All!`;
   }
