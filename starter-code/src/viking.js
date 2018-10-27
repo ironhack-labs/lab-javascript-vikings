3cad12b4d78d5b93792b11d40ba870cdac497581
Rachel Hofer <rhofer3@gmail.com>
2018-10-27 14:39:47 -0400

    Vikings v1

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,40 @@
 // Soldier
-function Soldier() {}
+
+class Soldier {
+
+    constructor(healthArg, strengthArg){
+
+      this.health = healthArg;
+      this.strength = strengthArg;
+
+    }
+
+    attack(){
+    return this.strength;
+
+    }
+    
+    receiveDamage(theDamage){
+      this.health -= theDamage;
+      
+    }
+}
+
+
 
 // Viking
-function Viking() {}
+
+class Viking extends Soldier {
+      
+    constructor(nameArg, healthArg, strengthArg){
+      super(healthArg, strengthArg)
+    this.name = nameArg;
+  }
+    attack(){
+      super.attack()
+  }
+
+}
 
 // Saxon
 function Saxon() {}
