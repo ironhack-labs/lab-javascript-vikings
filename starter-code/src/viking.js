1d3a7401ef5c074711569284189fe401729e1d7e
Sonia Bravo <rinbra0@hotmail.com>
2018-10-27 20:59:45 +0200

    vikingos without battlecry

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,43 @@
 // Soldier
-function Soldier() {}
+
+function Soldier (health, strength) {
+this.health = health;
+this.strength = strength;
+
+Soldier.prototype.attack= function attack(){
+  return this.strength}
+}
+
+Soldier.prototype.receiveDamage = function receiveDamage(damage){
+this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+
+function Viking (name, health, strength) {
+  Soldier.call (this, health,strength);
+  this.name = name;
+  
+}
+Viking.prototype =Object.create (Soldier.prototype);
+
+Viking.prototype.receiveDamage = function receiveDamage(damage){
+  var NewHealth= this.health -= damage;
+
+  if (NewHealth > 0) {return this.name +" has received " + damage + " points of damage"}
+  else { return this.name +" has died in act of combat"}
+  }
+
+  
+
+
+  // Vikings.prototype.battleCry= function battleCry(){
+  //   return
+  // }
+
+
+  
+
 
 // Saxon
 function Saxon() {}
