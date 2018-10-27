7b3047dd62e1071ab2b378db2414d6fb38dd285a
Jorge Devesa <jrgdevesa@gmail.com>
2018-10-27 21:43:04 +0200

    Viking & Soldier done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,37 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    
+}
+Soldier.prototype.attack = function attack(){
+    return this.strength; 
+}  
+Soldier.prototype.receiveDamage= function receiveDamage(damage){
+    this.health -= damage;
+}
+//if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength)
+    this.name = name;
+    
+}
 
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.receiveDamage = function receiveDamage(damage){
+   this.health -= damage
+    if (this.health > 0){
+        return this.name + " has received " + damage + " points of damage"
+    }
+    else{
+        return this.name + " has died in act of combat"
+    }
+}
+Viking.prototype.battleCry = function battleCry(){
+    return "Odin Owns You All!"
+}
 // Saxon
 function Saxon() {}
 
