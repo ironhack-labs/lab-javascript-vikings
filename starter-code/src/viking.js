248d9f41ce9df7b618df0dc3e6385346c52af6e5
Yared Santiago <yaredmyers@gmail.com>
2018-10-27 16:58:44 +0200

    ejercicio vikings. Hay un punto que me da error en vikingAttack, que no se donde se produce el error al ser similar al saxonAttack.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -20,6 +20,7 @@ this.battleCry = function() {
 }
 }
 Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 Viking.prototype.receiveDamage = function(damage) {
   this.health -= damage
   if (this.health === 0) {
@@ -29,7 +30,6 @@ Viking.prototype.receiveDamage = function(damage) {
   }
 }
 
-
 // Saxon
 function Saxon(healthSax, strengthSax) {
   Soldier.call(this, healthSax, strengthSax)
@@ -37,6 +37,7 @@ function Saxon(healthSax, strengthSax) {
 }
 
  Saxon.prototype = Object.create(Soldier.prototype);
+ Saxon.prototype.constructor = Saxon;
  Saxon.prototype.receiveDamage = function(damage) {
   this.health -= damage;
   if (this.health === 0) {
@@ -48,8 +49,50 @@ function Saxon(healthSax, strengthSax) {
   this.health -= damage;
  };
 
- 
-
-
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+War.prototype.addViking = function(Viking) {
+  this.vikingArmy.push(Viking);
+};
+
+War.prototype.addSaxon = function(Saxon) {
+  this.saxonArmy.push(Saxon)
+};
+
+War.prototype.vikingAttack = function() {
+ var randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length - 1) + 1];
+ var randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length - 1) + 1];
+
+ if (randomSaxon.health <= randomViking.strength) {
+   var indexSax = this.saxonArmy.indexOf(randomSaxon);
+   this.saxonArmy.splice(indexSax, 1);
+ }
+
+ return randomSaxon.receiveDamage(randomViking.strength);
+};
+
+War.prototype.saxonAttack = function() {
+  var randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length - 1) + 1];
+  var randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length - 1) + 1];
+
+  if (randomViking.health <= randomSaxon.strength) {
+    var indexVik = this.vikingArmy.indexOf(randomViking);
+    this.vikingArmy.splice(indexVik, 1);
+  }
+
+  return randomViking.receiveDamage(randomSaxon.strength);
+};
+
+War.prototype.showStatus = function() {
+if (this.saxonArmy.length === 0) {
+  return "Vikings have won the war of the century!";
+} else if (this.vikingArmy.length === 0){
+  return "Saxons have fought for their lives and survive another day...";
+} else if (this.vikingArmy.length > 0 && this.saxonArmy.length > 0) {
+  return "Vikings and Saxons are still in the thick of battle.";
+}
+};
