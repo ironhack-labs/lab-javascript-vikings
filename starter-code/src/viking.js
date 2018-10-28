83eccaf42efc83884b12c202806f235fa687372b
cocobilbao <cocobilbao@gmail.com>
2018-10-28 19:31:14 +0100

    Ejercicio viking hasta bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,77 @@
 // Soldier
-function Soldier() {}
-
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+    Soldier.prototype.attack = function attack(){
+  
+      return this.strength;
+    }
+    Soldier.prototype.reciveDamage = function reciveDamage(damage){
+      
+      this.health -= damage;
+      
+    }
+  }
+  
 // Viking
-function Viking() {}
+function Viking(nameArg, healthArg, strengthArg) {
+  this.name = nameArg;
+  this.health = healthArg;
+  this.strength = strengthArg ;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.attack = function attack(){
+
+  return this.strength;
+}
+
+Viking.prototype.reciveDamage = function reciveDamage(damage){
+
+  this.health -= damage;
+
+  if(this.health === 0){
+   return this.name +" has died in act of combat";
+  }else{
+    return this.name + " has received " + damage + " points of damage";
+  }
+  
+}
+
+Viking.prototype.battleCry = function battleCry(){
+
+  return "Odin Owns You All!"  ;
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+
+}
+Saxon.prototype = Object.create(Soldier.prototype)
+
+Saxon.prototype.attack = function attack(){
+
+  return this.strength;
+}
+Saxon.prototype.reciveDamage = function reciveDamage(damage){
+
+  this.health -= damage;
+
+  if(this.health === 0){
+    return  "A Saxon has died in combat";
+  }else{
+    return "A Saxon has received " + damage + " points of damage";
+  }
+}
+
 
 // War
-function War() {}
+function War() {
+
+
+}
+
+
