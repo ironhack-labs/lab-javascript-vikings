983c4d8f18d04bacce1c66c67e35560fb04afdc1
Jaime <herrerotajaime@gmail.com>
2018-10-29 07:18:58 +0100

    Me lie con jasmine ydescomente mas de lo que debia, pero creo que entendi el ejercicio

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,65 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health   = health;
+  this.strength = strength;
+  this.attack   = function() {
+    return strength;
+  }
+  this.receiveDamage = function(damage) {
+     this.damage = damage;
+    this.health -= damage;
+  }
+ }
+ Soldier.prototype.receiveDamage = function(damage) {
+   this.damage  =  damage;
+  this.health -= damage;
+}
+Soldier.prototype.attack = function() {
+  return this.strength;
+}
+  
+
+
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.attack();
+  this.name = name;
+  this.battleCry = function(){
+      return "Odin Owns You All!"
+  }
+  this.receiveDamage = function (damage) {
+    this.damage = damage;
+    this.health -= damage;
+    
+    if(this.health >  0){
+      return `${name} has received ${damage} points of damage`
+    } else {
+      return `${name} has died in act of combat`
+    }
+    
+  }
+}
+Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+  this.attack();
+  this.receiveDamage = function (damage) {
+    this.damage = damage;
+    this.health = health - damage;
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return "A Saxon has died in combat"
+    }
+  }
+}
+ Saxon.prototype = Object.create(Soldier.prototype);
+
 
 // War
-function War() {}
+function War() {
+}
