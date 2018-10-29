b14024da4c31c09e91ce2ea25ab1c4faf462ca49
Patricia LG <patri.lopg5@gmail.com>
2018-10-29 14:46:45 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-function Soldier() {}
+
+function Soldier(healthArm, strengthArm) {
+  this.health = healthArm;
+  this.strength = strengthArm;
+
+  this.attack = function() {
+    return this.strength;
+  };
+
+  this.receiveDamage = function(damageArm) {
+    this.health -= damageArm;
+  };
+}
 
 // Viking
-function Viking() {}
+
+function Viking(nameVik,healthVik, strengthVik, ) {
+  Soldier.call(this, healthVik, strengthVik);
+  this.name = nameVik;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.constructor = Viking;
+
+Vikings.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+
+  if (this.health > 0){
+    return `${this.name} has received ${damage} points of damage`;
+  }
+  else{
+    return `${this.name} has died in act of combat`;
+  }
+
+}
+
+/*
+
+
 
 // Saxon
 function Saxon() {}
 
 // War
 function War() {}
+*/
