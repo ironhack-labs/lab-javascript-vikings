478e5853b89f04c142c910a8b813cf786d61e437
Javier Requejo Melchor <javierrequejomelchor@gmail.com>
2018-11-06 00:14:45 +0100

    Saxon defined!

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -12,6 +12,8 @@ Soldier.prototype.receiveDamage = function (damage) {
   this.health -= damage;
 }
 
+
+
 // Viking
 function Viking(name, health, strength) {
   Soldier.call(this, health, strength);
@@ -33,8 +35,27 @@ Viking.prototype.battleCry = function () {
   return "Odin Owns You All!";
 }
 
+
+
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+
+Saxon.prototype.attack = function () {
+  return Soldier.prototype.attack.call(this);
+}
+
+Saxon.prototype.receiveDamage = function (damage) {
+  Soldier.prototype.receiveDamage.call(this, damage);
+  if (this.health>0) {
+    return 'A Saxon has received ' + damage + ' points of damage';
+  } else {
+    return 'A Saxon has died in combat';
+  }
+}
 
 // War
 function War() {}
