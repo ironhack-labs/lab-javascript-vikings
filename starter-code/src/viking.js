38013bea9b99821d2df87c1f3303b9d250512f90
jaqusa <dev@javierquinte.com>
2018-11-04 21:51:19 -0600

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,39 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health
+  this.strength = strength
+  this.attack=()=>{
+    return this.strength
+  }
+  this.receiveDamage= function (damage){
+    this.health = this.health - damage
+  }
+}
 
 // Viking
-function Viking() {}
+function Viking(name,health, strength) {
+  Soldier.call(this,health,strength)
+  this.name = name
+  this.receiveDamage = (damage)=>{
+      this.health = this.health - damage
+      if (this.health > 0) return this.name + " has received "+ damage +" points of damage"
+      else return this.name + " has died in act of combat"
+  }
+  this.battleCry= ()=>{
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health,strength) {
+  Soldier.call(this,health,strength)
+  this.receiveDamage=(damage)=>{
+      this.health = this.health - damage
+      if (this.health > 0) return "A Saxon has received " + damage + " points of damage"
+      else return "A Saxon has died in combat"   
+  }
+
+}
 
 // War
 function War() {}
