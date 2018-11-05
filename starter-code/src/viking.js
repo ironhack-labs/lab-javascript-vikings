042e82e14dd5b533c176ad8ec2e85daf634c2201
Fermin Lopez <fermin.lopez96@gmail.com>
2018-11-05 01:40:05 -0600

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,52 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  
+  this.receiveDamage = function(damage){
+   this.health -= damage;
+  }
+}
+
+  Soldier.prototype.attack = function(){
+    return this.strength;
+  }
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
 
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if(this.health > 0){
+    return (`${this.name} has recieved ${damage} points of damage`)
+    }else{
+      return(`${this.name} has died in the act of combat`)
+    }
+  }
+
+  this.battleCry = function(){
+    console.log('Odin Owns You All!')
+  }
+}
 // Saxon
-function Saxon() {}
+function Saxon(health, strength){
+ Soldier.call(this, health, strength);
+  this.health = health;
+  this.strength = strength;
+  
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if(this.health > 0){
+      return (`A Saxon has recieved ${damage} points of damage`)
+      }else{
+        return("A Saxon has died in the act of combat")
+      }
+  }
+}
 
 // War
 function War() {}
