f5ce873886114d3c6585075d73e163de58997620
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-04 23:05:10 -0600

    Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,41 @@
-// Soldier
-function Soldier() {}
+function Soldier(health, strength){
+  this.health = health
+  this.strength = strength 
+  this.attack = () => {
+      return this.strength
+  }
+    this.receiveDamage = (damage) => {
+    this.attack ()
+    this.health -= damage 
+  }
+}
 
-// Viking
-function Viking() {}
+function Viking (name, health, strength){
+  Soldier.call(this, health, strength)
+  this.name = name || "sinNombre"
+  this.receiveDamage = (damage) =>{
+      this.health -= damage
+      if (this.health <= 0){
+        return this.name + ' has died in act of combat' 
+      }
+      if(this.health > 0){
+        return this.name + " has received " + damage  + " points of damage"
+      } 
+  }
+  this.battlecry = () =>{
+    return console.log("Odin owns you")
+  }
+}
 
-// Saxon
-function Saxon() {}
-
-// War
-function War() {}
+function Saxon (health, strength){
+  Soldier.call(this, health, strength)
+  this.receiveDamage = (damage) =>{
+      this.health -= damage
+      if (this.health > 0){
+        return 'A Saxon has received ' + damage + ' points of damage'
+      }
+      if(this.health <= 0){
+        return "A Saxon has died in combat"
+      } 
+  }
+}
