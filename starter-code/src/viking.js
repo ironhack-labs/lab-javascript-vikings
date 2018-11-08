41a72f3e105d25181aede167486d16f88456a7e2
Paola Mejia <paolamejia@ANNAMEJIA.local>
2018-11-08 17:46:10 -0600

    try to complete it

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,26 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function (){
+    return this.strength;
+  }
+  this.receiveDamage = function(damage){
+     this.health -= damage;
+  }
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  Soldier.call(this, health, strength)
+}
+
+
+
+Viking.prototype = Object.create(Soldier.prototype);
+var Viking = new Soldier(health, strength);
+
 
 // Saxon
 function Saxon() {}
