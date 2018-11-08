c09e88473631cae29a5a4816f87db1529f82f5b7
Lucas <lucas@MacBook-Air-de-Lucas.local>
2018-11-08 20:33:11 +0100

    Estancado

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,6 +1,14 @@
 // Soldier
-function Soldier() {}
-
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function() {
+  return this.strength;
+}
+Soldier.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+}
 // Viking
 function Viking() {}
 
@@ -9,3 +17,5 @@ function Saxon() {}
 
 // War
 function War() {}
+
+
