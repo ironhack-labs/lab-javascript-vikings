1c18b40c3d5c60116d7460c9e1ea9870e64824d1
chaveroricardo <chavero.ricardo@gmail.com>
2018-11-08 17:19:08 -0600

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -12,14 +12,19 @@ function Soldier(health, strength) {
 
 // Viking
 function Viking(name,health,strength) {
-  this.name = name
-  Soldier.call(this, health, strength)
+  this.name = name;
+  Soldier.call(this, health, strength);
+  this.receiveDamageParent = this.receiveDamage
   this.receiveDamage = function(damage){
+    this.receiveDamageParent(damage)
     if(this.health >= 0){
       return this.name + " has received " + damage +"points of damage";
     } else{
       return this.name + " has died in act of combat";
     }
+  }
+  this.battleCry = function(){
+    return "Odin Owns You All!"
   }
 }
 
