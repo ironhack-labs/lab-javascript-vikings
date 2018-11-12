424333315cbf496a444333c84aa039e1fa583fd3
Nicolas Castro <nicolascastro@me.com>
2018-11-11 23:33:15 -0500

    Done with Viking structure function

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,33 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength
+}
+Soldier.prototype.attack = function(){
+  return this.strength
+}
+Soldier.prototype.receiveDamage = function(damage){
+  this.health -= damage
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this,health,strength);
+  this.name = name;
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
+Viking.attack = function(){
+  return this.strength
+}
+
+Viking.prototype.receiveDamage = function(damage){
+  this.health -= damage
+  if(this.health > 0){
+  return `${this.name} has received ${damage} points of damage`
+  }else {return `${this.name} has died in act of combat`}
+};
 // Saxon
 function Saxon() {}
 
