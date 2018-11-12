67018488dde61c4c7d0ca57e91848dd2dc19c745
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2018-11-11 23:06:14 -0500

    Viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,27 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, StrengthArg) {
+this.health = healthArg;
+this.strength= StrengthArg;
+this.attack = function (){
+  return this.strength;
+};
+this.receiveDamage = function(theDamage){
+        this.health -= theDamage;
+        
+};
+}
+
 
 // Viking
-function Viking() {}
+function Viking(name, health , strength) {
+  Soldier.call(this ,name , health , strength );
+  this.name = name;
+  this.health = health;
+  this.strength=strength;
+  this.attack = function(){
+      return this.strength;
+  };
+}
 
 // Saxon
 function Saxon() {}
