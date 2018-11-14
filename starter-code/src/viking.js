63427bd304ab329c5b46e3215d028e7d097ade54
Nicolas Castro <nicolascastro@me.com>
2018-11-14 18:50:16 -0500

    Viking lab complete

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -27,9 +27,25 @@ Viking.prototype.receiveDamage = function(damage){
   if(this.health > 0){
   return `${this.name} has received ${damage} points of damage`
   }else {return `${this.name} has died in act of combat`}
-};
+}
+Viking.prototype.battleCry = function(){
+  return "Odin Owns You All!"
+}
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
+Saxon.attack = function (){
+  return this.strength
+}
+Saxon.prototype.receiveDamage = function(damage){
+  this.health -= damage
+  if(this.health > 0){
+  return `A Saxon has received ${damage} points of damage`
+  }else {return `A Saxon has died in combat`}
+  }
 // War
 function War() {}
