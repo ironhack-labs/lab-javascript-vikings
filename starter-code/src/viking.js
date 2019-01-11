098c649c89aa0d9efbddc6c715f4a48d6db5c3ab
Cristiana Yasuda <cristiana.yasuda@gmail.com>
2019-01-11 16:38:37 -0200

    vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,80 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
+}
+
+Soldier.prototype.attack = function(){
+    return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
+}
+
+Viking.prototype = Object.create(Soldier.prototype); 
+Viking.prototype.constructor = Viking;
+Viking.prototype.receiveDamage = function(damage){
+    this.health -= damage;
+    if(this.health >= 1){
+        return this.name + " has received " + damage + " points of damage";
+    } else{
+        return this.name + " has died in act of combat";
+    }
+}
+
+Viking.prototype.battleCry = function(){
+    return "Odin Owns You All!";
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+}
 
-// War
-function War() {}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+Saxon.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+    if(this.health >= 1){
+        return "A Saxon has received " + damage + " points of damage";
+    } else {
+        return "A Saxon has died in combat";
+    }
+}
+
+/* War
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+
+}
+War.prototype.addViking = function(Viking){
+    this.vikingArmy.push(Viking);
+
+}
+War.prototype.addSaxon = function(Saxon){
+    this.saxonArmy.push(Saxon);
+
+}
+War.prototype.vikingAttack = function(){
+    let saxon = Math.floor(Math.random()*saxonArmy.length);
+    let viking = vikingArmy[Math.floor(Math.random()*vikingArmy.length)];
+   return saxon.receiveDamage(viking.strength);
+
+}; console.log(war); 
+
+
+
+War.prototype.saxonAttack = function(){
+
+}
+War.prototype.showStatus = function(){
+
+}*/
