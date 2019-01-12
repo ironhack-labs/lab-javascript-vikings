1c066461d9f2ae05c2aa4a3524157caf7b56f009
Mr.X <seniorito.x@icloud.com>
2019-01-12 20:47:23 +0100

    First commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,43 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  
+  this.health = healthArg;
+  this.strength = strengthArg;
+  }
+
+  Soldier.prototype.attack = function(){
+    return this.strength
+  }
+
+  Soldier.prototype.receiveDamage = function (damage){
+    this.health -= damage;
+  }
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+}
+
+Viking.prototype = Object.create(Soldier.prototype)
+
+Viking.prototype.receiveDamage = function(damage){
+  this.health -= damage;
+
+  if (this.health > 0){
+    return this.name + " has received " + damage + " points of damage"
+  } else {
+    return this.name + " has died in act of combat"
+  }
+}
+
+Viking.prototype.battleCry = function (){
+  return "Odin Owns You All!"
+
+}
+
+
 
 // Saxon
 function Saxon() {}
