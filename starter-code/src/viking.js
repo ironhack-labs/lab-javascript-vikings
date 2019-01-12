179125f6ab780d369ce51aef0ea7ca45423f8f89
Sofía <sofianombela@gmail.com>
2019-01-12 11:30:04 +0100

    Soldier done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,6 +1,15 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg;
 
+    this.attack = function(){
+        return this.strength;
+    }
+    this.receiveDamage = function(damage){
+        damage -= this.health - damage;
+    }
+}
 // Viking
 function Viking() {}
 
