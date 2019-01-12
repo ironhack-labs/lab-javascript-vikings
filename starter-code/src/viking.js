ac6cadb6ff387c65e7d5a7f8f37ca51f7eae928f
Miguelerja <miguelerja@gmail.com>
2019-01-12 14:10:34 +0100

    Add war methods

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -21,7 +21,7 @@ function Viking(name, health, strength) {
   this.name = name;
 };
 
-Viking.prototype = Object.create(Soldier.prototype);   //WTF
+Viking.prototype = Object.create(Soldier.prototype); //WTF
 
 Viking.prototype.receiveDamage = function (damage) {
   this.health = health - damage;
@@ -58,12 +58,43 @@ function War() {
   };
 
   this.vikingAttack = function vikingAttack() {
+    var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    var attackedSaxon = this.saxonArmy[chooseRandomSaxon];
+    var attackingViking = this.vikingArmy[chooseRandomViking];
 
-  }
+    attackedSaxon.receiveDamage(attackingViking.strength);
+
+    if (attackedSaxon.health <= 0) {
+      this.saxonArmy.splice(chooseRandomSaxon, 1);
+    };
+  };
+
+  this.saxonAttack = function saxonAttack() {
+    var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    var attackingSaxon = this.saxonArmy[chooseRandomSaxon];
+    var attackedViking = this.vikingArmy[chooseRandomViking];
+
+    attackedViking.receiveDamage(attackingSaxon.strength);
+
+    if (attackedViking.health <= 0) {
+      this.vikingArmy.splice(chooseRandomViking, 1);
+    };
+  };
+
+  this.showStatus = function showStatus() {
+    switch (true) {
+      case this.saxonArmy.length < 1:
+        return "Vikings have won the war of the century!";
+        
+      case this.vikingArmy.length < 1:
+        return "Saxons have fought for their lives and survive another day...";
+
+        default:
+        return "Vikings and Saxons are still in the thick of battle.";
+    };
+  };
 };
 
-var loki = new Viking("Loki", 150, 600);
 
-console.log(loki.receiveDamage(50));
-
-console.log(loki);
