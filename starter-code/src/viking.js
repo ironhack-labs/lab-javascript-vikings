b2615f608b242b6a0db792556dab781649477c89
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-01-12 14:59:25 +0100

    Soldier Done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,24 @@
 // Soldier
-function Soldier() {}
+function Soldier(health , strength) {
+  this.health = health;
+  this.strength = strength;
+  
+  
+  this.attack = function() {
+    
+      return this.strength;
+  }
+  
+  this.receiveDamage = function(thedamage) {
+      this.health -= thedamage;
+      
+      
+      
+  }
+}
+
+
+
 
 // Viking
 function Viking() {}
