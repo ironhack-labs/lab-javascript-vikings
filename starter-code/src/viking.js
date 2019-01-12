1b70b0a085c13e273c8f77c597f436aa91deadf4
Sofía <sofianombela@gmail.com>
2019-01-12 12:10:15 +0100

    Viking Soldier done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -7,11 +7,32 @@ function Soldier(healthArg, strengthArg) {
         return this.strength;
     }
     this.receiveDamage = function(damage){
-        damage -= this.health - damage;
+        this.health -= damage;
     }
 }
+
 // Viking
-function Viking() {}
+function Viking(nameArg, healthArg, strengthArg) {
+    Soldier.call(this, healthArg,strengthArg);
+    this.name = nameArg;
+
+    this.receiveDamage = function(damage) {
+        this.health -= damage;
+
+        if (this.health > 0){
+            return this.name + " has received " + damage + " points of damage"
+        } else {
+            return this.name + " has died in act of combat"
+        }
+    }
+
+    this.battleCry = function(){
+        return "Odin Owns You All!"
+    }
+}
+Viking.prototype = Object.create(Soldier.prototype)
+
+
 
 // Saxon
 function Saxon() {}
