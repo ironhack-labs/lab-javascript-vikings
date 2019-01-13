2ccecf273550617f7995f5d7310c56a70a1de38f
mocuishla <misabellanes@gmail.com>
2019-01-13 13:21:16 +0100

    Viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -12,13 +12,29 @@ function Soldier(health, strength) {
 
 // Viking
 function Viking(name, health, strength) {
+  this.name = name;
   Soldier.call(this, health, strength);
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if (this.health>0){
+      return this.name + " has received " + damage + " points of damage" 
+    } else {
+      return this.name + " has died in act of combat"
+    }
+  }
+  this.battleCry = function(){
+    return "Odin Owns You All!"
+  }
 }
 
 Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
-function Saxon() {}
+function Saxon() {
+  Soldier.call(health, strength);
+}
+
+Saxon.p
 
 // War
 function War() {}
