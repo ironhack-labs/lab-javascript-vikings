33939d0f8693302692f14291cd78806058591cff
Mr.X <seniorito.x@icloud.com>
2019-01-13 03:04:09 +0100

    Done including BONUS ᕙ(`▽´)ᕗ

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -40,7 +40,75 @@ Viking.prototype.battleCry = function (){
 
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+Saxon.prototype = Object.create(Soldier.prototype)
+
+Saxon.prototype.receiveDamage = function(damage){
+  this.health -= damage
+
+  if (this.health > 0){
+    return "A Saxon has received " + damage + " points of damage"
+  } else {
+    return "A Saxon has died in combat"
+  }
+}
 
 // War
-function War() {}
+
+function War() {
+
+  this.vikingArmy =[]
+
+  this.saxonArmy =[]
+
+  War.prototype.addViking = function(viking){
+    this.vikingArmy.push(viking)
+  }
+
+  War.prototype.addSaxon = function(saxon){
+    this.saxonArmy.push(saxon)
+  }
+
+  War.prototype.vikingAttack = function(){
+    var saxonRandom = Math.floor(Math.random()*this.saxonArmy.length)
+
+    var vikingRandom = Math.floor(Math.random()*this.vikingArmy.length)
+
+    var combat = this.saxonArmy[saxonRandom].receiveDamage(this.vikingArmy[vikingRandom].attack())
+
+    if (this.saxonArmy[saxonRandom].health <= 0){
+      this.saxonArmy.splice(saxonRandom, 1)
+    }
+
+    return combat
+  }
+
+  War.prototype.saxonAttack = function(){
+    var saxonRandom = Math.floor(Math.random()*this.saxonArmy.length)
+
+    var vikingRandom = Math.floor(Math.random()*this.vikingArmy.length)
+
+    var combat = this.vikingArmy[vikingRandom].receiveDamage(this.saxonArmy[saxonRandom].attack())
+
+    if (this.vikingArmy[vikingRandom].health <= 0){
+      this.vikingArmy.splice(vikingRandom, 1)
+    }
+
+    return combat
+  }
+
+  War.prototype.showStatus = function (){
+    
+    if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0){
+      return "Vikings and Saxons are still in the thick of battle."
+    }  else if (this.saxonArmy.length <= 0){
+      return "Vikings have won the war of the century!"
+    } else if (this.vikingArmy.length <= 0){
+      return "Saxons have fought for their lives and survive another day..."
+    }
+  }
+}
