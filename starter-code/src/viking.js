f3bba8a868a507c4b45b55db02dad195949d2d30
mocuishla <misabellanes@gmail.com>
2019-01-13 12:34:13 +0100

    Soldie done, Viking started

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,21 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function() {
+    return this.strength;
+  };
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+  };
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
 function Saxon() {}
