602b74f78d5674cba033b8a5be850936643d6d27
krls13 <krlsalberto13@hotmail.com>
2019-01-14 09:31:30 +0100

    firs commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,44 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    this.health = healthArg
+    this.strength = strengthArg
+    this.attack = function () {
+        return this.strength
+    }
+    this.receiveDamage = function (damage) {
+        this.health = this.health - damage;
 
+    }
+}
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name
+    this.receiveDamage = function (damage) {
+        this.health = this.health - damage
+        if (this.health > 0) {
+            return this.name + "has received" + damage + "points of damage"
+        } else {
+            return this.name + "has died in act of combat"
+        }
+    }
+    this.battleCry = function () {
+        return "Odins Owns You All!"
+    }
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+    Soldier.call(this.health, strength);
+    this.receiveDamage = function (damage) {
+        this.health = this.health - damage
+        if (this.health > 0) {
+            return "A Saxon has received " + damage + "points of damage"
+        } else{
+            return "A Saxon has died in combat"
+        }
+    }
+}
 
 // War
-function War() {}
+function War() { }
