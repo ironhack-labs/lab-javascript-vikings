5bab0e703f24025e576d703373c616991b0be61d
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-14 01:01:20 +0100

    Hecho todo menos el Bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg,strengthArg) {
+    this.health=healthArg;
+    this.strength=strengthArg;
+    this.attack=function(){
+        return strengthArg;
+    };
+     this.receiveDamage=function(damage){
+        this.health=this.health-damage;
+     };
+}
 
 // Viking
-function Viking() {}
+function Viking(name,healthArg,strengthArg) {
+    Soldier.call(this,healthArg,strengthArg);
+    this.name=name;
+    this.attack=function(){
+        return this.strength;
+    };
+    this.receiveDamage=function(damage){
+        this.health=this.health-damage;
+        if(this.health>0){ return this.name + " has received " + damage + " points of damage";}
+        else {return this.name + " has died in act of combat"}
+     };
+     this.battleCry=function(){
+         return "Odin Owns You All!";
+     }
+    
+
+}
+            Viking.prototype = Object.create(Soldier.prototype)
+            Viking.prototype.constructor = Viking;
+            
 
 // Saxon
-function Saxon() {}
+function Saxon(healthArg,strengthArg) {
+    Soldier.call(this,healthArg,strengthArg);
+
+    this.attack=function(){
+        return strengthArg;
+    };
+    this.receiveDamage=function(damage){
+        this.health=this.health-damage;
+        if(this.health>0){ return "A Saxon has received " + damage + " points of damage";}
+        else {return this.name + " has died in act of combat"}
+     };
+}
+
+            Saxon.prototype = Object.create(Soldier.prototype)
+            Viking.prototype.constructor = Viking;
 
 // War
 function War() {}
