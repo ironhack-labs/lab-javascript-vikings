e39423f119e5448795a6a1bf364ef85f5fb930ed
Miguelerja <miguelerja@gmail.com>
2019-01-14 10:14:33 +0100

    Refactor constructors to set methods into the prototype

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,117 +1,116 @@
 // Soldier
 
 function Soldier(health, strength) {
-  this.health = health;
-  this.strength = strength;
+	this.health = health;
+	this.strength = strength;
+};
 
-  this.attack = function attack() {
-    return this.strength;
-  };
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
 
-  this.receiveDamage = function receiveDamage(damage) {
-    this.health -= damage;
-  };
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
 };
 
 // Viking
 
 function Viking(name, health, strength) {
-  Soldier.call(this, health, strength);
-
-  this.name = name;
-
-  this.receiveDamage = function (damage) {
-    this.health -= damage;
-    if (this.health > 0) {
-      return `${this.name} has received ${damage} points of damage`;
-    } else {
-      return `${this.name} has died in act of combat`;
-    };
-  };
-};
+	Soldier.call(this, health, strength);
+	this.name = name;
+}
 
 Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
-Viking.prototype.battleCry = function () {
-  return "Odin Owns You All!";
+Viking.prototype.battleCry = function() {
+	return 'Odin Owns You All!';
+};
+
+Viking.prototype.receiveDamage = function(damage) {
+	this.health -= damage;
+	if (this.health > 0) {
+		return `${this.name} has received ${damage} points of damage`;
+	} else {
+		return `${this.name} has died in act of combat`;
+	}
 };
 
 // Saxon
 
 function Saxon(health, strength) {
-  Soldier.call(this, health, strength);
-
-  this.receiveDamage = function (damage) {
-    this.health -= damage;
-    if (this.health > 0) {
-      return `A Saxon has received ${damage} points of damage`;
-    } else {
-      return "A Saxon has died in combat";
-    };
-  };
+	Soldier.call(this, health, strength);
 };
 
 Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+Saxon.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return `A Saxon has received ${damage} points of damage`;
+  } else {
+    return 'A Saxon has died in combat';
+  }
+};
+
 
 // War
 
 function War() {
-  this.vikingArmy = [];
-  this.saxonArmy = [];
+	this.vikingArmy = [];
+	this.saxonArmy = [];
+}
 
-  this.addViking = function addViking(viking) {
-    this.vikingArmy.push(viking);
-  };
+War.prototype.addViking = function(viking) {
+	this.vikingArmy.push(viking);
+};
 
-  this.addSaxon = function addSaxon(saxon) {
-    this.saxonArmy.push(saxon);
-  };
+War.prototype.addSaxon = function(saxon) {
+	this.saxonArmy.push(saxon);
+};
 
-  this.vikingAttack = function vikingAttack() {
+War.prototype.vikingAttack = function() {
+	var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+	var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
 
-    var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-    var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+	var attackedSaxon = this.saxonArmy[chooseRandomSaxon];
+	var attackingViking = this.vikingArmy[chooseRandomViking];
 
-    var attackedSaxon = this.saxonArmy[chooseRandomSaxon];
-    var attackingViking = this.vikingArmy[chooseRandomViking];
+	var vikingAttack = attackedSaxon.receiveDamage(attackingViking.strength);
 
-    var vikingAttack = attackedSaxon.receiveDamage(attackingViking.strength);
+	if (attackedSaxon.health <= 0) {
+		this.saxonArmy.splice(chooseRandomSaxon, 1);
+	}
 
-    if (attackedSaxon.health <= 0) {
-      this.saxonArmy.splice(chooseRandomSaxon, 1);
-    };
+	return vikingAttack;
+};
 
-    return vikingAttack;
-  };
+War.prototype.saxonAttack = function() {
+	var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+	var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
 
-  this.saxonAttack = function saxonAttack() {
+	var attackingSaxon = this.saxonArmy[chooseRandomSaxon];
+	var attackedViking = this.vikingArmy[chooseRandomViking];
 
-    var chooseRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-    var chooseRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+	var saxonAttack = attackedViking.receiveDamage(attackingSaxon.strength);
 
-    var attackingSaxon = this.saxonArmy[chooseRandomSaxon];
-    var attackedViking = this.vikingArmy[chooseRandomViking];
+	if (attackedViking.health <= 0) {
+		this.vikingArmy.splice(chooseRandomViking, 1);
+	}
 
-    var saxonAttack = attackedViking.receiveDamage(attackingSaxon.strength);
+	return saxonAttack;
+};
 
-    if (attackedViking.health <= 0) {
-      this.vikingArmy.splice(chooseRandomViking, 1);
-    };
+War.prototype.showStatus = function() {
+	switch (true) {
+		case this.saxonArmy.length < 1:
+			return 'Vikings have won the war of the century!';
 
-    return saxonAttack;
+		case this.vikingArmy.length < 1:
+			return 'Saxons have fought for their lives and survive another day...';
 
-  };
-
-  this.showStatus = function showStatus() {
-    switch (true) {
-      case this.saxonArmy.length < 1:
-        return "Vikings have won the war of the century!";
-
-      case this.vikingArmy.length < 1:
-        return "Saxons have fought for their lives and survive another day...";
-
-      default:
-        return "Vikings and Saxons are still in the thick of battle.";
-    };
-  };
+		default:
+			return 'Vikings and Saxons are still in the thick of battle.';
+	}
 };
