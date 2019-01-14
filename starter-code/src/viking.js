48cdfe592873c263d06e4bda639c29a183158759
Pedro BM <p.bonilla.miralles@gmail.com>
2019-01-14 09:36:22 +0100

    BCN-PEDRO.BONILLA-WDFT19

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.healthArg = healthArg;
+  this.strengthArg = strengthArg;
+}
+
+Soldier.prototype.attack = function() {
+  return strengthArg;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+
+}
 
 // Viking
-function Viking() {}
+function Viking(healthArg, strengthArg, name) {
+  this.healthArg = healthArg;
+  this.strengthArg = strengthArg;
+  this.name = name;
+}
+
+Viking.prototype.attack = function() {
+  return strengthArg;
+}
+
+Viking.prototype.receiveDamage = function() {
+  if (healthArg !== 0) {
+    console.log(name + " has received " + damage + " points of damage")
+  } else {
+    console.log(name + " has died in act of combat")
+  }
+}
+
+Viking.prototype.battlecry = function() {
+  console.log("Odin Owns You All!");
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(healthArg, strengthArg) {
+  this.healthArg = healthArg;
+  this.strengthArg = strengthArg;
+}
+
+Saxon.prototype.attack = function() {
+  return strengthArg;
+}
+
+Saxon.prototype.receiveDamage = function(damage) {
+  
+}
 
 // War
 function War() {}
