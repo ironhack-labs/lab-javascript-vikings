78d7d6b80a7e07b1ee5b76a01eec2a256a603827
Raja Prahara <raja.ardri@gmail.com>
2019-01-15 15:00:13 +0100

    Paris: Poeli & Raja

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,47 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor (healthArg, strengthArg) {
+    this.health = healthArg
+    this.strength = strengthArg
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage 
+  } 
+}
+
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  
+  receiveDamage(damage) {
+    this.health = this.health - damage 
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    } else {
+      return this.name + " has died in act of combat";
+    } 
+
+    // if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
+    // if the Viking dies, it should return "NAME has died in act of combat"
+  } 
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
+
+viking.attack();
 
 // Saxon
 function Saxon() {}
