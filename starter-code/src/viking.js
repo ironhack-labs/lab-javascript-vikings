24d47d2ab77e6d7f114ad89b90320c86b61a32bb
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-28 11:03:12 -0500

    -finished consttructor functions

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,8 @@
 // Soldier
 function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength
+
   attack(){
     return strength
       }
@@ -11,6 +14,8 @@ function Soldier(health, strength) {
 // Viking
 function Viking extends Soilder(name, health, strength) {
   this.name = name
+  this.health = health;
+  this.strength = strength
 
   recieveDamage(damage){
     health -= damage
@@ -21,12 +26,30 @@ function Viking extends Soilder(name, health, strength) {
       this.name = 'has die in act of combat'
     }
   }
-
-  battleCry()
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
 }
 
 // Saxon
-function Saxon() {}
+function Saxon extends Soilder(health, strength) {
+  this.health = health;
+  this.strength = strength
+
+  attack(){
+    return strength
+  }
+  recieveDamage(damage){
+    health -= damage
+    if (this.health > 0){
+      return 'A Saxon has recieved' + damage + 'points of damage'
+    }
+    else{
+      return 'A saxon has died'
+    }
+  }
+
+}
 
 // War
 function War() {}
