1d0c112083c845a0074eca25228f9c90471685f6
nunna19 <nunnapat_ch@yahoo.com>
2019-01-28 12:48:54 -0500

    done but will do the bunus later

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,75 @@
 // Soldier
-function Soldier() {}
+// function Soldier(health, strength) {
+// this.health =health
+// this.strength = strength
+// }
+// User.prototype.attack = function() {
+//   return(this.strength);
+// }
+// user.prototype.receiveDamage = function(){
+//   sutper(health)
+// }
+
+class Soldier{
+  constructor(health,strength){
+    this.health = health
+    this.strength = strength
+  }
+    attack(){
+      return(this.strength);
+  }
+  receiveDamage(damage){
+    this.health -= damage
+  }
+}
 
 // Viking
-function Viking() {}
 
-// Saxon
-function Saxon() {}
+
+
+class Viking extends Soldier{
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage){
+    this.health = this.health -= damage
+    if(this.health <= 0){
+      return this.name +' has died in act of combat'
+    }else{
+      return this.name +' has received '+ damage+' points of damage'
+    }
+    
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+  }
+
+  // Saxon
+
+  class Saxon extends Soldier{
+    constructor(health,strength){
+      super(health,strength)
+    }
+    receiveDamage(damage){
+      this.health = this.health -= damage
+      if(this.health <= 0){
+        return 'A Saxon has died in combat'
+      }else{
+        return 'A Saxon has received '+ damage+' points of damage'
+      }
+  }
+}
+
+
+
+
+
+
+
+
+
 
 // War
 function War() {}
