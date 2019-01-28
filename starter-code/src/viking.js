5842b01a95b44e232a87d77255fad09fa6127808
cameron-chromiak <cam.chromiak@hotmail.com>
2019-01-28 10:55:15 -0500

    -up to viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,29 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  attack(){
+    return strength
+      }
+  recieveDamage(damage){
+    this.health - damage
+    }
+}
 
 // Viking
-function Viking() {}
+function Viking extends Soilder(name, health, strength) {
+  this.name = name
+
+  recieveDamage(damage){
+    health -= damage
+    if (health > 0){
+      return this.name = 'has recieved' + damage + 'points of damage'
+    }
+    else{
+      this.name = 'has die in act of combat'
+    }
+  }
+
+  battleCry()
+}
 
 // Saxon
 function Saxon() {}
