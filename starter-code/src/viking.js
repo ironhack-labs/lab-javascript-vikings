aab72f6cd3d333c65c3c7772348636e56e83a6f2
Victoria Hickam <vhickam@gmail.com>
2019-01-28 10:46:07 -0500

    done without bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-function Soldier() {}
+class Soldier{
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+}
+
+Soldier.prototype.attack = function(){
+  return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function(damage){
+  this.health = this.health - damage;
+}
 
 // Viking
-function Viking() {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if(this.health > 0){
+      return this.name + " has received " + damage + " points of damage";
+    }
+    else {
+      return this.name + " has died in act of combat";
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+
+
+}
 
 // Saxon
-function Saxon() {}
+class Saxon extends Soldier {
+  constructor(health, strength){
+    super(health,strength);
+  }
+
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if(this.health > 0){
+      return "A Saxon has received " + damage + " points of damage";
+    }
+    else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
 function War() {}
