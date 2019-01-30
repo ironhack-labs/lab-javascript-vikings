b79d7d2f53f3eb30b83890b116b41d909e92b8cd
lockeas16 <lockeas16@gmail.com>
2019-01-29 23:59:39 -0600

    saxon class done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+
+  this.attack = function (){
+    return this.strength;
+  };
+
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
 function Viking() {}
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+
+  this.receiveDamage = function (damage){
+    this.health -= damage;
+
+    if (this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    } else{
+      return `A Saxon has died in combat`;
+    }
+  }
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
 function War() {}
