10092de9dc00eb0e8e61a1d954ca3487f3cdd088
rpernicone <roberto.pernicone@gmail.com>
2019-01-31 19:08:28 -0600

    getting better

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,68 @@
-// Soldier
-function Soldier() {}
+Soldier
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function(){
+       return this.strength;
+    }
+    this.receiveDamage = function(damage){
+        this.receiveDamage;
+        this.health -= damage;
+    }
+}
 
-// Viking
-function Viking() {}
+Viking
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
 
-// Saxon
-function Saxon() {}
+     this.receiveDamage = function(damage){
+         this.receiveDamage;
+         this.health -= damage;
+         if (this.health > 0){
+         return name + " has received " + damage + " points of damage"};
+         if (this.health == 0){
+         return name + " has died in act of combat"};
+     }
+     this.battleCry = function(){
+         return "Odin Owns You All!";
+     }
+}
+    Viking.prototype = Object.create(Soldier.prototype);
+    Viking.prototype.contructor = Viking;
 
-// War
-function War() {}
+Saxon
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+    this.receiveDamage = function(damage){
+        this.receiveDamage;
+        this.health -= damage;
+        if (this.health > 0){
+        return "A Saxon has received " + damage + " points of damage"};
+        if (this.health == 0){
+        return "A Saxon has died in combat"};
+};
+};
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.contructor = Saxon;
+
+War
+function War() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+};
+War.prototype.addViking = function(viking){
+    this.vikingArmy = [viking];
+};
+War.prototype.addSaxon = function(saxon){
+    this.saxonArmy = [saxon];
+};
+War.prototype.vikingAttack = function(){
+    Saxon.receiveDamage(viking.strength) = saxon.health;
+}
+War.prototype.saxonAttack = function(){
+
+}
+War.prototype.showStatus = function(){
+
+}
