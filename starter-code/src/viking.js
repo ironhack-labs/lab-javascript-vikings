7302318d67b941ec10e0f8fc7a92a1b563dd55c4
Debasri Dasgupta <debasri_dasgupta@rediffmail.com>
2019-02-04 22:58:19 +0100

    Bonus to do

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,12 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+    //expect(Soldier.length).toEqual(2);
+    this.health = healthArg;
+    this.strength = strengthArg;
+    this.attack = function(){
+        return this.strength;
+    };
+}
 
 // Viking
 function Viking() {}
