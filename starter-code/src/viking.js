febdff05039323bb6bd06f11e75b730438fc2f42
David Gallego Gomez <davizs@hotmail.com>
2019-02-05 22:39:30 +0100

    Commit inicial

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-function Soldier() {}
-
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function () { return this.strength; };
+  this.receiveDamage = function (damage) { this.health -= damage; };
+}
+  
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.receiveDamage = function (damage) { 
+    this.health -= damage;
+    if (this.health > 0) { return ( this.name + ' has received ' + damage + ' points of damage' ); }
+    else { return ( this.name + ' has died in act of combat' ); }
+  };
+  this.battleCry = function () {
+    return ( 'Odin Owns You All!' );
+  };
+}
 
 // Saxon
 function Saxon() {}
