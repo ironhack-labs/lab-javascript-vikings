40d52ddb66807ae7f23658b8a53635ebf19eb6ed
NuriaTired <nuriatired@gmail.com>
2019-02-05 19:02:40 +0100

    completed

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,83 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+     this.health=health;
+     this.strength = strength;
+}
+
+Soldier.prototype.attack=function(){
+    return this.strength;
+}
+
+Soldier.prototype.receiveDamage = function (a) {
+    this.health=health-a;
+}
 
 // Viking
-function Viking() {}
+function Viking(name) {
+    Soldier.call(this, health, strength);
+    this.attack = function(){
+        return this.strength;
+    }
 
+    this.receiveDamage = function(b){
+        this.health = health - b;
+        if(this.health>b){
+            return this.name + " has received " + this.b + " points of damage"
+        }else{
+            return this.name + " has died in act of combat"
+        }
+    }
+
+    this.battleCry = function(){
+        return "Odin Owns You All!";
+    }
+}
+
+//si hago el call, debo declarar las variables, a parte?o debo entender que se heredaran?)
 // Saxon
-function Saxon() {}
+function Saxon() {
+    Soldier.call(this, health, strength);
+
+    this.attack = function () {
+        return this.strength;
+    }
+
+    this.receiveDamage = function (c) {
+        this.health = health - b;
+        if (this.health > b) {
+            return  "A Saxon has received " + this.b + " points of damage"
+        } else {
+            return  "A Saxon has died in act of combat"
+        }
+    }
+
+
+}
 
 // War
-function War() {}
+function War() {
+    this.vikingArmy=[];
+    this.saxonArmy=[];
+
+    this.addViking=function(viking){
+        this.vikingArmy.push(viking);
+    }
+
+    this.addSaxon=function(saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    this.vikingAttack=function(){
+        
+
+    }
+
+    this.saxonAttack=function(){
+
+    }
+
+    this.showStatus=function(){
+
+    }
+
+}
