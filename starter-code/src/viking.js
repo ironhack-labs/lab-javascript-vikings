3f0feb7d1e5e4209f3b5e535e8ae50d26f5827f5
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-05 21:57:12 +0100

    attack

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,12 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function(){
+  return strengthArg
+  }
+}
+
 
 // Viking
 function Viking() {}
