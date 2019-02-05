d59f2a9f8cb6e0e085c490b53cb4288d56590761
Josue Valencia <josue.valencia@domo.ec>
2019-02-05 21:40:39 +0100

    soldier, vikings and saxos

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,41 @@
 // Soldier
-function Soldier() {}
+function Soldier (health, strength) {
+    this.health = health;
+    this.strength = strength;
+
+    this.attack = () => {
+        return this.strength;
+    }
+
+    this.receiveDamage = (dmg) => {
+        this.health = this.health - dmg;
+    }
+}
 
 // Viking
-function Viking() {}
+function Viking (name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.name = name;
+    this.receiveDamage = (dmg) => {
+        this.health = this.health - dmg;
+        return this.health > 0 ? `${this.name} has received ${dmg} points of damage` : `${this.name} has died in act of combat`;
+    }
+
+    this.battleCry = () => {
+        return "Odin Owns You All!";
+    }
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
 
 // Saxon
-function Saxon() {}
-
+function Saxon(health, strength) {
+    Soldier.call(this, health, strength);
+    this.receiveDamage = (dmg) => {
+        this.health = this.health - dmg;
+        return this.health > 0 ? `A Saxon has received ${dmg} points of damage` : `A Saxon has died in combat`;
+    }
+}
+Saxon.prototype = Object.create(Soldier.prototype);
 // War
 function War() {}
