96d76106b556fb5e2fb1e614d9f908420c150671
Antonio Méndez <antonio.mendez.martin89@gmail.com>
2019-02-05 23:59:32 +0100

    exercise finished and bonus started

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -35,7 +35,34 @@ function Viking(name, health, strength) {
 
 
 // Saxon
-function Saxon() {}
+Saxon.prototype = Object.create(Soldier.prototype);
+
+
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+  this.receiveDamage = function (damage){
+    this.health = this.health - damage;
+    if (this.health > 0){
+      return "A Saxon has received " + damage + " points of damage"
+    } else{
+        return "A Saxon has died in combat"
+      }
+   }
+  }
+
 
 // War
-function War() {}
+function War() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+  this.addViking = function(Viking){
+  this.vikingArmy.push(Viking);
+  return};
+  this.addSaxon = function (Saxon){
+    this.saxonArmy.push(Saxon);
+    return};  
+  this.vikingAttack = function(){
+    
+  }  
+  }
+  
