9f3fd4336dacb6d302326a797a4d477538bfb894
Javi L <jlopezro@gmail.com>
2019-02-06 02:35:39 +0100

    realizado hasta attack() method

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,74 @@
 // Soldier
-function Soldier() {}
+
+/*function Soldier(healthArg, strengthArg) {
+
+    var soldier = {
+        health: healthArg,
+        strength: strengthArg
+    };
+       
+    return soldier;
+}
+*/
+
+function Soldier(health, strength) {
+
+        this.health = health;
+        this.strength = strength;
+
+        var soldier = {health, strength};
+       
+    return soldier;
+}
+
+//var attack = new Soldier("a", "b");
+
+/*
+var attack = function() {
+
+    strength = 150;
+    health   = 300;
+  
+    return "Soldier strength is " + this.strength;
+}
+*/
+
+/*function attack(){
+    return "Soldier strength is " + this.strength;
+  };
+*/
+
+var attack = new Soldier(150, 300);
+
+attack.showSoldierStrength = function(){
+    return "Soldier strength is " + this.strength;
+  };
+
+
+var receiveDamage = function(damage) {
+
+damage - this.health;
+
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+
+    Soldier.call(this, health, strength);
+    this.name = name;
+    var viking = {name, health, strength};
+    return viking;
+}
+
+var vikingAttack = new Viking();
+
+vikingAttack.showVikingStrength = function(){
+    return "Viking strength is " + this.strength;
+};
+
+
+
+//var battleCry = new Soldier();
 
 // Saxon
 function Saxon() {}
