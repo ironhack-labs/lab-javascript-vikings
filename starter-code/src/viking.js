66f750a96ed8253bb9a57d9701922239dd404d08
Nouri T <zorisse@gmail.com>
2019-02-09 15:30:20 +0100

    done without bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.battleCry = function() {
+    return `Odin Owns You All!`;
+  };
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
+
+    if (this.health > 0) {
+      return `${name} has received ${damage} points of damage`;
+    } else {
+      return `${name} has died in act of combat`;
+    }
+  };
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(this, health, strength);
+  this.receiveDamage = function(damage) {
+    this.health = this.health - damage;
 
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  };
+}
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 // War
 function War() {}
+
+// Methods
+
+Soldier.prototype.attack = function() {
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health = this.health - damage;
+};
