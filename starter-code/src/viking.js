1d2506f7c3e7486011178073a29999434ab1ae99
Cristina Blanco <cristinablanco@MacBook-Pro-de-Cristina.local>
2019-02-10 11:57:21 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,91 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg //health: healthArg
+  this.strength = strengthArg //strength: strengthArg
+}
+  Soldier.prototype.attack = function () {
+ return this.strength
+}
+  Soldier.prototype.receiveDamage = function (damage){
+  this.health -= damage
+  
+}
+
 
 // Viking
-function Viking() {}
+ function Viking(nameArg, healthArg, strengthArg) {
+   this.name = nameArg
+   this.health = healthArg
+   this.strength = strengthArg
+
+
+ }
+  Viking.prototype = Object.create(Soldier.prototype);
+  Viking.prototype.constructor = Viking;
+
+  Viking.prototype.receiveDamage = function (damage){
+    this.health -= damage
+}
+
+Viking.prototype.battleCry = function () {
+  return "Odin Owns You All!"
+}
+
+// if (Viking.health - Viking.prototype.receiveDamage) {
+//   return this.name + " has received  " + this.health +  " points of damage"
+// } else {
+//   return this.name + " has died in act of combat"
+// }
+
+
+
+
+
+
+
 
 // Saxon
-function Saxon() {}
+function Saxon(healthArg, strengthArg) {
+  this.health = healthArg
+  this.strength = strengthArg
+
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
+
+Saxon.prototype.receiveDamage = function (damage){
+  this.health -= damage
+  
+}
+
+
+
+
+
+
 
 // War
-function War() {}
+function War() {
+  this.vikingArmy = []
+  this.saxonArmy = []
+
+
+
+}
+
+
+
+War.prototype = Object.create(Soldier.prototype);
+War.prototype.constructor = War;
+
+War.prototype.addViking = function (arg){
+ 
+}
+War.prototype.addSaxon = function(arg){
+saxon.add(arg) 
+}
+War.prototype.vikingAttack = function (){}
+War.prototype.saxonAttack = function (){}
+War.prototype.showStatus = function (){}
+
