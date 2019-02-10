3c9798c87d749fdd8925d5b09d8967498389697b
Javier <javiermaker@gmail.com>
2019-02-11 00:23:55 +0100

    primer commit testing

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,6 +1,15 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health
+  this.strength = strength
 
+  Soldier.prototype.attack = function(){
+    return this.strength
+  }
+
+  Soldier.prototype.receiveDamage = function(damage){
+    this.health = health - damage
+ 
 // Viking
 function Viking() {}
 
