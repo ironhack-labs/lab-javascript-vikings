57f7a8abb1524f77be2bd1f91aea2c5d1d461039
baltasar torres garcia <baltasartorresgarcia@MacBook-Pro-de-amelia.local>
2019-02-11 09:13:58 +0100

    ultime commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-function Soldier() {}
+
+function Soldier(health, strength) {
+
+  this.health = health;
+  this.strength = strength;
+
+  this.attack = function () {
+    return this.strength
+  }
+  this.receiveDamage = function (damage) {
+
+    this.health = this.health - damage  
+
+  }
+
+}
 
 // Viking
-function Viking() {}
+
+function Viking(name, health, strength) {
+ 
+  Soldier.call(this,name,health,strength)
+  
+  this.name = name
+  this.health = health
+  this.strength = strength
+}
+Viking.prototype = Object.create(Soldier.prototype)
+Viking.prototype.constructor = Viking
+
+
+//this.battleCry = function () {
+
+ // return "Odin Owns You All!"
+//}
+
+
+
+
+
+
 
 // Saxon
-function Saxon() {}
+
+function Saxon(health, strength) {
+
+  this.health = health
+  this.strength = strength
+}
 
 // War
-function War() {}
+function War() { }
