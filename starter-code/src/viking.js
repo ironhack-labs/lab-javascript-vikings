4530c79732ac005cd44e33428dbc6186a88a9ec3
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-02-11 16:40:39 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,45 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    //Soldier.prototype = Object.create(Soldier.prototype)
+    //Soldier.prototype.constructor = Soldier
+    this.attack = function() {
+        return strength
+    }
+    this.receiveDamage = function(damage) {
+        this.health = this.health - damage
+    }
+}
 
+
+//function receiveDamage(damage) {
+//    delete Soldier.health
+//}
+/*Soldier.prototype.attack = function() {
+    return this.damage = 40
+}
+Soldier.prototype.attack(); {
+}*/
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Soldier.call(this, health, strength);
+    this.battleCry = function() {
+        return "Odin Owns You All!"
+    }
+    this.name = name;
+    if (this.receiveDamage > this.health) {
+        return this.name + " has died in act of combat"
+
+    } else {
+
+    }
+}
+//var viking = new Viking("viking", health, strength)
+Viking.prototype = Object.create(Soldier.prototype)
+Viking.prototype.constructor = Viking
 
 // Saxon
-function Saxon() {}
 
-// War
-function War() {}
+Saxon.prototype = Object.create(Soldier.prototype)
+Saxon.prototype.constructor = Saxon
