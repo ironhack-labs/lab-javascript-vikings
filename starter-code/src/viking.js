9165333ea67a81f365e2c80285b973c029a22a82
Dan Mihai Grigoriciuc <griganu06@gmail.com>
2019-02-11 09:07:48 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,31 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
+
+}
+Soldier.prototype.attack = function () {
+    return this.strength;
+}
+Soldier.prototype.receiveDamage = function (damage) {
+    this.health = this.health - damage;
+
+}
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    Viking.call(this, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
 
 // Saxon
-function Saxon() {}
+function Saxon(healt, strength) {}
 
 // War
-function War() {}
+function War(healt, strength) {}
