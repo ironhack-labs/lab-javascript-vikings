1ae319237f6be303460d0b76dc4bea7588ceb985
Javier <javiermaker@gmail.com>
2019-02-11 01:36:18 +0100

    depurando test

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,25 +1,31 @@
 // Soldier
 function Soldier(health, strength) {
-  this.health = health
-  this.strength = strength
+  this.health = health;
+  this.strength = strength;
 
   Soldier.prototype.attack = function(){
-    return this.strength
+    return this.strength;
   }
 
   Soldier.prototype.receiveDamage = function(damage){
-    this.health -= damage
+    this.health -= damage;
  
+
 // Viking
 function Viking(name, health, strength) {
    
-  Soldier.call(this, health, strength)
+  Soldier.call(this, health, strength);
   this.name = name 
 
 }
 Viking.prototype = Object.create(Soldier.prototype);
 	
 Viking.prototype.constructor = Viking;
+
+Viking.prototype.attack = function (){
+  return this.strength;
+};
+
 Viking.prototype.receiveDamage = function(damage){
   this.health -= damage;
   if (this.health > 0){
@@ -27,13 +33,15 @@ Viking.prototype.receiveDamage = function(damage){
     return this.name + text;
   }
   else {
-    return this.name + " has died in act of comba";
+    return this.name + " has died in act of combat";
   }
 }
 
 Viking.prototype.battleCry = function(){
   return "Odin Owns You All!";
-}
+};
+
+
 // Saxon
 function Saxon(health, strength) {
   Soldier.call(this, health, strength);
@@ -47,7 +55,7 @@ Saxon.prototype.receiveDamage = function(damage) {
   if (this.health > 0) {
     return `A Saxon has received ${damage} points of damage`;
   } else {
-    return 'A Saxon has died in combat';
+    return 'A Saxon has died in combate';
   }
 };
 
