33787eee404ab697eeac509dd7ad39865dd2afef
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-02-16 14:28:27 +0100

    soldier

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,15 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function attack() {
+  return this.strength;
+  }
+  this.receiveDamage = function receiveDamage(damage) {
+  this.health = health - damage;
+  }
+}
+
 
 // Viking
 function Viking() {}
