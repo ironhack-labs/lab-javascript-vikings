c8b28ba2d96bd5c784876c207055e6ad15538255
Natalia Franquet <n.franquet.lm@gmail.com>
2019-03-24 19:34:13 +0100

    unless the bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,6 +1,5 @@
 // Soldier
 class Soldier {
-  
   constructor (health, strength) {
     this.health = health;
     this.strength = strength;
@@ -21,13 +20,11 @@ class Viking extends Soldier {
     super (health, strength);
     this.name = name;
   }
+
   receiveDamage(damage) {
     this.health -=  damage;
-    if (this.health > 0) {
-      return `${this.name} has received ${damage} points of damage`;
-    } else {
-      return `${this.name} has died in act of combat`;
-    }
+    if (this.health > 0) return `${this.name} has received ${damage} points of damage`;
+      return `${this.name} has died in act of combat`; 
   }
 
   battleCry() {
@@ -36,7 +33,45 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor (health, strength) {
+    super (health, strength);
+  }
+
+  receiveDamage(damage) {
+    this.health -=  damage;
+    if (this.health > 0) return `A Saxon has received ${damage} points of damage`;
+      return `A Saxon has died in combat`;
+    }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    console.log('estoy dentro viking attack');
+    var saxon = new Saxon(40,40);
+    var viking = new Viking('',20,40);
+
+    var oldHealth = saxon[0].health;
+    saxon.health = oldHealth - viking.strength;
+    
+    
+  }
+  saxonAttack() {
+
+  }
+  showStatus() {
+
+  }
+}
