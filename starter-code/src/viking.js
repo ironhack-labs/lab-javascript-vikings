ed2b0f3caaecfa5267f0d04a6823836857a91bf5
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-25 17:04:23 +0100

    soldier done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,16 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damageArg) {
+    this.health -= damageArg;
+  }
+}
 
 // Viking
 class Viking {}
