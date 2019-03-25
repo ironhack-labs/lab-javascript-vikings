7c2092c52721f55435b7cd358407af40e5701de3
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-03-25 17:17:52 +0100

    viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -13,7 +13,26 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damageArg) {
+    this.health -= damageArg;
+    if (this.health > 0) {
+      return `${this.name} has received ${damageArg} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    };
+  };
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+};
 
 // Saxon
 class Saxon {}
