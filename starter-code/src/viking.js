580947b2718445606eec5856f26f857657d587e5
Mathis Gacon <gacon.mathis.lycee@gmail.com>
2019-03-27 10:09:05 +0100

    not done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,7 +1,23 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthArg, strengthArg){
+        this.health = healthArg;
+        this.strength = strengthArg;
+       
+    } 
+     attack(){
+    return this.strength;}
 
-// Viking
+    receiveDamage(damage){
+        
+        this.health = this.health - damage
+    
+    }
+ }
+    
+
+
+// Vikings
 class Viking {}
 
 // Saxon
