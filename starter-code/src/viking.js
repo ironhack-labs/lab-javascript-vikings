efde84c88b15123746d18d6c64f12c40e4c70d14
Marypztr <nutrimary.77@gmail.com>
2019-03-29 13:36:57 -0600

    PP MEX MARYJOSE Y GUSTAVO

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-class Soldier {}
+function Soldier (health, strength) {
+this.health = health
+this.strength = strength
+this.attack =()=> {
+  return this.strength
+}
+this.receiveDamage =(damage)=> {
+  this.health -= damage
+}
+}
 
 // Viking
-class Viking {}
+function Viking(name, health, strength) {
+Soldier.call(this,health, strength)
+this.name = name
+this.battleCry = ()=> {
+return "Odin Owns You All!"
+}
+this.receiveDamage = (damage) => {
+  this.health -= damage
+if (this.health <= 0 ){
+  return (`${this.name} has died in act of combat`)
+} else {
+  return (`${this.name} has received ${damage} points of damage`)
+  
+}
 
+}
+}
+Viking.prototype = Object.create ( Soldier.prototype)
 // Saxon
-class Saxon {}
-
+function Saxon (health, strength){
+  Soldier.call(this,health, strength)
+  this.receiveDamage = (damage) => {
+    this.health -= damage
+  if (this.health > 0 ){
+    return (`A Saxon has received ${damage} points of damage`)
+  } else {
+    return (`A Saxon has died in combat`)
+    
+  }
+}
+}
+Saxon.prototype = Object.create ( Soldier.prototype)
 // War
-class War {}
+// function War() {
+//   this.addViking() 
+// }
