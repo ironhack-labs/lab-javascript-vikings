71f3c1803a08837bbe5ee883be562be85569f098
davidh <iq.davidh@gmail.com>
2019-05-07 19:14:13 -0500

    avance vikingo

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,42 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        if (damage === null || undefined) {
+            return;
+        }
+
+        this.health -= parseFloat(damage);
+
+        //TODO esto parece que es death
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+
+    constructor(name, health, strength){
+
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon {
+}
 
 // War
-class War {}
+class War {
+}
