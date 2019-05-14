d6162278344d457718cc1e2227a7b738ec9fb093
Jarsory <arsory92@gmail.com>
2019-05-14 16:21:43 -0500

    .-.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-class Soldier {}
-
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
+class Soldier {
+    constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;
+    }
+    attack() {
+      return this.strength;
+    }
+    receiveDamage(thedamage) {
+      this.health -= thedamage;
+    }
+  }
+  
+  // Viking
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super(health, strength);
+      this.name = name;
+    }
+    receiveDamage(thedamage) {
+      //this.health -= thedamage;
+      super.receiveDamage(thedamage);
+      if (this.health < 1) {
+        return `${this.name} has died in act of combat`;
+      }
+      return `${this.name} has received ${thedamage} points of damage`;
+    }
+    battleCry() {
+      return "Odin Owns You All!";
+    }
+  }
+  
+  // Saxon
+  class Saxon extends Soldier {
+    constructor(health, strength) {
+      super(health, strength);
+    }
+    receiveDamage(thedamage) {
+      //this.health -= thedamage;
+      super.receiveDamage(thedamage);
+      if (this.health < 1) {
+        return `A Saxon has died in combat`;
+      }
+      return `A Saxon has received ${thedamage} points of damage`;
+    }
+  }
 // War
 class War {}
