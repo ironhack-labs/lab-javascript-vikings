0a7279e419fbfd53fede9662f9b3a1554dd7dece
Ceballos <franperceb@gmail.com>
2019-06-07 23:51:55 -0500

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,81 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (healthArg ,strengthArg){
+      this.health = healthArg 
+      this.strength = strengthArg
+    }
+ attack = () => {
+    return this.strength
+}
+ receiveDamage = (_thedamage) =>{
+    this.health -= _thedamage
+ }
+}
+
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super (health, strength)
+    this.name = name
+        }   
+        receiveDamage = (_thedamage) =>{
+          this.health -= _thedamage
+            if(this.health <= 0)  return `${this.name} has died in act of combat`
+              return `${this.name} has received ${_thedamage} points of damage`
+        }
+        battleCry = () => {
+          return `Odin Owns You All!`   
+        } 
+      }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health, strength) {
+      super (health, strength)
+    }
+    receiveDamage = (_thedamage) =>{
+      this.health -= _thedamage
+        if(this.health <= 0)  return `A Saxon has died in combat`
+          return `A Saxon has received ${_thedamage} points of damage`
+    }
+    
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+      this.vikingArmy = []
+      this.saxonArmy = []
+    }
+
+addViking = (viking1) => {
+this.vikingArmy.push(viking1)
+}
+addSaxon = (saxon1) =>{
+this.saxonArmy.push(saxon1)
+}
+
+vikingAttack = () => {
+ //se crea random
+rnd = Math.floor(Math.random()*this.saxonArmy.length)
+rnd2 = Math.floor(Math.random()*this.vikingArmy.length)
+
+this.saxonArmy[rnd].receiveDamage(this.vikingArmy[rnd2].strength)
+
+if(this.saxonArmy[rnd].health <=0){
+
+
+}
+
+}
+
+//se le agrega a receive damage
+
+
+
+
+}
+}
+
