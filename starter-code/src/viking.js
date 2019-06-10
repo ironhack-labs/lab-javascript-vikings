6ee29e83b098436841b954d52742bd5880257a6d
MicaelZV <micaelzv@gmail.com>
2019-06-10 09:28:46 +0200

    no fue muy mal el tema, los tests de Jasmine pasan, pero tengo mis dudas acerca la correcion de los argumentos intercambiados y la posible optimizacion del codigo.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthArg, strengthArg) {
+    (this.health = healthArg), (this.strength = strengthArg);
+  }
+
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damageArg) {
+    this.health -= damageArg;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, healthArg, strengthArg) {
+    super(healthArg, strengthArg);
+    this.name = `Harald`;
+  }
+  receiveDamage(damageArg) {
+    this.health -= damageArg;
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    } else {
+      return `${this.name} has received ${damageArg} points of damage`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(healthArg, strengthArg) {
+    super(healthArg, strengthArg);
+    this.strength=strengthArg;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damageArg) {
+    this.health -= damageArg;
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    } else {
+      return `A Saxon has received ${damageArg} points of damage`;
+    }
+  }
+}
 
 // War
 class War {}
