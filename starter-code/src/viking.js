61072f8dedcfc1941a17e75f80dc2cb854efc85b
Ashley Arrue <ashleydarrue@gmail.com>
2019-06-15 12:30:17 -0400

    lab 100% complete

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,14 +1,14 @@
 // Soldier
 class Soldier {
   constructor(health,strength){
-    this.health=(health)
-    this.strength=(strength)
+    this.health=(health);
+    this.strength=(strength);
   }
   attack(){
     return this.strength;
   }
   receiveDamage(thedamage){
-    this.health= this.health - thedamage;
+    this.health = this.health - thedamage;
     // return this.health;
   }
 
@@ -68,10 +68,40 @@ class War {
      let randomSaxon = this.saxonArmy[Math.floor(Math.random()* this.saxonArmy.length)];
      let randomViking = this.vikingArmy[Math.floor(Math.random()* this.vikingArmy.length)];
     // let oneOnOne = randomSaxon.receiveDamage(randomViking.strength);
-    let result= randomSaxon.receiveDamage(randomViking.attack);
-    if (randomSaxon.health <= 0){
-      this.saxonArmy.splice(randomSaxon); 
+   
+    let healthGuy = randomSaxon.health - randomViking.strength;
+
+    if (healthGuy <= 0){
+      this.saxonArmy.splice([Math.floor(Math.random()* this.vikingArmy.length)],1);
     }
+    let result= randomSaxon.receiveDamage(randomViking.attack());
     return result;
   }
+  
+  saxonAttack(){
+    let randomSaxon = this.saxonArmy[Math.floor(Math.random()* this.saxonArmy.length)];
+    let randomViking = this.vikingArmy[Math.floor(Math.random()* this.vikingArmy.length)];
+    // let oneOnOne = randomSaxon.receiveDamage(randomViking.strength);
+    let healthGuy = randomViking.health - randomSaxon.strength;
+    if (healthGuy <= 0){
+      this.vikingArmy.splice([Math.floor(Math.random()* this.saxonArmy.length)],1);
+    }
+    let result= randomViking.receiveDamage(randomSaxon.attack());
+    return result;
+  }
+  showStatus() {
+    if (this.saxonArmy.length === 0){
+      return `Vikings have won the war of the century!`;
+    }
+    if (this.vikingArmy.length === 0 ){
+      return `Saxons have fought for their lives and survive another day...`;
+    }
+    if ( this.vikingArmy.length === 1 && this.saxonArmy.length === 1){
+      return `Vikings and Saxons are still in the thick of battle.`;
+    
+    }
+
+    }
+  
+
 }
