5413d077e94e97be205456698f0722e01e5cb2af
Dusty Garon <dustygaron@hotmail.com>
2019-06-22 14:10:54 -0400

    MIA PTWD - Dusty Garon

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  constructor(health, strength) {
+    super(health, strength);
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+
+  }
+
+}
 
 // War
-class War {}
+// class War { }
+
+
+
+let myViking = new Viking('Bob', '10', '1')
+
+console.log(myViking.receiveDamage(2))
+
+// let mySaxon = new Saxon('Henry', '10', '1')
+
+// console.log(mySaxon.receiveDamage(2))
