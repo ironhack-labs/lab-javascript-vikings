e9a032f7926ea1c0035e3078bf4d04e38f8c3f0d
Carlos Kruger <carloskr@yahoo.com>
2019-07-06 14:17:01 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = 300
+        this.strength = 150
+    }
+    //attack() method
+    attack(){
+        return this.strength
+    }
+    // receiveDamage() method
+    receiveDamage(damage){
+        this.health = this.health - damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = "Harald"
+        this.health = 300
+        this.strength = 150
+    }
+    //attack() method
+    attack(){
+        return this.strength
+    }
+    // receiveDamage() method
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if (this.health > 0)
+          return `${this.name} has received ${damage} points of damage`
+        else
+          return `${this.name} has died in act of combat`
+    }
+    // battleCry() method
+    battleCry(){
+        return ("Odin Owns You All!")
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+        super(health, strength)
+        this.health = 60
+        this.strength = 25
+    } 
+     //attack() method
+     attack(){
+        return this.strength
+     }
+    // receiveDamage() method
+    receiveDamage(damage){
+        this.health = this.health  -  damage
+        if (this.health > 0)
+          return (`A Saxon has received ${damage} points of damage`)
+        else
+          return (`A Saxon has died in combat`)
+    
+    }
+}
 
 // War
-class War {}
+// class War {
+//     constructor(){
+//       this.vikingArmy = []
+//       this.saxonArmy = []
+//     }
+
+//     addViking(viking){
+//         this.vikingArmy.push(viking)
+//     }
+
+//     addSaxon(saxon)
+//     vikingAttack()
+//     saxonAttack()
+//     showStatus()
+// }
+
+
