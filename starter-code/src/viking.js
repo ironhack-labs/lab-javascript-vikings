282d0cf34cd17773939e6654071d2f4d6167680d
fernandorodrigueztig <fernandorodrigueztig@gmail.com>
2019-07-08 07:36:21 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,57 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+        constructor (healthArg , strengthArg) {
+            this._healthArg = healthArg
+            this._strenghtArg = healthArg
+        }
+}
+{
+    attack(this._strength= 'strengthArg') {
+        console.log('El soldado ha atacado con fuerza', strengthArg.length)
+}
+{
+    receiveDamage(damage){
+        this._healthArg - damage
+}
 
 // Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
+class Viking extends Soldier{        
+        constructor(name, healthArg , strengthArg, damage, alive) {
+            super(healthArg , strengthArg)                
+            this._damage = healthArg - damage
+            this._alive = (healthArg > 0)
+            this._name = name
+    }
+    attack(this._strength= 'strengthArg') {
+        console.log('El vikingo', this._name, 'ha atacado con fuerza', strengthArg.length)}
+    
+    function receiveDamage(alive){if (this._alive){
+        console.log(this._name, 'has received', this._damage, 'points of damage'}
+        else {console.log(this._name, 'has died in act of combat')}
+    
+newhealthArg(this._healthArg) {
+            this._healthArg - this._damage
+} 
+    function battleCry(){
+        console.log('Odin Owns You All!')
+    }
+        
+    // Saxon
+class Saxon extends Soldier{        
+    constructor(healthArg , strengthArg, damage, alive) {
+        super(healthArg , strengthArg, damage, alive)                
+}
+    attack(this._strength= 'strengthArg') {
+    console.log('El sajón ha atacado con fuerza', strengthArg.length)}
+    
+    function receiveDamage(alive){if (this._alive){
+        console.log('Saxon has received', this._damage, 'points of damage'}
+        else { 
+            console.log('Saxon has died in act of combat')}
+            }
+newhealthArg2(this._healthArg) {
+    this._healthArg - this._damage
+}        
 
 // War
 class War {}
