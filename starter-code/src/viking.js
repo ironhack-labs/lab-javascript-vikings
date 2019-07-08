431f1052ca82a7928f7741a85b74ba279f51b434
ckompier <kompi14@hotmail.com>
2019-07-08 09:34:17 +0200

    done2

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -87,8 +87,47 @@ class War {
 
 
     vikingAttack(){
+
+        
+           let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+           let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+
+           let fight = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+           if (this.saxonArmy[randomSaxon].health <= 0) {
+               this.saxonArmy.splice(randomSaxon,1)
+           }
+           return fight
         
     }
+
+
+    saxonAttack(){
+
+        
+        let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+
+        let fight2 = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
+        if (this.vikingArmy[randomViking].health <= 0) {
+            this.vikingArmy.splice(randomViking,1)
+        }
+        return fight2
+     
+ }
+
+ showStatus(){
+
+
+if (this.vikingArmy.length <= 0) {  
+    return "Saxons have fought for their lives and survive another day..."}
+ else if (this.saxonsArmy.length <=0){   
+        return "Vikings have won the war of the century!"
+    } else { 
+        return "Vikings and Saxons are still in the thick of battle."
+    }
+ }
+
+
 }
 
 
