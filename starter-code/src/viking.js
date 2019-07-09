3e0622f2c12f15f6a0e6d925e7f01f5d03d0432d
Evelyn Graetz <evelyn.graetz@gmx.de>
2019-07-09 21:37:03 +0200

    first try

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,27 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+
+
+
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+    }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking { }
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
