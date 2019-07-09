ff138dd72eb87054eb1f69d4f3be758b898b091a
Lena Lau <frau.lena.lau@gmail.com>
2019-07-09 21:42:20 +0200

    hmpf

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,9 +1,31 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+this.health = this.health - damage
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
 
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name
+    }
+        
+      
+}
+let viking1 = new Viking("Harald", 300, 150);
+   
 // Saxon
 class Saxon {}
 
