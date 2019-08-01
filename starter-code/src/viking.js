7b948a033c06cf5acc46c41893c9d1b97d9d8745
Patricia Prieto Caballero <reypatricia@android-16690d0e3deeac00.gateway.2wire.net>
2019-07-31 23:07:30 -0500

    Mex-pt Iván & Rey

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,72 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;        
+  }
+  attack(){
+    console.log(this.strength)
+    return this.strength;
+  }
+
+  receiveDamage(Damage){
+    this.health -= Damage; 
+  }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name,health,strength){
+    super(health,strength);
+    this.name = name;
+  }
+  receiveDamage(Damage){
+    this.health -= Damage; 
+    if(this.health > 0) return `${this.name} has received ${Damage} points of damage`
+    return `${this.name} has died in act of combat`;
+  }
+  battleCry(){
+    return `Odin Owns You All!`;
+  }
+  //if(this.health > 0) return `${name} has received ${damage} points of damage`
+  //return `${name} has died in act of combat`;
+  
+}
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(health,strength){
+    super(health,strength);
+  }
+  receiveDamage(Damage){
+    this.health -= Damage; 
+    if(this.health > 0) return `A Saxon has received ${Damage} points of damage`
+    return `A Saxon has died in combat`;
+  }
+
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    
+  }
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {}
+  saxonAttack() {}
+  showStatus() {}
+
+}
+
+war.addSaxon
