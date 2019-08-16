3a2c1bac4c50339dcfb1dc4bc9df3eb5903a3c36
Kary Mora <KaryMo@Karys-MacBook-Pro-2.local>
2019-08-16 18:13:46 -0500

    working on war

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,114 @@
 // Soldier
-class Soldier {}
+function Soldier(health,strength){
+  this.health=health
+  this.strength=strength
+  this.attack=function(){
+      return this.strength
+  }
+
+  this.receiveDamage=function(damage){
+    this.damage=damage
+    this.health=this.health-this.damage
+  }
+}
 
 // Viking
-class Viking {}
+function Viking (name,health,strength){
+  this.name=name
+  this.health=health
+  this.strength=strength
+  Soldier.call(this,health,strength)
+
+this.receiveDamage = function(damage){
+  this.health=this.health-damage
+  if(this.health>0){
+    return `${this.name} has received ${damage} points of damage`
+  }
+  else{
+      return `${this.name} has died in act of combat`
+  }
+}
+
+ this.battleCry=function(){
+   return "Odin Owns You All!"
+ }
+}
+
+
+Viking.prototype = Object.create(Soldier.prototype)
+
+
+
 
 // Saxon
-class Saxon {}
+function Saxon (health,strength){
+ this.health=health
+ this.strength=strength
+
+this.attack=function(){
+      return this.strength
+  }
+ this.receiveDamage=function(damage){
+
+ this.health=this.health-damage
+  if(this.health>0){
+    return `A Saxon has received ${damage} points of damage`
+  }
+  else{
+      return `A Saxon has died in combat`
+  }
+   
+ }
+}
+Saxon.prototype = Object.create(Soldier.prototype)
+
+Saxon.prototype.constructor = Saxon
+
 
 // War
-class War {}
+function War (){
+this.vikingArmy=[]
+this.saxonArmy=[]
+this.addViking=function(viking){
+this.vikingArmy.push(viking);
+}
+
+this.addSaxon=function(saxon){
+this.saxonArmy.push(saxon);
+}
+
+
+}
+function War(){
+  this.vikingArmy=[]
+  this.saxonArmy=[]
+  this.addViking = function(viking){
+   this.vikingArmy.push(viking)
+  }
+
+  this.addSaxon=function(saxon){
+    this.saxonArmy.push(saxon)
+  }
+
+  this.vikingAttack=function(){
+    const randomSaxon = Math.floor(Math.random()*this.saxonArmy)
+    const randomViking = Math.floor(Math.random()*this.vikingArmy)
+    if(this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength).includes('dead')){
+    this.saxonArmy[randomSaxon].splice(randomSaxon,1)
+    }
+    else{
+    return this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+    }
+  
+  }
+
+   this.saxonAttack=function(){
+    
+  }
+
+
+  this.showStatus=function(){
+    
+  }
+
+}
