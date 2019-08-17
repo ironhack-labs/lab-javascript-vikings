516506bfa132e2aaec053ae1ee4afa0bd2a67c49
Pauline Val <pauline@Paulines-MacBook-Pro.local>
2019-08-17 12:05:12 -0500

    Falta el ataque

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,77 @@
 // Soldier
-class Soldier {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function() {
+    return this.strength;
+  };
+  this.receiveDamage = function(damage) {
+    this.health -= damage;
+  };
+}
 
 // Viking
-class Viking {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+  this.receiveDamage = function(damage) {
+    this.health -= damage
+
+    if(this.health !== 0) {
+      return `${this.name} has received ${damage} points of damage`
+    }else{
+      return `${this.name} has died in act of combat`
+          }  
+    } 
+    this.battleCry = function(){
+    return `Odin Owns You All!`
+    }
+  }
+  
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
-class Saxon {}
+function Saxon(health, strength){
+  Soldier.call(this, health, strength)
+  this.receiveDamage = function(damage) {
+    this.health -= damage
+    if(this.health !== 0) {
+      return `A Saxon has received ${damage} points of damage`
+    }else{
+      return `A Saxon has died in combat`
+          }  
+    }  
+  }
+
+
+Saxon.prototype = Object.create(Soldier.prototype)
+Saxon.prototype.constructor = Saxon
 
 // War
-class War {}
+
+function War(){
+
+  this.vikingArmy = [],
+  this.saxonArmy = []
+
+  this.addViking = function(viking){ 
+    this.vikingArmy.push(viking)
+  }
+
+  this.addSaxon = function(saxon){
+    this.saxonArmy.push(saxon)
+  }
+  
+  this.vikingAttack = function(){
+  //Saxon.receiveDamage(this.damage) = Viking.strength
+    Saxon.prototype.attack = function(saxon){
+      saxon.receiveDamage(this.damage) 
+    } = Viking.strength
+  }
+  const vikingAttack = new Saxon()
+  vikingAttack.receiveDamage()
+}
+// Pokemon.prototype.atacar = function(pokemon){
+//   pokemon.receiveDamage(this.attack)
+// }
