b6d0d516fe84fa1e3b876974209931e62dc7b702
Paula Brinkhege <paula.brinkhege@gmail.com>
2019-08-19 16:10:10 +0100

    first draft from Daniel Santos and Paula

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,99 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthArg, strengthArg) {
+        this.health = healthArg;
+        this.strength = strengthArg;
+    }
+
+    attack () { 
+        return this.strength
+    }
+
+    receiveDamage (damage) {
+        this.health = this.health - damage
+    
+    }
+    
+}
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+    constructor (name, healthArg, strengthArg) {
+       super(healthArg, strengthArg);
+    this.name = name;
+}
+
+ receiveDamage(damage) {
+     this.health = this.health - damage
+        if (this.health === 0) {
+            return `${this.name} has died in act of combat`
+        }
+
+        else {
+            return `${this.name} has received ${damage} points of damage`
+        }
+ }
+
+ battleCry(){
+    return "Odin Owns You All!"
+ }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (healthArg, strengthArg) {
+        super(healthArg, strengthArg);
+} 
+
+attack () { 
+    return this.strength
+}
+
+receiveDamage (damage) {
+    this.health = this.health - damage
+        if (this.health === 0) {
+            return "A Saxon has died in combat"
+        }
+
+        else {
+            return `A Saxon has received ${damage} points of damage`
+        }
+
+}
+}
 
 // War
-class War {}
+
+function randomNumber (minimum, maximum) {
+    return Math.floor(Math.random() * (maximum - minimum + 1) + minimum);}
+
+class War {
+    constructor () {
+            this.vikingArmy = [];
+            this.saxonArmy = []
+    }
+    addViking(vikingObject) { 
+        this.vikingArmy.push(vikingObject)
+     }
+        
+    addSaxon(saxonObject) {
+        this.saxonArmy.push(saxonObject)
+    }
+
+    // not finished
+    
+    _genericAttack(attacker, victom) {
+        const vikingCount = this.vickingArmy.length;
+        const saxonCount = this.saxonArmy.length;
+        const vikingIndex = randomNumber(0, vikingCount -1);
+        const saxonIndex = randomNumber(0, saxonCount -1);
+
+        const viking = this.vikingArmy[vikingIndex];
+        const saxon = this.saxonArmy[saxonIndex];
+
+
+        
+    }
+}
