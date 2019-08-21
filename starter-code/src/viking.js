b72a812d001e9f3c063db62ae958818986bc8b00
KaterynaStadnyk <kateryna.stad@gmail.com>
2019-08-21 10:11:28 +0200

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -94,5 +94,12 @@ class War {
         return attackresult;
 
     }
-
+    showStatus (){
+        if (this.saxonArmy.length === 0) {
+        return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length === 0) {
+        return "Saxons have fought for their lives and survive another day...";
+        }
+        else return "Vikings and Saxons are still in the thick of battle.";
+}
 }
