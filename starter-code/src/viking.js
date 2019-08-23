3ccc0afa1a3dd2ec7773559296dbf3927a57e1b4
“Natalia <natgarea@gmail.com>
2019-08-23 18:30:06 +0200

    first commit, working on War class

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,74 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+    constructor(healthArg, strengthArg) {
+        this.health = healthArg,
+        this.strength = strengthArg
+    }
+
+    attack() {
+        return this.strength
+    }
+    
+    receiveDamage(damageArg) {
+        this.health -= damageArg
+    }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(nameArg, healthArg, strengthArg) {
+        super(healthArg, strengthArg);
+        this.name = nameArg
+    }
+
+    receiveDamage(damageArg) {
+        super.receiveDamage(damageArg)
+        if (this.health <= 0) { return `${this.name} has died in act of combat`}
+        return `${this.name} has received ${damageArg} points of damage`
+    }
+
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    constructor(healthArg, strengthArg) {
+        super(healthArg, strengthArg)
+    }
+
+    receiveDamage(damageArg) {
+        super.receiveDamage(damageArg)
+        if (this.health <= 0) { return `A Saxon has died in combat`}
+        return `A Saxon has received ${damageArg} points of damage`
+    }   
+}
 
 // War
-class War {}
+class War {
+
+    constructor() {
+        this.vikingArmy = [],
+        this.saxonArmy = []
+    }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack() {
+        let i = Math.floor(Math.random() * this.saxonArmy.length);
+        let j = Math.floor(Math.random() * this.vikingArmy.length);
+        saxonArmy[i].receiveDamage(vikingArmy[j].strength);
+    }
+}
