43a60875b4869c75333664d7d36eaff7ba4f0cb2
rashier <rashier13@gmail.com>
2019-08-25 00:56:36 +0200

    The exercise was very interesting and it makes me a little complicated to do the Array Filtering

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    }
+    return `${this.name} has received ${damage} points of damage`;
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+    return `A Saxon has received ${damage} points of damage`;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(newViking) {
+    this.vikingArmy.push(newViking);
+  }
+  addSaxon(newSaxon) {
+    this.saxonArmy.push(newSaxon);
+  }
+  vikingAttack(){
+    let resultVikingAttack = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].receiveDamage(this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)].attack());
+    this.saxonArmy = this.saxonArmy.filter( saxonSoldier => saxonSoldier.health > 0  )
+    return resultVikingAttack;
+  }
+  saxonAttack(){
+    let resultSaxonAttack = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].receiveDamage(this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)].attack());
+    this.vikingArmy = this.vikingArmy.filter( vikingSoldier => vikingSoldier.health > 0  );
+    return resultSaxonAttack
+  }
+  showStatus(){
+    if (this.saxonArmy.length === 0){
+      return "Vikings have won the war of the century!";
+    }
+    else if (this.vikingArmy.length === 0){
+      return "Saxons have fought for their lives and survive another day...";
+    }
+      return "Vikings and Saxons are still in the thick of battle."
+  }
+}
