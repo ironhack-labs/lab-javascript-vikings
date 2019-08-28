2b8d2d193c78354297f4fc38f96ed6491313882f
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-08-28 12:04:33 -0400

    Finished solider and viking class

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,42 @@
-// Soldier
-class Soldier {}
+// Solider 
+class Soldier {
+  constructor(healthArg, strengthArg){
+    this.health = healthArg;
+    this.strength = strengthArg;
+  }
+
+  attack(){
+    return this.strength;
+  }
+
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(nameArg, healthArg, strengthArg){
+    super(healthArg, strengthArg);
+    this.name = nameArg;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    const message = this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`;
+    return message;
+  }
+
+  battleCry(){
+    return "Odin Owns You All!";
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon {
+  
+}
 
 // War
 class War {}
