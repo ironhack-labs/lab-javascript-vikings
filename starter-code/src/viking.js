7561390d44144cff7128d21abc4b64c61fe5939f
Qiwei Lin <Qiwei.l.ql@gmail.com>
2019-08-28 13:01:49 -0400

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -30,13 +30,78 @@ class Viking extends Soldier{
   battleCry(){
     return "Odin Owns You All!";
   }
-
 }
 
 // Saxon
-class Saxon {
-  
+class Saxon extends Soldier{
+  constructor(healthArg, strengthArg) {
+    super(healthArg, strengthArg);
+  }
+
+  attack(){
+    return super.attack();
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    const message = this.health > 0 ? `A Saxon has received ${damage} points of damage` : `A Saxon has died in combat`;
+    return message;
+  }
 }
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(viking){
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack(){
+    const randomIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomSaxon = this.saxonArmy[randomIndex];
+    const randomViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+
+    const attackResult = randomSaxon.receiveDamage(randomViking.attack());
+
+    if(randomSaxon.health <= 0){
+      this.saxonArmy.splice(randomIndex, 1);
+    }
+    
+    return attackResult;
+  }
+
+  saxonAttack(){
+    const randomIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    const randomViking = this.vikingArmy[randomIndex];
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+    const attackResult = randomViking.receiveDamage(randomSaxon.attack());
+
+    if(randomViking.health <= 0){
+      this.vikingArmy.splice(randomIndex, 1);
+    }
+    
+    return attackResult;
+  }
+
+  showStatus(){
+    const vikingRemaining = this.vikingArmy.length;
+    const saxonRemaining = this.saxonArmy.length;
+    let message = "";
+    if(Math.min(vikingRemaining, saxonRemaining) != 0){
+      message = "Vikings and Saxons are still in the thick of battle."
+    }else{
+      message = vikingRemaining == 0 ? "Saxons have fought for their lives and survive another day..." : "Vikings have won the war of the century!";
+    }
+
+    return message;
+  }
+}
