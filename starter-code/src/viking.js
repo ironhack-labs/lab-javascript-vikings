bc9f81c069dccf3090096e02716fde5b0dbf0b2c
Bobby Wang <bobby.p.wang@gmail.com>
2019-08-28 12:11:05 -0400

    Completed the soldier tests.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthArg, strengthArg) {
+        this.health = healthArg,
+        this.strength = strengthArg,
+        this.attack = function () {
+            return this.strength
+        }
+        this.receiveDamage = function (theDamage) {
+            this.health -= theDamage
+        }
+    }
+}
+
+
 
 // Viking
 class Viking {}
