80565ebb5b7b2b9f9298f193d749ae527508e430
Bobby Wang <bobby.p.wang@gmail.com>
2019-08-29 12:00:29 -0400

    Completed viking iterations.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,30 +1,82 @@
 // Soldier
 class Soldier {
-    constructor(healthArg, strengthArg) {
-        this.health = healthArg,
-        this.strength = strengthArg,
-        this.attack = function () {
-            return this.strength
-        }
-        this.receiveDamage = function (theDamage) {
-            this.health -= theDamage
-        }
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
     }
 }
 
 
 
 // Viking
-class Viking {
-    constructor(name, heath, strength) {
-        this.name = name,
-        this.health = health,
-        this.strength = strength
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health < 1) {
+            return `${this.name} has died in act of combat`
+        } else {
+            return `${this.name} has received ${damage} points of damage`
+        }
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!'
     }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health, strength) {
+        super(health, strength)
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+        console.log (this.health)
+        if (this.health < 1) {
+            return "A Saxon has died in combat"
+        } else {
+            return `A Saxon has received ${damage} points of damage`
+        }
+    }
+}
+
 
 // War
-class War {}
+class War {
+    vikingArmy = []
+    saxonArmy = []
+
+    randomFighter(arr) {
+        return Math.floor(Math.random() * arr.length)
+    }
+
+    addViking(argg) {
+        this.vikingArmy.push(argg)
+    }
+
+    addSaxon(argg) {
+        this.saxonArmy.push(argg)
+    }
+
+    vikingAttack() {
+        
+    }
+
+    saxonAttack() {}
+
+    showStatus() {}
+}
