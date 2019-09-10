bb2656f9f6966baf73dd89c6016b12ba2f4cbb20
unknown <rober.araya.gil@gmail.com>
2019-09-10 21:10:37 +0200

    done with super methods

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -22,7 +22,7 @@ class Viking extends Soldier {
     }
 
     receiveDamage(damage){
-        this.health -= damage
+        super.receiveDamage(damage)
         if (this.health > 0){
             return `${this.name} has received ${damage} points of damage`
         }else{
@@ -42,7 +42,7 @@ class Saxon extends Soldier {
     }
 
     receiveDamage(damage){
-        this.health -= damage
+        super.receiveDamage(damage)
         if (this.health > 0){
             return `A Saxon has received ${damage} points of damage`
         }else{
