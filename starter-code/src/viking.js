b3ef2daca32e8faf29f44de4bd2d4400ae51dd2e
Clara Rodriguez <clara92r@gmail.com>
2019-09-10 22:22:44 +0200

    first commit

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,77 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthArg, strengthArg) {
+        this.health =300
+        this.strength=150
+        
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health -=damage
+        
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+    super(health,strength);
+    this.name=name
 
+  }
+  attack(){
+    return this.strength
+}
+receiveDamage(damage){
+    this.health -=damage
+    if(this.health===0){
+        return `${this.name} has died in act of combat`
+    }
+    else if(this.health!==0){
+        return `${this.name} has received ${damage} points of damage`
+    }
+    
+}
+battleCry(){
+    return `Odin Owns You All!`
+}
+}
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor( health, strength){
+      super(health,strength)
+          this.health=health
+          this.strength=strength
+      }
+      attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health -=damage
+        if(this.health===0){
+            return `A Saxon has died in combat`
+        }
+        else if(this.health!==0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+    }
+}
+
 
 // War
-class War {}
+class War {
+    constructor()
+    {
+        this.vikingArmy=[]    
+        this.saxonArmy=[]  
+    }
+
+    addViking(Viking){
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+}
