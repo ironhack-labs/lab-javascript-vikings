9c57d2b56403ee216a7b5e911b069a46d3fd3aa0
nasciba <amandanasciba@gmail.com>
2019-09-12 16:58:52 -0300

    2nd commit - few bonus tasks missing

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -51,5 +51,39 @@ class Saxon extends Soldier {
 
 // War
 class War {
-    
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy=[];
+    }
+    addViking(Viking) {
+        this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon);
+    }
+    vikingAttack() {
+        this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].receiveDamage(this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].strength);
+        if(Saxon.health < 0) {
+
+        }
+    }
+    saxonAttack() {
+        this.vikingArmy[Math.floor(Math.random()* this.vikingArmy.length)].receiveDamage(this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].strength);
+    }
+
 }
+/**
+var rand = myArray[Math.floor(Math.random() * myArray.length)];
+ vikingAttack() method
+1.A Saxon (chosen at random) 
+2. has their receiveDamage() method called 
+3. with the damage equal to the strength of a Viking 4. (also chosen at random). 
+This should only perform a single attack and the Saxon doesn't get to attack back.
+
+should be a function
+should receive 0 arguments
+should make a Saxon receiveDamage() equal to the strength of a Viking
+should remove dead saxons from the army
+should return result of calling receiveDamage() of a Saxon with the strength of a Viking
+ 
+**/
