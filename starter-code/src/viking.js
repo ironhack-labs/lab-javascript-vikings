2387d918a58ad78efc552b532706e7369af1fc0b
Clara Rodriguez <clara92r@gmail.com>
2019-09-12 21:01:37 +0200

    done, really

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -66,7 +66,10 @@ class War {
     {
         this.vikingArmy=[]    
         this.saxonArmy=[]  
+		
+		
     }
+	
 
     addViking(Viking){
         this.vikingArmy.push(Viking)
@@ -74,13 +77,51 @@ class War {
     addSaxon(Saxon){
         this.saxonArmy.push(Saxon)
     }
-    vikingAttack(){
-        
-        //const damageVicking=new this.Vicking.strength
-        //const saxonDamage=new this.Saxon.receiveDamage(damageVicking)
-
-       //return saxon.receiveDamage(this.vicking.strength)
-
-        
-    }
+	
+	vikingAttack(){
+		//elige a un vikingo del array para que ataque y a un sajón para que reciba
+		 const randomVikingAttack = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+         const randomSaxonReciveDamage = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+		// Llamando a la funcion de recibir daño del sajon con el parámetro de fuerza de un vikingo 	
+		 const saxonDamage= randomSaxonReciveDamage.receiveDamage(randomVikingAttack.strength);
+		//Retirar sajones muertos	
+			this.saxonArmy=this.saxonArmy.filter((aliveSaxon)=>aliveSaxon.health>0)
+		//Comprobar si el sajón está vivo o muerto
+		if(randomSaxonReciveDamage.health<=0){
+			return `A Saxon has died in combat`
+		}
+		else{
+		return `A Saxon has received ${randomVikingAttack.strength} points of damage`
+		}
+	}
+	
+	saxonAttack(){
+		//elige a un vikingo del array para que ataque y a un sajón para que reciba
+		 const randomSaxonAttack = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+         const randomVikingReciveDamage = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+		// Llamando a la funcion de recibir daño del sajon con el parámetro de fuerza de un vikingo 	
+		 const vikingDamage= randomVikingReciveDamage.receiveDamage(randomSaxonAttack.strength);
+		//Retirar sajones muertos	
+			this.vikingArmy=this.vikingArmy.filter((aliveViking)=>aliveViking.health>0)
+		//Comprobar si el sajón está vivo o muerto
+		if(randomVikingReciveDamage.health<=0){
+			return `A Viking has died in combat`
+		}
+		else{
+		return `${randomVikingReciveDamage.name} has received ${randomSaxonAttack.strength} points of damage`
+		}
+	}
+	
+	showStatus(){
+		if(this.vikingArmy.length===0){
+			return "Saxons have fought for their lives and survive another day..."
+		}
+		else if(this.saxonArmy.length===0){
+			return "Vikings have won the war of the century!"
+		} 
+		else{
+			return "Vikings and Saxons are still in the thick of battle."
+		}
+	}
+	
 }
