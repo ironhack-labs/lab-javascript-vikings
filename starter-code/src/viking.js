11e99081966b845d137e150cd2ecda0f70fedd4c
oscarmtzo <icehman@hotmail.com>
2019-09-18 11:07:11 -0500

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,83 +1,61 @@
 // Soldier
 class Soldier {
-    constructor (health,strength) {
+    constructor(health, strength) {
         this.health = health
         this.strength = strength
-        this.attack = function(){
-            return strength
-        }
-        this.receiveDamage = function(damage){
-            this.damage=50
-            this.health=this.health-this.damage
-        }
+    }
+    attack (){
+        return this.strength
+    }
+    receiveDamage( damage){
+        this.health = this.health - damage
     }
 }
 
 // Viking
-class Viking extends Soldier {
-    constructor (name, health, strength) {
-        super (health,strength)
-        this.name=name
-        this.attack = function(){
-            return strength
+ //va a heredar las propiedades de la clase soldier y vamos a agregar su nombre, con extends vamos a extender las propiedades de la clase 
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength)//es la notacion que va a traer todo del constructor de Soldie
+        this.name = name    
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health == 0 ){
+            return `${this.name} has died in act of combat`
+        }else{
+            return `${this.name} has received ${damage} points of damage`
         }
-        this.receiveDamage = function(damage){
-            this.health=this.health-damage
-            if(this.health>0)return `${name} has received ${damage} points of damage`
-            if (this.health===0)return `${name} has died in act of combat` 
-        }
-        this.battleCry = function(){
-            return "Odin Owns You All!"
-        }
+    }
+    attack (){
+        return this.strength
+    }
+    battleCry(){
+        return "Odin Owns You All!"
     }
 }
 
+
 // Saxon
 class Saxon extends Soldier {
-    constructor (health, strength) {
-        super (health,strength)
-        this.name=name
-        this.attack = function(){
-            return strength
-        }
-        this.receiveDamage = function(damage){
-            this.health=this.health-damage
-            if(this.health>0)return `A Saxon has received ${damage} points of damage`
-            if (this.health===0)return `A Saxon has died in combat` 
-        }
+    constructor(health, strength) {
+      super(health, strength);
     }
-}
+    attack (){
+        return this.strength
+    }
+    receiveDamage(damage) {
+      this.health = this.health - damage;
+      if (this.health > 0)
+        return `A Saxon has received ${damage} points of damage`;
+      else return `A Saxon has died in combat`;
+    }
+  }
 
 // War
 class War {
     constructor(){
-        this.vikingArmy=[]
-        this.saxonArmy=[]
+        this.armyViking = []
     }
-        addViking(Viking){
-            this.vikingArmy.push(Viking)
-        }
-        addSaxon(Saxon){
-            this.saxonArmy.push(Saxon)
-        }
-        vikingAttack (){
-            let randomSaxon= Math.floor(Math.random*this.saxonArmy.length)
-            let randomViking= Math.floor(Math.random*this.vikingArmy.length)
-            let remove = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength) 
-            if (this.saxonArmy[randomSaxon].health<=0){
-                this.saxonArmy.splice(randomSaxon,1)
-            }
-            return remove
-        }
-        saxonAttack (){
-            let randomSaxon= Math.floor(Math.random*this.saxonArmy.length)
-            let randomViking= Math.floor(Math.random*this.vikingArmy.length)
-            let remove = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength) 
-            if (this.vikingArmy[randomViking].health<=0){
-                this.vikingArmy.splice(randomViking,1)
-            }
-            return remove
-        }
-        showStatus() 
     
 }
