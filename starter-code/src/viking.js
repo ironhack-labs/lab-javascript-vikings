ae532e26e060c8451187dd6dd29395baefb4ace6
Marie Greiner <marie@MacBook-Air-de-Marie.local>
2019-10-24 23:11:55 +0200

    Lab Vikings - first try on soldier and viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.attack = function returnStrength() {
+            return strength};
+        this.receiveDamage = function hurt(damage) {
+                if (damage < 1) {
+                    health === (health - damage);
+                };
+            }
+        }
+    }
 
 // Viking
-class Viking {}
+class Viking extends Soldier(health, strength) {
+    constructor(name, health, strength) {
+        super(name, health, strength);
+        this.receiveDamage = function hurtV(damage) {
+            if (damage < 1) {
+                health === (health - damage);
+            }
+            if (health > 0) {
+                return `${NAME} has received ${DAMAGE} points of damage`
+            }
+            if (health <= 0) {
+                return `${NAME} has died in act of combat`
+            }
+        this.battleCry = function cries() {
+            return "Odin Owns You All!";
+        }
+      }  
+    }}
 
-// Saxon
-class Saxon {}
+// // Saxon
+// class Saxon {}
 
-// War
-class War {}
+// // War
+// class War {}
