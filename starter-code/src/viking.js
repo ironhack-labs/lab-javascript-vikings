6a0f40a06f23e9d4bc9e32c87f8e1cf173f66a53
jesus-flores <chucho_quidproquo@hotmail.com>
2019-10-26 23:54:10 -0500

    PP-WDFT-MEX Roberto Correa & Jesus Flores

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,38 @@
 // Soldier
-class Soldier {}
-
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+      }
+    receiveDamage(damage){
+        this.health -= damage;
+      }
+  }
+  // Viking
+  class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+        }
+        receiveDamage(damage){
+          this.health -= damage;
+        }
+        
+    battleCry(){
+        return 'Odin Owns You All!';
+    }
+  }
+  // Saxon
+  class Saxon extends Soldier{
+    constructor(health, strength){
+        super(health, strength);
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+      }
+  }
+  // War
+  class War {}
