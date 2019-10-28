526fd55c67a91d4fbd5d370aa6cf764bfb3ab291
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-10-28 14:13:33 +0100

    Vikings

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+
+
+        
+
+    }
+  
+     attack() {
+      return this.strength;
+     }
+      receiveDamage(theDamage) {
+          this.health -= theDamage
+          
+     }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends soldier{
+    constructor(name, health, strength){
+    super (health , strength)
+    this.name = name
+    this.health = health
+    this.strength = strength
+    }
+    
+    battleCry(){
+       (`Odin los posee a todos ustedes`)
+       }
+        receiveDamage(theDamage){
+            this.health -= thedamage
+            if (this.health > 0){
+                return `a saxon has received ${theDamage}points of the damage`;
+            } else {
+                return `a saxon has died in combat`
+                }
+            }
+
+        
+}
+    
+
+    
+
+
+    
+    
+
 
 // Saxon
-class Saxon {}
+class Saxon extends soldier {
+    constructor
+}
 
 // War
 class War {}
