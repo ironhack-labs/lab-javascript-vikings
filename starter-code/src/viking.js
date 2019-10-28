95d31c80dc502d44ad9857612b7a9af248d8a16b
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-28 17:43:15 -0400

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -11,26 +11,40 @@ class Soldier {
         this.health -= damage
     }
 }​
+
 // Viking
 class Viking extends Soldier {
     constructor(name, health, strength) {
-        super(health, strength)
+        super(health, strength) //recieve health and strength from parent class
         this.name = name
     }
-    receiveDamage(damage) {
-        this.health -= damage
-        if (this.health <= 0) {
-            return `${this.name} has died in act of combat`
-        } else {
-            return `${this.name} has received ${damage} points of damage`
-        }
+    receiveDamage(damage){
+        super.receiveDamage(damage)
+        this.health >= 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`
     }
+    // receiveDamage(damage) {
+    //     this.health -= damage
+    //     if (this.health <= 0) {
+    //         return `${this.name} has died in act of combat`
+    //     } else {
+    //         return `${this.name} has received ${damage} points of damage`
+    //     }
+    // }
     battleCry() {
         return 'Odin Owns You All!'
     }
 }​
 // Saxon
 class Saxon extends Soldier {
+    // receiveDamage = () => {
+    //     this.health -= damage
+    //     return {
+    //         this.health < 0 ?
+    //         `A Saxon has died in combat` :
+    //         `A Saxon has received ${damage} points of damage`
+    //     }
+    // }
+
     receiveDamage(damage) {
         this.health -= damage
         if (this.health <= 0) {
@@ -40,34 +54,48 @@ class Saxon extends Soldier {
         }
     }
 }​
+
 // War
 class War {
     constructor() {
         this.vikingArmy = []
         this.saxonArmy = []
     }​
+
     addViking(Viking) {
         this.vikingArmy.push(Viking)
     }
+
     addSaxon(Saxon) {
         this.saxonArmy.push(Saxon)
-    }​
+    }
+    ​
     vikingAttack() {
-        let rondomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
-        let attackedSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]​
-        if (attackedSaxon.health - rondomViking.strength <= 0) {
-            this.saxonArmy.splice(this.saxonArmy.indexOf(attackedSaxon), 1)
+        let saxIndex = Math.floor(Math.random() * this.saxonArmy.length)
+        let vikIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        let rondomViking = this.vikingArmy[vikIndex]
+        let rondomSaxon = this.saxonArmy[saxIndex]
+        ​
+        if (rondomSaxon.health - rondomViking.strength <= 0) {
+            this.saxonArmy.splice(saxIndex, 1)
+            // this.saxonArmy.splice(this.saxonArmy.indexOf(rondomSaxon), 1)
         }
-        return attackedSaxon.receiveDamage(rondomViking.strength)
+        return rondomSaxon.receiveDamage(rondomViking.strength)
     }
+
     saxonAttack() {
-        let attackedViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
-        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
-        if (attackedViking.health - randomSaxon.strength <= 0) {
-            this.vikingArmy.splice(this.vikingArmy.indexOf(attackedViking), 1);
+        let saxIndex = Math.floor(Math.random() * this.saxonArmy.length)
+        let vikIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        let rondomViking = this.vikingArmy[vikIndex];
+        let randomSaxon = this.saxonArmy[saxIndex];
+
+        if (rondomViking.health - randomSaxon.strength <= 0) {
+            this.vikingArmy.splice(vikIndex, 1);
+            // this.vikingArmy.splice(this.vikingArmy.indexOf(rondomViking), 1);
         }
-        return attackedViking.receiveDamage(randomSaxon.strength);
+        return rondomViking.receiveDamage(randomSaxon.strength);
     }
+
     showStatus() {
         if (this.saxonArmy.length === 0) {
             return 'Vikings have won the war of the century!';
