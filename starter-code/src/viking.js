ecf0de111360422f23f8a07713cc1bbb2666c9c2
alejandro alarco <alejandroalarco@alejandros-MacBook-Pro-3040.local>
2019-10-28 12:10:12 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,64 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    recieveDamage(damage) {
+        this.health -= damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(health, strength, name) {
+        this.name = name
+        //this.health = health
+        //this.strength = strength
+    }
+    recieveDamage (damage) {
+        this.health -= damage
+        if (this.health > 0) {
+            return '${this.name} has recived ${damage} points of damage'
+        } else {
+            return '${this.name} has died in combat'
+        }
+    }
+    battleCry(){
+        return 'Odin Owns You All'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength,) {
+        super (health, strength)
+    }
+    recieveDamage (damage) {
+        this.health -= damage;
+        if (damage > 0) {
+            return 'a Saxon has recieved ${damage} points of damage'
+        } else {
+            return 'A Saxon has died in combat'
+        }
+    }
+}
+
+
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking (Viking) {
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon (Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+}
