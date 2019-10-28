5f8d253246edf872b1bc179a45c21fc6b3ee2f11
mendosaurus <masaki.endo@nex.nikkei.com>
2019-10-28 17:52:32 -0400

    done

--- starter-code/tests/VikingSpec.js
+++ starter-code/tests/VikingSpec.js
@@ -11,11 +11,11 @@ describe('Soldier', () => {
     it('should receive 2 arguments (health & strength)', () => {
       expect(Soldier.length).toEqual(2);
     });
-
+    
     it('should receive the health property as its 1st argument', () => {
       expect(soldier.health).toEqual(health);
     });
-
+    
     it('should receive the strength property as its 2nd argument', () => {
       expect(soldier.strength).toEqual(strength);
     });
@@ -25,11 +25,11 @@ describe('Soldier', () => {
     it('should be a function', () => {
       expect(typeof soldier.attack).toBe('function');
     });
-
+    
     it('should receive 0 arguments', () => {
       expect(soldier.attack.length).toEqual(0);
     });
-
+    
     it('should return the strength property of the Soldier', () => {
       expect(soldier.attack()).toEqual(strength);
     });
