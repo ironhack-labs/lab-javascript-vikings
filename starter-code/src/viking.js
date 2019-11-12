b16d45082eb8d3a22d8c35cbfc8bc1c05f2d1c1d
RFeldman84 <rachel.feldman84@gmail.com>
2019-11-12 02:24:58 -0500

    initial commit oct

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,133 +1,92 @@
-//attack funtion removed b/c no changes and inherited from 1st call above
-//super. means inherit (copying) from parent function then rest of argumenrts are new
-// extends mean copying function, super. is calling a function from the parent
-//super with no .(dot) copiny constructor ex.super(health, strength) 
-
-//reimplement --super take from parent then new return is the reimplemented arguments
-
-
-
 // Soldier
 class Soldier {
-    constructor (health, strength){
-    this.health= health ;
-    this.strength= strength;  
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+ attack() {
+    return this.strength
+ }
+
+ receiveDamage(damage){
+     this.health = this.health - damage
+ }
 }
 
-attack (){
-    return this.strength;
-}
 
-
-receiveDamage(damage){
-  this.health = this.health - damage;
-}
-}
 
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health, strength){
-        super(health, strength)  //calling function from parent and passing parameters necessary
-         this.name= name;    
-     }
+    constructor(name, health, strength) {
+            super(health, strength)
+            this.name = name
+        }
+    
+    attack() {
+            return this.strength
+         }
+    
+     receiveDamage(damage){
+        super.receiveDamage(damage);
+        if (this.heath > 0){
+              return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`}
+   }
 
-
-   receiveDamage(damage){
-       super.receiveDamage(damage)
-       
-       if (this.health <= 0){ 
-           return `${this.name} has died in act of combat`
-        } else { return `${this.name} has received ${damage} points of damage`
-    }
-}     
-    battleCry(){
-        return "Odin Owns You All!";   
-    }
-
-}
-
-
-// Saxon
-class Saxon extends Soldier{
-    constructor (health, strength){
-        super(health, strength);
-    }
-  //would never do this in real life wasting keystrokes
-  //if you simply dont define it this method will inherit perfectly from parents
-  //but since we redefine it, we must call super in order to inherit from parents  
-    receiveDamage(damage){
-    super.receiveDamage(damage)
-    if(this.health <= 0){ 
-        return "A Saxon has died in combat";
-     } else { 
-         return `A Saxon has received ${damage} points of damage`;
+   battleCry(){
+    return "Odin Owns You All!";
  }
-}
-}
-
-
-
-
-// War 
-class War {
-    constructor(){
-        this.vikingArmy = [];
-        this.saxonArmy = [];
-    }
-
-    addViking(aVikingObject){
-        this.vikingArmy.push(aVikingObject);
-    }
-
-    addSaxon(aSaxonObject){
-        this.saxonArmy.push(aSaxonObject);
-    }
-
-
-    vikingAttack(){
-        let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
-        let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
-        let randomSaxon = this.saxonArmy[randomSaxonIndex];
-        let randomViking = this.vikingArmy[randomVikingIndex];
-
-        let theAmount = randomViking.attack()
-
-       let result = randomSaxon.receiveDamage(theAmount);
-
-        if(randomSaxon.health <= 0){
-            this.saxonArmy.splice(randomSaxonIndex, 1);
-        }
-        return result;
-    }
-
-    saxonAttack(){
-        let randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
-        let randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
-        let randomSaxon = this.saxonArmy[randomSaxonIndex];
-        let randomViking = this.vikingArmy[randomVikingIndex];
-
-        let theAmount = randomSaxon.attack()
-
-       let result = randomViking.receiveDamage(theAmount);
-
-        if(randomViking.health <= 0){
-            this.vikingArmy.splice(randomVikingIndex, 1);
-        }
-        return result;
-    }
-
-
-    showStatus(){
-        if(this.saxonArmy.length === 0){
-            return "Vikings have won the war of the century!"
-        }
-
-        if(this.vikingArmy.length ===0){
-            return "Saxons have fought for their lives and survive another day..."
-        }
-
-        return "Vikings and Saxons are still in the thick of battle."
-    }
 
 }
+
+
+
+class Saxon extends Soldier {
+    
+    
+    
+    receiveDamage(number){
+      super.receiveDamage(number);
+        if(this.health > 0){
+            return `A Saxon has received ${number} points of damage`
+        } else {
+             return `A Saxon has died in combat`
+                }
+            }
+        }
+
+
+
+}
+
+
+// class Saxon extends Soldier {
+//     constructor(firstArg, secondArg){ //whay different arg not health,strenght`
+//         super(firstArg, secondArg);
+//     }
+//     // you would never do this in real life because youre just wasting keystrokes
+//     // if you simply dont define this method, you will inherit perfectly from the parents class
+//     // but since we redefine it, we must call super in order to inherit from the parents consturctor function
+
+
+//     receiveDamage(number){
+//         super.receiveDamage(number);
+//         if(this.health > 0){
+//             return `A Saxon has received ${number} points of damage`
+//         } else {
+//            return `A Saxon has died in combat`
+//         }
+//     }
+
+// }
+
+
+
+
+
+
+// War
+class War {}
