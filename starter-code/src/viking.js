bf08c250b257704c656f685549f124eb763491ce
Grecia Patricia <grecia.busquets@gmail.com>
2019-11-23 11:04:01 +0100

    Inicio de la practica: Soldier and Viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack () {
+        return this.strength;
+    };
+    receiveDamage (damage){
+        this.health - damage;
+
+    };
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+   constructor (name, health, strength) {
+       this.name = name;
+       super(health, strength)
+   }
+   battleCry () {
+       return "Odin Owns You All!"
+   }
+} 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+}
 
 // War
 class War {}
