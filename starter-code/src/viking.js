32cecc3c35cc9fba846890c21ed33ef82807e214
Borja Domínguez <borja.d.atienza@gmail.com>
2019-11-23 20:39:08 +0100

    DONE

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -21,6 +21,7 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
+
   receiveDamage(damage){
     this.health = this.health - damage;
     if(this.health <= 0){
@@ -55,6 +56,8 @@ class War {
   constructor(){
     this.vikingArmy = []
     this.saxonArmy = []
+    console.log(this.vikingArmy)
+    console.log(this.saxonArmy)
   }
 
   addViking(viking){
@@ -65,9 +68,48 @@ class War {
     this.saxonArmy.push(saxon)
   }
 
-  // vikingAttack(){}
+  vikingAttack(){
+    function randomSoldier(arr) {
+        return arr[Math.floor(Math.random() * (arr.length))];
+      }
 
-  // saxonAttack(){}
+      let viking = randomSoldier(this.vikingArmy);
+      let saxon = randomSoldier(this.saxonArmy);
 
-  // showStatus(){}
+      let attack = saxon.receiveDamage(viking.strength);
+
+      if(attack === "A Saxon has died in combat") {
+            this.saxonArmy.splice(this.saxonArmy.indexOf(saxon),1);
+          }
+          
+    return attack
+
+  }
+    
+  saxonAttack(){
+    function randomSoldier(arr) {
+        return arr[Math.floor(Math.random() * (arr.length))];
+      }
+    
+      let viking = randomSoldier(this.vikingArmy);
+      let saxon = randomSoldier(this.saxonArmy);
+
+      let attack = viking.receiveDamage(saxon.strength)
+
+      if(attack === `${viking.name} has died in act of combat`){
+          this.vikingArmy.splice(this.vikingArmy.indexOf(viking),1);
+      }
+    
+      return attack
+  }
+
+     showStatus(){
+         if(!this.saxonArmy.length){
+             return "Vikings have won the war of the century!"
+         } else if(!this.vikingArmy.length) {
+             return "Saxons have fought for their lives and survived another day..."
+         } else if(this.vikingArmy.length === 1 && this.saxonArmy.length === 1){
+             return "Vikings and Saxons are still in the thick of battle."
+         }
+     }
 }
