c43726784ddc9a7e9ac45f72c67c7601f5e68519
Román Méndez <mendezbeck@gmail.com>
2019-11-23 16:08:39 +0100

    vikings done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -49,14 +49,43 @@ class War {
         this.vikingArmy = [];
         this.saxonArmy = [];
     }
-    addViking(viking){
-        this.vikingArmy.push(viking);
+    addViking(Viking){
+        this.vikingArmy.push(Viking);
     }
-    addSaxon(saxon){
-        this.saxonArmy.push(saxon);
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
     }
     vikingAttack(){
+        let random = Math.floor(Math.random());
+        let viking = this.vikingArmy[random * this.vikingArmy.length];
+        let saxon = this.saxonArmy[random * this.saxonArmy.length];
+        let attack = saxon.receiveDamage(viking.strength)
+        if (saxon.health <= 0){
+            this.saxonArmy.splice(saxon, 1)
+            return attack;
+        } else {
+            return attack;
+        }
     }
-    saxonAttack()
-    showStatus()
+    saxonAttack(){
+        let random = Math.floor(Math.random());
+        let viking = this.vikingArmy[random * this.vikingArmy.length];
+        let saxon = this.saxonArmy[random * this.saxonArmy.length];
+        let attack = viking.receiveDamage(saxon.strength)
+        if (viking.health <= 0){
+            this.vikingArmy.splice(saxon, 1)
+            return attack;
+        } else {
+            return attack;
+        }
+    }
+    showStatus(){
+        if(this.saxonArmy.length == 0){
+            return "Vikings have won the war of the century!"
+        } else if(this.vikingArmy.length == 0){
+            return "Saxons have fought for their lives and survived another day..."
+        } else {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
 }
