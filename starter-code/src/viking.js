b3625624268eb8da4dae4b0555cfcdc9ffba7e2f
zante <dza.devandcomputing@gmail.com>
2019-11-23 11:24:48 +0100

    first commit_soldier and viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
 
-// Viking
-class Viking {}
+    constructor(health, strength){
+
+        this.health = health;
+
+        this.strength = strength;
+
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+      this.health -= damage;
+    }
+
+}
+
+// Vikingg
+class Viking extends Soldier{
+
+    constructor(name, health, strength){
+        
+        super(health, strength);
+        this.name = name;
+
+        // this.health = health;
+
+        // this.strength = strength;
+
+
+    }
+
+    receiveDamage(damage){
+
+        this.health -= damage;
+
+        if(this.health > 0){
+            return `${name} has received ${damage} points of damage`;
+        }else{
+            return `${name}has died in act of combat`
+        }
+
+    }
+
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+
+}
 
 // Saxon
 class Saxon {}
