f5de9891b4a359104fc03602d600a6bd311699e2
Strapak, Sergej <strapak@mediamarktsaturn.com>
2019-11-26 13:59:34 +0100

    Finished all

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -81,4 +81,17 @@ class War {
         }
         return msg;
     }
+    showStatus(){
+        if (this.saxonArmy.length < 1){
+            return("Vikings have won the war of the century!");
+        }
+        else if (this.vikingArmy.length < 1){
+            return("Saxons have fought for their lives and survived another day...");
+        }
+        else if (this.saxonArmy.length == 1 && this.vikingArmy.length == 1){
+            return("Vikings and Saxons are still in the thick of battle.");
+        }
+    };
 }
+
+
