11190e71ea3d393c145f5ee16f2a13f58568c9a1
Javi L <jlopezro@gmail.com>
2019-11-26 22:25:07 +0100

    Soldier class and its methods created

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,17 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = 300;
+        this.strength = 150;
+        this.attack = function() {
+            return this.strength;
+        }
+        this.receiveDamage = function(damage) {
+           this.health -= damage;
+        }
+    }
+
+}
 
 // Viking
 class Viking {}
