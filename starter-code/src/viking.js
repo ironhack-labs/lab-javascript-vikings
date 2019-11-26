aff541b59d0b681f79aea452df9b0221884380b3
Malte Schenkling <schenkling@mediamarktsaturn.com>
2019-11-26 13:39:05 +0100

    Vikings_Armies_Final_upd

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -81,9 +81,7 @@ class War {
         if (this.vikingArmy.length === 0){
             return "Saxons have fought for their lives and survived another day...";
         }
-        if (this.saxonArmy.length === 1  && this.vikingArmy.length === 1 ) {
-            return 'Vikings and Saxons are still in the thick of battle.';
-        }
+        return 'Vikings and Saxons are still in the thick of battle.';
     }
 }
 
