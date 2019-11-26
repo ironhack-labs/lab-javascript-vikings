f1474bb15e0a2adebf68af4d15efaec524bd626a
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2019-11-26 13:43:19 +0100

    Vikings with bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -36,6 +36,9 @@ class Viking extends Soldier{
 
 // Saxon
 class Saxon extends Soldier {
+    constructor (health, strength){
+        super(health, strength);
+    }
     receiveDamage(damage){
         this.health -= damage;
         if (this.health > 0){
@@ -47,4 +50,43 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack(){
+        const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const saxon = this.saxonArmy[saxonIndex];
+        const viking = this.vikingArmy[vikingIndex];
+        const message = saxon.receiveDamage(viking.strength);
+        if (saxon.health < 1){
+            this.saxonArmy.splice(saxonIndex, 1);
+        }
+        return message;
+    } 
+    saxonAttack(){
+        const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const saxon = this.saxonArmy[saxonIndex];
+        const viking = this.vikingArmy[vikingIndex];
+        const message = viking.receiveDamage(saxon.strength);
+        if (viking.health < 1){
+            this.vikingArmy.splice(vikingIndex, 1);
+        }
+        return message;
+    }
+    showStatus(){
+        if (!this.saxonArmy.length) return "Vikings have won the war of the century!";
+        if (!this.vikingArmy.length) return "Saxons have fought for their lives and survived another day...";
+        return "Vikings and Saxons are still in the thick of battle.";
+    }
+
+}
