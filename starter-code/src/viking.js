720272eab839fea8503861cea6e1210420b39822
Javi L <jlopezro@gmail.com>
2019-11-27 01:58:37 +0100

    Viking completed

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,20 +1,37 @@
 // Soldier
 class Soldier {
-    constructor(health, strength){
-        this.health = 300;
-        this.strength = 150;
-        this.attack = function() {
-            return this.strength;
-        }
-        this.receiveDamage = function(damage) {
-           this.health -= damage;
-        }
-    }
-
+  constructor(health, strength) {
+    this.health = 300;
+    this.strength = 150;
+    this.attack = function() {
+      return this.strength;
+    };
+    this.receiveDamage = function(damage) {
+      this.health -= damage;
+    };
+  }
 }
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    super.receiveDamage = function(damage) {
+      this.health -= damage;
+      if (this.health > 0) {
+        return this.name + " has received " + damage + " points of damage";
+      } else {
+        return this.name + " has died in act of combat";
+      }
+
+    };
+    this.battleCry = function(){
+        return 'Odin Owns You All!'
+    }
+  }
+}
 
 // Saxon
 class Saxon {}
