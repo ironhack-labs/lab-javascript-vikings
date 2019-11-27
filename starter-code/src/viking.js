6bfcd8a603f2735c107294ee21e86dd3efaa289b
Javi L <jlopezro@gmail.com>
2019-11-27 03:10:36 +0100

    Saxon completed

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,14 +1,14 @@
 // Soldier
 class Soldier {
   constructor(health, strength) {
-    this.health = 300;
-    this.strength = 150;
-    this.attack = function() {
-      return this.strength;
-    };
-    this.receiveDamage = function(damage) {
-      this.health -= damage;
-    };
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
   }
 }
 
@@ -18,23 +18,31 @@ class Viking extends Soldier {
   constructor(name, health, strength) {
     super(health, strength);
     this.name = name;
-    super.receiveDamage = function(damage) {
-      this.health -= damage;
-      if (this.health > 0) {
-        return this.name + " has received " + damage + " points of damage";
-      } else {
-        return this.name + " has died in act of combat";
-      }
-
-    };
-    this.battleCry = function(){
-        return 'Odin Owns You All!'
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    } else {
+      return this.name + " has died in act of combat";
     }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
   }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
 class War {}
