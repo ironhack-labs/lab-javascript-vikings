2487813d4026b07dad9b7c44e8aa4a80f84b5d9d
Mac <mac@android-c27ab30654549d4c.localdomain>
2019-12-02 16:11:45 -0300

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,41 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health = health;
+        this.strength= strength;
+    }
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health = this.health - damage;
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health,strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage){
+        if (this.health){
+            
+        }
+    return `${this.name} has received ${this.damage} points of damage`;
+    return `${this.name} has died in act of combat`;
+        
+    }
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon {
+
+}
 
 // War
 class War {}
