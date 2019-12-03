90b14d3c51f33988d3288d38fdc00f36cb8943f8
Juliane <juliane_ch.arruda@live.com>
2019-12-03 00:14:54 -0300

    completed exercises lack bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,40 +1,53 @@
 // Soldier
 class Soldier {
-    constructor(health,strength){
-        this.health = health;
-        this.strength= strength;
-    }
-    attack(){
-        return this.strength;
-    }
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
 
-    receiveDamage(damage){
-        this.health = this.health - damage;
-    }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
 }
 
 
 // Viking
-class Viking extends Soldier{
-    constructor(name, health, strength){
-        super(health,strength);
-        this.name = name;
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else if (this.health === 0) {
+      return `${this.name} has died in act of combat`;
     }
 
-    receiveDamage(damage){
-        if (this.health){
-            
-        }
-    return `${this.name} has received ${this.damage} points of damage`;
-    return `${this.name} has died in act of combat`;
-        
-    }
-    
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+
 }
 
 // Saxon
-class Saxon {
+class Saxon extends Soldier {
 
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health === 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
 }
 
 // War
