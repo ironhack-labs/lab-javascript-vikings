54e77735de843db33127bfc6e2b55255bb349dc1
javier Varela <javivarela@javiers-MacBook-Pro.local>
2020-01-09 01:26:47 +0100

    viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -14,8 +14,24 @@ class Soldier {
     }
 }
 
+
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        return this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`;
+    }
+
+    battleCry(){
+        return 'Odin Owns You All!';
+    }
+
+}
 
 // Saxon
 class Saxon {}
