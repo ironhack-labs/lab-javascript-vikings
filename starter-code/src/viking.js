bd13e70a7929857195f96cb7377f9c25eda0a199
Edith LE <edith.lop.estrada@gmail.com>
2020-01-10 18:23:42 -0600

    falta super bonus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,84 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health = health
+        this.strength= strength 
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){ 
+        this.health -= damage     
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name
+    }
+    receiveDamage(damage){
+        this.health-= damage
+        return (this.health > 0)
+                ? `${this.name} has received ${damage} points of damage`
+                : `${this.name} has died in act of combat`
+
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health-= damage
+        return (this.health > 0)
+                ? `A Saxon has received ${damage} points of damage`
+                : "A Saxon has died in combat"
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy =[]
+        this.saxonArmy =[]
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking)
+        
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        const saxonRandomIndex = Math.floor(Math.random() * this.saxonArmy.length)
+        const saxonRandom =  this.saxonArmy[saxonRandomIndex]
+        const vikingRandomIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        const vikingRandom =  this.vikingArmy[vikingRandomIndex]
+
+        
+        
+        saxonRandom.receiveDamage(vikingRandom.strength) 
+        if( saxonRandom.health <= 0){
+            this.saxonArmy.splice(saxonRandomIndex, 1)
+            return "A Saxon has died in combat"
+        }
+    }
+    
+    saxonAttack(){
+        const saxonRandomIndex = Math.floor(Math.random() * this.saxonArmy.length)
+        const saxonRandom =  this.saxonArmy[saxonRandomIndex]
+        const vikingRandomIndex = Math.floor(Math.random() * this.vikingArmy.length)
+        const vikingRandom =  this.vikingArmy[vikingRandomIndex]
+        
+        vikingRandom.receiveDamage(saxonRandom.strength) 
+        if(vikingRandom.health <= 0){
+            this.vikingArmy.splice(vikingRandomIndex, 1)
+            return "Odin Owns You All!"
+        }
+        return `${vikingRandom.name} has received ${saxonRandom.strength} points of damage`
+    }
+}
