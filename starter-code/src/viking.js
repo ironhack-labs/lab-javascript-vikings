331f24e61718b42c5d0eabf11dd98df9d7c12541
Carles Galí Bou <carles@MacBook-Pro-de-Carles.local>
2020-01-12 17:07:21 +0100

    Class Soldier and Viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -15,7 +15,27 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength) {
+        this.name = name;
+        super(health, strength);
+    };
+
+    receiveDamage(theDamage) {
+        this.health = this.health - theDamage;
+        if (this.health > 0){
+            return `${this.name} has received ${theDamage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }
+        
+    }    
+
+    battleCry () {
+        return "Odin Owns You All!!!"
+    }
+}
 
 // Saxon
 class Saxon {}
