6317689428b21ce142fee8305f08efaf26283420
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-13 22:02:06 +0100

    Viking done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -8,12 +8,31 @@ class Soldier {
        return this.strength;
     }
     receiveDamage(theDamage){
-        this.health = this.health - theDamage;
+        this.health -= theDamage;    
     }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if (this.health > 0){
+            return `${this.name} has received ${theDamage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
 class Saxon {}
