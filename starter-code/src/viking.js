8d6b4f83f4946cef355c030fbc2504364135d7b5
Pablolo <pablo_olondriz@hotmail.com>
2020-01-13 18:55:52 +0100

    starting war

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,9 +4,11 @@ class Soldier {
     this.health = health;
     this.strength = strength;
   }
+
   attack() {
     return this.strength;
   }
+
   receiveDamage(damage) {
     this.health -= damage;
   }
@@ -18,12 +20,14 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
+
   receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
       return `${this.name} has received ${damage} points of damage`;
     } return `${this.name} has died in act of combat`;
   }
+
   battleCry() {
     return "Odin Owns You All!";
   }
@@ -41,7 +45,44 @@ class Saxon extends Soldier {
 
 // War
 class War {
-  constructor() {
-    
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(Viking){
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  
+  vikingAttack() {
+    let randSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    randSaxon.receiveDamage(randViking.strength);  
+    if ()
+  }
+  
+  saxonAttack() {
+
+  }
+  
+  showStatus() {
+
   }
 }
+
+
+let viking1 = new Viking("fuckit", 100, 20);
+let viking2 = new Viking("joder", 110, 30);
+
+let saxon1 = new Saxon(120, 40);
+let saxon2 = new Saxon(125, 55);
+
+let batalla = new War();
+
+batalla.addViking(viking1);
+batalla.addViking(viking2);
+batalla.addSaxon(saxon1);
+batalla.addSaxon(saxon2);
+
+batalla.vikingAttack();
