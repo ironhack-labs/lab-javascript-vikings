38ce968c2cba268be950824b254ab13544371898
Leinadm <danielmonteru@gmail.com>
2020-01-15 20:28:47 +0100

    Saxon

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -32,7 +32,18 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+receiveDamage (damage) {
+  super.receiveDamage(damage);
+  if (this.health > 0) {
+    return `A Saxon has received ${damage} points of damage`;
+  } else {
+    return "A Saxon has died in combat";
+  }
+}
+
+}
 
 // War
 class War {}
