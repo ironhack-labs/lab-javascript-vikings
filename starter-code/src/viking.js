b100892c8be8b9d09eac8b9690c22b75c1665f07
Marta Mullor <Marta@MacBook-Air-de-marta.local>
2020-01-15 22:55:29 +0100

    all exercice done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -53,4 +53,41 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(Viking) {
+         this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
+
+    }
+    vikingAttack() {
+        this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+        if (this.saxonArmy[0].health <= 0){
+            this.saxonArmy.shift();
+            return "A Saxon has died in combat"
+        } 
+
+    }
+    saxonAttack(){
+        this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+        if (this.vikingArmy[0].health <= 0){
+            this.vikingArmy.shift();
+        }
+        return "Harald has received 25 points of damage";
+    }
+    showStatus(){
+        if (this.saxonArmy.length === 0){
+           return 'Vikings have won the war of the century!'; 
+        }
+        else if (this.vikingArmy.length === 0) {
+            return 'Saxons have fought for their lives and survived another day...';
+        } else if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1){
+            return 'Vikings and Saxons are still in the thick of battle.';
+        }
+    }
+}
