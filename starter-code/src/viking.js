c82c840e3b7b432ec44f97202f7a0d1263a5c4d7
Leinadm <danielmonteru@gmail.com>
2020-01-16 00:02:22 +0100

    SuperBonus showStatus

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -79,4 +79,17 @@ class War {
     return `Harald has received 25 points of damage`;
   }
 
+  // SUPER BONUS
+
+  showStatus(){
+    if (this.saxonArmy.length === 0){
+       return 'Vikings have won the war of the century!'; 
+    }
+    else if (this.vikingArmy.length === 0) {
+        return 'Saxons have fought for their lives and survived another day...';
+    } else if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1){
+        return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+
 }
