dc670069f0e814c95057e8e8377afe666e38e5af
Leinadm <danielmonteru@gmail.com>
2020-01-15 23:54:36 +0100

    Bonus War

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,7 +4,10 @@ class Soldier {
     this.health = health;
     this.strength = strength;
   }
-  attack = () => this.strength;
+  attack() {
+    return this.strength;
+  }
+
   receiveDamage(damage) {
     this.health -= damage;
   } 
@@ -46,4 +49,34 @@ receiveDamage (damage) {
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+
+  vikingAttack() {
+    this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+    if (this.saxonArmy[0].health <= 0){
+      this.saxonArmy.shift();
+      return 'A Saxon has died in combat';
+    } 
+
+  }
+  saxonAttack(){
+    this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength)
+    if(this.vikingArmy[0].health <= 0){
+     this.vikingArmy.shift();
+    }
+    return `Harald has received 25 points of damage`;
+  }
+
+}
