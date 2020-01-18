2fa06038d85bd6d16935b349aabeae07d6ce04d8
arnoldosicilia <arnoldo.sicilia.labarquilla@gmail.com>
2020-01-18 13:51:11 +0100

    Clases creadas falta la guerra

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -14,10 +14,41 @@ class Soldier {
 }
 
 // // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+
+    }
+
+    battleCry = () => 'Odin Owns You All!'
+}
 
 // // Saxon
-// class Saxon {}
+class Saxon extends Soldier {
+
+    receiveDamage(damage) {
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+
+    }
+
+}
 
 // // War
 // class War {}
