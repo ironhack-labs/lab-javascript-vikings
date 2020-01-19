931042a92168e845fa5faa6dd9eb1b8158383497
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-01-19 17:00:18 +0100

    done - pero en war los viking y saxon atack no me pasan jasmine

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -47,4 +47,47 @@ class Saxon extends Soldier {
 }
 
 // War
-class War { }
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking = function (viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon = function (saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  /*Los saxonAttack y vikingAttack no consigo que me pasen los test de jasmine, no se si es que no he usado correctamente los metodos 
+  Math.floor y random (los mire por internet y en la leccion class and inheritance) o si el error es debido a otra cosa*/
+
+  vikingAttack = function () {
+
+    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length - 1);
+
+    let saxonPlayer = this.saxonArmy[randomSaxon];
+    let vikingPlayer = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length - 1)];
+
+    let result = saxonPlayer.receiveDamage(vikingPlayer.attack());
+    if (saxonPlayer.health <= 0) {
+      //quitarlo del array saxonArmy
+      this.saxonArmy.splice(randomSaxon, 1);
+    }
+    return result;
+  }
+  saxonAttack = function () {
+
+    let ramdonViking = Math.floor(Math.random() * this.vikingArmy.length - 1);
+
+    let vikingPlayer = this.vikingArmy[ramdonViking];
+    let saxonPlayer = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length - 1)];
+
+    let result = vikingPlayer.receiveDamage(saxonPlayer.attack());
+    if (vikingPlayer.health <= 0) {
+      //quitarlo del array vikingArmy
+      this.vikingArmy.splice(ramdonViking, 1);
+    }
+    return result;
+  }
+}
