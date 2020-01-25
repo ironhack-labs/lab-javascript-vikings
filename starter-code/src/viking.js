81199e6e707352f490c7a6d17a4b4d1112096ab0
Chelsey Grice <chelseygrice92@gmail.com>
2020-01-25 12:27:50 +0100

    first commit(Soldier,Viking,Saxson)

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,59 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+        }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(theDamage) {
+        theDamage = this.health - theDamage;
+        return theDamage
+    }
+    
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(theDamage){ 
+        theDamage = this.health - theDamage;
+
+        if (this.health > 0) {
+            console.log(`${name} has recieved ${theDamage} points of damage `);
+        }
+        else if (this.health <= 0) {
+            console.log(`${name} has died in combat`);
+        }
+    }
+} 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    attack() {
+        return this.strength
+    }
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if (this.health > 0){
+            console.log(`a saxson has recieved ${theDamage} points of damage `)
+        } else {
+            console.log(`A saxson has died in combat`)
+        }
+    }
+
+}
 
 // War
 class War {}
