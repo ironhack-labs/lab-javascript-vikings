97bbaaa87d80bd88183a0a79a85adf64a0c80a21
Adrian Vide <adrian.vide.paris@gmail.com>
2020-02-21 17:57:38 +0100

    first

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,71 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+    super(health, strength)
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return (this.name + ' has received ' + damage + ' points of damage')
+        } else {
+            return (this.name + ' has died in act of combat')
+        }
+    }
+    battleCry() {
+        return ('Odin Owns You All!');
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+    super(health, strength)
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return ('A Saxon has received ' + damage + ' points of damage')
+        } else {
+            return ('A Saxon has died in combat')
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(newViking) {
+        this.vikingArmy.push(newViking);
+    }
+    addSaxon(newSaxon) {
+        this.saxonArmy.push(newSaxon);
+    }
+    vikingAttack() {
+        let violentViking = Math.floor(Math.random()*(this.vikingArmy.length()+1));
+        let attackedSaxon = Math.floor(Math.random()*(this.saxonArmy.length()+1));
+        receiveDamage(violentViking.strength);
+        attackedSaxon.health -= violentViking.strength;
+
+    }
+}
+
+
+
