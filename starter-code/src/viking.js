d364b0733d4f97238a5ed205c93c1213efb41e50
Unai Gonzalez <unaigoe91@gmail.com>
2020-02-21 21:52:53 +0100

    BCN-FT- Ejercicio Unai Gonzalez, intento subir batalla fin de semana!

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,75 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health,
+            this.strength = strength
+    }
 
-// Viking
-class Viking {}
+     attack() {
+    return this.strength
+}
+
+
+    receiveDamage(damage){
+        this.health -= damage
+
+}
+
+}
+
+class Viking extends Soldier {
+    constructor(health, strength, name) {
+        super(health, strength)
+        this.name = 'Harald'
+        this.health= 300  
+        this.strength = 150
+    }
+
+    
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health >= 0) {
+            return  `${this.name} has received ${damage} points of damage`
+        }else if(damage === 300){
+            return  `${this.name} has died in act of combat`
+
+     }  
+        return receiveDamage(300);
+    }
+        
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
+
+
 
 // Saxon
-class Saxon {}
 
-// War
-class War {}
+class Saxon extends Soldier{
+        
+attack(){
+    return this.strength
+}
+
+receiveDamage(damage) {
+    this.health-=damage
+    
+    if(this.health >= 0){
+
+         return `A Saxon has received ${damage} points of damage`
+    } else{
+        return ` A SAXON has died in combat`
+
+        }
+        return receiveDamage(50);
+    }
+   
+}
+
+const aSoldier = new Soldier()
+const aViking = new Viking ()
+const aSaxon = new Saxon()
+
+// W
+class War { }
