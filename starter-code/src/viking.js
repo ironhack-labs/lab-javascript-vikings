a86905c19d860956336146bd13a17bd77bfd5c61
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-02-24 17:46:11 +0100

    done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,93 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+attack() {
+    return this.strength;
+}
+
+receiveDamage(damage){
+    this.health = this.health - damage;
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+     constructor(name, health, strength){
+        super(health, strength)
+         this.name = name;
+     } 
+
+receiveDamage(damage){
+        this.health = this.health - damage;
+     if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`;
+             } else if (this.health <= 0) { 
+        return `${this.name} has died in act of combat`;
+    }
+}
+
+battleCry(){
+    return `Odin Owns You All!`  
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
 
-// War
-class War {}
+    receiveDamage(damage){
+        this.health = this.health - damage;
+        if (this.health > 0) {
+           return `A Saxon has received ${damage} points of damage`;
+                } else if (this.health <= 0) { 
+           return `A Saxon has died in combat`;
+       }
+    }
+}
+
+// let saxon = new Soldier (health, strength);
+
+//  // Saxon
+// class Saxon extends Soldier{
+//     constructor(health, strength) {
+//         this.health = health
+//         this.strength = strength
+//     }
+//     attack() {
+//         return this.strength;
+//     }
+// }
+
+ 
+
+
+//     receiveDamage(damage){
+//         let soldierHealth = soldier.health-damage;
+//     }
+// }
+
+//         let messageAlive = `${name} has received ${damage} points of damage`
+//         let messageDead = `${name} has died in the act of combat`
+//         if (soldierHealth >= 0) {
+//             return messageAlive;
+//         } else {
+//             return messageDead; 
+//         }
+//     }
+
+//     battleCry(){
+//         let messageOdin = `Odin owns you all`
+//         return messageOdin
+//     }
+//     }
+
+// // Saxon
+// class Saxon {}
+
+// // War
+// class War {}
+
+// (this.health <= 0) {
