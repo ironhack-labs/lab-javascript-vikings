1a3065e9a037dd8cc146307e1ef58aeff103ee7f
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-27 16:29:24 +0100

    define War constructor

--- src/viking.js
+++ src/viking.js
@@ -55,4 +55,18 @@ class Saxon extends Soldier {
 }
 
 // War
-class War{}
+class War{
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking()
+
+  
+  // addSaxon()
+  // vikingAttack()
+  // saxonAttack()
+  // showStatus()
+
+}
