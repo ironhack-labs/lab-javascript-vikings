8a8d0b4c2169ec96164341c63ea3201426b7df32
Elvira Ramírez <elvirarp92@gmail.com>
2020-03-27 16:15:57 +0100

    define Viking and its function receiveDamage

--- src/viking.js
+++ src/viking.js
@@ -4,7 +4,7 @@ class Soldier {
     this.health = health;
     this.strength = strength;
   }
-  
+
   attack() {
     return this.strength;
   }
@@ -15,7 +15,24 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    //     if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
+    // if the Viking dies, it should return "NAME has died in act of combat"
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+
+    return `${this.name} has died in act of combat`;
+  }
+}
 
 // Saxon
 class Saxon {}
