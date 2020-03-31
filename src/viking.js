159e32789e89a9173efb4817afcbc8c00108b26a
William Hermozo <william.hermozo@protonmail.com>
2020-03-31 17:31:45 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,120 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+
+    // why doesn't ternary operator doesn't work here?:
+    // this.health > 0
+    // ? `A Saxon has received ${damage} points of damage`
+    // : `A Saxon has died in combat`;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack() {
+    let viking = this.vikingArmy[
+      Math.floor(Math.random() * this.vikingArmy.length)
+    ];
+    let saxon = this.saxonArmy[
+      Math.floor(Math.random() * this.saxonArmy.length)
+    ];
+
+    saxon.receiveDamage(viking.strength);
+
+    if (saxon.health <= 0) {
+      this.saxonArmy.splice(this.saxonArmy.indexOf(saxon), 1);
+      return 'A Saxon has died in combat';
+    }
+  }
+
+  saxonAttack() {
+    let viking = this.vikingArmy[
+      Math.floor(Math.random() * this.vikingArmy.length)
+    ];
+    let saxon = this.saxonArmy[
+      Math.floor(Math.random() * this.saxonArmy.length)
+    ];
+
+    viking.receiveDamage(saxon.strength);
+
+    viking.health <= 0
+      ? this.vikingArmy.splice(this.vikingArmy.indexOf(viking), 1)
+      : 'A Viking has died in combat';
+
+    return `${viking.name} has received ${saxon.strength} points of damage`;
+  }
+
+  showStatus() {
+    if (!this.saxonArmy.length) {
+      return 'Vikings have won the war of the century!';
+    } else if (!this.vikingArmy.length) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+}
