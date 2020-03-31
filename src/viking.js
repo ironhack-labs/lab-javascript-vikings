aad6769bb05aa5c71cffdae22f925e7648e7960d
Karl Secen <karl.secen@gmail.com>
2020-03-31 19:11:54 +0200

    done except bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) return `${this.name} has received ${damage} points of damage`;
+        return `${this.name} has died in act of combat`
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!';
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) return `A Saxon has received ${damage} points of damage`;
+        return `A Saxon has died in combat`
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking){
+        this.vikingArmy.push(viking);
+
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+        
+    }
+    vikingAttack(){
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length-1)];
+        const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length-1)];
+        
+        /* return randomSaxon.receiveDamage(randomViking.strength);
+        if(randomViking.health < 0) this.saxonArmy() -- remove Saxon from army */
+        
+
+        
+    }
+    saxonAttack(){
+        
+    }
+    showStatus(){
+        
+    }
+}
+
+
