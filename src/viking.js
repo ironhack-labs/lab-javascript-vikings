ed6590ead1dfd19ea7dfb29d196f846f3aff8cfa
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-01 12:10:27 +0200

    added super bonus

--- src/viking.js
+++ src/viking.js
@@ -75,4 +75,17 @@ class War {
         }
         return result;
     }
+    showStatus() { 
+        if (this.saxonArmy.length == 0) {
+            return 'Vikings have won the war of the century!';
+        }
+        else if (this.vikingArmy.length == 0) {
+            return 'Saxons have fought for their lives and survived another day...';
+        }
+        else if (this.vikingArmy.length >= 1 && this.saxonArmy.length >= 1) {
+            return 'Vikings and Saxons are still in the thick of battle.';
+        }
+    }
 }
+
+
