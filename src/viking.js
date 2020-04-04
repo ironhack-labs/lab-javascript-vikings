fabfa7718822485d97a23e9324db8c4a1673595b
Marc De Mena <mdemena@gmail.com>
2020-04-04 17:51:10 +0200

    BONUS- Iteration 4: War Finished

--- src/viking.js
+++ src/viking.js
@@ -14,14 +14,71 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-    constructor(){
-        super();
-        
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage){
+        super.receiveDamage(damage);
+        if (this.health>0) { 
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        return `${this.name} has died in act of combat`;
+    }
+    battleCry(){
+        return 'Odin Owns You All!';
     }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength);
+    }
+    receiveDamage(damage){
+        super.receiveDamage(damage);
+        if (this.health>0) { 
+            return `A Saxon has received ${damage} points of damage`;
+        } 
+        return `A Saxon has died in combat`;
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack(){
+        let message = '';
+        if (this.vikingArmy.length>0 && this.saxonArmy.length>0){
+            let viking = this.vikingArmy[0];
+            let saxon =  this.saxonArmy[0];
+            message = saxon.receiveDamage(viking.strength);
+            this.saxonArmy = this.saxonArmy.filter(s => s.health>0);
+        }
+        return message;
+    }
+    saxonAttack(){
+        let message = '';
+        if (this.vikingArmy.length>0 && this.saxonArmy.length>0){
+            let viking = this.vikingArmy[0];
+            let saxon =  this.saxonArmy[0];
+            message = viking.receiveDamage(saxon.strength);
+            this.vikingArmy = this.vikingArmy.filter(v => v.health>0);
+        }
+        return message;
+    }
+    showStatus(){
+        if (this.saxonArmy.length===0) {return 'Vikings have won the war of the century!';}
+        else if (this.vikingArmy.length===0) {return 'Saxons have fought for their lives and survived another day...';}
+        else {return 'Vikings and Saxons are still in the thick of battle.'}
+    }
+}
