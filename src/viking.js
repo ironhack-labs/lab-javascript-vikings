5f0f8f2874ddb2f5d5bd614549e080ca1a0b3507
irene villanueva <irens.vm@gmail.com>
2020-04-06 08:11:07 +0200

    1sSaving

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,63 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+            
+    }
+}
 
 // Viking
-class Viking {}
+//class Viking extends Soldier{
+  // constructor(name, health, strength){
+  //     super(health,strength);
+  //     this.name = name;
+  // }
+  // receiveDamage(damage){
+  //     this.health -= damage;
+  //     if (this.health > 0) {
+  //         return `${this.name} has received ¢{damage} points of damage`;
+  //     }
+  //      return `${this.name} has died in act of combat.`;
+  //     }
+
+  // }
+  // battleCry() {
+  //     return "Odin Owns You All!";
+  // 
+  // }
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    
+    constructor(health,strength){
+        super(health,strength);
+    }
+        
+
+    receiveDamage(damage) {
+
+        if (this.health > 0){
+
+
+
+        }
+    }
+
+
+
+
+}
 
 // War
 class War {}
