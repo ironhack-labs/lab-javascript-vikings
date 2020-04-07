2edf685ada35c91ec5f1e453039037efbb8df086
Toni <sancheztoni.garcia@gmail.com>
2020-04-07 13:01:44 +0200

    Lab finished. Problems with bonus 1 solved. SuperBonus done

--- src/viking.js
+++ src/viking.js
@@ -13,12 +13,13 @@ class Soldier {
 
 }
 
-// Viking
+// Viking extends soldier and also has its own name.
 class Viking extends Soldier {
     constructor(name, health, strength) {
         super(health, strength);
         this.name = name;
     }
+    //receiveDamage extends from soldier but add two returns according viking health.
     receiveDamage(damage) {
         this.health -= damage;
         if (this.health > 0) {
@@ -26,16 +27,18 @@ class Viking extends Soldier {
         }
         return `${this.name} has died in act of combat`;
     }
+    //Add the battleCry method
     battleCry() {
         return 'Odin Owns You All!'
     }
 }
 
-// Saxon
+// Saxon extends soldier too, but don't have name like viking
 class Saxon extends Soldier {
     constructor(health, strength) {
         super(health, strength);
     }
+    //Here , receiveDamage it's like in the viking method, with only changes the name returned.
     receiveDamage(damage) {
         this.health -= damage
         if (this.health > 0) {
@@ -47,39 +50,61 @@ class Saxon extends Soldier {
 }
 
 class War {
+    // Create two empty array for put the new soldiers
     constructor() {
         this.vikingArmy = [];
         this.saxonArmy = [];
     }
     addViking(viking) {
-        let newViking = new Viking('Harald', 300, 150);
-        this.vikingArmy.push(newViking);
+        this.vikingArmy.push(viking);
     }
     addSaxon(saxon) {
-        let newSaxon = new Saxon(60, 25);
-        this.saxonArmy.push(newSaxon);
+        this.saxonArmy.push(saxon);
     }
     vikingAttack() {
-        let newAttack = this.vikingArmy[this.getRandomViking()].attack();
-        let saxon = this.saxonArmy[this.getRandomSaxon()];
-        saxon.receiveDamage(newAttack);
-        //* If the saxon is dead, we remove from the array.
-        if (saxon.health < 0) {
-            this.saxonArmy.splice(saxon, 1);
-        }
+        // newAttack stores the attack method of a random Viking
+        let newAttack = this.vikingArmy[this.getRandomSoldier('viking')].attack();
+        // saxon stores only a random Saxon
+        let saxon = this.saxonArmy[this.getRandomSoldier('saxon')];
+        // We evaluate if the hit kills the Saxon. If return true, remove this Saxon from the array.
+        if (saxon.health - newAttack <= 0) {
+            this.saxonArmy.splice(this.saxonArmy.indexOf(saxon), 1);
+            return saxon.receiveDamage(newAttack);
+        } // If the hit return false, only return the action.
         return saxon.receiveDamage(newAttack);
     }
     saxonAttack() {
-
+        // Works like vikingAttack but changing the army
+        let newAttack = this.saxonArmy[this.getRandomSoldier('saxon')].attack();
+        let viking = this.vikingArmy[this.getRandomSoldier('viking')];
+        //* If the saxon is dead, we remove from the array.
+        if (viking.health - newAttack <= 0) {
+            this.vikingArmy.splice(this.vikingArmy.indexOf(viking), 1);
+            return viking.receiveDamage(newAttack);
+        } else {
+            return viking.receiveDamage(newAttack);
+        }
     }
     showStatus() {
+        // showStatus evaluates the status of war. Evaluates different scenaries and return a string in consecuence
+        if (!this.saxonArmy.length) {
+            return 'Vikings have won the war of the century!';
+        }
+        if (!this.vikingArmy.length) {
+            return 'Saxons have fought for their lives and survived another day...';
+        }
+        if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1) {
+            return 'Vikings and Saxons are still in the thick of battle.';
+        }
+    }
+    getRandomSoldier(army) {
+        // This method it's used to select a random soldier from army depends of the input valor.
+        if (army == 'viking') {
+            return Math.floor(Math.random() * Math.floor(this.vikingArmy.length));
+        } else {
+            return Math.floor(Math.random() * Math.floor(this.saxonArmy.length));
+        }
 
-    }
-    getRandomViking() {
-        return Math.floor(Math.random() * Math.floor(this.vikingArmy.length));
-    }
-    getRandomSaxon() {
-        return Math.floor(Math.random() * Math.floor(this.saxonArmy.length));
     }
 }
 
