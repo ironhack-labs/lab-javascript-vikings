bbeb8a99b07c9db9067ef8a8aac751046950f466
Aida <aida.loemba@outlook.com>
2020-05-11 14:13:07 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = function () {
+            return strength
+        }
+        this.receiveDamage = function (theDamage) {
+            this.health -= theDamage;
+        }
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super(health, strength)
+        this.name = name;
+        this.attack = function () {
+            return strength
+        }
+        this.receiveDamage = function (theDamage) {
+            this.health -= theDamage;
+            if (this.health > 0) {
+                return `${this.name} has received ${theDamage} points of damage`
+            } else {
+                return `${this.name} has died in act of combat`
+            }
+        }
+        this.battleCry = function () {
+            return "Odin Owns You All!"
+        }
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health, strength) {
+        super(health, strength)
+        this.attack = function () {
+            return strength
+        }
+        this.receiveDamage = function (theDamage) {
+            this.health -= theDamage;
+            if (this.health > 0) {
+                return `A Saxon has received ${theDamage} points of damage`
+            }  else {
+                return "A Saxon has died in combat"
+            }
+        }
+    }
+}
 
 // War
 class War {}
