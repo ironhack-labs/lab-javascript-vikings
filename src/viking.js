94dda073ba3cda3bba1934f445a04c2d73a76394
Aurore CR <aurorenaturerouge@hotmail.fr>
2020-05-11 23:16:52 +0200

    finito

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,73 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+     attack(){
+       return this.strength;
+   } 
+// should be a function
+// should receive 0 arguments
+// should return the strength property of the Soldier
 
-// Viking
-class Viking {}
+receiveDamage (damage){
+ this.health -= damage;
+} 
+}
+
+ class Viking extends Soldier {
+    constructor  (name, health, strength){
+ super(name,health, strength)
+     this.name = name;
+     this.health = health;
+     this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    } 
+    receiveDamage(damage) {
+        if (this.health -= damage) {
+          return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+       } 
+
+    //    should remove the received damage from the health property
+    //    if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
+    //    if the Viking dies, it should return "NAME has died in act of combat"     
+
+       battleCry(){
+           return ("Odin Owns You All!");
+       }  
+ }
+
+//  should remove the received damage from the health property
+//  if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
+//  if the Viking dies, it should return "NAME has died in act of combat"
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    attack(){
+        return this.strength;
+    } 
+
+    receiveDamage(damage){
+    if (this.health -= damage) {
+        return `A Saxon has received ${damage} points of damage`;
+      } else {
+          return `A Saxon has died in combat`;
+      }
+}
+
+
 
 // War
-class War {}
+class War () {
+    this.vikingArmy  =  [] ;
+    this.saxonArmy = [];
+
+}
+
+ 
