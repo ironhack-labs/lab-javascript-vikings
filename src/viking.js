28dd618431e26cb7618aab5ea5acba9b77751fd1
Miguel Valle <miguelbachi5a@gmail.com>
2020-05-13 22:26:23 +0200

    Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -7,7 +7,7 @@ class Soldier {
 
     attack () {
         return this.strength
-    },
+    }
 
     receiveDamage (damage) {
         this.health -= damage
@@ -36,7 +36,17 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        } 
+        return `A Saxon has died in combat`
+    }
+}
+
 
 // War
 class War {}
