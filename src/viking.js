583031b05a265c3930bf3a84f247355b9ccd4bc0
TL Garcia <tomas.bl.garcia@gmail.com>
2020-05-14 12:16:02 +0200

    Finished iterations 2 and 3 passing all tests

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,6 @@
 // Soldier
 class Soldier {
-    constructor (health, strength) {
+    constructor(health, strength) {
         this.health = health
         this.strength = strength
     }
@@ -10,28 +10,45 @@ class Soldier {
     }
 
     receiveDamage(damage) {
-        this.health -= damage 
+        this.health -= damage
     }
 }
 
 // Viking
 class Viking extends Soldier {
-    constructor (name, health, strength) {
-        super (health, strength)
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
 
-        this.name = name  
     }
 
     receiveDamage(damage) {
         this.health -= damage
 
-        health > 0 ? `${this.name} has received ${damage} points of damage` : 
-        `${this.name} has died in act of combat`
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!'
     }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
