c421067e327dcc824babf0e6e096726c4341e5cb
Juan Romero <jrg89@hotmail.es>
2020-05-15 02:54:46 +0200

    Bonus- Iteration 4: War

--- src/viking.js
+++ src/viking.js
@@ -38,4 +38,31 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack(){
+        
+        const saxonRandom = Math.floor(Math.random() * this.saxonArmy.length)
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length)
+        
+        const resultCombat = this.saxonArmy[saxonRandom].receiveDamage(this.vikingArmy[vikingRandom].strength)
+        this.saxonArmy = this.saxonArmy.filter(saxon => saxon.health > 0)
+        return resultCombat        
+    }
+    saxonAttack(){
+        const saxonRandom = Math.floor(Math.random() * this.saxonArmy.length)
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length)
+        
+        const resultCombat = this.vikingArmy[vikingRandom].receiveDamage(this.saxonArmy[saxonRandom].strength)
+        this.vikingArmy = this.vikingArmy.filter(viking => viking.health > 0)
+        return resultCombat  
+
+    }
+}
