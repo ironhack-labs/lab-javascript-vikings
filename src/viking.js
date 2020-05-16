2313a95de2ef4ea656e2047d9bcbc09cfd98d179
martaguirre91 <marta@FONETIC-MSI93.fonetic.local>
2020-05-16 10:39:57 +0200

    optmizacion codigo

--- src/viking.js
+++ src/viking.js
@@ -10,7 +10,6 @@ class Soldier {
     receiveDamage(damage) {
         this.health -= damage;
     }
-
 }
 
 // Viking
@@ -33,7 +32,6 @@ class Viking extends Soldier {
     }
 }
 
-
 // Saxon
 class Saxon extends Soldier {
     receiveDamage(damage) {
@@ -54,36 +52,31 @@ class War {
 
     addViking(v){
         this.vikingArmy.push(v);
-
     }
+
     addSaxon(s){
         this.saxonArmy.push(s);
-
     }
+
     vikingAttack(){
         const v = this.vikingArmy[parseInt(Math.random()*this.vikingArmy.length)];
         const s = this.saxonArmy[parseInt(Math.random()*this.saxonArmy.length)];
-        return s.receiveDamage(v.strength);
+        const dam = s.receiveDamage(v.strength);
+        this.saxonArmy = this.saxonArmy.filter(s => s.health > 0);
+        return dam
     }
     saxonAttack(){
         const v = this.vikingArmy[parseInt(Math.random()*this.vikingArmy.length)];
         const s = this.saxonArmy[parseInt(Math.random()*this.saxonArmy.length)];
-        return v.receiveDamage(s.strength);
+        const dam = v.receiveDamage(s.strength);
+        this.vikingArmy = this.vikingArmy.filet(v => v.health >0);
+        return dam
     }
 
     showStatus(){
-        for (let v in this.vikingArmy){
-            if (this.vikingArmy[v].health <= 0) delete this.vikingArmy[v];
-        }
-        const filteredViking = this.vikingArmy.filter(Boolean);
-        for (let s in this.saxonArmy){
-            if (this.saxonArmy[s].health <= 0) delete this.saxonArmy[s]
-        }
-        const filteredSaxon = this.saxonArmy.filter(Boolean);
-
-        if (this.saxonArmy.length === 0 || filteredSaxon.length === 0)
+        if (this.saxonArmy.length === 0)
             return "Vikings have won the war of the century!"
-        if (this.vikingArmy.length === 0 || filteredViking.length === 0)
+        if (this.vikingArmy.length === 0)
             return "Saxons have fought for their lives and survived another day..."
         if  (this.vikingArmy.length >= 1 && this.saxonArmy.length >= 1)
             return "Vikings and Saxons are still in the thick of battle."
