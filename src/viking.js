01ecda2eafff3524691d03c4497d22daba613aef
Rodrigo Poblacion <rodrigopoblacion@gmail.com>
2020-05-17 20:48:47 +0200

    Until iteration 3 with 1 mistake

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        let isAllive;
+        if (this.health < damage) {
+            isAllive = `${this.name} has died in act of combat`;
+        } else { 
+            isAllive = `${this.name} has received ${damage} points of damage`;
+        }
+        return isAllive;
+    }
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        let isAlliveSaxon;
+        if (this.health < damage) {
+            isAlliveSaxon = "A Saxon has died in combat";
+        } else { 
+            isAlliveSaxon = `A Saxon has received ${damage} points of damage`;
+        }
+        return isAlliveSaxon;
+    }
+}
 
 // War
-class War {}
+class War {
+
+    addViking(arr={}){
+        this.VikingArmy = arr;
+    }
+    addSaxon(){}
+    vikingAttack(){}
+    saxonAttack(opts=[]){}
+    showStatus(){}
+}
