08d547b500cb9e1651a83a0f62405eb57b7840d8
Old <onelifedesigning@gmail.com>
2020-05-17 11:13:00 +0200

    create soldier and viking class extendit from soldier

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,36 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.damage = damage
+        this.health -= this.damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+    receiveDamage(damage) {
+        super.receiveDamage(damage)
+        if (this.health > 0) {
+            return `${this.name} has received ${this.damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+}
 
 // Saxon
 class Saxon {}
