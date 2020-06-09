814a1ed5d60a5fce6be6decf431d08cfbbf1348e
Anna Weingart <annaraewein@gmail.com>
2020-06-09 19:41:27 +0200

    first and final commit

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength; 
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage (damage) {
+        this.health -= damage;   
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    attack() {
+        console.log(`attack`)
+    }
+    battleCry() {
+        return (`Odin Owns You All!`);
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+    if (this.health > 0) {
+        return (`${this.name} has received ${this.damage} points of damage`);
+    }
+    else {
+        return (`${this.name} has died in act of combat`);
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+    attack() {
+        console.log(`attack`);
 
-// War
-class War {}
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    
+
+    if (this.health > 0) {
+        return (`A Saxon has received DAMAGE points of damage`);
+    }
+    else {
+        return (`A Saxon has died in combat`);
+    }
+
+}
+}
