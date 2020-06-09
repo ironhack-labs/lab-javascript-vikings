7f4b1409f33c1128fdf699de1d8dc26b01c53c08
Romulo Galiao <Romulo.Galiao@googlemail.com>
2020-06-09 17:42:07 +0200

    done , no bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){ // insit is an argument. 
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {  // bekommt kein argument
+       return Soldier.strength;
+    }
+    receiveDamage(damage) { // damage is the argument 
+        this.health -= damage; 
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength) { 
+        this.name = name; 
+        super(health,strength); // super: get the health and strength property from the otriginal class
+    }
+    receiveDamage(damage) { // damage is the argument 
+        this.health -= damage; 
+        if (this.health >  0){
+            console.log(`${this.name} has received ${this.damage} points of damage`);
+        } else {
+            console-log(`${this.name} has died in act of combat.`);
+        }
+    } 
+    battleCry() {
+        console.log("Odin Owns You All!"); 
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {   // extends,inheritance => Soldier
+    constructor(health, strength){
+        super(health,strength) // super` refers to the constructor of the parent (Animal)
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0){
+        console.log(`A Saxon has received ${this.damage} points of damage`)
+    } else {
+        console.log(`A Saxon has died in combat`)
+    }
+}
+}
+
 
 // War
-class War {}
+class War {
+    constructor();
+}
