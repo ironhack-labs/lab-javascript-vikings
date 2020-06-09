5c5a2a8978ba677871eb44be60c89b4857ad9a8e
carlosziegler <carlos.ziegler@gmail.com>
2020-06-09 17:32:21 +0200

    bonus

--- src/viking.js
+++ src/viking.js
@@ -86,7 +86,15 @@ class War {
         return result
     }
     showStatus() {
-
+        if (this.saxonArmy.length === 0) {
+            return "Vikings have won the war of the century!"
+        }
+        if (this.vikingArmy.length === 0) {
+            return "Saxons have fought for their lives and survived another day..."
+        }
+        if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1) {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
     }
 }
 const game = new War()
