06e5e392bf87b17aed21389e64e1e4521be4b1ac
Arianna Valle <ariannamarbella@gmail.com>
2020-06-15 16:58:36 -0400

    Complete iteration 4 and pass all tests

--- src/viking.js
+++ src/viking.js
@@ -43,4 +43,39 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack(){
+        let randomIndexSaxon = Math.floor(Math.random()*this.saxonArmy.length) 
+        let randomIndexViking = Math.floor(Math.random()*this.vikingArmy.length)
+        let saxonDamage = this.saxonArmy[randomIndexSaxon].receiveDamage(this.vikingArmy[randomIndexViking].strength)
+        if (this.saxonArmy[randomIndexSaxon].health <= 0) this.saxonArmy.splice(randomIndexSaxon,1)
+        return saxonDamage
+    }
+
+    saxonAttack(){
+        let randomIndexSaxon = Math.floor(Math.random()*this.saxonArmy.length) 
+        let randomIndexViking = Math.floor(Math.random()*this.vikingArmy.length)
+        let vikingDamage = this.vikingArmy[randomIndexViking].receiveDamage(this.saxonArmy[randomIndexSaxon].strength)
+        if (this.vikingArmy[randomIndexViking].health <= 0) this.vikingArmy.splice(randomIndexViking,1)
+        return vikingDamage
+    }
+
+    showStatus(){
+        if (this.saxonArmy.length === 0) return `Vikings have won the war of the century!`
+        if (this.vikingArmy.length === 0) return `Saxons have fought for their lives and survived another day...`
+        if (this.vikingArmy.length > 0  && this.saxonArmy.length > 0) return `Vikings and Saxons are still in the thick of battle.`
+    }
+}
