641a7524ca4b7734634ffe2a0815273d5ef649d9
antguts <antgut035@gmail.com>
2020-06-15 13:04:44 -0400

    Finished first 3 classes. War is left

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health=health
+        this.strength=strength
+    }
+    attack(){return this.strength}
+    receiveDamage(damage){this.health-=damage}
+}//end soldier
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health,strength){
+        super(health,strength)
+        this.name=name
+    }
+    receiveDamage(damage){
+        this.health-=damage
+        return (this.health>0)? `${this.name} has received ${damage} points of damage`:`${this.name} has died in act of combat`
+    }//end receiveDamage method
+    battleCry(){
+        return "Odin Owns You All!"
+    }//end battleCry method
+
+}//end viking
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health-=damage
+        return (this.health>0)? `A Saxon has received ${damage} points of damage`:`A Saxon has died in combat`
+    }
+}//end Saxon class
+
 
 // War
-class War {}
+class War {}//end war class
