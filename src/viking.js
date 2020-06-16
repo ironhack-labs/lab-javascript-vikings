aa908d24324b7f9f9768ec9c83c789d1c3f50ffc
killshot13 <dmreh@outlook.com>
2020-06-16 00:04:59 -0500

    edits made to viking.js and viking.spec.js -- iterations 1, 2, 3 c/w

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,45 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack = () => {
+        return this.strength;
+    }
+    receiveDamage = (theDamage) => {
+        this.health -= theDamage;
+    }
 // Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage = (theDamage) => {
+        this.health -= theDamage;
+        if (this.health > 0) {
+            return `${this.name} has received ${this.damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry = () => {
+         this.battlecry = 'Odin Owes You All'
+    }
+    // Saxon
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage = (theDamage) => {
+        this.health -= theDamage;
+        if this.health > 0 {
+            return `A Saxon has received ${this.damage} points of damage`
+        } else {
+            return 'A Saxon has died in combat'
+        }
+    }
