88b84aec39f4f3f04e6f1399935ea3eb0f4ab314
RFeldman84 <rachel.feldman84@gmail.com>
2020-06-22 07:07:46 -0400

    war methods add and attack

--- src/viking.js
+++ src/viking.js
@@ -15,8 +15,12 @@ class Soldier {
 
   }
 
-
 }
+
+// let soldier;
+//   const strength = 150;
+//   const health = 300;
+// soldier = new Soldier(health, strength)
 
 
 //Viking
@@ -27,10 +31,16 @@ class Viking extends Soldier {
 
   }
 
+
+ // attack() {
+    //         return this.strength;
+    //      }
+    //not needed will inherit from parent
+
   receiveDamage(theDamage){
-    this.health -= theDamage 
-    if(this.health > 0) return `${this.name} has received ${theDamage} points of damage`
-    if(this.health <=0) return `${this.name} has died in act of combat`
+    super.receiveDamage(theDamage)
+    if(this.health > 0) return `${this.name} has received ${theDamage} points of damage`;
+    else return `${this.name} has died in act of combat`;
   }
 
   battleCry(){
@@ -39,25 +49,94 @@ class Viking extends Soldier {
     
 }
 
+// let viking;
+// const strength = 150;
+// const health = 300;
+// const name = 'Harald';
+// viking = new Viking(name, health, strength);
 
 
 
 // Saxon
 class Saxon extends Soldier{
-  constructor(health, strength){
-    super(health, strength)
+ 
+  // since redefine it we must call super in order to inherit from parents 
+  receiveDamage(theDamage){
+    super.receiveDamage(theDamage)
+    if(this.health > 0) return `A Saxon has received ${theDamage} points of damage`;
+    else return "A Saxon has died in combat"
   }
 
-  receiveDamage(theDamage){
-    this.health -= theDamage 
-    if(this.health > 0) return `A Saxon has received ${theDamage} points of damage`
-    if(this.health <=0) return "A Saxon has died in act of combat"
+}
+
+// let saxon;
+// const health = 60;
+// const strength = 25;
+
+// saxon = new Saxon(health, strength);
+
+
+// War
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
   }
+
+  addViking(aVikingObject){
+    this.vikingArmy.push(aVikingObject)
+  }
+
+  addSaxon(aSaxonObject){
+    this.saxonArmy.push(aSaxonObject)
+
+  }
+
+  vikingAttack(){
+    let saxonIndex = Math.floor(Math.random()*this.saxonArmy.length)
+    let randomSaxon = this.saxonArmy[saxonIndex]
+    let vikingIndex = Math.floor(Math.random()*this.vikingArmy.length)
+    let randomViking = this.vikingArmy[vikingIndex]
+
+    let saxonDamage = randomSaxon.receiveDamage(randomViking.attack())
+
+    if(randomSaxon.health <= 0)
+    this.saxonArmy.splice(saxonIndex, 1)
+    
+    return saxonDamage
+   
+    
+  }
+
+  saxonAttack(){
+    let saxonIndex = Math.floor(Math.random()*this.saxonArmy.length)
+    let randomSaxon = this.saxonArmy[saxonIndex]
+    let vikingIndex = Math.floor(Math.random()*this.vikingArmy.length)
+    let randomViking = this.vikingArmy[vikingIndex]
+
+    let vikingDamage = randomViking.receiveDamage(randomSaxon.attack())
+
+    if(randomViking.health <= 0)
+    this.vikingArmy.splice(vikingIndex, 1)
+    
+    return vikingDamage
+   
+    
+  }
+
+showStatus(){
 
 }
 
 
 
+  
+  
+}
 
-// War
-class War {}
+
+
+
+
+
+
