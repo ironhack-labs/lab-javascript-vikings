64ebd3e8679a1d7d0d500ba09de7b4e6ef3f698b
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-06-25 17:48:00 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,62 @@
-// Soldier
-class Soldier {}
+// Soldier iteration 1
+class Soldier {
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+    attack (){
+        return this.strength
+    }
+    receiveDamage (damage){
+        this.health += - damage
+    }
+}
 
-// Viking
-class Viking {}
+// Viking iteration 2
+class Viking {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
 
-// Saxon
-class Saxon {}
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+}
 
-// War
-class War {}
+// Saxon iteration 3
+class Saxon {
+
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+
+}
+
+// War iteration 4
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking)
+
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+}
