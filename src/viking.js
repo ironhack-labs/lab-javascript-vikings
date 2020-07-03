41d54be220e1825647b16ab5e3b46f252d26afca
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-03 16:46:45 +0200

    finish with bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,113 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    (this.health = health), (this.strength = strength);
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= 50;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength), (this.name = name);
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+    if (this.health === 0) {
+      return `${this.name} has died in act of combat`;
+    }
+    return result;
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(health, strength) {
+    super(health, strength)
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health-=damage
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    }
+    if (this.health === 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy=[]
+    this.saxonArmy=[]
+  }
+  
+
+  addViking(Viking){
+    this.vikingArmy.push(Viking)
+  }
+  addSaxon(Saxon){
+    this.saxonArmy.push(Saxon)
+  }
+
+  vikingAttack(){
+    let result = '';
+    this.vikingArmy.forEach(
+      viking => this.saxonArmy.forEach(
+        (saxon, index) => 
+          {
+            saxon.receiveDamage(viking.strength)
+            if(saxon.health <= 0) {
+              this.saxonArmy.splice(index,1)
+              result ='A Saxon has died in combat'
+            }
+          }
+      )
+    )
+    return result
+  }
+  saxonAttack(){
+    let result = ``;
+    this.saxonArmy.forEach(
+      saxon => this.vikingArmy.forEach(
+        (viking, index) => 
+          {
+            viking.receiveDamage(saxon.strength)
+            if(viking.health <= 0) {
+              result = `A viking has gone to Valhalla`
+              this.vikingArmy.splice(index,1)
+            }
+            result = `${viking.name} has received ${saxon.strength} points of damage`
+          }
+      )
+    )
+    return result
+  }
+  showStatus(){
+    if(this.saxonArmy.length===0){
+      return 'Vikings have won the war of the century!'
+    } else if( this.vikingArmy.length===0){
+      return 'Saxons have fought for their lives and survived another day...'
+    } else {
+      return 'Vikings and Saxons are still in the thick of battle.'
+    }
+
+  }
+}
