61bbb2a470ca4479ed183f9ff622525cdcb353d9
carlos-82 <cdelafuenteh@gmail.com>
2020-07-04 10:23:41 +0200

    Vikings practice done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+    battleCry() {}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name
+    }
 
-// Saxon
-class Saxon {}
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+        return `${this.name} has died in act of combat`
+    }
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+}
+
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        return 'A Saxon has died in combat'
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxon.Army = []
+    }
+}
