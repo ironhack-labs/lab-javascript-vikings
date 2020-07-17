8190bd8b2c9ddca4816139710475be517212fad8
Lisette Venema <lisettevenema@hotmail.com>
2020-07-17 10:15:09 +0200

    done without bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,44 @@
-// Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+      this.health = health
+      this.strength = strength
+}
+attack() {
+  return this.strength
+}
+receiveDamage(damage) {
+  this.health -= damage
+}
+}
 
-// Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strenght) {
+    super (health, strenght)
+      this.name = name
+  }
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) { 
+        return `${this.name} received ${damage} points of damage` 
+    } else { 
+        return `${this.name} has died in act of combat`
+    }
+  }
+batteCry() {
+    return "Odin Owns You All"
+}
+  }
 
-// Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+    super.receiveDamage(damage); 
+    if (this.health > 0)  {
+        return `${this.name} A Saxon has received ${damage} points of damage}`
+    } else {
+        return `A Saxon has died in combat`
+    }
+}
+    }
 
-// War
-class War {}
+class War {
+}
