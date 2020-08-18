4f6a2ab265f128ac4566d0c6bf18cbf2f5dfdbd0
macman <hayrapetyan.hamlet@gmail.com>
2020-08-18 19:29:20 +0200

    All Iterations done. Part vikingAttack method done with classmates

--- src/viking.js
+++ src/viking.js
@@ -51,5 +51,47 @@ class Saxon extends Soldier {
 
 // War
 class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(Viking) {
+        this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack() {
+        let randViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let randSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        let saxSoldier = this.saxonArmy[randSaxon];
+        let vikSoldier = this.vikingArmy[randViking];
+        let receivedDamage = saxSoldier.receiveDamage(vikSoldier.strength);
+        if (saxSoldier.health <= 0){
+            this.saxonArmy.splice(randSaxon, 1);
+        }
+        return receivedDamage
+    };
 
+    saxonAttack() {
+        let randViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let randSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        let saxSoldier = this.saxonArmy[randSaxon];
+        let vikSoldier = this.vikingArmy[randViking];
+        let receivedDamage = vikSoldier.receiveDamage(saxSoldier.strength);
+        if (vikSoldier.health <= 0){
+            this.vikingArmy.splice(randViking, 1);
+        }
+        return receivedDamage
+    };
+
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+            return `Vikings have won the war of the century!`;
+        } 
+        if (this.vikingArmy.length === 0) {
+            return `Saxons have fought for their lives and survived another day...`;
+        }
+        return `Vikings and Saxons are still in the thick of battle.`
+    } 
 }
