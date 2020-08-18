1213b370d91223c3fa1c17dad26c933339ac7b22
macman <hayrapetyan.hamlet@gmail.com>
2020-08-18 18:21:04 +0200

    Iteration 1, 2, 3(now 3 finish)

--- src/viking.js
+++ src/viking.js
@@ -32,12 +32,21 @@ class Viking extends Soldier {
      battleCry() {
         return "Odin Owns You All!"
      }
-     
 }
 
 // Saxon
-class Saxon {
-
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+    receiveDamage (damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            return `A Saxon has died in combat`
+        } if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+     }
 }
 
 // War
