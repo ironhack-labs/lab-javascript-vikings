e727bb916126cae232cb7a6f1d25391f1cc12004
logansbacca <61002844+logansbacca@users.noreply.github.com>
2020-08-20 02:21:42 +0200

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constuctor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+
+    attack() {
+
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health-damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constuctor(name, health, strength) {
+        super(health,strength)
+        this.name = name
+    }
+
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage)
+        if (health > 0) {
+            return this.name + (" has recieved " + damage + " points of damage " )
+        }
+        else return this.name + (" has died in combat ")
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage)
+        if (health > 0) {
+            return  ("A Saxon has recieved " + damage + " points of damage " )
+        }
+        else return (" A Saxon has died in combat")
+    }
+
+
+
+
+}
+
 
 // War
 class War {}
