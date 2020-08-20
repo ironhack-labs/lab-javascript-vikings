4a4efb7785c3de9faafadc51a189d70cb6e33fd9
“Lorie <lorie.rocher@gmail.com>
2020-08-20 10:48:49 +0200

    new save

--- src/viking.js
+++ src/viking.js
@@ -35,7 +35,63 @@ class Viking extends Soldier {
 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            if (this.health <= 0) {
+                return "A Saxon has died in combat"
+            }
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking);
+    }
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon);
+    }
+
+    vikingAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+        let result = randomSaxon.receiveDamage(randomViking.strength);
+
+        if (randomSaxon.health <= 0) {
+            this.saxonArmy.splice((this.saxonArmy.indexOf(randomSaxon)), 1)
+            return result
+        }
+    }
+
+    saxonAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+        let result = randomViking.receiveDamage(randomSaxon.strength);
+
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice((this.vikingArmy.indexOf(randomViking)), 1)
+
+        }
+        return result;
+
+    }
+
+    showStatus() {}
+}
