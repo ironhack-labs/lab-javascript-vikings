dd8dfbe209a238f90a5b6a93cfe5516b1e4b2f54
carolinacervelaf <carolinacervelaf@gmail.com>
2020-08-21 19:11:34 -0300

    Done, but I didin't make all the bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
+
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage (theDamage) {
+        if (theDamage < this.health) {
+            this.health -= theDamage;
+        }
+    }
+
+}
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier { 
+    constructor(name, health, strength) {
+        super (health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(theDamage){
+       if (theDamage < this.health){
+           this.health -= theDamage
+           return `${this.name} has received ${theDamage} points of damage`
+       } else {
+           return `${this.name} has died in act of combat`
+       }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    
+receiveDamage(theDamage){
+    if (theDamage < this.health){
+        this.health -= theDamage
+        return `A Saxon has received ${theDamage} points of damage`
+    } else {
+        return "A Saxon has died in combat"
+    }
+}
+ }
+
+
 
 // War
-class War {}
+class War {
+     constructor() {
+         this.vikingArmy = [];
+         this.saxonArmy = [];
+     }
+
+
+    addViking(aViking){
+
+
+    }
+
+    addSaxon(aSaxon){
+
+    }
+
+    vikingAttack(){
+
+    }
+
+    saxonAttack(){
+
+    }
+ }
