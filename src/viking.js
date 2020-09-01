13ddcd869dc43cccb1513ac3ce5411d19fad1e8e
Lcdevi <caillauxlaura@gmail.com>
2020-09-01 16:13:07 +0200

    iteration 3 : done

--- src/viking.js
+++ src/viking.js
@@ -30,12 +30,21 @@ class Viking extends Soldier {
     }
 
     battleCry() {
-        return `Odin Owns You All!`;
-    }
+        return 'Odin Owns You All!';
+    }    
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage (damage) {
+        this.health -= damage;
+        if(this.health > 0) {
+             return `A Saxon has received ${damage} points of damage`
+         } else {
+             return `A Saxon has died in combat`
+         }
+    }
+}
 
 // War
 class War {}
