2037b9eaf3bfcc25dce74d54ada7d2998436cdf3
laurent vuillaume <figura.graphik@gmail.com>
2020-09-01 16:42:38 +0200

    add class Viking

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,30 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+
+        this.name = name;
+    }
+    
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(dmg) {
+        this.health -= dmg;
+        if(this.health <= 0){
+            return `${this.name} has died in act of combat`
+        } else {
+            return `${this.name} has received ${dmg} points of damage`;
+        }
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!';
+    }
+}
 
 // Saxon
 class Saxon {}
