b12b73f02b2a1c4af7cdd0bd6786482d7a882e39
sechs <philippe.buteau@skema.edu>
2020-09-01 15:58:29 +0200

    iteration 2 completed

--- src/viking.js
+++ src/viking.js
@@ -16,7 +16,26 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+     constructor(name, health, strength) {
+     super(health, strength);
+     this.name = name;
+    }
+
+    receiveDamage(dmg){
+        this.health -= dmg;
+        if (this.health > 0) {
+            return `${this.name} has received ${dmg} points of damage`;
+         } else {
+            return `${this.name} has died in act of combat`;
+            }
+    }
+    
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
 class Saxon {}
