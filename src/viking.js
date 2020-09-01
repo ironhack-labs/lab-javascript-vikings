bf428b22ed1ec45dfde8b5fd6b504a8099b6a02f
sechs <philippe.buteau@skema.edu>
2020-09-01 16:04:38 +0200

    Iteration 3 completed

--- src/viking.js
+++ src/viking.js
@@ -1,44 +1,51 @@
 // Soldier
 class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-    constructor(health, strength) {
-        this.health = health;
-        this.strength = strength;
-    }
+  attack() {
+    return this.strength;
+  }
 
-    attack() {
-        return this.strength;
-    }
-
-    receiveDamage(dmg){
-        this.health -= dmg;
-    }
+  receiveDamage(dmg) {
+    this.health -= dmg;
+  }
 }
 
 // Viking
 class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
 
-     constructor(name, health, strength) {
-     super(health, strength);
-     this.name = name;
+  receiveDamage(dmg) {
+    this.health -= dmg;
+    if (this.health > 0) {
+      return `${this.name} has received ${dmg} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
     }
+  }
 
-    receiveDamage(dmg){
-        this.health -= dmg;
-        if (this.health > 0) {
-            return `${this.name} has received ${dmg} points of damage`;
-         } else {
-            return `${this.name} has died in act of combat`;
-            }
-    }
-    
-    battleCry(){
-        return "Odin Owns You All!";
-    }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(dmg) {
+    this.health -= dmg;
+    if (this.health > 0) {
+      return `A Saxon has received ${dmg} points of damage`;
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
 class War {}
