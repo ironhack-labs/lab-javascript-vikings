209c3170899e641888e7cd214430f18347c70071
Lcdevi <caillauxlaura@gmail.com>
2020-09-01 17:03:57 +0200

    iteration 4 - bonus : done

--- src/viking.js
+++ src/viking.js
@@ -47,4 +47,39 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+    
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+        let i = Math.floor(Math.random()*this.vikingArmy.length);
+        let j = Math.floor(Math.random()*this.saxonArmy.length);
+        
+        const attack = this.saxonArmy[j].receiveDamage(this.vikingArmy[i].strength);
+
+        if (this.saxonArmy[j].health <= 0) {
+            this.saxonArmy.splice(j, 1);
+        }
+        return attack;
+    }
+
+    saxonAttack() {
+        let i = Math.floor(Math.random()*this.vikingArmy.length);
+        let j = Math.floor(Math.random()*this.saxonArmy.length);
+
+        const attack = this.vikingArmy[i].receiveDamage(this.saxonArmy[j].strength);
+
+        if (this.vikingArmy[i].health <= 0) {
+            this.vikingArmy.splice(i, 1);
+        }
+        return attack;
+    }
+}
