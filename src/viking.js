fad8ecff1c2a61f7974183e7000e3456c17bd350
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-09-01 14:12:17 -0400

    update

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,37 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack = () => {
+    return this.strength;
+  };
+  receiveDamage = (damage) => {
+    this.health -= damage;
+  };
+}
 
 // Viking
-class Viking {}
+class Viking {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  attack = () => {
+    return this.strength;
+  };
+  receiveDamage = (damage) => {
+    this.health -= damage;
+    switch (this.health > 0) {
+      case true:
+        return `${this.name} has received ${damage} points of damage`;
+      default:
+        return `${this.name} has died in act of combat`;
+    }
+  };
+}
 
 // Saxon
 class Saxon {}
