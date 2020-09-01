88ca3eb51c80310ea0954f4da41b1a6e3dcec46b
salim ameur <salim.ameur@gmail.com>
2020-09-01 16:44:48 +0200

    iteration 2

--- src/viking.js
+++ src/viking.js
@@ -13,10 +13,32 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super (health, strength);
+        this.name = name;
+    }
+    receiveDamage(dmg) {
+        this.health -= dmg;
+        if(this.health <= 0){
+            return `${this.name} has died in act of combat`
+        } else {
+            return `${this.name} has received ${dmg} points of damage`;
+        }
+    }
+   
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+   
+}
+
+
 
 // Saxon
-class Saxon {}
+class Saxon {
+    
+}
 
 // War
 class War {}
