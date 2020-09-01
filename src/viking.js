e9f419f5d75d9194f7eafe04988233e64a3c6782
laurent vuillaume <figura.graphik@gmail.com>
2020-09-01 21:22:10 +0200

    add class War done

--- src/viking.js
+++ src/viking.js
@@ -61,4 +61,69 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(vik) {
+        this.vikingArmy.push(vik);
+    }
+
+    addSaxon(sax) {
+        this.saxonArmy.push(sax);
+    }
+
+    vikingAttack() {
+        console.log('------vikiking attack------');
+        if(this.vikingArmy.length > 0) {
+            for(var i = 0; i < this.vikingArmy.length; i++){
+                let dmg = this.vikingArmy[i].attack();
+                let recDmg = this.saxonArmy[i].receiveDamage(dmg);
+       
+                if(this.saxonArmy[i].health <= 0) {
+                    this.saxonArmy.shift(i);
+                    return recDmg;
+                } else {
+                    return recDmg;
+                }
+
+            }  
+        } else {
+            return 0;
+        }
+    }
+
+    saxonAttack() {
+        if(this.saxonArmy.length > 0) {
+            for(var j = 0; j < this.saxonArmy.length; j++) {
+                let dmg = this.saxonArmy[j].attack();
+
+                let recDmg = this.vikingArmy[j].receiveDamage(dmg);
+
+                if(this.vikingArmy[j].health <= 0) {
+                    this.vikingArmy.shift(j);
+                    return recDmg;
+                } 
+                return recDmg;
+            }  
+        } else {
+            return 0;
+        }
+    }
+
+    showStatus() {
+        if(this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+            return "Vikings and Saxons are still in the thick of battle.";
+        } else {
+            if(this.saxonArmy.length == 0 && this.vikingArmy.length > 0) {
+                return "Vikings have won the war of the century!";
+            } else {
+                return "Saxons have fought for their lives and survived another day...";
+            }
+        }
+
+    }
+}
+
