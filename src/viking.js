45c52f0e8e219d7d8784144870501f4d73de080b
laurent vuillaume <figura.graphik@gmail.com>
2020-09-01 15:51:20 +0200

    add class soldier done

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(dmg) {
+        this.health = this.health - dmg;
+    }
+}
 
 // Viking
 class Viking {}
