ea7051116736e23ac13ebec4a52c55f10bf5a025
Rico <rasim.rico@gmail.com>
2020-09-01 18:08:03 +0200

    With Bonus

--- src/viking.js
+++ src/viking.js
@@ -99,9 +99,34 @@ class War {
     //return result of recieve damage
   }
 
+   //*BONUS*/ 
+   generalAttack(attackingArmy, receivingArmy){
+    // create random number;
+    let receivers = `${receivingArmy.toLowerCase()}Army`
+    let attackers = `${attackingArmy.toLowerCase()}Army`
+    let randomReceiverIndex = Math.floor(Math.random() * this[receivers].length);
+    let randomAttackerIndex = Math.floor(Math.random() * this[attackers].length);
+    //pick a random saxon;
+    let randomReceiver = this[receivers][randomReceiverIndex];
+    //pick random viking;
+    let randomAttacker = this[attackers][randomAttackerIndex];
+    //saxon recieve damage from viking
+    let resultOfBattle = randomReceiver.receiveDamage(randomAttacker.strength);
+    //condition remove saxon
+    if (randomReceiver.health <= 0) {
+    this[receivers].splice(randomReceiverIndex, 1);
+    return resultOfBattle;
+    }
+    return resultOfBattle;
+}
+   //*BONUS*/
+
+
   showStatus() {
     if (this.saxonArmy.length === 0) return "Vikings have won the war of the century!";
     if (this.vikingArmy.length === 0) return "Saxons have fought for their lives and survived another day...";
     return "Vikings and Saxons are still in the thick of battle.";
   }
 }
+
+
