1632cb569bcb0b852a6a171b07978753a081e101
Bouziane <bey.bouziane@live.fr>
2020-09-01 19:06:34 +0200

    attacks done

--- src/viking.js
+++ src/viking.js
@@ -16,7 +16,7 @@ class Soldier {
 class Viking extends Soldier {
     constructor(name, health, strength){
         super(health, strength);
-        this.name = name;
+        this.name = "Harald";
     }
 
     receiveDamage(damages){
@@ -31,13 +31,61 @@ class Viking extends Soldier {
     battleCry(){
         return "Odin Owns You All!";
     }
-    
-
-
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+     receiveDamage(damages){
+        super.receiveDamage(damages);
+        if (this.health ===  0){
+        return`A Saxon has died in combat`
+        } else {
+        return `A Saxon has received ${damages} points of damage`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+    
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    getRandomFighters(){
+        return {
+            viking : this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)],
+            saxon : this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)]
+        }
+    }
+   
+    vikingAttack(){
+        const fighter = this.getRandomFighters();
+        const phrase = "A Saxon has died in combat"
+        fighter.saxon.receiveDamage(fighter.viking.strength);
+        if (fighter.saxon.health <= 0){
+            this.saxonArmy.splice(this.saxonArmy.indexOf(fighter.saxon), 1);
+            return phrase
+        }
+        
+    }
+
+    saxonAttack(){
+        const fighter = this.getRandomFighters();
+        const phrase = `${fighter.viking.name} has received ${fighter.saxon.strength} points of damage`;
+        fighter.viking.receiveDamage(fighter.saxon.strength);
+        if (fighter.viking.health <= 0){
+            console.log(phrase);
+            this.vikingArmy.splice(this.vikingArmy.indexOf(fighter.viking), 1);
+        } else {
+            console.log(phrase);
+            return phrase
+        }
+    }
+}
