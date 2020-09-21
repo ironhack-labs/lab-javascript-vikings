a95369bd85929fce1d6a5f1dd3935bce825b1587
Vykintas Petrikas <vykintas.petrikas@gmail.com>
2020-09-22 00:12:38 +0300

    Sorry its not finished, started very late and right now I am very very tired. On removing the dead saxons/vikings it jasmine turns green only in this instance, when the health is above 0 if it 0 it says cant get health of undefined, not sure how to combat that. Ill finish it up! Sorry that its halfway

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,84 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(dmg){
+        this.health -= dmg
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+    receiveDamage(dmg){
+        this.health -= dmg
+        if (this.health > 0) {
+            return `${this.name} has received ${dmg} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(dmg){
+        this.health -= dmg
+        if (this.health > 0) {
+            return `A Saxon has received ${dmg} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack() {
+        let rng = Math.floor(Math.random())
+        let rndSaxon = this.saxonArmy[rng]
+        let rndViking = this.vikingArmy[rng].strength
+        if (rndSaxon.health > 0) {
+            this.saxonArmy.splice(rng,1) 
+        }
+        return rndSaxon.receiveDamage(rndViking)
+
+        // return 
+    }
+    saxonAttack() {
+        let rng = Math.floor(Math.random())
+        let rndViking = this.vikingArmy[rng]
+        let rndSaxon = this.saxonArmy[rng].strength
+        if (rndViking.health < 0) {
+            this.vikingArmy.splice(rng, 1)
+        }
+        return rndViking.receiveDamage(rndSaxon)
+    }
+    showStatus() {
+        
+    }
+}
+
