73854558e4dfbfb4aabd9077a9e7336d25827582
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-10-02 19:41:44 +0200

    Trying that bonuses...

--- src/viking.js
+++ src/viking.js
@@ -57,4 +57,18 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {}
+  saxonAttack() {}
+  showStatus() {}
+}
