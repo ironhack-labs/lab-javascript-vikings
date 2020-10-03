7c25744c2e17fae50fee009d4a1ae12f6f21a8d5
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-03 17:19:45 +0200

    [0209] Andreu Mtez - js vikings complete all including bonus v2

--- src/viking.js
+++ src/viking.js
@@ -65,7 +65,9 @@ class War {
 
     vikingAttack () {
         let saxonRandom = Math.floor(Math.random() * (this.saxonArmy.length));
+        console.log("Viking attack -> saxonRandom: " + saxonRandom);
         let vikingRandom = Math.floor(Math.random() * (this.vikingArmy.length));
+        console.log("Viking attack -> vikingRandom: " + vikingRandom);
         let vikingAttackRes = this.saxonArmy[saxonRandom].receiveDamage(this.vikingArmy[vikingRandom].strength);
         if (vikingAttackRes === "A Saxon has died in combat") {
             this.saxonArmy.splice(saxonRandom,1);
@@ -75,7 +77,9 @@ class War {
 
     saxonAttack () {
         let saxonRandom = Math.floor(Math.random() * (this.saxonArmy.length));
+        console.log("Saxon attack -> saxonRandom: " + saxonRandom);
         let vikingRandom = Math.floor(Math.random() * (this.vikingArmy.length));
+        console.log("Saxon attack -> vikingRandom: " + vikingRandom);
         let saxonAttackRes = this.vikingArmy[vikingRandom].receiveDamage(this.saxonArmy[saxonRandom].strength);
         if (saxonAttackRes === `${this.vikingArmy[vikingRandom].name} has died in act of combat`) {
             this.vikingArmy.splice(vikingRandom,1);
@@ -93,3 +97,49 @@ class War {
         }
     }
 }
+
+let vikingOne = new Viking('Odin', 10, 10);
+let saxonOne = new Saxon(5, 5);
+let vikingTwo = new Viking('Niamh', 8, 8);
+let saxonTwo = new Saxon(4, 4);
+let vikingThree = new Viking('Vicky', 6, 6);
+let saxonThree = new Saxon(3, 3);
+let war = new War();
+war.addViking(vikingOne);
+war.addSaxon(saxonOne);
+war.addViking(vikingTwo);
+war.addSaxon(saxonTwo);
+war.addViking(vikingThree);
+war.addSaxon(saxonThree);
+console.log("The war starts: -------------");
+console.log(war);
+
+let index = 0;
+ while (war.vikingArmy.length > 0 && war.saxonArmy.length > 0) {
+    if (index % 2 === 1) {
+        war.vikingAttack();
+        console.log(war);
+    } else {
+        war.saxonAttack();
+        console.log(war);
+    }
+    console.log(`${index} pass: ` + war);
+    index++;
+ }
+
+//console.log('Army Viking is: ' + war.vikingArmy[0].name);
+
+// console.log(
+//   'Viking One is: ' + vikingOne.name,
+//   vikingOne.strength,
+//   vikingOne.health
+// );
+// console.log('Saxon One is: ' + saxonOne.strength, saxonOne.health);
+
+//console.log("Viking Two is: " + vikingTwo.name, vikingTwo.strength, vikingTwo.health);
+//console.log("Saxon Two is: " + saxonTwo.strength, saxonTwo.health );
+//console.log("Viking One is: " + vikingThree.name, vikingThree.strength, vikingThree.health);
+//console.log("Saxon One is: " + saxonThree.strength, saxonThree.health );
+
+console.log('The message is: ' + war.showStatus());
+
