33882ac99413ea01196ca80d06eae1dc0104796e
Zhenni Qian <zhenni.qian913@gmail.com>
2020-10-03 23:42:15 +0200

    all bonus done

--- src/viking.js
+++ src/viking.js
@@ -57,12 +57,58 @@ class War {
     }
 
     vikingAttack() {
-        const ramdomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
         const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
-        const result = randomSaxon.receiveDamage(ramdomViking.strength);
+        const result = randomSaxon.receiveDamage(randomViking.strength);
         if (randomSaxon.health <= 0) {
             this.saxonArmy.splice(this.saxonArmy.indexOf(randomSaxon), 1);
         }
         return result;
+    }
+
+    saxonAttack() {
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        const result = randomViking.receiveDamage(randomSaxon.strength);
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice(this.vikingArmy.indexOf(randomViking), 1);
+        }
+        return result;
+    }
+
+//refactor vikingAttack and saxonAttack
+    attack(attacker) {
+        let attackingArmy;
+        let attackedArmy;
+   
+        if (attacker !== "viking" && attacker !== "saxon") {
+            throw new Error(`It's a war between vikings and saxons. You must enter "viking" or "saxon"`);
+        } else if (attacker === "viking") {
+            attackingArmy = this.vikingArmy;
+            attackedArmy = this.saxonArmy;
+        } else {
+            attackingArmy = this.saxonArmy;
+            attackedArmy = this.vikingArmy;
+        }
+
+        attackingSoldier = attackingArmy[Math.floor(Math.random() * this.attackingArmy.length)];
+        attackedSoldier = attackedArmy[Math.floor(Math.random() * this.attackedArmy.length)];
+        
+        const result = attackedSoldier.receiveDamage(attackingSoldier.strength);
+        if (attackedSoldier.health <= 0) {
+            attackedArmy.splice(attackedArmy.indexOf(attackedSoldier), 1);
+        }
+        return result;
+    }
+
+    showStatus() {
+        if (this.vikingArmy.length > 0 && this.saxonArmy.length >0) {
+            return "Vikings and Saxons are still in the thick of battle.";
+        } else if (this.saxonArmy.length === this.vikingArmy.length === 0) {
+            return "The war has killed all the Vikings and Saxons!";
+        } else if (this.saxonArmy.length === 0) {
+            return "Vikings have won the war of the century!";
+        }
+        return "Saxons have fought for their lives and survived another day...";
     }
 }
