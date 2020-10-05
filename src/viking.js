21735fe15ef58b237f8dcf5735a1bdea13d20e29
erifylo <erif.evangelou@gmail.com>
2020-10-05 23:11:36 +0200

    aun me da errores..

--- src/viking.js
+++ src/viking.js
@@ -6,7 +6,7 @@ class Soldier {
     }
 
     attack() {
-        return Soldier.strength; }
+        return this.strength; }
     
     receiveDamage(damage) {
         this.damage = damage;
@@ -24,6 +24,7 @@ class Viking extends Soldier {
     }
     attack()
     { return Viking.strength} // como se hace ? (This method should be inherited from Soldier, no need to re-implement it.)
+}
 
     receiveDamage(damage) {
         this.health -= damage;
@@ -37,8 +38,7 @@ class Viking extends Soldier {
 
     battleCry () {
         return "Odin Owns You All!" }
-    }
-}
+    
 
 
 // Saxon
@@ -46,19 +46,66 @@ class Saxon extends Soldier {
     attack() {
         return this.strength; }
     receiveDamage(damage) {
-        this.health -= damage
+        this.health -= damage}
     if (this.health > 0) {
     return (`A Saxon has received ${this.damage}  points of damage`) 
         }
         else {
     return (`A Saxon has died in act of compact`)
         }
-
 }
+
 
 // War
 
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let damage = randomViking.attack();
+        let fightDamage = randomSaxon.receiveDamage(randomViking.attack());
+        
+        if (randomSaxon.health <= 0) {
+            this.saxonArmy.splice(randomSaxon,1)
+        }
+        return fightDamage
+    }
 
 
-
+    saxonAttack() { 
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let damage = randomViking.attack();
+        let fightDamage = randomViking.receiveDamage(randomSaxon.attack());
+    
+    if (randomViking.health <= 0) {
+        this.vikingArmy.splice(randomSaxon,1)
+    }
+    return fightDamage
 }
+    showStatus() {
+            if (this.saxonArmy.length === 0) {
+                return "Vikings have won the war of the century!"
+            }
+            else if (this.vikingArmy.length === 0) {
+                return "Saxons have fought for their lives and survived another day..."
+            }
+            else {
+                return "Vikings and Saxons are still in the thick of battle."
+            }
+        }
+    
+  
