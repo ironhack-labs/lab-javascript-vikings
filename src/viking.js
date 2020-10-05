c1612c8a745f2b765b3196fa77e596a33fc6e487
Sara Amantegui <saiduna@gmail.com>
2020-10-05 17:44:53 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,69 @@
 // Soldier
-class Soldier {}
+
+
+class Soldier {
+    constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;
+      }
+    
+    attack() {
+      return `${this.strength}`;
+        }
+    receiveDamage(theDamage) {
+      if (theDamage > 0) {
+      this.health -= theDamage)
+        }
+      }
+   } 
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+    super (health, strength); // esto no deberia estar?
+    this.name = name;
+    }
+    
+    receiveDamage(theDamage){
+    if (theDamage >= 0) {
+    this.health -= theDamage)
+    }
+    if (this.health - theDamage >= 0) {
+        console.log(`${name} has received ${theDamage} points of damage`);      
+    } else {
+        console.log (`${name} has died in act of combat`);
+    }
+
+    }
+    
+    battleCry() {
+        console.log ("Odin owns you all");
+    }
+
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health,strength){
+        super(health,strength);
+    }
+
+    receiveDamage(theDamage){
+        if (theDamage >= 0) {
+        this.health -= theDamage)
+        }
+        if (this.health - theDamage >= 0) {
+            console.log(`A Saxon has received ${theDamage} points of damage`);      
+        } else {
+            console.log (`A Saxon has died in combat`);
+        }
+    
+        }
+
+
+}
 
 // War
 class War {}
