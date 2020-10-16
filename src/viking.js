f1d3ed935657b9144d71caa651acbb675f09fc4e
Pablo Fernandez <pablo_fz@hotmail.com>
2020-10-16 13:19:42 +0200

    All done with Bonus

--- src/viking.js
+++ src/viking.js
@@ -27,7 +27,7 @@ class Viking extends Soldier{
         this.health= this.health - danger;
         if(this.health > 0)return `${this.name} has received ${danger} points of damage`;
         else return `${this.name} has died in act of combat`;
-        // this.health >= 0 ? return `${this.name} has received ${danger} points of damage` : return `${this.name} has died in act of combat`;
+        
     }
 
     battleCry(){
@@ -58,10 +58,30 @@ class War {
         this.saxonArmy = [];
     }
     addViking(viking){
-        this.viking = this.vikingArmy.push(viking);
+        this.vikingArmy.push(viking);
     }
-    addSaxon(){}
-    vikingAttack(){}
-    saxonAttack(){}
-    showStatus(){}
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack(){
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length); 
+        const saxonRandom = Math.floor(Math.random() * this.vikingArmy.length); 
+        const battleResult = this.saxonArmy[saxonRandom].receiveDamage(this.vikingArmy[vikingRandom].strength);
+        if(battleResult.includes("died")) this.saxonArmy.splice(saxonRandom,1);
+        return battleResult;
+
+    }
+    saxonAttack(){
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length); 
+        const saxonRandom = Math.floor(Math.random() * this.vikingArmy.length); 
+        const battleResult = this.vikingArmy[vikingRandom].receiveDamage(this.saxonArmy[saxonRandom].strength);
+        if(battleResult.includes("died")) this.vikingArmy.splice(vikingRandom,1);
+        return battleResult;
+
+    }
+    showStatus(){
+        if(!this.saxonArmy[0])return 'Vikings have won the war of the century!';
+        else if(!this.vikingArmy[0]) return 'Saxons have fought for their lives and survived another day...';
+        else return 'Vikings and Saxons are still in the thick of battle.';
+    }
 }
