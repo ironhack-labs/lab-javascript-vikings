403691a357d8c7db21aa1c2b845d94786712ad70
teof95 <teof95@gmail.com>
2020-10-16 19:54:27 +0200

    soldier attack and receiveDamage created

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,45 @@
-// Soldier
-class Soldier {}
+// this is the class for Soldier
 
+class Soldier {
+    constructor(health, strength) {
+        //Those are the properties
+        this.health = health;
+        this.strength = strength;
+    }
+
+
+    // those are the methods for the Soldier
+
+    // attack is the first method
+    attack() {
+        return this.strength;
+    }
+
+    //recieveDamage is the second method
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 // Viking
-class Viking {}
+class Viking {
+    constructor(name) {
+        this.name = name;
+    }
+}
+attack()
+
+
+// those are the methods for the Viking
+receiveDamage()
+
+battleCry()
+
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
+
+let Soldier1 = new Soldier('100', '50')
