364b89bc08e7e0f9897dc5cdf1b6b9ebe68165f4
Nina van den Braak <ninavandenbraak@gmail.com>
2020-10-16 14:13:37 +0200

    One war function left to do

--- src/viking.js
+++ src/viking.js
@@ -53,7 +53,55 @@ class Saxon extends Soldier {
 
     }
 }
-    // War
-    class War {
-        
+
+// War
+class War {
+    constructor() {
+        this.vikingArmy = [],
+            this.saxonArmy = []
     }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack() {
+        let saxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        let viking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+
+        saxon = saxon.receiveDamage(viking.strength)
+
+        this.saxonArmy = this.saxonArmy.filter(item => item.health > 0)
+
+        return saxon
+
+    }
+
+    saxonAttack() {
+        let saxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        let viking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+
+        viking = viking.receiveDamage(saxon.strength)
+
+        this.vikingArmy = this.vikingArmy.filter(item => item.health > 0)
+
+        return viking
+    }
+
+    showStatus() {
+        return
+    }
+}
+
+let viking1 = new Viking("Piet", 20, 30)
+let viking2 = new Viking("Piet", 20, 30)
+let saxon = new Saxon(40, 50)
+let war = new War
+
+war.addViking(viking1)
+war.addSaxon(saxon)
+war.vikingAttack()
