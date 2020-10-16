934d4f1bfd00a94e4d157f2fbf7151e3e7c38f7e
Tomy Tubert <tomytubert@gmail.com>
2020-10-16 19:36:31 +0200

    hecho sin acabar los bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,71 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+    this.health = health;
+    this.strength = strength;
+    }
+    attack(){
+    return this.strength
+    }
+    receiveDamage(theDamage){
+    this.health = this.health - theDamage  
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name,health,strength){
+    super(health,strength)
+    this.name = name
+    }
+    battleCry(){
+    return "Odin Owns You All!"
+    }
+    receiveDamage(theDamage){
+    this.health = this.health - theDamage 
+    if(this.health>0){
+    return`${this.name} has received ${theDamage} points of damage`
+    }
+    else{
+    return`${this.name} has died in act of combat`
+    }
+    }
+  }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(theDamage){
+     this.health = this.health - theDamage 
+     if(this.health>0){
+      return`A Saxon has received ${theDamage} points of damage`
+    }
+    else{
+      return`A Saxon has died in combat`
+    }
+    }
+  }
 
 // War
-class War {}
+class War {
+   constructor(){
+   this.vikingArmy=[]
+   this.saxonArmy=[]
+   }
+    addViking(viking){
+    this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        const saxonHealth = saxon.receiveDamage(viking.strength)
+        if (saxon.health <=0){
+            this.saxonArmy.splice(1);
+        }
+        else{
+            return saxonHealth;
+        }
+          }
+    saxonAttack(){}
+    showStatus(){}
+  }
