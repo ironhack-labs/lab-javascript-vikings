8932dfef9ff2f243a247f09e6f6ee0346f738ae9
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-10-16 12:22:53 -0500

    battleCry() method

--- src/viking.js
+++ src/viking.js
@@ -29,13 +29,17 @@ class Viking extends Soldier {
     if (this.health > 0) {
       this.alive = true;
       return `${this.name} has received ${damage} points of damage`
-    }
-    else if(this.health === 0){
+    } else if (this.health === 0) {
       this.alive = false;
       return `${this.name} has died in act of combat`
 
     }
   }
+
+  battleCry(){
+    return `Odin Owns You All!`
+  }
+
 }
 
 // Saxon
