bb13a9577ebd80d9229ca78439b2ce4fcb14e342
Maya Ausset <mayaausset@gmail.com>
2020-10-16 14:53:10 +0200

    almost done

--- src/viking.js
+++ src/viking.js
@@ -56,21 +56,57 @@ class War {
     addSaxon(Saxon){
         this.saxonArmy.push(Saxon);
     }
-    vinkingAttack(){
-           
-
+    vikingAttack(){
+        let saxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        let viking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        saxon = saxon.receiveDamage(viking.strength)
+        this.saxonArmy = this.saxonArmy.filter(item => item.health > 0)
+        return saxon    
     }
     saxonAttack(){
-
+        let saxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        let viking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        viking = viking.receiveDamage(saxon.strength)
+        this.vikingArmy = this.vikingArmy.filter(item => item.health > 0)
+        return viking    
     }
     showStatus(){
-
+        if (!this.saxonArmy.length){
+            return "Vikings have won the war of the century!"
+        }
+        else if (!this.vikingArmy.length){
+           return "Saxons have fought for their lives and survived another day..."
+        }
+        else if(this.saxonArmy.length === 1 && this.vinking.Army === 1){
+            return "Vikings and Saxons are still in the thick of battle."
+        }
     }
 };
 
-let war = new War;
-let viking1 = new Viking('Svend',20,20);
-let viking2 = new Viking('Kristian',20,20);
-let viking3 = new Viking('Alex',20,20);
-let viking4 = new Viking('Symon',20,20);
+
+
+
+let viking1 = new Viking("Piet", 20, 30)
+let viking2 = new Viking("Piot", 20, 30)
+let viking3 = new Viking("Piat", 20, 30)
+
+let saxon1 = new Saxon(40, 50)
+let saxon2 = new Saxon(40, 50)
+let saxon3 = new Saxon(40, 50)
+
+let war = new War
+
+war.addViking(viking1)
+war.addViking(viking2)
+war.addViking(viking3)
+
+war.addSaxon(saxon1)
+war.addSaxon(saxon2)
+war.addSaxon(saxon3)
+
+// console.log(war.vikingArmy);
+// console.log(war.saxonArmy);
+
+war.vikingAttack()
+
 
