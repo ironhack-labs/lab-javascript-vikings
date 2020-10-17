e94662c851ed11bfcb7ee5d3403514936d57da1d
Abbie <abbie@summers.gg>
2020-10-18 00:09:08 +0200

    Completed 1st Iteration

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+      this.strength = strength;
+      this.health = health;
+    }
+    attack() {
+       return this.strength;
+    }
+     
+   receiveDamage(damage){
+     // damage is a method-level variable that is received as input
+     this.health -= damage;
+
+   }
+  }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  battleCry() {}
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
+function testFunc(param1) {
+  console.log(param1);
+  return param1;
+}
+
+let soldierOne = new Soldier(5, 1);
+// console.log(soldierOne.attack());
+console.log(soldierOne.health);
+soldierOne.receiveDamage(2);
+console.log(soldierOne.health);
