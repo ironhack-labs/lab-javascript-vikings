80ede4fbc35b43c3dfb4348a326da10bf3683964
Abbie <abbie@summers.gg>
2020-10-18 23:53:55 +0200

    Completed Iterations 2 and 3

--- src/viking.js
+++ src/viking.js
@@ -17,22 +17,42 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-  battleCry() {}
+constructor(name, health, strength){
+  super(health, strength)
+  this.name = name;
+}
+receiveDamage(damage) {
+  this.health -= damage;
+  if (this.health >0) {
+    return `${this.name} has received ${damage} points of damage`; 
+    } else {
+    return `${this.name} has died in the act of combat`;
+  }
 }
 
+battleCry() {
+  return "Odin Owns You All!"
+}}
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  receiveDamage(damage) {
+    this.health -= damage; 
+    if (this.health >0) {
+      return `A Saxon has received ${damage} points of damage`; 
+      } else {
+      return `A Saxon has died in the act of combat`; 
+  }
+}}
 
-// War
-class War {}
+/*// War
+// class War {}
 
 function testFunc(param1) {
   console.log(param1);
   return param1;
-}
 
-let soldierOne = new Soldier(5, 1);
-// console.log(soldierOne.attack());
+
+let soldierOne = new Viking("blargh", 5, 1);
 console.log(soldierOne.health);
-soldierOne.receiveDamage(2);
-console.log(soldierOne.health);
+console.log(soldierOne.receiveDamage(2));
+console.log(soldierOne.health);*/
