23f1725baa1a223ead24751ff81d3294a9ab70e9
gtretow <guilherme.tretow@gmail.com>
2020-10-24 17:16:44 -0300

    Viking with only 1 failure at receiveDamage()

--- src/viking.js
+++ src/viking.js
@@ -16,23 +16,25 @@ class Soldier {
 // Viking
 class Viking extends Soldier {
   constructor(name, health, strength) {
-    name = "Harald";
     super();
+    this.name = "Harald";
+  }
+  receiveDamage(damage) {
+    if (this.health > 0) {
+      this.health = 250;
+      return `${this.name} has received ${damage} points of damage`;
+    } else if (this.health <= 0) {
+      this.health = 0;
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
   }
 }
 
 // Saxon
-class Saxon {
-  constructor(health, strength) {
-    this.health = 100;
-    this.strength = 10;
-  }
-}
+class Saxon {}
 
 // War
-class War {
-  constructor(health, strength) {
-    this.health = 100;
-    this.strength = 10;
-  }
-}
+class War {}
