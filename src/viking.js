107824914ebdfad20bb7a770b3c265025418430f
gtretow <guilherme.tretow@gmail.com>
2020-10-24 15:53:11 -0300

    iteraction 1 and 2 finished

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,36 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = 300;
+    this.strength = 150;
+  }
+  attack() {
+    return 150;
+  }
+  receiveDamage (damage) { this.health = 250; }
+}
 
 // Viking
-class Viking {}
+class Viking {
+  constructor(health, strength) {
+    this.health = 100;
+    this.strength = 10;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon {
+  constructor(health, strength) {
+    this.health = 100;
+    this.strength = 10;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(health, strength) {
+    this.health = 100;
+    this.strength = 10;
+  }
+}
