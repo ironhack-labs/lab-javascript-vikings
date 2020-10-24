fed23b4c3a95c7bece5543f2893bbd35da5142a1
Patrick Brito <patrickbrito95@gmail.com>
2020-10-24 16:25:48 -0300

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,46 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+     }
+    attack(){
+         return this.strength;
+    }
+    receiveDamage(damage){      
+     }
+    }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+     constructor(name, health, strength){
+         super(health, strength);
+         this.name = name;
+     }
+     attack(){
+         return this.strength;
+     }
+     receiveDamage(damage){
+         
+    }  
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+     constructor(health, strength){
+         super(health, strength);
+     }
+    
+     attack(){
+         return this.strength;
+     }
+
+}
 
 // War
-class War {}
+class War {
+    
+}
