27d819218c21ccbdbf8307657708703a0218a3c2
Tassia Accioly <tassia.accioly@gmail.com>
2020-10-24 15:41:29 -0300

    Implemented automated war and visual inputs for the player through console.log

--- src/viking.js
+++ src/viking.js
@@ -99,16 +99,23 @@ class War {
 
   //   let randomViking = this.vikingArmy[randomVikingIndex];
 
-  //   let battleRoyale = randomViking.receiveDamage(randomSaxon.strength);
+  //   let battleResult = randomViking.receiveDamage(randomSaxon.strength);
 
   //   if (randomViking.health <= 0) {
   //     this.vikingArmy.splice(randomVikingIndex, 1);
   //   }
-  //   return battleRoyale;
+  //   return battleResult;
   // }
 
-  //Refactoring both saxonAttack() and vikingAttack()
+  //REFACTORING BOTH saxonAttack() AND vikingAttack() INTO ONE SINGLE METHOD:
+
+  //attacker and defender are vikingArmy array and saxonArmy array interchangeably;
+  //Console.log()s add visual input of the battle for the player
   goToWar(attacker, defender) {
+    console.log(
+      `The ${attacker} is attacking and the ${defender} is defending!`
+    );
+
     let randomAttIndex = Math.floor(Math.random() * attacker.length);
 
     let randomAtt = attacker[randomAttIndex];
@@ -117,12 +124,54 @@ class War {
 
     let randomDef = defender[randomDefIndex];
 
-    let battleRoyale = defender.receivesDamage(attacker.strength);
+    let battleResult = randomDef.receivesDamage(randomAtt.strength);
+
+    console.log(
+      `The soldier ${randomDef} took ${randomAtt.strength} of damage from ${randomAtt}, his health is at ${randomDef.health} points.`
+    );
 
     if (randomDef.health <= 0) {
+      console.log(
+        `${randomDef} is out of battle. He will be removed from the field.`
+      );
       defender.splice(randomDefIndex, 1);
     }
-    return battleRoyale;
+    console.log(
+      `${randomDef} is not out of battle yet! Let's keep on fighting!`
+    );
+    return battleResult;
+  }
+
+  //ATTEMPT TO IMPLEMENT AUTOMATION IN THE WAR:
+
+  //army1 and army2 are arrays of saxons and vikings, interchangeably
+  battleRoyale(army1, army2) {
+    if (!army1.length || !army2.length) {
+      return `One or both of the armies don't have soldiers, this can't be a battle`;
+    } else {
+      while (army1.length > 0 || army2.length > 0) {
+        if (
+          (army1.length === 0 && army2.length > 0) ||
+          (army1.length > 0 && army2.length === 0)
+        ) {
+          return this.showWinner();
+        } else if (army1.length > army2.length) {
+          return this.goToWar(army2, army1);
+        } else if (army1.length < army2.length) {
+          return this.goToWar(army1, army2);
+        }
+      }
+    }
+  }
+
+  //using the method above we don't need the last else in showStatus(), so for the goToWar() method we could use the following method:
+
+  showWinner() {
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+    } else if (this.vikingArmy.length === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+    }
   }
 
   showStatus() {
