1cb23f4e95e2fe205c1a35bc4c52145b3a0254cc
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2020-10-26 20:48:52 +0100

    took out methods from constructors

--- src/viking.js
+++ src/viking.js
@@ -6,9 +6,9 @@ class Soldier {
         this.attack = function (){
             return this.strength;
         }
-        this.receiveDamage = function (damage){
-            this.health -= damage;
-        }
+    }
+    receiveDamage = function (damage){
+        this.health -= damage;
     }
 }
 
@@ -18,35 +18,35 @@ class Viking extends Soldier {
     constructor(name, health, strength){
         super(health, strength);
         this.name = name;
-        this.receiveDamage = function (damage){
-            this.health -= damage;
-            if(this.health>0){
-                return `${this.name} has received ${damage} points of damage`;
-            }else{
-                return `${this.name} has died in act of combat`
-            }
+    }
+    receiveDamage = function (damage){
+        this.health -= damage;
+        if(this.health>0){
+            return `${this.name} has received ${damage} points of damage`;
+        }else{
+            return `${this.name} has died in act of combat`
         }
-        this.battleCry = function (){
-            return `Odin Owns You All!`
-        }
-    }  
+    }
+    battleCry = function (){
+        return `Odin Owns You All!`
+    }
 }
 
 
 // Saxon
 class Saxon extends Soldier {
     constructor (health, strength){
-        super (health, strength);
-        this.attack = function (){
-            return this.strength;
-        }
-        this.receiveDamage = function (damage){
-            this.health -= damage;
-            if(this.health>0){
-                return `A Saxon has received ${damage} points of damage`;
-            }else{
-                return `A Saxon has died in combat`
-            }
+        super (health, strength);  
+    }
+    attack = function (){
+        return this.strength;
+    }
+    receiveDamage = function (damage){
+        this.health -= damage;
+        if(this.health>0){
+            return `A Saxon has received ${damage} points of damage`;
+        }else{
+            return `A Saxon has died in combat`
         }
     }
 
