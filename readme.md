a9b325769e6aa10646e3658a7435f15aba109060
William Campbell <me@williamcampbell.ie>
2020-10-27 22:02:43 +0100

    done

--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@
 ## Introduction
 
 We have learned Object-oriented programming and how `class` and inheritance work in JavaScript. Now lets work with our Viking friends, applying all of the concepts we have just learned.
-
+ 
 ## Requirements
 
 - Fork this repo
--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,6 @@
 
 // Soldier
-class Soldier {
+class Soldier { 
     constructor(health, strength) {
         this.health = health;
         this.strength = strength;
@@ -19,15 +19,77 @@ class Viking extends Soldier {
         super(health, strength);
         this.name = name;
     }
-            battleCry() {
-                return 'i am flying'
-            }
-
+    battleCry() {
+        return "Odin Owns You All!";
+    }   
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${name} has received ${this.damage} points of damage`;
+        } else {
+            return `${name} has died in the act of combat`;
+        }
+    }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+constructor(health, strength, receiveDamage) { 
+    super(health, strength);
+}
+receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+        return `A Saxon has received ${damage} points of damage`;
+    } else {
+        return `A Saxon has died in combat`;
+    }
+
+}
+
+
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    };
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    };
+
+    vikingAttack() {
+        let oldHealth = saxon.health;
+        saxon.health = oldHealth - viking.strength;
+        if (saxon.health <=0){
+            saxonArmy.splice(0, 1);
+        }
+
+    }
+
+    saxonAttack() {
+        let oldHealth = viking.health;
+        viking.health = oldHealth - saxon.strength;
+        if (viking.health <=0){
+            vikingArmy.splice(viking.name);
+        }
+
+    }
+
+
+    showStatus() {
+        if (this.saxonArmy.length == 0) {
+            return "Vikings have won the war of the century!";
+        }
+        if (this.vikingArmy.length == 0) {
+            return "Saxons have fought for their lives and survived another day...!";
+        }
+        return "Vikings and Saxons are still in the thick of battle.";
+
+    }
+
+}
  
