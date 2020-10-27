27b3c5586931639bb8f21bc4b3665e76e16e082f
ararita <munjizavesna@gmail.com>
2020-10-27 17:29:44 +0100

    Finish iteration 11

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,34 @@
 // Soldier
-class Soldier {}
+
+// class
+
+// should receive 2 arguments (health & strength)
+// should receive the health property as its 1st argument
+// should receive the strength property as its 2nd argument
+
+// attack() method
+// should be a function
+// should receive 0 arguments
+// should return the strength property of the Soldier
+
+// receiveDamage() method
+// should be a function
+// should receive 1 argument (the damage)
+// should remove the received damage from the health property
+// shouldn't return anything
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
 class Viking {}
