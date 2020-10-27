1fc330d4515238cb87ce332fb78052ef6d939460
Jan <jan.leon.voelker@gmail.com>
2020-10-27 18:22:59 +0100

    Done with everything but the bonus bonus :)

--- src/viking.js
+++ src/viking.js
@@ -3,9 +3,6 @@ class Soldier {
     constructor (health, strength) {
         this.health = health;
         this.strength = strength;
-
- 
-
     }
     attack() {
         return this.strength;
@@ -51,4 +48,60 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor () {
+    this.saxonArmy = [];
+    this.vikingArmy = [];
+    }
+    addViking(viking) {
+      this.vikingArmy.push(viking);
+    };
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    };
+    vikingAttack(){
+        let victim = pickRandom(this.saxonArmy);
+        let victimIndex = this.saxonArmy.indexOf(victim);
+        let attacker = pickRandom(this.vikingArmy);
+        console.log(victim);
+        if ((victim.health - attacker.strength)<= 0) {
+            let oldArmy = this.saxonArmy;
+            this.saxonArmy = this.saxonArmy.slice(0,victimIndex).push;
+            this.saxonArmy = oldArmy.slice(victimIndex+1);
+        }
+        return victim.receiveDamage(attacker.strength);
+    };
+    saxonAttack(){
+        let victim = pickRandom(this.vikingArmy);
+        let victimIndex = this.vikingArmy.indexOf(victim);
+        let attacker = pickRandom(this.saxonArmy);
+        console.log(victim);
+        if ((victim.health - attacker.strength)<= 0) {
+            let oldArmy = this.vikingArmy;
+            this.vikingArmy = this.vikingArmy.slice(0,victimIndex).push;
+            this.vikingArmy = oldArmy.slice(victimIndex+1);
+        }
+        return victim.receiveDamage(attacker.strength);
+    };
+    showStatus(){
+//         if the Saxon array is empty, should return 
+if (this.saxonArmy.length === 0) {
+    return "Vikings have won the war of the century!";
+}
+else if (this.vikingArmy.length === 0) {
+    return "Saxons have fought for their lives and survived another day..."
+}
+else if (this.vikingArmy && this.saxonArmy) {
+return "Vikings and Saxons are still in the thick of battle.";
+}
+else {
+    console.log("There must be some battle dust on the arrays..")
+}
+    };
+
+}
+
+function pickRandom (array) {
+    let number = Math.floor(Math.random() * array.length);
+    return array[number];
+}
