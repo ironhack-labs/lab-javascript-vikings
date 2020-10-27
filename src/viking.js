21afe7c2cfbbb6ad7b7aeca48fa03099903d8d5d
ararita <munjizavesna@gmail.com>
2020-10-27 18:02:32 +0100

    Finish the exercise

--- src/viking.js
+++ src/viking.js
@@ -31,10 +31,61 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = "Harald";
+    this.strength = 150;
+    this.health = 300;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    // console.log("damage", damage);
+    this.health -= damage;
+    // console.log("health", this.health);
+    if (this.health === 0) {
+      return `${this.name} has died in act of combat`;
+    } else {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+
+// attack() method
+// (This method should be inherited from Soldier, no need to re-implement it.)
+
+// should be a function
+// should receive 0 arguments
+// should return the strength property of the Saxon
+// receiveDamage() method
+
+// (This method needs to be re-implemented for Saxon because the Saxon version needs to have different return values.)
+
+// should be a function
+// should receive 1 argument (the damage)
+// should remove the received damage from the health property
+// if the Saxon is still alive, it should return "A Saxon has received DAMAGE points of damage"
+// if the Saxon dies, it should return "A Saxon has died in combat"
+class Saxon extends Soldier {
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
