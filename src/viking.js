d3cd2fb2c1625b482f637bd2ef12c3d5b51d6b9a
“dialmojordan” <d.andreani2@gmail.com>
2020-10-27 21:51:00 +0100

    add #1-4

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,67 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return `Odin Ows You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  //Saxon.receiveDamage();
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A ${this.name} has received ${damage} points of damage`;
+    } else {
+      return `A ${this.name} has died in act of combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  let vikingArmy = [],
+  let saxonArmy = [], 
+  addViking(){
+      vikingArmy.push(++)
+  }
+  addSaxon()
+  vikingAttack()
+  saxonAttack()
+  showStatus()
+}
