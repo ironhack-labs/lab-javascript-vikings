8dd94205183f683432f1a21bcd88dd4f6e5588f8
Sebastian Montoya <seb246@gmail.com>
2020-10-28 21:20:02 -0400

    adding iteration 2

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,28 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health,strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+
+    }
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if(this.health > 0){
+            return `${this.name} has received ${theDamage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+
+    }
+    battleCry(){
+        return `Odin Owns You All!`;
+    }
+
+}
 
 // Saxon
 class Saxon {}
