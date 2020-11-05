73dc6612ca8aa6b4c23b66e8880281169dd8cccc
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-11-05 00:11:18 -0500

    almost done

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ window.onload = () => {
     console.log("Connected to JS File, TO BATTLE!!!");
 };
 
+let warGame;
 const vikingNames = [
     "Halthor",
     "Olaf",
@@ -13,67 +14,120 @@ const vikingNames = [
     "Bjorn",
 ];
 
-// document.getElementById('gameBoard');
 const gameBoard = document.querySelector("#gameBoard");
+const armySize = 5;
+const textDisplay = document.querySelector("#textDisplay");
+const startGameButton = document.querySelector("#startGameButton");
 
-document.querySelector("#startGameButton").onclick = () => {
+startGameButton.onclick = (button) => {
     console.log("Starting the War");
-    const warGame = new War();
+    warGame = new War();
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < armySize; i++) {
         warGame.addViking(
             new Viking(
                 vikingNames[Math.floor(Math.random() * vikingNames.length)],
-                Math.floor(Math.random() * 100) + 50,
-                Math.floor(Math.random() * 50) + 350,
-                i
+                Math.floor(Math.random() * 200) + 50,
+                Math.floor(Math.random() * 50) + 50
             )
         );
 
         warGame.addSaxon(
             new Saxon(
-                Math.floor(Math.random() * 800) + 50,
-                Math.floor(Math.random() * 40) + 15
+                Math.floor(Math.random() * 200) + 50,
+                Math.floor(Math.random() * 30) + 50
             )
         );
     }
 
     console.log({ warGame });
 
-    displayRandomViking(
-        warGame.vikingArmy[
-            Math.floor(Math.random() * warGame.vikingArmy.length)
-        ]
-    );
+    displayArmies();
+    button.target.setAttribute("style", "display: none;");
+
+    textDisplay.innerHTML = "The War has Begun!";
 };
 
-function displayRandomViking(viking) {
-    console.log({ viking });
-    const image = document.createElement("img");
-    image.src = viking.image;
-    const.div = document.createElement('div');
-    div.innerHTML =
-    <img src="${viking.image}" />
-    <div>          </div>
-    <h3>Hp</h3>
+function displayArmies() {
+    gameBoard.innerHTML = "";
+    document.querySelector("#playerScore").innerHTML =
+        armySize - warGame.saxonArmy.length;
+    document.querySelector("#saxonScore").innerHTML =
+        armySize - warGame.vikingArmy.length;
 
-    gameBoard.appendChild(image);
+    const vikingContainer = document.createElement("div");
+    const saxonContainer = document.createElement("div");
+
+    vikingContainer.classList.add("vikingContainer");
+    saxonContainer.classList.add("saxonContainer");
+
+    gameBoard.appendChild(vikingContainer);
+    gameBoard.appendChild(saxonContainer);
+
+    warGame.vikingArmy.forEach((viking) => {
+        displayVikings(viking);
+    });
+
+    warGame.saxonArmy.forEach((saxon) => {
+        displaySaxons(saxon);
+    });
+}
+
+function displayVikings(viking) {
+    const vikingContainer = document.querySelector(".vikingContainer");
 
     const div = document.createElement("div");
+    div.classList.add("vikingBox");
     div.innerHTML = `
-    <h2>Hello All</h2>
-    <h3>See this works!</h3>
-    <h1>For The Horde!!</h1>
+    <img src="${viking.image}" />
+    <div>
+    <h3>HP: ${viking.health}</h3>
+    <h3>ATK: ${viking.attack()}</h3>
+    </div>
+    <button onClick="attackRandomSaxon(this)">Attack</button>
     `;
 
-    gameBoard.insertBefore(div, image);
+    vikingContainer.appendChild(div);
+}
+
+function displaySaxons(saxon) {
+    const saxonContainer = document.querySelector(".saxonContainer");
+
+    const div = document.createElement("div");
+    div.classList.add("saxonBox");
+    div.innerHTML = `
+    <div>
+    <h3>HP: ${saxon.health}</h3>
+    <h3>ATK: ${saxon.attack()}</h3>
+    </div>
+    <img src="${saxon.image}" />
+    `;
+
+    saxonContainer.appendChild(div);
 }
 
 function attackRandomSaxon(event) {
-    const parent = document.querySelector('.vikingBox');
-    console.log({
-        event: event.parentNode 
-        index: [...parent.children].indexOf(event.parentNode),
-    });
-    
+    if (warGame.vikingArmy.length === 0 || warGame.saxonArmy.length === 0) {
+        return gameOver();
+    }
+    const vikingContainer = document.querySelector(".vikingContainer");
+
+    warGame.vikingAttack(
+        [...vikingContainer.children].indexOf(event.parentNode)
+    );
+
+    setTimeout(() => {
+        const result = warGame.saxonAttack();
+
+        gameOver();
+    }, 1000);
+    gameOver();
+}
+
+function gameOver() {
+    if (warGame.vikingArmy.length === 0 || warGame.saxonArmy.length === 0) {
+        startGameButton.setAttribute("style", "display: unset;");
+    }
+    textDisplay.innerHTML = "";
+    textDisplay.innerHTML = warGame.showStatus();
 }
--- src/viking.js
+++ src/viking.js
@@ -1,131 +1,154 @@
 // Soldier
 class Soldier {
-      constructor(health, strength) {
-            this.health = health;
-            this.strength = strength;
-      }
-      attack() {
-          return this.strength;
-      }
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+        this.image = `./images/squire${Math.floor(Math.random() * 4)}.png`;
+    }
 
-      receiveDamage(dmg) {
-          this.health -= dmg;
-      }
-     
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(dmg) {
+        this.health -= dmg;
+    }
 }
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health,strength) {
+    constructor(name, health, strength) {
         super(health, strength);
         this.name = name;
+        this.image = `./images/viking${Math.floor(Math.random() * 4)}.png`;
     }
 
     receiveDamage(dmg) {
         this.health -= dmg;
 
         return this.health > 0
-        ?`${this.name} has received ${dmg} points of damage`
-        ?`${this.name} has died in act of combat`;
+            ? `${this.name} has received ${dmg} points of damage`
+            : `${this.name} has died in act of combat`;
     }
 
     battleCry() {
-        return `odin owns All`;
+        return "Odin Owns You All!";
     }
 }
 
 // Saxon
 class Saxon extends Soldier {
     constructor(health, strength) {
-        super(health,strength);
+        super(health, strength);
+        this.image = `./images/saxon${Math.floor(Math.random() * 4)}.png`;
     }
 
-receiveDamage(dmg) {
-    this.health -= dmg;
+    receiveDamage(dmg) {
+        this.health -= dmg;
 
-    return this.health > 0
-    ? `${this.name} has received ${dmg} points of damage`
-    ? `${this.name} has died in act of combat`;
-
-  }
+        return this.health > 0
+            ? `A Saxon has received ${dmg} points of damage`
+            : `A Saxon has died in combat`;
+    }
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
 
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
 
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
 
+    vikingAttack(theVikingIndex) {
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        // const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomViking = theVikingIndex;
 
+        console.log("...>", {
+            saxon: this.saxonArmy[randomSaxon].health,
+        });
 
+        const theResultOfCombat = this.saxonArmy[randomSaxon].receiveDamage(
+            this.vikingArmy[randomViking].attack()
+        );
 
+        console.log({
+            saxon: this.saxonArmy[randomSaxon].health,
+            viking: this.vikingArmy[randomViking],
+        });
 
+        if (this.saxonArmy[randomSaxon].health <= 0) {
+            this.saxonArmy.splice(randomSaxon, 1);
+        }
 
+        console.log({ saxonArmySize: this.saxonArmy.length });
+        displayArmies();
 
+        return theResultOfCombat;
+    }
 
+    saxonAttack() {
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
 
+        // optional chaining for object and keys is when you add ? before the . when calling the key. object?.key
+        // link to documentation about optional chaining.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+        const theResultOfCombat = this.vikingArmy[randomViking].receiveDamage(
+            this.saxonArmy[randomSaxon].attack()
+        );
 
+        // let theResultOfCombat;
 
+        // if (this.vikingArmy[randomViking] && this.saxonArmy[randomSaxon]) {
+        //     theResultOfCombat = this.vikingArmy[randomViking].receiveDamage(
+        //         this.saxonArmy[randomSaxon].attack())
+        // };
 
+        if (this.vikingArmy[randomViking].health <= 0) {
+            this.vikingArmy.splice(randomViking, 1);
+        }
 
+        displayArmies();
 
+        return theResultOfCombat;
+    }
 
+    showStatus() {
+        // if (this.saxonArmy.length === 0) {
+        //     return "Vikings have won the war of the century!";
+        // } else if (this.vikingArmy.length === 0) {
+        //     return "Saxons have fought for their lives and survived another day...";
+        // } else {
+        //     return "Vikings and Saxons are still in the thick of battle.";
+        // }
 
-
-
-
-
-
-
-
-
-
-addViking(viking) {
-    this.vikingArmy.push(viking);
+        // ternary else if
+        return this.saxonArmy.length === 0
+            ? "Vikings have won the war of the century!"
+            : this.vikingArmy.length === 0
+            ? "Saxons have fought for their lives and survived another day..."
+            : "Vikings and Saxons are still in the thick of battle.";
+    }
 }
 
-addSaxon(saxon) {
-    this.saxonArmy.push(saxon);
-}
 
-vikingAttack() {
-    const randomSaxon = math.floor(match.random() * this.saxonArmy.length);
-    const randomViking = math.floor(math.random() * this.vikingArmy.length);
 
-    const theResultofCombat
-    this.vikingArmy[randomViking].attack()
-);
 
-if (
-    this.saxonArmy[randomSaxon]?.health -
-    this.vikingArmy[randomViking].attack()   mising this part symbol  0
 
-) {
-    this.saxonArmy.splice(randomSaxon, 1);
-}
 
-   return  theResultofCombat;
-}
 
-saxonAttack() {
-    const randomSaxon = math.floor(match.random() * this.saxonArmy.length);
-    const randomViking = math.floor(math.random() * this.vikingArmy.length);
 
-    // conditional rendering for object and keys is to add ? before the . when calling the key.object?. key
-    const theResultofCombat = this.vikingArmy[randomViking]?.receiveDamage(
-        this.saxonArmy[randomSaxon].attack()
-    );
 
-if (
-    this.vikingArmy[randomViking]?.health -
-    this.saxonArmy[randomSaxon].attack() < 0
-) {
-    this.vikingArmy[randomViking]?.health -
-      this.saxonArmy[randomSaxon].attack() < 0
-} {
 
-  this.vikingArmy.splice(randomSaxon, 1);  
-}
 
-   return theResultofCombat;
-   }
-}
+
+
