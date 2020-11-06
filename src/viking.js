cb6185c44103a1693470fc1b7e92a0b1bdd3cb66
unknown <fernandadelleprane@gmail.com>
2020-11-05 22:39:55 -0300

    init JS

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,50 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength
+
+    }
+    attack() {
+        return this.strength
+
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name
 
-// Saxon
-class Saxon {}
+    }
+    battleCry() { return (`Odin Owns You All!`) }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) return (`${this.name} has received ${damage} points of damage`);
+        return (`${this.name} has died in act of combat`)
+    }
 
+
+
+}
+
+Saxon
+class Saxon extends Soldier{
+    super(health,strength);
+
+    
+
+receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) return (`A Saxon has received ${damage} points of damage`);
+        return (`A Saxon has died in combat`)
+ }
+}
 // War
-class War {}
+class War { }
