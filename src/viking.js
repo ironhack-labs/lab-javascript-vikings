20f65ae36fe0383c098e5777f1afc3b42fcb62c6
MkAlba <albacastello74@gmail.com>
2020-11-08 10:04:27 +0100

    fix: ultimo ejercicio

--- src/viking.js
+++ src/viking.js
@@ -41,9 +41,9 @@ class Saxon extends Soldier {
     
     receiveDamage(damage) {
         
-       // super.receiveDamage (damage) ; 
+       super.receiveDamage (damage) ; 
 
-        if (super.receiveDamage(damage) > 0 ) {
+        if (this.health > 0 ) {
 
             return `A Saxon has received ${damage} points of damage`;
         } 
