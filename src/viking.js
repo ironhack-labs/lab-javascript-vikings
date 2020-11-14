b1d07a512f1886d2ec7b7d93c4ac06bb87663f43
Dardannya Patente <dardannyapatente@hotmail.com>
2020-11-14 13:53:56 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,67 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack () {
+        this.health.push = health;
+    }
+
+    receiveDamage (the damage) {
+        this.health
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strenght)
+        this.name = name;
+    }
+
+    battleCry () {
+        return 'Odin Owns You All';
+    }
+
+    receiveDamage (the damage) {
+        if ()
+        return '${name} has received ${the damage} points of damage';
+        if ()
+        return '${name} has died in act of combat';
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+}
 
 // War
-class War {}
+class War {
+    constructor(vikingArmy, saxonArmy) {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+}
+
+    addViking()
+
+    addSaxon()
+
+    vikingAttack()
+
+    saxonAttack()
+
+    showStatus() {
+        if (saxonArmy === 0);
+        return 'Vikings have won the war of the century!';
+        else if (vikingArmy !== 0);
+        return 'Saxons have fought for their lives and survived another day...';
+        else {
+        return 'Vikings and Saxons are still in the thick of battle.'
+        }
+    }
+}
