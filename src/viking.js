027d7884831ccd82854097e546032076d536f123
Duarte <duarte.paradela@gmail.com>
2020-11-18 09:25:03 +0100

    All 3 iterations done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,70 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+       this.health = this.health - damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+   
+
+    receiveDamage(damage) {
+        let msg;
+        this.health = this.health - damage
+        if (this.health < damage) msg = `${this.name} has died in act of combat`
+        else msg = `${this.name} has received ${damage} points of damage`
+        return msg;
+        
+     }
+
+     battleCry() {
+         return `Odin Owns You All!`
+     }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }  
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(Viking){
+       this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack(){
+        
+    }
+    // saxonAttack(){}
+    // showStatus(){}
+}
