fc7868f3c920eb7df14ea595e5b6eba2fbde50f2
Carlos Martín <karuvsentor@gmail.com>
2020-11-25 21:34:29 +0100

    iteration 1 done

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveAttack(damage) {
+        this.health -= damage;
+        // buscar como implemetar dos opciones en return....   
+        /*return if(this.health > 0){
+            (`${this.name} has received ${damage} points of damage`)
+            (`${this.name} has died in act of combat`)}
+        }*/
+    }
+}
+
 
 // Saxon
 class Saxon {}
