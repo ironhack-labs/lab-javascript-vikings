15e44fd3a5152b061026ef5e9bcada840d1902a2
Preacherush <jlrascon.job@gmail.com>
2020-11-25 22:12:39 +0100

    It. 2 (Viking) DONE.

--- src/viking.js
+++ src/viking.js
@@ -13,7 +13,25 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name
+        this.attack = function(){
+            return strength
+        }
+        this.receiveDamage = function(damage){
+            this.health = this.health - damage
+            if(damage < this.health){
+                return `${this.name} has received ${damage} points of damage`
+            }
+            return `${this.name} has died in act of combat`
+        }
+        this.battleCry = function(){
+            return `Odin Owns You All!`
+        }
+    }
+}
 
 // Saxon
 class Saxon {}
