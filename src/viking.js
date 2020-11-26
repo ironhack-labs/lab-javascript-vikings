92909c75b12053f7c769baa86598526272a6fc19
Preacherush <jlrascon.job@gmail.com>
2020-11-26 12:18:39 +0100

    It. 3 (Saxon) Done. Working on Bonus-4. Minor changes at It.2

--- src/viking.js
+++ src/viking.js
@@ -17,12 +17,12 @@ class Viking extends Soldier {
     constructor(name, health, strength){
         super(health, strength)
         this.name = name
-        this.attack = function(){
-            return strength
-        }
+        //this.attack = function(){
+        //    return strength
+        //} ==== Attack method recived from Soldier ====
         this.receiveDamage = function(damage){
             this.health = this.health - damage
-            if(damage < this.health){
+            if(this.health){
                 return `${this.name} has received ${damage} points of damage`
             }
             return `${this.name} has died in act of combat`
@@ -34,7 +34,27 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage = function(damage){
+        this.health = this.health - damage
+        if(this.health){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        return `A Saxon has died in combat`
+    }
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = []
+    saxonArmy = []
+    addViking(vinking){
+        this.vikingArmy.push(vinking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+
+    }
+}
