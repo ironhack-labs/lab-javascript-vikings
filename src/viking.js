592e0147e13763731b541ff78797430eaa87dded
Alicia Parra <alicia@redparra.com>
2020-11-27 20:31:03 +0100

    Do superBonus. All test working

--- src/viking.js
+++ src/viking.js
@@ -41,9 +41,6 @@ class Viking extends Soldier {
 }
 
 
-
-
-
 // Saxon
 class Saxon extends Soldier {
 
@@ -69,7 +66,7 @@ class War {
         this.vikingArmy= [];
         this.saxonArmy=[];
     }
-
+    //Adding warriors to army Methods
     addViking(viking){
         this.vikingArmy.push(viking);
     }
@@ -78,35 +75,76 @@ class War {
         this.saxonArmy.push(saxon);
     }
 
-    
-    randomViking(){return this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]}
-    randomSaxon(){return this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)]}
+    //Methods to get a random warrior
+    randomViking(){
+        return this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]
+    }
 
+    randomSaxon(){
+        return this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)]
+    }
+
+    //Basic attack methods. Needed to be uncommented to pass all Jasmine tests
     vikingAttack(){
         let choosenSaxon= this.randomSaxon();
         let damageSaxon= choosenSaxon.receiveDamage(this.randomViking().strength);
-        if(choosenSaxon.health<=0){ this.saxonArmy.splice(this.saxonArmy.indexOf(choosenSaxon),1)} 
+            if(choosenSaxon.health<=0){ 
+                this.saxonArmy.splice(this.saxonArmy.indexOf(choosenSaxon),1) //Delete dead warriors from the army array
+            } 
         return damageSaxon;
     }
 
     saxonAttack(){
         let choosenViking= this.randomViking();
         let damageViking= choosenViking.receiveDamage(this.randomSaxon().strength);
-        if(choosenViking.health<=0){ this.vikingArmy.splice(this.vikingArmy.indexOf(choosenViking),1)} 
+            if(choosenViking.health<=0){ 
+                this.vikingArmy.splice(this.vikingArmy.indexOf(choosenViking),1)
+            } 
         return damageViking;
     }
-    
 
+    //superBonus generic attack method
+    attack(objetive){
+        objetive=objetive.toLowerCase();
+        switch(objetive){
+            case 'saxon':
+                let choosenSaxon= this.randomSaxon();
+                let damageSaxon= choosenSaxon.receiveDamage(this.randomViking().strength);
+                    if(choosenSaxon.health<=0){ this.saxonArmy.splice(this.saxonArmy.indexOf(choosenSaxon),1)} 
+                return damageSaxon; 
+
+            case 'viking':
+                let choosenViking= this.randomViking();
+                let damageViking= choosenViking.receiveDamage(this.randomSaxon().strength);
+                    if(choosenViking.health<=0){ this.vikingArmy.splice(this.vikingArmy.indexOf(choosenViking),1)} 
+                return damageViking;
+
+            default:
+                return `The objetive must be a viking or a saxon`
+
+        }
+    }
     
+    showStatus(){
+        if(this.vikingArmy.length === 0){
+          return `Saxons have fought for their lives and survived another day...`
+        }else if(this.saxonArmy.length === 0){
+            return `Vikings have won the war of the century!`
+        }else{
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+          
+    }  
 }
 
-//testing
-const viking1= new Viking('Bjorn',100,80);
+//Testing calls 
+
+const viking1= new Viking('Bjorn',100,70);
 const viking2= new Viking('Odin',100,100);
-const viking3= new Viking('Ragnar',100,80);
+const viking3= new Viking('Ragnar',80,70);
 
 const saxon1= new Saxon(100,100);
-const saxon2= new Saxon(100,50);
+const saxon2= new Saxon(100,70);
 const saxon3= new Saxon(100,50);
 
 const war1= new War();
@@ -119,14 +157,23 @@ war1.addSaxon(saxon1);
 war1.addSaxon(saxon2);
 war1.addSaxon(saxon3);
 
+/*
+console.log(war1.vikingAttack());
+console.log(war1.saxonAttack());
+console.log(war1.vikingAttack());
+console.log(war1.saxonAttack());
+console.log(war1.vikingAttack());
+console.log(war1.saxonAttack());
+*/
 
-console.log(war1.vikingAttack());
-console.log(war1.saxonAttack());
-console.log(war1.vikingAttack());
-console.log(war1.saxonAttack());
-console.log(war1.vikingAttack());
-console.log(war1.saxonAttack());
-
+console.log(war1.attack('viking'));
+console.log(war1.attack('saxon'));
+console.log(war1.attack('Viking'));
+console.log(war1.attack('Saxon'));
+console.log(war1.showStatus());
+console.log(war1.attack('Viking'));
+console.log(war1.attack('Saxon'));
+console.log(war1.showStatus());
 
 
 
