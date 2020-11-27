a3239de49026abc7a36f109bd454b163d855635f
Alicia Parra <alicia@redparra.com>
2020-11-27 20:46:33 +0100

    BetterCode V1. Fixes the error when an Army is empty before finishing the battle. Fixes the error when no warrior is added to the army

--- src/viking.js
+++ src/viking.js
@@ -105,28 +105,36 @@ class War {
 
     //superBonus generic attack method
     attack(objetive){
+        if(this.vikingArmy.length>=1 && this.saxonArmy.length>=1){
         objetive=objetive.toLowerCase();
-        switch(objetive){
-            case 'saxon':
-                let choosenSaxon= this.randomSaxon();
-                let damageSaxon= choosenSaxon.receiveDamage(this.randomViking().strength);
-                    if(choosenSaxon.health<=0){ this.saxonArmy.splice(this.saxonArmy.indexOf(choosenSaxon),1)} 
-                return damageSaxon; 
+            switch(objetive){
+                case 'saxon':
+                    let choosenSaxon= this.randomSaxon();
+                    let damageSaxon= choosenSaxon.receiveDamage(this.randomViking().strength);
+                        if(choosenSaxon.health<=0){ this.saxonArmy.splice(this.saxonArmy.indexOf(choosenSaxon),1)} 
+                    return damageSaxon; 
 
-            case 'viking':
-                let choosenViking= this.randomViking();
-                let damageViking= choosenViking.receiveDamage(this.randomSaxon().strength);
-                    if(choosenViking.health<=0){ this.vikingArmy.splice(this.vikingArmy.indexOf(choosenViking),1)} 
-                return damageViking;
+                case 'viking':
+                    let choosenViking= this.randomViking();
+                    let damageViking= choosenViking.receiveDamage(this.randomSaxon().strength);
+                        if(choosenViking.health<=0){ this.vikingArmy.splice(this.vikingArmy.indexOf(choosenViking),1)} 
+                    return damageViking;
 
-            default:
-                return `The objetive must be a viking or a saxon`
+                default:
+                    return `The objetive must be a viking or a saxon`
 
+            }
+        }
+        else{
+            return this.showStatus(); //If there are no warriors on the army there will be no attack and it will show the status of the battle.
         }
     }
     
     showStatus(){
-        if(this.vikingArmy.length === 0){
+        if(this.vikingArmy.length === 0 && this.saxonArmy.length === 0 ){ //In this case any warrior was added to the army
+            return `At least one warrior must join to the army`
+        }
+        else if(this.vikingArmy.length === 0){
           return `Saxons have fought for their lives and survived another day...`
         }else if(this.saxonArmy.length === 0){
             return `Vikings have won the war of the century!`
@@ -139,9 +147,9 @@ class War {
 
 //Testing calls 
 
-const viking1= new Viking('Bjorn',100,70);
-const viking2= new Viking('Odin',100,100);
-const viking3= new Viking('Ragnar',80,70);
+const viking1= new Viking('Bjorn',100,170);
+const viking2= new Viking('Odin',100,120);
+const viking3= new Viking('Ragnar',80,150);
 
 const saxon1= new Saxon(100,100);
 const saxon2= new Saxon(100,70);
@@ -170,10 +178,12 @@ console.log(war1.attack('viking'));
 console.log(war1.attack('saxon'));
 console.log(war1.attack('Viking'));
 console.log(war1.attack('Saxon'));
-console.log(war1.showStatus());
 console.log(war1.attack('Viking'));
 console.log(war1.attack('Saxon'));
-console.log(war1.showStatus());
+console.log(war1.attack('viking'));
+console.log(war1.attack('saxon'));
+
+
 
 
 
