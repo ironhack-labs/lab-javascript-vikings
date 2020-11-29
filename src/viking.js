fd18724771626ccd2f06cf17ba98037d6f4b428e
Carlos Martín <karuvsentor@gmail.com>
2020-11-29 13:42:54 +0100

    fixed bug in iteration 2

--- src/viking.js
+++ src/viking.js
@@ -19,7 +19,7 @@ class Viking extends Soldier {
         this.name = name;
     }
 
-    receiveAttack(damage) {
+    receiveDamage(damage) {
         this.health -= damage;
       
         if (this.health > 0){
