9574da35c035b12fc6b36c04be1cae39297f60e6
Pedro Frias <plfriasf@gmail.com>
2020-12-02 04:59:34 -0800

    SUPER BONUS: showStatus() ready

--- src/viking.js
+++ src/viking.js
@@ -82,5 +82,15 @@ class War {
     }
     return result;
   }
-  showStatus() {}
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else if (this.vikingArmy.length === this.saxonArmy.length) {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
 }
+
+//refactoring the saxon/viking atack method
