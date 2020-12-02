4b487b199955425e91d4a8ebe21bdd85b8823d18
Nicolas del Brio <nicodelbrio@telefonica.net>
2020-12-02 18:50:54 +0100

    iteration 2&3 // DONE

--- src/viking.js
+++ src/viking.js
@@ -15,8 +15,38 @@ class Soldier {
 // Viking
 class Viking {}
 
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
+
 // Saxon
 class Saxon {}
 
+class Viking extends Soldier {
+    receiveDamage(damage) {
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return 'A Saxon has died in combat'
+        }
+    }
+}
+
 // War
 class War {}
+
