0292ce678b8424ee7e48b1d74e86406decc84789
Gaston <gastonsalimbene@gmail.com>
2021-01-15 21:47:42 +0100

    lab-js-vikings

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){ 
+    this.health =health;
+    this.strength =strength;
+    this.attack = function (){
+        return strength;
+    }; 
+    this.receiveDamage = function(damage){
+        this.health =this.health - damage;
+}
+}}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super(health, strength);
+        this.name= name;
+        this.strength;
+        this.receiveDamage = function(damage){
+            this.health =this.health - damage;  
+            if (this.health > 0) {
+                return `${this.name} has received ${damage} points of damage`;
+            } else {
+                return `${this.name} has died in act of combat`;
+            }
+        };
+        this.battleCry = function (){
+            return "Odin Owns You All!";
+        };
+
+        
+}};
+
+
 
 // Saxon
-class Saxon {}
+
+class Saxon extends Soldier {
+        receiveDamage = function(damage){
+            this.health= this.health - damage;
+            if (this.health > 0) {
+                return `A Saxon has received ${damage} points of damage`
+            } else {
+                return `A Saxon has died in combat`
+            };
+        };
+    }
+
+
 
 // War
-class War {}
+
