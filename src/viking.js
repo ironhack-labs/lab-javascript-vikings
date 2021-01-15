b314d920d03dcd750b74b12e12036160f75d07fa
Jaime <jaimeferncast@gmail.com>
2021-01-15 17:07:02 +0100

    incluido test del superbonus

--- src/viking.js
+++ src/viking.js
@@ -69,35 +69,35 @@ class War {
         this.saxonArmy.push(saxon)
     }
 
-    vikingAttack() {
-        const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
-        const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
-        const vikingRandom = this.vikingArmy[vikingIndex]
-        const saxonRandom = this.saxonArmy[saxonIndex]
-        const damage = saxonRandom.receiveDamage(vikingRandom.strength)
+    // vikingAttack() {
+    //     const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+    //     const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
+    //     const vikingRandom = this.vikingArmy[vikingIndex]
+    //     const saxonRandom = this.saxonArmy[saxonIndex]
+    //     const damage = saxonRandom.receiveDamage(vikingRandom.strength)
         
-        if (damage === `A Saxon has died in combat`) {
-            this.saxonArmy.splice(saxonIndex, 1)
-            return damage
-        } else {
-            return damage
-        }
-    }
+    //     if (damage === `A Saxon has died in combat`) {
+    //         this.saxonArmy.splice(saxonIndex, 1)
+    //         return damage
+    //     } else {
+    //         return damage
+    //     }
+    // }
 
-    saxonAttack() {
-        const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
-        const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
-        const saxonRandom = this.saxonArmy[saxonIndex]
-        const vikingRandom = this.vikingArmy[vikingIndex]
-        const damage = vikingRandom.receiveDamage(saxonRandom.strength)
+    // saxonAttack() {
+    //     const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
+    //     const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+    //     const saxonRandom = this.saxonArmy[saxonIndex]
+    //     const vikingRandom = this.vikingArmy[vikingIndex]
+    //     const damage = vikingRandom.receiveDamage(saxonRandom.strength)
         
-        if (damage === `${vikingRandom.name} has died in act of combat`) {
-            this.vikingArmy.splice(vikingIndex, 1)
-            return damage
-        } else {
-            return damage
-        }
-    }
+    //     if (damage === `${vikingRandom.name} has died in act of combat`) {
+    //         this.vikingArmy.splice(vikingIndex, 1)
+    //         return damage
+    //     } else {
+    //         return damage
+    //     }
+    // }
 
     
     showStatus() {
@@ -109,31 +109,40 @@ class War {
             return `Vikings and Saxons are still in the thick of battle.`
         }
     }
+    // attack(tribe) {
+    //     const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
+    //     const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
+    //     const saxonRandom = this.saxonArmy[saxonIndex]
+    //     const vikingRandom = this.vikingArmy[vikingIndex]
+    //     let damage
+    //     switch (tribe) {
+    //         case "vikingAttack":
+    //             damage = saxonRandom.receiveDamage(vikingRandom.strength)
+    //             if (damage === `A Saxon has died in combat`) {
+    //                 this.saxonArmy.splice(saxonIndex, 1)
+    //                 return damage
+    //             } else {
+    //                 return damage
+    //             }
+            
+    //         case "saxonAttack":
+    //             damage = vikingRandom.receiveDamage(saxonRandom.strength)
+    //             if (damage === `${vikingRandom.name} has died in act of combat`) {
+    //                 this.vikingArmy.splice(vikingIndex, 1)
+    //                 return damage
+    //             } else {
+    //                 return damage
+    //             }
+    //         }
+    // }
 }
 
-// attack(tribe) {
-//     const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length)
-//     const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length)
-//     const saxonRandom = this.saxonArmy[saxonIndex]
-//     const vikingRandom = this.vikingArmy[vikingIndex]
 
-//     switch (tribe) {
-//         case "vikingAttack":
-//             const damage = saxonRandom.receiveDamage(vikingRandom.strength)
-//             if (damage === `A Saxon has died in combat`) {
-//                 this.saxonArmy.splice(saxonIndex, 1)
-//                 return damage
-//             } else {
-//                 return damage
-//             }
-        
-//         case "saxonAttack":
-//             const damage = vikingRandom.receiveDamage(saxonRandom.strength)
-//             if (damage === `${vikingRandom.name} has died in act of combat`) {
-//                 this.vikingArmy.splice(vikingIndex, 1)
-//                 return damage
-//             } else {
-//                 return damage
-//             }
-//         }
-// }
+// const newWar = new War()
+// const saxon1 = new Saxon(20, 3)
+// const viking1 = new Viking(`pepe`, 40, 6)
+// newWar.addViking(viking1)
+// newWar.addSaxon(saxon1)
+// newWar.attack(`vikingAttack`)
+// console.log(saxon1.health)
+
