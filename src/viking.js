618b649da57bcf2436531af68261ca5ea9736bd5
Felipe Freitas <afelipe.freitas@gmail.com>
2021-01-15 18:48:47 -0300

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,85 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+    let pickedSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let pickedViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+    this.saxonArmy[pickedSaxon].receiveDamage(
+      this.vikingArmy[pickedViking].strength
+    );
+    if (this.saxonArmy[pickedSaxon].health <= 0) {
+      this.saxonArmy.pop();
+      return "A Saxon has died in combat";
+    } else {
+      return `A Saxon has received ${this.vikingArmy[pickedVikingn].strength} points of damage`;
+    }
+  }
+  saxonAttack() {
+    let pickedSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let pickedViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+    this.vikingArmy[pickedViking].receiveDamage(
+      this.saxonArmy[pickedSaxon].strength
+    );
+
+    if (this.vikingArmy[pickedViking].health <= 0) {
+      this.vikingArmy.pop();
+      return "A Viking has died in combat";
+    } else {
+      return `${this.vikingArmy[pickedSaxon].name} has received ${this.saxonArmy[pickedSaxon].strength} points of damage`;
+    }
+  }
+  showStatus() {}
+}
