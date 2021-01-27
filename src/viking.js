2bafbee301134888ab5a243fb15c455662674577
unknown <ionut_vasile85@hotmail.com>
2021-01-26 23:29:04 -0500

    creates VikingAtack and SaxonAtack

--- src/viking.js
+++ src/viking.js
@@ -53,5 +53,67 @@ class Saxon extends Soldier{
 
     }
 
-// War
-class War {}
+
+    // War
+
+function getRandInteger(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
+    class War {
+    vikingArmy = []
+    saxonArmy = []
+
+        addViking(barbarian){
+            this.vikingArmy.push(barbarian)
+        }
+
+        addSaxon(frank){
+            this.saxonArmy.push(frank)
+        }
+
+    
+
+        vikingAttack(){
+            let soldierAttacking = getRandInteger(this.vikingArmy.length)
+            console.log(soldierAttacking)
+            let soldierDef = getRandInteger(this.saxonArmy.length)
+            console.log(soldierDef)
+
+            console.log(
+            this.saxonArmy[soldierDef].receiveDamage(this.vikingArmy[soldierAttacking].strength)
+
+            )
+
+            if(this.saxonArmy[soldierDef].health <= 0)
+            {
+                this.saxonArmy.splice(soldierDef,1);
+                return 'A Saxon has died in combat'
+            }else{
+                return `A Saxon has received ${this.vikingArmy[soldierAttacking].strength} points of damage`;
+            }
+        }
+
+        saxonAttack(){
+            let soldierAttacking = getRandInteger(this.saxonArmy.length)
+            let soldierDef = getRandInteger(this.vikingArmy.length)
+
+            this.vikingArmy[soldierDef].receiveDamage(this.saxonArmy[soldierAttacking].strength)
+            
+            if(this.vikingArmy[soldierDef].health <= 0)
+            {
+                this.vikingArmy.splice(soldierDef,1);
+                return 'A Saxon has died in combat'
+            }else{
+                return `${this.vikingArmy[soldierDef].name} has received ${this.saxonArmy[soldierAttacking].strength} points of damage`;
+            }
+
+        }
+
+
+
+        showStatus(){
+
+        }
+
+
+}
