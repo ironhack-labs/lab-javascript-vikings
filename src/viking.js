99925f94f8af1baf2df17c2714f98c4603b10fbc
Henrique Morikawa <henriqueyukiharu@gmail.com>
2021-01-30 14:02:09 -0300

    super bonus

--- src/viking.js
+++ src/viking.js
@@ -58,27 +58,31 @@ class War {
         this.saxonArmy.push(Saxon)
     }
     vikingAttack(){
+        return this.superBonus('viking')
+    }
+    saxonAttack(){
+        return this.superBonus('saxon')
+    }
+
+    superBonus(character){
         const randomViking = Math.floor(Math.random()*this.saxonArmy.length);
         const randomSaxon = Math.floor(Math.random()*this.vikingArmy.length);
+        let result = 0;
+        
+        if(character === 'saxon'){
+            result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
+        } else if(character === 'viking'){
+            result = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+        }
 
-        let result = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
-
-        if(this.saxonArmy[randomSaxon].health <= 0){
+        if(this.vikingArmy[randomViking].health <= 0){
+            this.vikingArmy.splice(randomViking, 1)
+        } else if(this.saxonArmy[randomSaxon].health <= 0){
             this.saxonArmy.splice(randomSaxon, 1)
         }
         return result
     }
-    saxonAttack(){
-        const randomViking = Math.floor(Math.random()*this.saxonArmy.length);
-        const randomSaxon = Math.floor(Math.random()*this.vikingArmy.length);
 
-        let result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
-
-        if(this.vikingArmy[randomViking].health <= 0){
-            this.vikingArmy.splice(randomViking, 1)
-        }
-        return result
-    }
     showStatus(){
         if(this.saxonArmy.length === 0){
             return "Vikings have won the war of the century!"
