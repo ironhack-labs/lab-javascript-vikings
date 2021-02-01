a5f249d8d9d59e3ecc62eba8028f72825acd5fc7
Tomsylva <Tomsylvanz@gmail.com>
2021-02-01 15:09:30 +0100

    started bonuses

--- src/viking.js
+++ src/viking.js
@@ -17,6 +17,7 @@ class Soldier {
 // Viking 
 // I don't know why the "viking" doesn't inherit this.health & this.strength from Soldier
 // I got this to pass Jasmine by hard coding but would like to know how to do this dynamically
+
 class Viking extends Soldier{
     constructor(name, health, strength){
         super(name);
