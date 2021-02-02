375d0e3886d8e4dcf25a64d37d38f0ca1242d046
deboranis <dc.nisen@hotmail.com>
2021-02-02 10:39:32 -0300

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,57 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return 'A Saxon has died in combat'
+        }
+    }
+}
 
-// War
-class War {}
+// // War
+// class War {
+//     addViking()
+//     addSaxon()
+//     saxonAttack()
+//     vikingAttack()
+//     showStatus()
+// }
