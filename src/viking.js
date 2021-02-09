a374df8f4f3472b061515ca12b3db43b9ddfe588
Naomi Yamanaka <n.yamanaka70@gmail.com>
2021-02-09 19:24:25 +0100

    I did my best but i couldn't finish all

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-class Soldier {}
+class Soldier {
+ constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+ }
+  attack () {
+      return this.strength;
+  }
+  receiveDamage (damage){
+    this.health = this.health - damage;
+  }
+}  
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+   constructor(name,strength,health) {
+   super(strength,health);
+   this.name = name; 
+}
+  attack (){
+      return this.strength;
+  }
+   receiveDamage (damage){
+       this.health = this.health - damage;
+       if (this.health > 0){
+        return `${this.name} has received ${damage} points of damage`
+     } else {
+        return `${this.name} has died in act of combat`
+     }
+   }
+   battleCry(){
+       return 'Odin Owns You All!'
+   }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(strength,health) {
+    super(strength,health)
+}
+
+attack (){
+    return this.strength;
+}
+receiveDamage(damage){
+
+    this.health = this.health - damage;
+       if (this.health > 0){
+        return `${this.name} has received ${damage} points of damage`
+     } else {
+        return `${this.name} has died in act of combat`
+     }
+   }
+}
+
 
 // War
-class War {}
+class War {
+    
+}
