ab5ed0524f42c2f89895457e772407ec3218eb30
Yuka TANAKA <seien_0823@icloud.com>
2021-02-09 18:50:25 +0100

    there are still 13 to solve, I just push what I've done:

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(a, b) {
+
+ this.health = a;   
+ this.strength = b;
+  };
+
+  attack(){
+  
+    return  this.strength;
+  };
+
+  receiveDamage(thedamage){
+   this.health -= thedamage;
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+constructor(name,a,b){
+  super(a,b);
+  this.name = name;
+}
+
+receiveDamage(thedamage){
+  
+  this.health -= thedamage;
+ 
+ if(super.receiveDamage> thedamage)
+ {
+   return `${this.name } has been received  ${thedamage} + points of damage`
+} 
+else if( super.receiveDamage < thedamage)
+{
+return this.name +"has died in act of combat"
+}
+}
+
+battleCry(){
+  return 'Odin Owns You All!'
+}
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(a,b){
+    super(a,b,)
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(vikAmy){
+    this.vikingArmy.unshift(vikAmy);
+  }
+
+  addSaxon(saxAmy){
+    this.saxonArmy.unshift(saxAmy);
+  }
+
+  vikingAttack(){
+    this.saxonArmy.pop();
+    // Saxon.super.receiveDamage(thedamage) === Viking.super.attack();
+  }
+
+  saxonAttack(){
+
+    this.vikingArmy.pop();
+    // Viking.super.receiveDamage(thedamage) === Saxon.super.attack();
+  }
+}
