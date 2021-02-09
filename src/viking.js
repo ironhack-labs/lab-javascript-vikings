330c395ac3d7a99c7575da577008569dd8c06d0a
Anna Lefour <anna.lefour@laposte.net>
2021-02-09 17:56:33 +0100

    Done until 2nd test of bonus

--- src/viking.js
+++ src/viking.js
@@ -1,52 +1,75 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {
-        this.health = health;
-        this.strength = strength;
-    }
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
-    attack() {
-        return this.strength;
-    }
+  attack() {
+    return this.strength;
+  }
 
-    receiveDamage(damage) {
-        this.health = this.health - damage;
-    }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
 }
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health, strength) {
-        super(health, strength)
-        this.name = name;
-    }
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
 
-    receiveDamage(damage) {
-        this.health = this.health - damage;
-        if (this.health > 0) {
-            return `${this.name} has received ${damage} points of damage`
-        } else {
-            return `${this.name} has died in act of combat`
-        }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
     }
+  }
 
-    battleCry() {
-        return "Odin Owns You All!"
-    }
-
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 }
 
 // Saxon
 class Saxon extends Soldier {
-    receiveDamage(damage) {
-        this.health = this.health - damage;
-        if (this.health > 0) {
-            return `A Saxon has received ${damage} points of damage`
-        } else {
-            return `A Saxon has died in combat`
-        }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
     }
+  }
 }
 
 // War
-class War {}
+class War {
+  constructor() {}
+
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(saxon) {
+      this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack(){
+    receiveDamage(damage) {
+        
+  }
+}
+}
+
+  // saxonAttack()
+
+  // showStatus()
+// }
