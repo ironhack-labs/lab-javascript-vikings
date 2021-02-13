70bb011749bf963d8faebe9432ee65d739b1d2e8
Daniela Alonzo <monsterjunior@outlook.es>
2021-02-13 12:04:27 -0600

    done

--- src/viking.js
+++ src/viking.js
@@ -49,24 +49,59 @@ class Saxon extends Soldier{
 // War
 class War {
     constructor() {
-        this.vikingArmy=vikingArmy
-        this.saxonArmy=saxonArmy
+      this.vikingArmy = [];
+      this.saxonArmy = [];
     }
-    addViking(vikingObject){
-        this.vikingObject=vikingObject
-
+    addViking(viking) {
+      this.vikingArmy.push(viking);
     }
-    addSaxon(saxonObject){
-        this.saxonObject=saxonObject
+    addSaxon(saxon) {
+      this.saxonArmy.push(saxon);
     }
-
+    vikingAttack() {
+      var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+      var saxonSoldier = this.saxonArmy[randomSaxon];
+      var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+      var vikingSoldier = this.vikingArmy[randomViking];
+      var result = saxonSoldier.receiveDamage(vikingSoldier.attack());
+      if (saxonSoldier.health <= 0) {
+        this.saxonArmy.splice(randomSaxon, 1);
+      }
+      return result;
+    }
+    saxonAttack() {
+      var randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+      var saxonSoldier = this.saxonArmy[randomSaxon];
+      var randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+      var vikingSoldier = this.vikingArmy[randomViking];
+      var result = vikingSoldier.receiveDamage(saxonSoldier.attack());
+      if (vikingSoldier.health <= 0) {
+        this.vikingArmy.splice(randomViking, 1);
+      }
+      return result;
+    }
+showStatus() {
+      if (this.saxonArmy.length === 0) {
+        return `Vikings have won the war of the century!`;
+      } else if (this.vikingArmy.length === 0) {
+        return `Saxons have fought for their lives and survived another day...`;
+      } else {
+        return `Vikings and Saxons are still in the thick of battle.`;
+      }
+    }
+    // SUPER BONUS
+    attack(defendersArmy, attackersArmy) {
+      let defender = defendersArmy[Math.floor(Math.random() * defendersArmy.length)];
+      let attacker = attackersArmy[Math.floor(Math.random() * attackersArmy.length)];
+      if(defender.health <= attacker.strength) {
+        defendersArmy.splice(defendersArmy.indexOf(defender), 1);
+      }
+      return defender.receiveDamage(attacker.strength);
+    }
+    saxonAttacking() {
+      return this.attack(this.vikingArmy, this.saxonArmy);
+    }
+    vikingAttacking() {
+      return this.attack(this.saxonArmy, this.vikingArmy);
+    }
 }
-
-class Armies {
-    constructor(){
-        
-    }
-}
-
-
-
