e56b12687267d12d4d25134504d6a71bc5586052
Luciano Colabraro <luciano.colabraro@gmail.com>
2021-03-03 18:27:25 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -9,16 +9,97 @@ class Soldier {
     }
     receiveDamage(damage) {
        
-        return this.health - receiveDamage;
+        this.health = this.health - damage;
+    }
+}
+
+// Viking
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super (health, strength);
+        this.name = name;
+    } 
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+}
+
+// Saxon
+class Saxon extends Soldier {
+
+    receiveDamage(damage) {
+        this.health = this.health - damage
+          if (this.health > 0) {
+              return `A Saxon has received ${damage} points of damage`;
+          } 
+          else {
+              return `A Saxon has died in combat`;
+          }
     }
 
 }
 
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
 // War
-class War {}
+class War {
+    constructor () {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    
+    }
+
+addViking(viking) {
+    this.vikingArmy.push(viking);
+   
+    }
+
+addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+
+}
+
+vikingAttack () {
+
+
+   let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+   let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+   let result = randomSaxon.receiveDamage(randomViking.strength);
+
+   if (randomSaxon.health <=0) {
+       this.saxonArmy.pop(randomSaxon);
+   }
+
+   return result;
+
+
+}
+
+saxonAttack() {
+
+    let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+    let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+    let result = randomViking.receiveDamage(randomSaxon.strength);
+
+    if (randomViking.health <=0) {
+        this.vikingArmy.pop(randomViking);
+    }
+
+    return result;
+        
+
+    
+}
+}
+
