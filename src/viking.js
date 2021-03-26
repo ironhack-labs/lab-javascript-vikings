6848cf0701a228fbd07d031f92481a44cc04519d
Raquel Rodriguez <srtademaciel@gmail.com>
2021-03-26 20:58:39 +0100

    first bonus

--- src/viking.js
+++ src/viking.js
@@ -4,7 +4,7 @@ class Soldier {
         this.health = health,
         this.strength = strength
     }
-    attack() {
+    attack(){
         return this.strength
     }
     receiveDamage(damage) {
@@ -45,4 +45,47 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack() {
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        
+        /* const warDamage = randomSaxon.receiveDamage(randomViking.strength)
+        if (randomSaxon.health <= 0){
+            return this.saxonArmy.splice(randomSaxon, 1)
+        }
+        return warDamage */
+        const warDamage = randomSaxon.receiveDamage(randomViking.strength)
+        
+        if(randomSaxon.health <= 0) {
+            this.saxonArmy.splice(randomSaxon, 1)
+        }
+        return warDamage
+    }
+
+    saxonAttack() {
+        
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        
+        const warDamage = randomViking.receiveDamage(randomSaxon.strength)
+        
+        if(randomViking.health <= 0) {
+            this.vikingArmy.splice(randomViking, 1)
+        }
+        return warDamage
+    }
+}
+
