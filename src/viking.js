7cdb07c1f7822d333eebe7aa3c6b61dbd5bfe2c2
Andrea Sforza <andrearobertosforza@gmail.com>
2021-03-26 15:57:03 +0100

    Removed Saxon class constructor as per exercise instructions

--- src/viking.js
+++ src/viking.js
@@ -40,10 +40,6 @@ class Viking extends Soldier {
 
 // Saxon
 class Saxon extends Soldier {
-    constructor(health, strength) {
-        super(health, strength);
-    }
-
     receiveDamage(damage) {
         this.health -= damage;
 
