b10a3c53c5e413ba7b3e1952aa0070ef57691a6b
Susana Prado <susana.prado01@gmail.com>
2021-03-26 16:17:56 +0100

    finish iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,32 @@
-// Soldier
-class Soldier {}
+// Iteration 1 - Soldier
 
-// Viking
-class Viking {}
+let soldier;
+const strength = 150;
+const health = 300;
 
-// Saxon
-class Saxon {}
+class Soldier {
+  constructor(health, strength) {
+    (this.health = health), (this.strength = strength);
+  }
 
-// War
-class War {}
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    if (damage > 0) {
+      this.health -= damage;
+    }
+  }
+}
+
+//   soldier = new Soldier(health, strength);
+//   const strength = 150;
+//   const health = 300;
+
+// Iteration 2 - Viking
+// 
+// // Iteration 3 - Saxon
+
+// // War
+// class War {}
