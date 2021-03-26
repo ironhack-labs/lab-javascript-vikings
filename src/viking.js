2acf7a4acead387643346af84643fb24b8e0989f
Taisia Makhmutova <taia.makh@gmail.com>
2021-03-26 14:10:09 +0100

    ADD iteration 1 to 4

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        if(damage < this.health){
+            this.health -= damage;
+            return `${this.name} has received ${damage} points of damage`
+        }   
+        else if(damage >= this.health){
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+        super(health, strength);
+        this.health = health;
+        this. strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        if(damage <this.health){
+            this.health -= damage;
+            return `A Saxon has received ${damage} points of damage`
+        }   
+        else if(damage >= this.health){
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    addViking(){
+
+    }
+    addSaxon(){
+
+    }
+    vikingAttack(){
+
+    }
+    saxonAttack(){
+
+    }
+    showStatus(){
+
+}
+}
