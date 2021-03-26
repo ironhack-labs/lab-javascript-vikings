0f65c01f787ffb7c42a9e0191ea197cbccc69f02
QuimRobres <quim.robres@gmail.com>
2021-03-26 16:22:08 +0100

    from first iteration to part of iteration 2

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,29 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  attack() {
+    super.attack();
+    return this.strength;
+  }
+}
 
 // Saxon
 class Saxon {}
