a7966cf7b92af39232dd2c7447fbd1483b2adb39
Adrian <adrian.asencio92@hotmail.com>
2021-03-26 17:30:35 +0100

    terminado faltan bonus.

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(num) {
+        this.health -= num;
+
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+    receiveDamage(num) {
+        this.health -= num;
+        if (this.health <= 0) {
+            return `${this.name} has died in act of combat`;
+        } else {
+            return `${this.name} has received ${num} points of damage`;
+        }
+    }
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+    attack() {
+        super.attack();
+        return this.strength;
+    }
+    receiveDamage(num) {
+        this.health -= num;
+        if (this.health <= 0) {
+            return `A Saxon has died in combat`;
+        } else {
+            return `A Saxon has received ${num} points of damage`;
+        }
+    }
+}
+
+
+
+
 
 // War
 class War {}
