49149aabbee31168d3f1c6a742342eab34e5bf5e
Silinde87 <paurodriguezmolina@gmail.com>
2021-03-26 16:38:15 +0100

    Done Bonus Iteration 4 until VikingAttack()

--- src/viking.js
+++ src/viking.js
@@ -36,4 +36,32 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+	constructor(){
+		this.vikingArmy = [];
+		this.saxonArmy = [];
+	}
+
+	addViking(viking){
+		this.vikingArmy.push(viking);
+	}
+	addSaxon(saxon){
+		this.saxonArmy.push(saxon);
+	}
+	vikingAttack(){
+		let indexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+		let randomSaxon = this.saxonArmy[indexSaxon];
+		let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+		let output = randomSaxon.receiveDamage(randomViking.strength);
+		if(randomSaxon.health <= 0){
+			this.saxonArmy.splice(indexSaxon, 1);
+		} 
+		return output;
+	}
+	saxonAttack(){
+
+	}
+	showStatus(){
+
+	}
+}
