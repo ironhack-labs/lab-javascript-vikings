ced98eabfa31bb7ea3e30f165c6e27b33958e3ff
Susana Prado <susana.prado01@gmail.com>
2021-03-26 16:30:11 +0100

    finish iteration 2

--- src/viking.js
+++ src/viking.js
@@ -25,7 +25,35 @@ class Soldier {
 //   const health = 300;
 
 // Iteration 2 - Viking
-// 
+
+// let viking;
+// const name = 'Harald';
+// const strength = 150;
+// const health = 300;
+
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength);{
+      this.name = name;
+    }
+  }
+
+  receiveDamage(damage) {
+    if (damage > 0) {
+      this.health -= damage;
+      if (this.health > 0){
+        return `${this.name} has received ${damage} points of damage`;
+      } else {
+        return `${this.name} has died in act of combat`;
+      }
+    }
+  }
+
+  battleCry(){
+    return "Odin Owns You All!";
+  }
+}
+
 // // Iteration 3 - Saxon
 
 // // War
