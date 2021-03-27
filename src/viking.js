60ae32a710ce743dadf0f7432a4f1f0ab14d1f27
Bea Avis <bea.avis.p@gmail.com>
2021-03-27 12:43:48 +0100

    Done Iteration 2

--- src/viking.js
+++ src/viking.js
@@ -12,3 +12,25 @@ class Soldier {
     this.health -= damage;
   }
 }
+
+//Iteration 2
+// Viking
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage); //Health is modified
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
