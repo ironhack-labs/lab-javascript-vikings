a0a31c831ef33253d15bd915cead405b5c8611e4
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-03-27 11:44:37 +0100

    ADD Bonus

--- src/viking.js
+++ src/viking.js
@@ -26,7 +26,7 @@ class Viking extends Soldier{
         this.health -= damage;
         if (this.health > 0) {
             return `${this.name} has received ${damage} points of damage`;
-        } else if (this.health === 0) {
+        } else if (this.health <= 0) {
             return `${this.name} has died in act of combat`;
         }
     }
@@ -45,14 +45,71 @@ class Saxon extends Soldier {
         this.health -= damage;
         if (this.health > 0) {
             return `A Saxon has received ${damage} points of damage`;
-        } else if (this.health === 0) {
+        } else if (this.health <= 0) {
             return 'A Saxon has died in combat';
         }
     }
 }
 
 // War
-class War {}
+
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+
+    }
+    vikingAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let attackResult = randomSaxon.receiveDamage(randomViking.strength);
+        this.saxonArmy = this.saxonArmy.filter(saxon => {
+            return saxon.health > 0;
+        });
+        return attackResult;
+    }
+    saxonAttack() {
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let attackResult = randomViking.receiveDamage(randomSaxon.strength);
+        this.vikingArmy = this.vikingArmy.filter(viking => {
+            return viking.health > 0;
+        });
+        return attackResult;
+    }
+
+    showStatus() {
+
+    }
+}
+let war = new War();
+let viking = new Viking('Ragnar', 100, 10);
+war.addViking(viking);
+war.addViking(viking);
+war.addViking(viking);
+
+let saxon = new Saxon(50, 5);
+war.addSaxon(saxon);
+war.addSaxon(saxon);
+war.addSaxon(saxon);
+
+war.vikingAttack();
+war.vikingAttack();
+war.vikingAttack();
+
+war.saxonAttack();
+war.saxonAttack();
+war.saxonAttack();
+
+
 
 
 
