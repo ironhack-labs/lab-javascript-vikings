3ac073c9e96df4a46ac3a628abaf17d221d781f7
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-04-10 18:57:41 +0200

    iteration 4: SaxonAttack

--- src/viking.js
+++ src/viking.js
@@ -5,7 +5,7 @@ class Soldier {
         this.strength = strength
     }
 
-    attack(){
+    attack() {
         return this.strength;
     }
 
@@ -15,37 +15,90 @@ class Soldier {
 
 }
 
-const soldier = new Soldier(100,100);
+const soldier = new Soldier(100, 100);
 
 // Viking
 class Viking extends Soldier {
-    constructor(name, health, strength){
-        super(health,strength);
+    constructor(name, health, strength) {
+        super(health, strength);
         this.name = name;
     }
 
-    receiveDamage(damage){
-        const life = this.health -= damage
-        if (life <= 0){
-            return `${this.name} has died in act of combat.`
+    receiveDamage(damage) {
+        this.health -= damage
+        const life = this.health
+        if (life > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else if (life <= 0) {
+            return `${this.name} has died in act of combat`
         }
-        return `${this.name} has received ${damage} points of damage.`
     }
 
-    battleCry(){
-        return "Odin Owns You All!!";
+    battleCry() {
+        return "Odin Owns You All!";
     }
 
 }
 
-const fromen = new Viking ("Fromen", 50, 50);
+const fromen = new Viking("Fromen", 50, 50);
 
 // Saxon
-class Saxon extends Soldier{
-    constructor(){
-        
+class Saxon extends Soldier {
+
+    receiveDamage(damage) {
+        this.health -= damage
+        const life = this.health
+        if (life > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else if (life <= 0) {
+            return `A Saxon has died in combat`
+        }
     }
+
 }
 
 // War
-class War {}
+class War {
+
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack() {
+        const saxonRandom = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        const vikingRandom = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        const atackRestult = saxonRandom.receiveDamage(vikingRandom.strength)
+        if (saxonRandom.health <= 0) {
+            const index = this.saxonArmy.indexOf(saxonRandom)
+            this.saxonArmy.splice(index, 1)
+        }
+        return atackRestult;
+    }
+
+    saxonAttack() {
+        const saxonRandom = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        const vikingRandom = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        const atackRestult = saxonRandom.receiveDamage(vikingRandom.strength)
+        if (saxonRandom.health <= 0) {
+            const index = this.saxonArmy.indexOf(saxonRandom)
+            this.saxonArmy.splice(index, 1)
+        }
+        return atackRestult;
+    }
+
+    showStatus() {
+
+    }
+
+}
+
+const warCraft = new War();
