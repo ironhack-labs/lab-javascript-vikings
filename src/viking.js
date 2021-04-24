ca1d564461b2590d6bd1cbeb96735d4bf496ee4a
Cesar Carreras <cesar.carreras95@gmail.com>
2021-04-24 15:34:56 -0500

    done with Iterations and half bonuses, I will update them later

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,83 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength;
+    }
+    
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+
+      super(health, strength)
+        this.name = name
+        
+    }
+
+      receiveDamage(damage){
+        this.health -= damage
+        if(this.health >= 1){
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+        
+    }
+
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+    attack(){
+        return this.strength
+    }
+
+    receiveDamage(damage){
+        this.health -= damage
+        if(this.health >= 1){
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+
+/* should return "A Saxon has received DAMAGE points of damage", 
+ if the Saxon is still alive
+ should return "A Saxon has died in combat", if the Saxon dies
+        */
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }  
+    
+    addViking(Viking){
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack(){}
+    saxonAttack() {}
+    showStatus() {}
+    
+}
