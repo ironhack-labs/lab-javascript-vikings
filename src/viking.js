b113ec6bb3432cedb7ea5f7aebe68bb5fa66adad
Cesar Carreras <cesar.carreras95@gmail.com>
2021-04-26 12:34:20 -0500

    Updated

--- src/viking.js
+++ src/viking.js
@@ -42,10 +42,6 @@ class Viking extends Soldier {
 // Saxon
 class Saxon extends Soldier{
 
-    attack(){
-        return this.strength
-    }
-
     receiveDamage(damage){
         this.health -= damage
         if(this.health >= 1){
@@ -63,17 +59,49 @@ class War {
         this.saxonArmy = []
     }  
     
-    addViking(Viking){
-        this.vikingArmy.push(Viking)
+    addViking(viking){
+        this.vikingArmy.push(viking)
     }
 
-    addSaxon(Saxon){
-        this.saxonArmy.push(Saxon)
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
     }
 
 
-    vikingAttack(){}
-    saxonAttack() {}
+    vikingAttack(){
+
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)] 
+
+        let result = randomSaxon.receiveDamage(randomViking.strength)
+
+        randomSaxon.receiveDamage(randomViking.strength)
+            if(randomSaxon.health <= 0){
+            this.saxonArmy.splice(randomSaxon,1)
+            }
+
+        return result
+    
+    }
+
+
+    saxonAttack() {
+
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)] 
+
+        let result = randomViking.receiveDamage(randomSaxon.strength)
+
+        randomViking.receiveDamage(randomSaxon.strength)
+            if(randomViking.health <= 0){
+            this.vikingArmy.splice(randomViking,1)
+        }
+
+        return result
+
+    }
+
+
     showStatus() {}
     
 }
