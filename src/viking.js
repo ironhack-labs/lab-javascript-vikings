ec3b7f0d4eb3c0f836cfbdc17e5038a48f00f4d6
jonathanbbarbosa <jonathanbbarbosa@gmail.com>
2021-04-27 18:42:43 -0500

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier 
+{
+    constructor(health,strength)
+    {
+        this.health=health;
+        this.strength=strength;
+    }
+    attack()
+    {
+        return this.strength;
+    }
+    receiveDamage(damage)
+    {
+        this.health-=damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier
+{
+    constructor(name,health,strength)
+    {
+        this.name=name;
+        super(health,strength)  
+    }
+    receiveDamage(damage)
+    {
+        this.health-=damage;
+        if(Viking.health>0)
+        {
+            return `${this.name} has received ${this.damage} pints of damage`
+        }
+        else
+        {
+            return `${this.name} has died in combat`
+        }
+    }
+    battleCry()
+    {
+        return "Odin Owns You All!"
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier
+{
+    attack()
+    {
+        return this.strength
+    }
+    receiveDamage(_damage)
+    {
+        return `Saxon has received ${this.damage} pints of damage`
+    }
+    else
+    {
+        return `A Saxon has died in combat`
+    }
+}
 
 // War
-class War {}
+class War 
+{
+    constructor()
+    {
+        this.saxonArmy=[]
+        this.vikingArmy=[]
+    }
+    addViking(Viking)
+    {
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(_Viking)
+    {
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack(){}
+    saxonAttack(){}
+    showStatus(){}
+}
