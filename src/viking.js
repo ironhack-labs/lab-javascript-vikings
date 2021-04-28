6a60678078f761f1771448f0ea95bbb7ef14624c
Andrei <andrei_1002@hotmail.com>
2021-04-28 21:35:25 +0200

    terminada iteración 1 y parte de la 2

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,32 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+       return this.strength
+    }
+
+    receiveDamage(damage) {
+         this.health -= damage
+    }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+   
+    receiveDamage(damage) {
+        
+    }
+}
 
 // Saxon
 class Saxon {}
