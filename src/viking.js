eebd2af579b69e39d2b47d499ac16d26534f9b26
Paloma Borro <palomabs93@gmail.com>
2021-04-28 21:29:54 +0200

    primeras iteraciones

--- src/viking.js
+++ src/viking.js
@@ -1,9 +1,25 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+      return this.strength;
+}
+
+    receiveDamage(damage) {
+      this.health = this.health - damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
 
+}
+}
 // Saxon
 class Saxon {}
 
