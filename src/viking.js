d1b012189ce4c7530c5ea25aa4b1fd72c4326e1a
Andrei <andrei_1002@hotmail.com>
2021-04-28 22:41:11 +0200

    terminadas iteraciones 2 & 3

--- src/viking.js
+++ src/viking.js
@@ -24,12 +24,37 @@ class Viking extends Soldier {
     }
    
     receiveDamage(damage) {
-        
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+
+        if (this.health <= 0) {
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
     }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+    receiveDamage(damage) {
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+
+        if (this.health <= 0) {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
