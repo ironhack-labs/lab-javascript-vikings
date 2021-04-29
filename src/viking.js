ec833430ed461684968bc6fb6bdfae8e4c6335a7
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2021-04-29 09:39:41 +0200

    Iteration3

--- src/viking.js
+++ src/viking.js
@@ -40,7 +40,31 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+        super(health, strength);
+    }
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        if(this.health <= 0) {
+            return 'A Saxon has died in combat'
+        }
+    }
+
+    
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.SaxonArmy = [];
+    }
+}
