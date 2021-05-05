bdc44ae60727173ed8b797611475334081bdf315
Pablo Borjas <pabloborjas_0708@hotmail.com>
2021-05-05 15:31:27 +0200

    primera y egunda iteracion listas

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack () {
+       return this.strength = 150;
+    }
+    receiveDamage (theDamage){
+         this.health -= theDamage;
+             
+    }   
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if (this.health > 0){
+            return `${this.name} has received ${theDamage} points of damage`;
+        }
+        else ( this.health <= 0)
+            return `${this.name} has died in act of combat`;
+        
+        
+    }
+}
 
 // Saxon
 class Saxon {}
