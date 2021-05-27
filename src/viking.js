4df8b5388288b881cc187dbd672ed9273dcab278
ross-u <cirkovic.uros.srb@gmail.com>
2021-05-28 01:34:44 +0200

    Update comments

--- src/viking.js
+++ src/viking.js
@@ -10,7 +10,10 @@ class Saxon {}
 // War
 class War {}
 
-// The following is required to make unit tests work. Please ignore it.
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
