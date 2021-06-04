84c6e7941cede254e03e3dd361ccd474a71af29e
javierceballosaltuna <javierceballosaltuna@gmail.com>
2021-06-04 18:55:50 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,125 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthValue, strengthValue) {
+    this.health = healthValue
+    this.strength = strengthValue
 
-// Viking
-class Viking {}
+  }
 
-// Saxon
-class Saxon {}
 
-// War
-class War {}
+  attack() {
+
+    return this.strength
+
+  }
+
+
+  receiveDamage(thedamage) {
+
+    this.health = this.health - thedamage
+
+    // soldier.receiveDamage(50);
+    // expect(soldier.health).toEqual(health - 50)
+
+
+
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+//Viking
+class Viking extends Soldier {
+
+  constructor(nameValue, healthValue, strengthValue) {
+
+    super(healthValue, strengthValue)
+
+    this.name = nameValue
+    this.health = healthValue
+    this.strength = strengthValue
+  }
+
+  receiveDamage(thedamage) {
+
+    this.health = this.health - thedamage
+
+    if (this.health > 0) {
+
+      return `${this.name} has received ${thedamage} points of damage`
+
+
+    }
+
+    else {
+
+      return `${this.name} has died in act of combat`
+    }
+
+  }
+
+  battleCry() {
+
+    return "Odin Owns You All!"
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Saxon
+
+    class Saxon extends Soldier {
+
+      receiveDamage(thedamage) {
+
+        this.health = this.health - thedamage
+
+        if (this.health > 0) {
+
+          return `A Saxon has received ${thedamage} points of damage`
+
+
+        }
+
+        else {
+
+          return `A Saxon has died in combat`
+        }
+
+      }
+
+
+}
+
+
+
+
+
+//War
+class War { }
 
 
 
