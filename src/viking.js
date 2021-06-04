e3b2d704d708443b763a12e3bcdcdb9ff3a0e67e
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-04 15:21:28 +0200

    task so far

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,77 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+// attack method
+attack () {
+return this.strength
+}
+
+//damage received method
+receiveDamage (theDamage) {
+this.health -= theDamage
+}
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(theDamage) {
+    this.health -= theDamage
+    if (this.health > 0) {
+      return `${this.name} has received ${theDamage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  //battle cry method
+battleCry() {
+  return "Odin Owns You All!"
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor (health, strength, name) {
+    super(name, health, strength);
+    this.health = health;
+    this.strength = strength;
+}
+receiveDamage(theDamage) {
+  this.health -= theDamage
+  if (this.health > 0) {
+    return `A Saxon has received ${theDamage} points of damage`
+  } else {
+    return `A Saxon has died in combat`;
+  }
+}
+}
 
 // War
-class War {}
+class War {
+constructor() {
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+  addViking(newViking) {
+    if (this.vikingArmy += newViking)
+  this.vikingArmy =;
+  }
+  addSaxon(newSaxon) {
+    this.saxonArmy += newSaxon;
+  }
+  vikingAttack(){
+
+  }
+  saxonAttack(){}
+  showStatus(){}
+}
 
 
 
@@ -17,3 +80,6 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+
+// how to create a class in another class
