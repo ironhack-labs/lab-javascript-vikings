015da33e159ca24c8ca48f9c830fb4236f28950b
pasqualipedro <pasqualipedro@gmail.com>
2021-06-05 00:16:45 +0100

    Versao Final com Iteracao 5 - Bonus.

--- src/viking.js
+++ src/viking.js
@@ -6,15 +6,12 @@ class Soldier {
     this.health = 300;
   }
   attack() { return this.strength }
-  receiveDamage(damage) { this.health - damage }
+  receiveDamage(damage) { this.health = this.health - damage }
 }
-
-let Soldier1 = new Soldier
-
-console.log(Soldier1)
-console.log(Soldier1.attack())
-/* console.log(Soldier1.receiveDamage(100)) */
-
+let soldier1 = new Soldier
+  // console.log(soldier1)
+  // console.log(soldier1.attack())
+  // console.log(soldier1.receiveDamage(100)) 
 // Viking
 //---ITERATION 02----
 class Viking extends Soldier {
@@ -23,42 +20,73 @@ class Viking extends Soldier {
     this.name = name
   }
   receiveDamage(damage) {
-    if((this.health - damage)>0) {
-      return `${this.name} has received ${damage} points of damage. Ele é forte, não morre não!`}
-      return `${this.name} has died in act of combat.`
+    this.health -= damage
+    if ( this.health > 0) {
+      return `${this.name} has received ${damage} points of damage.`
+    }
+    return `${this.name} has died in act of combat`
   }
-  battleCry() { return `Odin Owns You All!`}
+  battleCry() { return `Odin Owns You All!` }
 }
-
-let Viking1 = new Viking(`Ragnarok`)
-
-console.log(Viking1)
-console.log(Viking1.attack())
-console.log(Viking1.receiveDamage(100))
-console.log(Viking1.battleCry())
-
+let viking1 = new Viking(`Ragnarok`)
+  // console.log(viking1)
+  // console.log(viking1.attack())
+  // console.log(viking1.receiveDamage(100))
+  // console.log(viking1.battleCry())
 // Saxon
-class Saxon extends Soldier{
-  constructor(health, strength) {
-    super (health, strength)
+class Saxon extends Soldier {
+  constructor() {
+    super(60, 25);
+    this.health = 60;
+    this.strength = 25;
   }
   receiveDamage(damage) {
-    if((this.health - damage)>0) {
-      return `A Saxon has received ${damage} points of damage.`}
-      return `A Saxon has died in combat.`
- }
+    this.health -= damage
+    if ( this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    }
+    return `A Saxon has died in combat`
+  }
 }
-
-let Saxon1 = new Saxon
-
-console.log(Saxon1)
-console.log(Saxon1.receiveDamage(400))
-
+let saxon1 = new Saxon
+  // console.log(saxon1)
+  // console.log(saxon1.receiveDamage(400))
 // War
-class War { }
-
-
-
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking){
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack(){
+    let saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+    let vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+    let saxonDamage = this.saxonArmy[saxonRandom].receiveDamage(this.vikingArmy[vikingRandom].strength);
+    if(this.saxonArmy[saxonRandom].health <= 0 ){
+      this.saxonArmy.splice(saxonRandom, 1);
+    };
+    return saxonDamage
+  }
+  saxonAttack(){
+    let saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+    let vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+    let vikingDamage = this.vikingArmy[vikingRandom].receiveDamage(this.saxonArmy[vikingRandom].strength);
+    if(this.vikingArmy[vikingRandom].health <= 0){
+      this.vikingArmy.splice(vikingRandom, 1);
+    }
+    return vikingDamage
+  }
+  showStatus(){
+    if (!this.saxonArmy.length) return "Vikings have won the war of the century!";
+    if (!this.vikingArmy.length) return "Saxons have fought for their lives and survived another day...";
+    if (this.vikingArmy.length && this.saxonArmy.length >= 1) return "Vikings and Saxons are still in the thick of battle.";
+  }
+}
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
