f1d14869091d3def045d231bcddaa84228cc639e
Flavio-Pestana <flaviopestan@gmail.com>
2021-06-05 13:58:40 -0300

    solved conflicts

--- src/viking.js
+++ src/viking.js
@@ -4,18 +4,17 @@ class Soldier {
     this.health = health;
     this.strength = strength;
   }
-  
   attack() {
     return this.strength;
-
   }
-
   receiveDamage(damage) {
-    this.damage = damage;
     this.health = this.health - damage;
-
   }
 }
+
+// console.log(soldierSly.receiveDamage(90))
+
+// -----------------------------------------------------------------------------------------------
 
 // Viking
 class Viking extends Soldier {
@@ -24,30 +23,6 @@ class Viking extends Soldier {
     this.name = name;
   }
 
-  attack() {
-    return this.strength;
-
-  }
-
-  receiveDamage(damage) {
-    this.health -= damage;
-    if (this.health > 0) {
-      return `${this.name} has received ${damage} points of damage`;
-    } else {
-      return `${this.name} has died in act of combat`;
-    }
-  }
-
-
-  battleCry() {
-    return "Odin Owns You All!";
-  }
-
-}
-
-
-// Saxon
-/*class Saxon {
   receiveDamage(damage) {
     this.health = this.health - damage;
     if (this.health > 0) {
@@ -56,33 +31,32 @@ class Viking extends Soldier {
       return `${this.name} has died in act of combat`;
     }
   }
-
   battleCry() {
     return 'Odin Owns You All!';
   }
 }
-
 const vikingStrong = new Viking('Harald', 200, 40);
- console.log(vikingStrong.receiveDamage(300))*/
+// console.log(vikingStrong.receiveDamage(300))
+
+// -----------------------------------------------------------------------------------------------
 
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage) {
+    this.health = this.health - damage;
     if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`;
-    } else (this.health <= 0)
-    return `A Saxon has died in combat`;
+    } else if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
   }
-
-
 }
-
-
-
+const saxonNervous = new Saxon(200, 50);
+// console.log(saxonNervous.receiveDamage(300))
 // War
 class War {
-  vikingArmy = []
-  saxoArmy = []
+  vikingArmy = [];
+  saxonArmy = [];
 
   addViking(viking) {
     this.vikingArmy.push(viking);
@@ -122,11 +96,15 @@ class War {
   }
 }
 
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = { Soldier, Viking, Saxon, War };
+  module.exports = {
+    Soldier,
+    Viking,
+    Saxon,
+    War
+  };
 }
 
 function selectRandom(array) {
