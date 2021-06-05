0663b857d254d2eb37b3a157d6e9606d96335c4c
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-06-05 08:11:51 -0300

    Alterações do Game

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,103 @@
 // Soldier
-class Soldier {}
+class Soldier { 
+  constructor (health, strength){
+     this.health = health;
+    this.strength = strength;
+  }
+
+  attack (){
+    
+    return this.strength
+  }
+  receiveDamage (damage){
+    this.health -= damage
+   }
+
+}
+
+const soldado = new Soldier( 10, 20);
+
+console.log(soldado.strength)
+
+
+
+console.log(this.strength)
+
+
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health,strength){
+    super(strength)
+    this.name = name; 
+    this.health = health;
+    this.strength = strength;
+  }
+  receiveDamage (damage){
+    this.health -= damage
+    if (this.health > 0){
+      return `${this.name} has received ${damage} points of damage`
+    }
+
+      return `${this.name} has died in act of combat`
+    
+
+  }
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+   
+     
+}
+  
+
+  
+ 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor (health, strength){
+   super(strength)
+    this.health = health;
+   this.strength = strength;
+ }
+  receiveDamage (damage){
+    this.health -= damage
+    if (this.health > 0){
+      return `A Saxon has received ${damage} points of damage`
+  }
+
+      return 'A Saxon has died in combat'
+  
+  }
+}
 
 // War
-class War {}
+class War  {
+constructor(){
+  this.vikingArmy = [];
+  this.saxonArmy = [];
+}
+
+  addViking(Viking){
+    this.vikingArmy.push(Viking)
+
+  }
+  addSaxon(Saxon){
+    //(a) => this.saxonArmy.push(Saxon)
+    this.saxonArmy.push(Saxon)
+  }
+  vikingAttack(){
+  
+  
+    
+  
+  }
+  saxonAttack(){}
+  showStatus(){}
+}
 
 
 
@@ -17,3 +106,4 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
