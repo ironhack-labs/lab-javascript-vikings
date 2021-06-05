7e1c3edc60ad0c2f0acaea9bfc5c171765d58db7
Gustavo Barasini <barasinigustavo@gmail.com>
2021-06-05 00:14:37 -0300

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,46 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
 
+  attack() {
+    return this.strength; 
+}  
+  receiveDamage(damage){
+    this.health -= damage;
+}
+}
+  
 // Viking
-class Viking {}
-
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(health,strength);
+    this.name = name;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.health > 0){
+      return `${this.name} has received ${damage} points of damage`;
+    }else()
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health -= damage;
+
+  }
+}
 
 // War
-class War {}
+class War {
+damage()
+}
 
 
 
