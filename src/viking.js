1b0d6052bb85483d012d48210307828e925f11b4
Enrique VOZA <enriquevozmediano@yahoo.es>
2021-06-06 23:33:13 +0200

    WDFTMAY2021 JOSE MORCILLO ENRIQUE VOZMEDIANo

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,65 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+
+
+  }
+
+  attack() {
+    return this.strength
+
+  }
+
+  receiveDamage(strength) {
+    this.health -= strength
+
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(health, strength, name) {
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(strength) {
+    this.health -= strength
+    if (health > 0) {
+      return `${this.name} has received ${strength} pouints of damage`
+
+    }
+    else {
+      return `${this.name} has died in act combat`
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+
+  }
+  receiveDamage(strength) {
+    if (health > 0) {
+      return `Saxon received ${strength} points of damage`
+    }
+    else {
+      return `Saxon has died in act combat`
+    }
+  }
+
+}
 
 // War
-class War {}
+class War { }
 
 
 
