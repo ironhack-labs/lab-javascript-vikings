917b08fa62cfdbf0d965fffb34f0c9224e66d5a2
Gabriel Santos <selvagedev@gmail.com>
2021-06-07 16:21:26 +0100

    solution

--- src/viking.js
+++ src/viking.js
@@ -76,10 +76,10 @@ class War {
   showStatus(){
     if (this.saxonArmy.length === 0) {
       return "Vikings have won the war of the century!";
-    }else if (this.vikingArmy.length === 0) {
+    }else if (this.vikingArmy.length === 0){
       return "Saxons have fought for their lives and survived another day...";
     }
-    else {
+    else if (this.saxonArmy.length === 1 && this.vikingArmy.length === 1){
       return "Vikings and Saxons are still in the thick of battle.";
       } 
   }
