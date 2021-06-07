9a0a180e02511cdef2213ecab7dac11037d96a7f
codervinicius <vinicaixx@gmail.com>
2021-06-07 19:08:39 -0300

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -94,3 +94,7 @@ class War {
 }
 }
 
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = { Soldier, Viking, Saxon, War };
