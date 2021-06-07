9b48464f7cc1ee468fe1bb76848e2323d6c91b50
annasun919 <yuval_sun@yahoo.com.hk>
2021-06-07 22:33:36 +0200

    part of bonus

--- src/viking.js
+++ src/viking.js
@@ -23,7 +23,7 @@ class Viking extends Soldier{
   receiveDamage(theDamage){
     this.health = this.health-theDamage
 
-    if(this.health != 0){
+    if(this.health > 0){
       return `${this.name} has received ${theDamage} points of damage`
 
     }
@@ -50,7 +50,7 @@ class Saxon extends Soldier{
   receiveDamage(theDamage){
     this.health = this.health-theDamage
 
-    if(this.health != 0){
+    if(this.health > 0){
       return `A Saxon has received ${theDamage} points of damage`
 
     }
@@ -65,7 +65,61 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy=[]
+    this.saxonArmy=[]
+  }
+  
+  
+
+addViking(viking){
+  this.vikingArmy.push(viking)
+  
+}
+addSaxon(saxon){
+  this.saxonArmy.push(saxon)
+}
+vikingAttack(){
+  let aRandomforviking = Math.floor(Math.random()*this.vikingArmy.length)
+  let aRandomforSaxon = Math.floor(Math.random()*this.saxonArmy.length)
+
+  let theSaxon = this.saxonArmy[aRandomforSaxon]
+  let theViking = this.vikingArmy[aRandomforviking]
+
+  let battle  = theSaxon.receiveDamage(theViking.strength)
+  
+  
+  if (theSaxon.health <= 0){
+    this.saxonArmy.splice(aRandomforSaxon, 1);}
+
+
+return battle
+}
+
+saxonAttack(){
+  let aRandomforviking = Math.floor(Math.random()*this.vikingArmy.length)
+  let aRandomforSaxon = Math.floor(Math.random()*this.saxonArmy.length)
+
+  let theSaxon = this.saxonArmy[aRandomforSaxon]
+  let theViking = this.vikingArmy[aRandomforviking]
+
+  let battle = theViking.receiveDamage(theSaxon.strength)
+  
+  
+  if (theViking.health <= 0){
+    this.vikingArmy.splice(aRandomforviking, 1);
+  }
+
+  return battle
+}
+
+}
+
+
+showStatus(){
+  return status;
+}
 
 
 
