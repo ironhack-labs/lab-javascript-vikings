aa7a880c80c0f80f62e13e342f5593a6783a5505
“Zhanna“ <maekot@googlemail.com>
2021-06-09 01:04:11 +0200

    with bonus but not sure it works

--- src/viking.js
+++ src/viking.js
@@ -57,11 +57,53 @@ console.log(viking1);
 console.log(viking1.receiveDamage(2));
 console.log(viking1.receiveDamage(2));
 console.log(viking1.receiveDamage(2));
-console.log(viking1.battleCry()); 
+console.log(viking1.battleCry());
+
+//const viking = new Viking(randomName, randomHealth, random Strength);
+//Math.floor(1 + 6 * Math.random());
+
 // War
-class War {}
+class Attack {
 
+}
 
+class War {
+  constructor () {
+    let vikingArmy = [];
+    let saxonArmy = [];
+  };
+  addViking(Viking) {
+    vikingArmy.push(Viking);
+  };
+  addSaxon(Saxon) {
+    saxonArmy.push(Saxon);
+  };
+  vikingAttack() {
+    Saxon.receiveDamage(Viking.strength);
+    if (this.health <= 0) {
+      this.Army -= 1
+    }
+    return receiveDamage(damage)
+  };  
+  saxonAttack() {
+    Viking.receiveDamage(Saxon.strength);
+    if (this.health <= 0) {
+      this.Army -= 1
+    }
+    return receiveDamage(damage)
+  };
+  showStatus() {
+    if (saxonArmy === []) {
+      return ('Vikings have won the war of the century!')
+    } else if (vikingArmy == []) {
+      return ('Saxons have fought for their lives and survived another day...')
+    } else {
+      return ('Vikings and Saxons are still in the thick of battle.')
+    }
+  };
+}
+
+const war = new War ()
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
