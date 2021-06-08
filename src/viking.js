e380778103ea01447027f5c9faec269756e93619
Raoul Ullens <raoulullens@live.com>
2021-06-08 17:56:34 +0200

    Up to iteration 4

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,83 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+
+  }
+  attack () {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  attack () {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if(this.health > 0) {
+      console.log(this.name + ' has received ' + damage + ' points of damage')
+    } else {
+      console.log(this.name + ' has died in a brave act of combat')
+    };
+    }
+    battleCry() {
+      return "Odin Owns You All!"
+  }
+}
+
+// Test
+const viktor = new Viking('Viktor', '100', '50');
+console.log(viktor)
+console.log(viktor.attack ())
+console.log(viktor.receiveDamage (110))
+console.log(viktor.battleCry())
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+receiveDamage(damage) {
+  this.health = this.health - damage;
+  if(this.health > 0) {
+    console.log('A Saxon has received ' + damage + ' points of damage')
+  } else {
+    console.log('A Saxon has died in a brave act of combat')
+  };
+}
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+  this.vikingArmy = [];
+  this.saxonArmy = [] ;
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking)
+    // return vikingArmy
+
+  }
+  // addSaxon()
+  // vikingAttacks()
+  // saxonAttack()
+  // showStatus()
+
+}
+let greatWar = new War()
+greatWar.addViking('James')
+console.log(greatWar)
 
 
 
@@ -17,3 +86,6 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+
+// Question: why undefined when I console.log(viktor.receiveDamage (110))?
