c44b1c9cd4ba81adc461277e374847aa124d0904
Mangonando <juan.f.ramirezv@gmail.com>
2021-06-08 18:16:18 +0200

    not finished

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = 150;  
+  }
+  attack = function attack() {
+    return this.strength
+  }
+  receiveDamage = function receiveDamage(damage) {
+    this.health -= damage;
+
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    // this.health -= damage;
+    // super.receiveDamage(damage);
+    console.log('this is the health' + this.health)
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`
+    } else {
+      return `${this.name} has received ${damage} points of damage`
+    }
+    // return 1
+  }
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+}
+
 
 // Saxon
 class Saxon {}
@@ -17,3 +49,7 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+let viking1 = new Viking(`Thor`, 100, 150)
+viking1.receiveDamage(20)
+// console.log(viking1.health)
