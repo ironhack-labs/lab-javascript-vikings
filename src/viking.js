716116bba2a6705b4826920cb96d5cc09e2d1909
Andrew Oppo <andrew.oppo@gmail.com>
2021-06-08 18:08:55 +0200

    submitting lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,86 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if(this.health > 0) {
+      return this.name + ' has received ' + damage + ' points of damage'
+    } else {
+      return this.name + ' has died in act of combat'
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if(this.health > 0) {
+      return 'A Saxon has received ' + damage + ' points of damage'
+    } else {
+      return 'A Saxon has died in combat'
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy =[];
+  }
+  addViking(viking){
+    this.vikingArmy.push(viking)
+  }
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+  }
+  vikingAttack(){
+    const damage = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].receiveDamage(this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].strength);
+    for (let saxon of this.saxonArmy) {
+      if (saxon.health <= 0) {
+         let index = this.saxonArmy.indexOf(saxon);
+         this.saxonArmy.splice(saxon, 1)
+      }
+    }
+    return damage;
+  }
+  saxonAttack(){
+    const damage = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].receiveDamage(this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].strength);
+    for (let viking of this.vikingArmy) {
+      if (viking.health <= 0) {
+         let index = this.vikingArmy.indexOf(viking);
+         this.vikingArmy.splice(viking, 1)
+      }
+    }
+    return damage;
+  }
+  showStatus(){}
+}
 
 
 
