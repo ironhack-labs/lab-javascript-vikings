e8df4197d8c1d57ff8eefbfb900c8a1b4fe89376
Josue Prieto-Paz <jprietopaz3@gatech.edu>
2021-06-09 16:07:43 +0200

    finished

--- src/viking.js
+++ src/viking.js
@@ -1,12 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health = this.health-damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+    this.name = name;
+    super(health, strength)
+  }
+  receiveDamage(damage){
+    let damageReceived = super.receiveDamage()
+    if (this.health > 0){
+      console.log(`${name} has received ${damage} points of damage`)
+    }
+    else{
+      console.log(`${name} has died in combat`)
+    }
+  }
+  battleCries(){
+    return "Odin Owns you all!"
+  }
+}
 
 // Saxon
-class Saxon {}
-
+class Saxon extends Soldier{}
+  0
 // War
 class War {}
 
