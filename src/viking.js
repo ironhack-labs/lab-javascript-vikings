8c1502aa6a28f3e96de8f9d6137a1a041c9d95fd
joannakot <joanna.kot.ok@gmail.com>
2021-06-09 13:41:04 +0200

    done without bonuses

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+    attack() {
+      return this.strength;
+    }
+    receiveDamage (damage) {
+      //this.health.pop(damage);
+      this.health -= damage
+    }
+  }
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+    attack() {
+      return super.attack();
+  }
+    receiveDamage (damage) {
+      this.health -= damage; 
+      if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else if (this.health <= 0) {
+      return `${this.name} has died in act of combat`
+    }
+  }
+    battleCry() {
+      return 'Odin Owns You All!'
+  }  
+}
+//console.log(`${this.name} has died in act of combat`);
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(damage) {
+    this.health -= damage; 
+      if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else if (this.health <= 0) {
+      return `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
 class War {}
