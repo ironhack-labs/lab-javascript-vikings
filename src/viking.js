4fe4fd02230ecb9e693cb374e2382cc0cfbbee73
rodrigofxxx <rodrigofelix.leite@gmail.com>
2021-06-10 19:09:21 -0300

    Lab made  until third interation

--- src/viking.js
+++ src/viking.js
@@ -1,15 +1,44 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor ( health, strength) {
+      this.health = health,
+      this.strength = strength      
+  }
+  attack () {
+      return this.strength
+  }
+  receiveDamage (damage) {
+      this.health = this.health - damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  battleCry() {
+    return 'Odin Owns You All!'
+  } 
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) return this.name + `${name} has received ${damage} points of damage`;
+    if (this.health <= 0) return this.name + `${name} has died in act of combat`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) return `A Saxon has received ${damage} points of damage`;
+    if (this.health <= 0) return `A Saxon has died in combat`;
+  }
+}
 
 // War
 class War {}
-
 
 
 // The following is required to make unit tests work.
