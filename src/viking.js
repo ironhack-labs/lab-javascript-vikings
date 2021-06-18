3c5fb765bab7f5a8ab75b2600826b949e2cf6065
Florian Becker <becker_florian@online.de>
2021-06-18 08:31:11 +0200

    Solved more or less Iteration 1 & 2, progress needs to come, aswell for Iteration 2 and then maybe trying to solve the Bonus part too.

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,36 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health,
+    this.strength = strength
+  }
+  attack = () => {
+    return `The strength : ${this.strength}`
+  }
+  receiveDamage = (theDamage) => {}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+  attack = () => {
+    return `${this.strength} of the Viking.`
+  }
+  receiveDamage = (theDamage) => {
+    return `${this.name.toUpperCase} has received Damage.toUpperCase point of damage.`
+    }
+    battleCry = () => {
+      return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage = (theDamage) => {}
+}
 
 // War
 class War {}
