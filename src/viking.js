1b941646cdec1f09ebbda101751702eeb5abeb15
Karla Huerta <KMHS@MacBook-Air-de-Karla.local>
2021-06-26 23:20:18 -0500

    solved lab-vikings

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,97 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    
+    this.health = health;
+    this.strength = strength;
+
+  }
+  attack () {
+
+    console.log (this.strength)
+    return this.strength
+
+  }
+
+  receiveDamage (theDamage) {
+    this.health -= theDamage
+
+  }
+
+}
+
+// class HurtedSoldier extends Soldier {
+//   constructor (health, strenght, damage){
+//     super (health, strenght, damage)
+// //     this.damage = damage
+//   }
+
+// }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor (name, health, strength) {
+    super (health, strength)
+    this.name = name
+
+  }
+  attack () {
+    
+    console.log (this.strength)
+    return this.strength
+
+  }
+  
+  receiveDamage (theDamageViking) {
+
+    this.health -= theDamageViking
+
+    if (this.health > theDamageViking){
+      return `${this.name} has received ${theDamageViking} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  
+  battleCry(){
+    return "Odin Owns You All!";
+  
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage (damage) {
+    this.health = this.health - damage
+
+    if (this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`
+    }
+  }
+
+}
+
+
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+
+  addViking (swedish) {
+    this.vikingArmy.push (swedish)
+  }
+
+  addSaxon (saxonA) {
+    this.saxonArmy.push (saxonA)
+  }
+
+}
 
 
 
