c74fa81b099fc51d04cff8f2a0b7b1c7cf32fa17
Flow-Fly <florian.aube@gmail.com>
2021-07-06 15:10:43 +0200

    implemented Viking

--- src/viking.js
+++ src/viking.js
@@ -2,7 +2,23 @@
 class Soldier {}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name) {
+    super(health, strength)
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`
+    } else {
+      return `${this.name} has received ${damage} points of damage`
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+}
 
 // Saxon
 class Saxon {}
