8af7bca5b1339c57a5afdde5a64026f539b294ba
Ghada <ghadamannai9@gmail.com>
2021-07-06 18:01:05 +0200

    Lab-progress

--- src/viking.js
+++ src/viking.js
@@ -1,15 +1,15 @@
 // Soldier
 class Soldier {
-  constructor(health, strength){
+  constructor(health, strength) {
     this.health = health;
     this.strength = strength;
   }
-  attack(){
-  return this.strength;
+  attack() {
+    return this.strength;
   }
 
-  receiveDamage(damage){
-  this.health -= damage;
+  receiveDamage(damage) {
+    this.health -= damage;
 
   }
 
@@ -17,34 +17,75 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-  constructor (name, health, strength){
+  constructor(name, health, strength) {
     super(health, strength);
     this.name = name;
   }
-  attack(){
+  attack() {
     return this.strength;
   }
 
-  receiveDamage(damage){
+  receiveDamage(damage) {
     super.receiveDamage(damage);
-    if(this.health < damage){
-    return `${this.name} has died in act of combat`;
+    if (this.health < damage) {
+      return `${this.name} has died in act of combat`;
     }
     else {
       return `${this.name} has received ${damage} points of damage`;
-        }
-  
     }
-  
-
-  
   }
 
+  battleCry(){
+    return 'Odin Owns You All!';
+  }
+}
+
 // Saxon
-class Saxon {}
+class Saxon extends Soldier { 
+  
+  attack() {
+    return this.strength;
+  }
+ 
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+    else {
+      return `A Saxon has received ${damage} points of damage`;
+    }
+  }
+}
 
 // War
-class War {}
+class War { 
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+}
+addViking(Viking){
+  
+  this.vikingArmy.push(Viking);
+}
+
+addSaxon(Saxon){
+  this.saxonArmy.push(Saxon);
+}
+
+vikingAttack() {
+    let randomIndexSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    
+    let randomIndexViking = Math.floor(Math.random() * this.vikingArmy.length);
+    
+    this.saxonArmy[randomIndexSaxon].receiveDamage(
+      
+    this.vikingArmy[randomIndexViking].strength
+    );
+  }
+  
+}
+
 
 
 
