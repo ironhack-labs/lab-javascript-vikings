c4065dc7e5a8d75201e03478ba183b275bb4d41b
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-07-11 15:56:33 +0300

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -95,7 +95,15 @@ class War {
 
     return result;
   }
-  showStatus() {}
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return `Vikings have won the war of the century!`;
+    } else if (this.vikingArmy.length === 0) {
+      return `Saxons have fought for their lives and survived another day...`;
+    } else if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+      return `Vikings and Saxons are still in the thick of battle.`;
+    }
+  }
 }
 
 // The following is required to make unit tests work.
