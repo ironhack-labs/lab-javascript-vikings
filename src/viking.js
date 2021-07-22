d38fb2c670209684716d8d5ffac8fa49d83e0a71
Juan Eugenio <leuviah77@gmail.com>
2021-07-22 22:36:10 +0200

    First commit

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  battleCry() {}
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
