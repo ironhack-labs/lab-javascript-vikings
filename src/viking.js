f1c18a2bd6de8ef5ce08f642c9f6fe9f1811acac
Daniel td <danieltd@gmail.com>
2021-07-25 17:25:00 +0200

    Done for now. I will take a look at the bonus

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,79 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (theDamage > this.health) {
+      return `${this.name} has died in act of combat`;
+    } else {
+      return `${this.name} has received ${theDamage} points of damage`;
+    }
+  }
+
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  super(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(theDamages) {
+    this.health -= theDamages;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamages} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
 
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
 
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack() {}
+
+  saxonAttack() {}
+
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
