93f6a0692daf8c1f2845f1e54074a737f064316b
Pablo Alonso Lorente <pablochux@me.com>
2021-07-28 20:06:14 +0200

    Finished everything, even the Super bonus iteration

--- src/viking.js
+++ src/viking.js
@@ -51,11 +51,94 @@ class War {
     this.vikingArmy = [];
     this.saxonArmy = [];
   }
-  addViking() {}
-  addSaxon() {}
-  vikingAttack() {}
-  saxonAtacck() {}
-  showStatus() {}
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  // What are the parts that are generic about the code?
+  genericAttackMethod(attackerArmy, defenderArmy) {
+    // 1 - Chose reandom attacked soldier
+    const attackedSoldierId = randomNum(0, defenderArmy.length - 1);
+    const attackedSoldier = defenderArmy[attackedSoldierId];
+
+    // 2 - Choose a random attacking soldier
+    const attackingSoldier =
+      attackerArmy[randomNum(0, attackerArmy.length - 1)];
+
+    // 3 - Make sure that the defender receives the attacker attack
+    const result = attackedSoldier.receiveDamage(attackingSoldier.strength);
+    // 3 - If the saxon is dead, we delete it from the army
+    if (attackedSoldier.health <= 0) {
+      defenderArmy.splice(attackedSoldierId, 1);
+    }
+    return result;
+  }
+
+  vikingAttack() {
+    return this.genericAttackMethod(this.vikingArmy, this.saxonArmy);
+  }
+
+  saxonAttack() {
+    return this.genericAttackMethod(this.saxonArmy, this.vikingArmy);
+  }
+
+  // First iteration of the function
+  //   vikingAttack() {
+  //     // 1 - Choose random saxon
+  //     const attackedSaxonId = randomNum(0, this.saxonArmy.length - 1);
+  //     const attackedSaxon = this.saxonArmy[attackedSaxonId];
+  //     // 2 - Choose a random viking
+  //     const attackingViking =
+  //       this.vikingArmy[randomNum(0, this.vikingArmy.length - 1)];
+
+  //     // we make the saxon receive attack
+  //     const result = attackedSaxon.receiveDamage(attackingViking.strength);
+  //     // 3 - If the saxon is dead, we delete it from the army
+  //     if (attackedSaxon.health <= 0) {
+  //       this.saxonArmy.splice(attackedSaxonId, 1);
+  //     }
+  //     return result;
+  //   }
+  /**
+   * // First iteration of the function. Random saxon attacks random viking
+   * @returns message from
+   */
+  //   saxonAttack() {
+  //     // 1 - Choose random viking to be attacked
+  //     const attackedVikingId = randomNum(0, this.vikingArmy.length - 1);
+  //     const attackedViking = this.vikingArmy[attackedVikingId];
+
+  //     // 2 - Choose a random viking to receive the attack
+  //     const attackingSaxon =
+  //       this.saxonArmy[randomNum(0, this.saxonArmy.length - 1)];
+
+  //     // atacckingSaxon attacks attackedVikingi receive attack
+  //     const result = attackedViking.receiveDamage(attackingSaxon.strength);
+  //     // 3 - If the saxon is dead, we delete it from the army
+  //     if (attackedViking.health <= 0) {
+  //       this.vikingArmy.splice(attackedVikingId, 1);
+  //     }
+  //     return result;
+  //   }
+
+  showStatus() {
+    if (this.vikingArmy.length == 0) {
+      return 'Saxons have fought for their lives and survived another day...';
+    }
+    if (this.saxonArmy.length == 0) {
+      return 'Vikings have won the war of the century!';
+    }
+    if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+}
+
+function randomNum(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
 // The following is required to make unit tests work.
