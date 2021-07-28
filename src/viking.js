052af89962e64a54b36f0b8d222145cff539e52d
Pablo Alonso Lorente <pablochux@me.com>
2021-07-28 13:34:20 +0200

    First iteration done, working on second iteration

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,38 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage() {}
+
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
