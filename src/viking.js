477dac26db4970a47276746fd5d9248ade0598ea
lauraAlabau <arlaura3@gmail.com>
2021-08-13 13:58:10 +0200

    Iteration 2: Viking - Complete

--- src/viking.js
+++ src/viking.js
@@ -14,11 +14,24 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier{
-
+  constructor(name, health, strength){
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if(this.health !== 0){
+      return `${this.name} has received ${damage} points of damage`
+    } 
+    return `${this.name} has died in act of combat`
+  }
+  battleCry(){
+    return `Odin Owns You All!`
+  }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{}
 
 // War
 class War {}
