ec8ecc119776f607a31290b346b81a29212d9292
thomaskno <thomas.kno@yahoo.com>
2021-08-13 17:57:11 -0400

    Added viking

--- src/viking.js
+++ src/viking.js
@@ -2,7 +2,20 @@
 class Soldier {}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health){
+    super(health, strength)
+    this.name = name;
+  }
+
+  receiveDamage(){
+    return 0;
+  }
+
+  battleCry(){
+    return 0;
+  }
+}
 
 // Saxon
 class Saxon {}
