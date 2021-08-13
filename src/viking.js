7651c137b336da56cc0c47b5413816179c840cfe
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-08-13 15:01:37 +0200

    done until bonus

--- src/viking.js
+++ src/viking.js
@@ -1,19 +1,67 @@
 // Soldier
-class Soldier {}
+class Soldier {
+	//cosntructor
+	constructor(health, strength) {
+		this.health = health;
+		this.strength = strength;
+	}
+
+	//metodos
+	attack() {
+		return this.strength;
+	}
+
+	receiveDamage(damage) {
+		this.damage = damage;
+		this.health -= this.damage;
+
+		return;
+	}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+	constructor(name, health, strength) {
+		super(health, strength);
+
+		this.name = name;
+		this.health = health;
+		this.strength = strength;
+	}
+
+	battleCry() {
+		return `Odin Owns You All!`;
+	}
+
+	receiveDamage(damage) {
+		this.health -= damage;
+
+		// serviria aquí != 0, no trataría negativos?
+		return this.health <= 0
+			? `${this.name} has died in act of combat`
+			: `${this.name} has received ${damage} points of damage`;
+	}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+	attack() {
+		return this.strength;
+	}
+
+	receiveDamage(damage) {
+		this.health -= damage;
+
+		// serviria aquí != 0, no trataría negativos?
+		return this.health <= 0 ? 'A Saxon has died in combat' : `A Saxon has received ${damage} points of damage`;
+	}
+}
 
 // War
 class War {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = { Soldier, Viking, Saxon, War };
+	module.exports = { Soldier, Viking, Saxon, War };
 }
