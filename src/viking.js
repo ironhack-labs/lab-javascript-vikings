ad55b293d440df6a554e1327bfdd2293e104b959
Juan De Santis <juanbds66@gmail.com>
2021-08-13 18:42:13 +0200

    Juan y Paula done sin bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,134 @@
 // Soldier
-class Soldier {}
+class Soldier { 
+  
+  constructor(health, strength) {
+    
+  this.health = health
+  this.strength = strength
+  this.isSoldier = true
 
+}
+ 
+  
+
+  attack() {
+
+    return this.strength;
+  }
+
+  receiveDamage(theDamage) {
+
+    const damage = theDamage;
+    let actualHealth = this.health - damage
+
+    this.health = actualHealth
+  }
+
+}
 // Viking
-class Viking {}
+class Viking extends Soldier{    
+
+    // contructor class
+  constructor(name, health, strength){
+
+    super(health, strength)
+    this.name = name
+
+  }
+
+    // attack function
+  attack() {
+
+    return this.strength;
+
+  }
+
+    // receive damage function
+
+  receiveDamage(theDamage) {
+
+    const damage = theDamage;
+    let actualHealth = this.health - damage
+
+    this.health = actualHealth
+
+    if ( this.health > damage ){
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+
+      return `${this.name} has died in act of combat`
+    }
+
+  }
+
+    // battlecry function
+
+    battleCry() {
+
+      return "Odin Owns You All!"
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  
+  constructor( health, strength){
+
+    super(health, strength)
+   
+
+  }
+  attack() {
+
+    return this.strength
+  }
+  
+  receiveDamage(theDamage) {
+
+    const damage = theDamage;
+    let actualHealth = this.health - damage
+
+    this.health = actualHealth
+
+    if ( this.health > 0 ){
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+
+      return `A Saxon has died in combat`
+    }
+
+
+  
+}
+
+}
 
 // War
-class War {}
+class War {
+
+  constructor (){
+
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+
+  
+  addViking(viking)  {
+    
+    this.vikingArmy.push(viking)
+    
+
+  }
+
+  
+
+
+
+  
+  
+
+}
 
 
 
@@ -17,3 +137,5 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+
