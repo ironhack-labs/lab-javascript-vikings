778e344efc7e469b4291b20ff97a8aaed832dbb8
Andriw Tapanes <andriwtapanes@gmail.com>
2021-08-14 15:18:35 -0400

    working on the war class

--- src/viking.js
+++ src/viking.js
@@ -5,7 +5,7 @@ class Soldier {
     this.strength = strength;
   }
   attack() {
-    return this.stregnth;
+    return this.strength;
   }
   receiveDamage(damage) {
     this.health -= damage;
@@ -14,16 +14,16 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-  constructor(name, health){
-    super(health, strength)
+  constructor(name, health) {
+    super(health, strength);
     this.name = name;
   }
 
-  receiveDamage(){
+  receiveDamage() {
     return 0;
   }
 
-  battleCry(){
+  battleCry() {
     return 0;
   }
 }
@@ -32,7 +32,38 @@ class Viking extends Soldier {
 class Saxon {}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+
+  vikingAttack() {
+    let maxValueV = this.vikingArmy.length === 0 ? 1 : this.vikingArmy.length;
+    // evaluates to a number example : 8
+    let randomVikingIndex = Math.floor(Math.random() * (maxValueV - 1));
+    //evaluates to a viking from the viking army array
+    let randomViking = this.vikingArmy[randomVikingIndex];
+
+    let maxValueS = this.saxonArmy.length === 0 ? 1 : this.saxonArmy.length;
+    // evaluates to a number example : 8
+    let randomSaxonIndex = Math.floor(Math.random() * (maxValueS - 1));
+    //evaluates to a saxon from the saxon army array
+    let randomSaxon = this.saxonArmy[randomSaxonIndex];
+  }
+
+  saxonAttack() {}
+
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
