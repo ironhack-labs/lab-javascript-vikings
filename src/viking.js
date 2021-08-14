73465e6adb8a77d45f08e8d63647c670a36bcbd6
jaumebt90 <jaume.mx90@gmail.com>
2021-08-14 17:31:17 +0200

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -39,10 +39,10 @@ constructor(health, strength){
 }
 receiveDamage(damage){
   this.health -= damage;
-  if(this.health === 0){ 
-    return `A Saxon has died in combat`
-  } else { 
+  if(this.health > 0){ 
     return `A Saxon has received ${damage} points of damage`
+  } else { 
+    return `A Saxon has died in combat`
 }
 }
 }
