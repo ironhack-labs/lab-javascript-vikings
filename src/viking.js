865deb87e1fc0787db4585266a36831cf6dffb8e
jaumebt90 <jaume.mx90@gmail.com>
2021-08-14 16:57:26 +0200

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,69 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
-
+class Viking extends Soldier{
+  constructor(health, strength, name){
+    super(health, strength);
+    this.name = 'Harald';
+    this.health = 300;
+    this.strength = 150;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+    if(this.health === 0) { 
+      return `${this.name} has died in act of combat`
+    } else { 
+      return `${this.name} has received ${damage} points of damage` 
+    }
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+}
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+constructor(health, strength){
+  super(health, strength);
+  this.health = 60;
+    this.strength = 25;
+}
+receiveDamage(damage){
+  this.health -= damage;
+  if(this.health === 0){ 
+    return `A Saxon has died in combat`
+  } else { 
+    return `A Saxon has received ${damage} points of damage`
+}
+}
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(viking){}
+
+  addSaxon(saxon){}
+
+  vikingAttack(){}
+
+  saxonAttack(){}
+}
 
 
 
