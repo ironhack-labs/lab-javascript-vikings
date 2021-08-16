2f78e20848734007cb99188cb5477b66d4598c2b
Ricardo Zurita <hola@ricardo-zurita.com>
2021-08-16 08:09:56 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,13 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+}
+function attack() {
+  return this.strength;
+}
 
 // Viking
 class Viking {}
@@ -9,8 +17,6 @@ class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
