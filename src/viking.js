50b4c771141d28691b5a254503bb4127c9d5fb7d
Alice <alice.jost@outlook.de>
2021-09-24 23:34:52 +0200

    Iteration 2.1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,8 @@
 // Soldier
 class Soldier {
   constructor(health, strength) {
-    this.health = health,
-    this.strength = strength
+    this.health = health;
+    this.strength = strength;
   };
 
   attack () {
@@ -11,13 +11,33 @@ class Soldier {
 
 //TODO: receiveDamage() should remove the received damage from the health property
   receiveDamage (damage) {
-    this.health - damage
+    this.health - damage;
   };
 
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  };
+
+receiveDamage(damage) {
+  this.health - this.damage;
+  if (this.health > 0) {
+    return `${this.name} has received ${this.damage} points of damage`
+  } else {
+    return `${this.name} has died in act of combat`
+  }
+};
+
+battleCry () {
+  return "Odin Owns You All!"
+};
+
+}
 
 // Saxon
 class Saxon {}
