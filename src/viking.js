0d7ec8a948f71ec78e743135b6375f099884b931
Dominika Lipka <dominika.lipka96@gmail.com>
2021-09-24 19:57:11 +0200

    iterations 1-4 done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  };
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+        return `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = []
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+    let indexOfRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomViking = this.vikingArmy[indexOfRandomViking];
+    let indexOfRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomSaxon = this.saxonArmy[indexOfRandomSaxon];
+    let battleOutcome = randomSaxon.receiveDamage(randomViking.strength);
+    if (randomSaxon.health <= 0) {
+      this.saxonArmy.splice(indexOfRandomSaxon,1);
+    } return battleOutcome;
+  } 
+  saxonAttack(){
+    let indexOfRandomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let randomViking = this.vikingArmy[indexOfRandomViking];
+    let indexOfRandomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomSaxon = this.saxonArmy[indexOfRandomSaxon];
+    let battleOutcome = randomViking.receiveDamage(randomSaxon.strength);
+    if (randomViking.health <= 0) {
+      this.vikingArmy.splice(indexOfRandomViking,1);
+    
+    } return battleOutcome;
+  }
+}
 
 
 
