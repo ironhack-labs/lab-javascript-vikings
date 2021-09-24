73eb0df4c5882c448bbeb875e69c4332094792f9
jeannette.valencia <jfatima.valencia@gmail.com>
2021-09-24 23:12:21 +0200

    classes declarations

--- src/viking.js
+++ src/viking.js
@@ -2,7 +2,7 @@
 class Soldier {
   constructor (health, strength){
     this.health = health;
-    this.strength = this.strength;
+    this.strength = strength;
   }
 
   attack (){
@@ -15,13 +15,64 @@ class Soldier {
 }
 
 // Viking
-class Viking extends Soldier {}
+class Viking extends Soldier {
+ constructor (vikingName, health, strength){
+   super ();
+   this.name = vikingName;
+   this.health = health;
+   this.strength = strength;
+ }
+
+ receiveDamage(damage) {
+   if (Viking === 'alive') {
+     return `${this.name} has received ${this.damage} points of damage`
+   } else if (Viking === 'dies') {
+    return `${this.name} has died in act of combat`
+   }
+ }
+
+ battleCry() {
+   return 'Odin Owns You All!'
+ }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+   if (saxon = "alive"){
+     return 'a Saxon has received damage points of damage'
+   } else {
+     return 'a Saxon has died in combat'
+   }
+ }
+}
 
 // War
-class War {}
+class War {
+  constructor () {
+    super();
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(Viking){
+    this.vikingArmy.push(this.name)
+  };
+
+  addSaxon(Saxon){
+    this.saxonArmy.push(this.name)
+  };
+
+  vikingAttack(){
+    
+  };
+
+  saxonAttack(){
+
+  };
+  
+  showStatus();
+}
 
 
 
