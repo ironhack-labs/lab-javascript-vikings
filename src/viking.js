391f4b79a628c91ce9b33f28d6054d4a207da094
karlajaramillo <karlav.jaramillo@gmail.com>
2021-09-30 21:11:08 +0200

    Add Iteration # 2 - done

--- src/viking.js
+++ src/viking.js
@@ -13,7 +13,26 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  // call parent class to create the object
+  constructor(name, health, strength) {
+    super(health, strength); // pass the parent class the arguments
+    this.name = name; // assign the new attribute
+  }
+
+  receiveDamage (damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry () {
+    return `Odin Owns You All!`
+  }
+}
 
 // Saxon
 class Saxon {}
