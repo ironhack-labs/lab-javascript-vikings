2cac88fe1052fb819d5bbdcdefc5886d2fe07234
Fernando Cardona <fernandocardona@MacBook-Pro-de-FernandoCardona.local>
2021-10-22 16:43:47 +0200

    primera subida

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,66 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health , strength) {
+      this.health = health;
+      this.strength = strength;
+      // this.attacks = {
+      //     punch: 100,
+      //     kick: 200
+      // }
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength ) {
+      super(health , strength)
+      this.name = name;    
+  }
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0){
+        return `${this.name} has received ${damage} points of damage`
+    }else {
+        return `${this.name} has died in act of combat`
+    }
+  }
+}
+
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength ) {
+      super(health , strength)
+          
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    
+  }
+}
 
 // War
 class War {}
