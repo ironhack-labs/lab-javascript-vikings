efd37a4ce2f740d9291d6e1f124c775cacfddb41
Raul <raulgamci@gmail.com>
2021-10-22 16:49:11 +0200

    Glory to Odin, our magestic father

---
+++ src/vikingscopy.js
@@ -0,0 +1,32 @@
+// Soldier
+class Soldier {
+    constructor (health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(health) {
+
+    }
+ }
+
+// Viking
+class Viking { }
+
+// Saxon
+class Saxon { }
+
+// War
+class War { }
+
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+    module.exports = { Soldier, Viking, Saxon, War };
+}
