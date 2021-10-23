7da06ed7d80fa733d843ebbea48b4afc77673edc
Greg Munro <gregmunro90@gmail.com>
2021-10-23 15:27:34 +0200

    iteration 3

--- src/viking.js
+++ src/viking.js
@@ -19,20 +19,36 @@ class Viking extends Soldier {
     super (health, strength);
     this.name = name;
     this.attack
+
     if (this.health > 0) {
       console.log(`${this.name}has received ${this.damage} points of damage.`);
-    }
-    else if (this.health <= 0){
+    } else if (this.health <= 0){
       console.log(`${this.name}has died in act of combat`);
     }
+    
   }
-  battleCry() {
+    receiveDamage(damage){
+    this.health -= damage;
+    }
+    battleCry() {
     return 'Odin Owns You All!';
   }
 };
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    receiveDamage(damage){
+    this.health -= damage;
+    }
+
+    if (this.health > 0) {
+      console.log(`A Saxon has received ${this.damage} points of damage.`);
+    } else if (this.health <= 0){
+      console.log(`A Saxon has died in combat`);
+}
+
+
 
 // War
 class War {}
