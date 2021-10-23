5c6396f428c2defc4fe6582e6eceb227bb1c06f1
Dianelys Rodriguez Sarmientos <diany@Efrens-MacBook-Pro.local>
2021-10-23 12:01:03 -0400

    updating this LAB

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage){
+
+  }
+  battleCry() {
+
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health , strength){
+    super(health , strength);
+  }
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if(this.health <= 0){
+      return "A Saxon has died in combat";
+    } else {
+      return `A Saxon has received ${damage} points of damage` 
+    }
+  }
+}
+const Efren = new Saxon(50 , 1000);
 
 // War
 class War {}
