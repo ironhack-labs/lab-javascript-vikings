07e126ed0fef5aeaa4a5a0d86dd875c6a9fa33f5
pilauria <ptr.lauria@gmail.com>
2021-10-23 09:58:52 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -23,9 +23,8 @@ class Viking extends Soldier {
     this.health -= damage;
     if (this.health > 0) {
       return `${this.name} has received ${damage} points of damage`;
-    } else {
-      return `${this.name} has died in act of combat`;
     }
+    return `${this.name} has died in act of combat`;
   }
 
   battleCry() {
@@ -39,9 +38,8 @@ class Saxon extends Soldier {
     this.health -= damage;
     if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`;
-    } else {
-      return `A Saxon has died in combat`;
     }
+    return `A Saxon has died in combat`;
   }
 }
 
@@ -57,9 +55,41 @@ class War {
   addSaxon(saxon) {
     this.saxonArmy.push(saxon);
   }
-  vikingAttack() {}
-  saxonAttack() {}
-  showStatus() {}
+  vikingAttack() {
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+    let battle = randomSaxon.receiveDamage(randomViking.strength);
+
+    this.saxonArmy = this.saxonArmy.filter((saxonUnit) => saxonUnit.health > 0);
+
+    return battle;
+  }
+  saxonAttack() {
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+
+    let battle = randomViking.receiveDamage(randomSaxon.strength);
+
+    this.vikingArmy = this.vikingArmy.filter(
+      (vikingUnit) => vikingUnit.health > 0
+    );
+
+    return battle;
+  }
+  showStatus() {
+    if (this.saxonArmy.length === 0) {
+      return 'Vikings have won the war of the century!';
+    } else if (this.vikingArmy.length === 0) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
 }
 
 // The following is required to make unit tests work.
