34edaa2eb6a5c6a640d220aea767fcf7f0e6157a
Fernando Cases <nandocasesgarcia@gmail.com>
2021-10-25 13:36:09 +0200

    work in progress

--- src/viking.js
+++ src/viking.js
@@ -1,19 +1,35 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(health, strength) {
+    this.health = 300;
+    this.strength = 150;
+    this.name = 'Harald';
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage){
+     this.health -= damage
+  }
+}
 // Viking
-class Viking {}
-
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+  }
+}
 // Saxon
 class Saxon {}
-
 // War
 class War {}
-
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+
+
+
+
+
