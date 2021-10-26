d6d0ed609b2baeb6ae6dd36afd6f621b43ec1564
Noam Shani <Noams156@gmail.com>
2021-10-26 18:04:53 +0200

    lab 50% done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,90 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+  attack(){
+
+  return this.strength
+  }
+
+  receiveDamage(damage){
+ 
+    return this.health -= damage
+
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+
+  constructor(name){
+   super(health,strenght)
+   this.name = name
+}
+
+
+  receiveDamage(damage){
+ 
+    this.health -= damage 
+    
+    if (this.health>0){
+
+      return (`${this.name} has received ${this.damage} points of damage`)
+
+    }  return (`${this.name} has died in act of combat`)
+
+  }
+
+  battleCry(){
+
+  return "Odin Owns You All!"
+
+  }
+
+} 
 
 // Saxon
-class Saxon {}
+ 
+class Saxon extends Soldier {
+
+  receiveDamage(damage){
+ 
+    this.health -= damage 
+    
+    if (this.health>0){
+
+      return (`A Saxon has received ${this.damage} points of damage`)
+
+    }  return (`A saxon has died in combat`)
+
+  }
+
+
+
+}
 
 // War
-class War {}
+class War {
+  
+  
+  addViking(Viking){
+        this.vikingArmy = Viking
+  }
+
+  addSaxon()
+  vikingAttack()
+  saxonAttack()
+  showStatus()
+
+
+
+  
+}
 
 
 
