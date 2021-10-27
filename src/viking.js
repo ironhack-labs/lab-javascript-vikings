18299889e1c28ef109854a30bf080e50b02388f4
Norbert Guasch <norbertguach@infojobs.net>
2021-10-27 10:16:41 +0200

    improved classes code

--- src/viking.js
+++ src/viking.js
@@ -1,92 +1,133 @@
 // Soldier
 class Soldier {
-  constructor(health, strength){
+  constructor(health, strength) {
     this.health = health;
-    this.strength = strength
+    this.strength = strength;
   }
 
   attack() {
-    return this.strength
+    return this.strength;
   }
 
   receiveDamage(damage) {
-    this.health = this.health - damage
+    this.health = this.health - damage;
   }
 }
 
 // Viking
 class Viking extends Soldier {
   constructor(name, health, strength) {
-    super(Soldier);
+    super(health, strength);
     this.name = name;
-    this.health = health
-    this.strength = strength
   }
 
   receiveDamage(damage) {
-    this.health = this.health - damage
-    
-
-    if(this.health > 0) {
-      return `${this.name} has received ${damage} points of damage`
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
     } else {
-      return `${this.name} has died in act of combat`
+      return `${this.name} has died in act of combat`;
     }
   }
 
   battleCry() {
-    return "Odin Owns You All!"
+    return 'Odin Owns You All!';
   }
 }
 
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage) {
-    this.health = this.health - damage
+    this.health = this.health - damage;
 
-    if(this.health > 0) {
-      return `A Saxon has received ${damage} points of damage`
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
     } else {
-      return `A Saxon has died in combat`
+      return `A Saxon has died in combat`;
     }
   }
 }
 
 // War
-
-
 class War {
-  vikingArmy = []
-  saxonArmy = []
-
-  
+  vikingArmy = [];
+  saxonArmy = [];
 
   addViking(Viking) {
-    let newViking = new Viking(Viking)
-    vikingArmy.push(newViking)
+    this.vikingArmy.push(Viking);
   }
 
   addSaxon(Saxon) {
-    let newSaxon = new Viking(Saxon)
-    saxonArmy.push(newSaxon)
+    this.saxonArmy.push(Saxon);
   }
-  
+
   vikingAttack() {
-    vikingArmy.forEach( (elViking) => {
-      saxonArmy.forEach( (elSaxon) => {
-        elSaxon.receiveDamage(elViking.strength)
-        if(elSaxon.health < 0 ){
-          this.saxonArmy.pop(elSaxon)
-        }
-      })
-     })
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+    let vikingAttack = randomSaxon.receiveDamage(randomViking.strength);
+
+    if (randomSaxon.health < 0) {
+      this.saxonArmy = this.saxonArmy.filter((item) => item !== randomSaxon);
+    }
+
+    return vikingAttack;
+  }
+
+  saxonAttack() {
+    let randomViking =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    let randomSaxon =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+    let saxonAttack = randomViking.receiveDamage(randomSaxon.strength);
+    if (saxonAttack === randomViking.name + ' has died in act of combat') {
+      this.vikingArmy = this.vikingArmy.filter(
+        (item) => item.name !== randomViking.name
+      );
+    }
+
+    return saxonAttack;
+  }
+
+  showStatus() {
+    if (this.saxonArmy.length < 1) {
+      return 'Vikings have won the war of the century!';
+    }
+
+    if (this.vikingArmy.length < 1) {
+      return 'Saxons have fought for their lives and survived another day...';
+    }
+
+    if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
   }
 }
 
+//Vikings
+const Alfhild = new Viking('Alfhild', 200, 150);
+const Aslaug = new Viking('Aslaug', 100, 200);
 
+//saxons
+const Siena = new Saxon(50, 20);
+const Ethan = new Saxon(110, 400);
 
+const WWIII = new War();
 
-// The following is required to make unit tests work.
+WWIII.addViking(Alfhild);
+WWIII.addViking(Aslaug);
+
+WWIII.addSaxon(Siena);
+WWIII.addSaxon(Ethan);
+
+WWIII.vikingAttack();
+WWIII.saxonAttack();
+
+WWIII.showStatus();
+
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
