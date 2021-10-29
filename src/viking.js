324f82b5d3e381a19efeb23fc37cb64c30864297
Miguel Garcia <gunsandgamer@hotmail.com>
2021-10-29 18:23:32 -0500

    se completo iteracion 1 2 3

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,42 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, streght) {
+        this.health = health;
+        this.streght = streght;
+        this.attack = function() {
+            return this.streght
+        }
+        this.receiveDamage = function(damage) {
+            this.health -= damage
+        }
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, streght) {
+        super(health, streght);
+        this.name = name;
+        this.receiveDamage = function(damage) {
+            this.health -= damage;
+            return this.health > 0 ? `$(this.name) has received $(damage) points of damage` : `$(this.name) has died in act of combat`
+        }
+        this.battleCry = function() {
+            return "Odin Owns You All"
+        }
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, streght) {
+        super(health, streght)
+        this.receiveDamage = function(damage) {
+            this.health -= damage;
+            return this.health > 0 ? `À Saxon has received $(damage) points of damage ` : `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
@@ -15,5 +46,5 @@ class War {}
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = { Soldier, Viking, Saxon, War };
+    module.exports = { Soldier, Viking, Saxon, War };
 }
