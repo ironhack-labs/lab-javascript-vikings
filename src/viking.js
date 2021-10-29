aba0bbe5d399deb18e8f9d1dae1c0efb4dfdedce
pablovicho <pablovvb@gmail.com>
2021-10-29 13:34:51 -0500

    all mandatory exercises done. still working on the bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,64 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  
+    this.attack = function() {
+    return this.strength
+  }
+
+  this.receiveDamage = function(damage) {
+    this.health -= damage
+  }
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    
+    this.receiveDamage = function(damage) {
+      this.health -= damage;
+      return this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`
+    }
+
+    this.battleCry = function() {
+      return "Odin Owns You All!"
+    }
+
+    }
+
+
+  }
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+
+    this.receiveDamage = function(damage) {
+      this.health -= damage;
+      return this.health > 0 ? `A Saxon has received ${damage} points of damage` : `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  
+    addViking = function(Viking) {
+      vikingArmy.push(new Viking(health, strength))
+    }
+  
+}
+}
 
 
 
