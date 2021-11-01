596d55f3dbd799a5fa4f867f055dc47625a091d5
Javier Herrera <jaaah0010@gmail.com>
2021-11-01 08:58:35 -0600

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,54 @@
-// Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    
+
+  }
+  attack(){
+   return  this.strength;
+  }
+
+  receiveDamage(attack){
+    this.health -= attack
+  }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength);
+    this.name = "name";
+  }
+}
+
+
+
+
+
+
+
 
 // Saxon
 class Saxon {}
 
+
+
+
 // War
 class War {}
+
+
+
+
+
+
+
+
+
+
 
 
 
