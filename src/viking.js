171e3cc4ed82b39e769549e4154e1ee9dd0af47e
joaonacho <joaonacho@gmail.com>
2021-11-19 18:16:14 +0000

    stucked in VK attack

--- src/viking.js
+++ src/viking.js
@@ -18,18 +18,65 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
+
   receiveDamage(damage) {
     this.health = this.health - damage;
-    return `${this.name} has received ${damage} points of damage`;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
   }
-  battleCry() {}
+
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+
+  vikingAttack() {
+    let vikingWarrior =
+      this.vikingArmy[Math.round(Math.random() * this.vikingArmy.length)];
+
+    let saxonWarrior =
+      this.saxonArmy[Math.round(Math.random() * this.saxonArmy.length)];
+  }
+  saxonAttack() {}
+
+  showStatus() {
+    if (!this.saxonArmy.length) {
+      return 'Vikings have won the war of the century!';
+    } else if (!this.vikingArmy.length) {
+      return 'Saxons have fought for their lives and survived another day...';
+    } else {
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
