87fb8578f22ac64d4af8925544855db0c9e01b9c
Vitor Afonso <vittokm@hotmail.com>
2021-11-19 21:38:27 +0000

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,94 @@
+/* jshint esversion: 8*/
+
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+
+    this.health -= damage;
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+
+    } else {
+
+      return `${this.name} has died in act of combat`;
+    }
+
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+
+    let randNum = Math.floor(Math.random() * (this.saxonArmy.length + 1));
+
+    this.saxonArmy[randNum].receiveDamage(this.vikingArmy[randNum].strength);
+
+    this.saxonArmy.forEach(value => {
+
+      if (value.health < 0) {
+        this.saxonArmy.splice(value,1);
+      }
+    });  
+    
+    // return result of calling receiveDamage() of a Saxon with the strength of a Viking
+    
+  }
+  saxonAttack() {
+
+  }
+  showStatus() {
+    
+  }
+}
 
 
 
