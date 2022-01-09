b9eefb2cd79407b4663366057bc689f684cf2000
Ádám Báthori-Kiss <adam.bathori-kiss@lego.com>
2022-01-09 21:57:26 +0100

    AdamBathoriKiss-Vikings

--- src/viking.js
+++ src/viking.js
@@ -1,19 +1,84 @@
 // Soldier
-class Soldier {}
-
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
-// War
-class War {}
-
-
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = { Soldier, Viking, Saxon, War };
-}
+class Soldier {
+  constructor(health,strength) {
+    this.health = health;
+    this.strength = strength;
+    
+  }
+       attack(){
+      return  this.strength;
+    }
+  
+     receiveDamage(damage){
+  
+      this.health -= damage;
+    }
+  
+  }
+  // Viking
+  class Viking extends Soldier {
+  
+    constructor(name,health,strength){
+      super(health, strength);
+      this.name = name;
+    }
+  
+    receiveDamage(damage){
+  
+      this.health -= damage;
+  
+      if(this.health === 0){
+  
+        return `${this.name} has died in act of combat`;
+        
+      }
+  
+      else {
+        return `${this.name} has received ${damage} points of damage`;
+      }
+  
+  
+    }
+  
+    battleCry(){
+      return 'Odin Owns You All!';
+    }
+  
+  }
+  
+  // Saxon
+  class Saxon extends Soldier{
+  
+    super(health,strength){};
+  
+  
+    receiveDamage(damage){
+  
+      this.health -= damage;
+  
+      if(this.health === 0){
+  
+        return `A Saxon has died in combat`;
+        
+      }
+  
+      else {
+        return `A Saxon has received ${damage} points of damage`;
+      }
+  
+  
+    }
+  
+  }
+  
+  // War
+  class War {}
+  
+  
+  
+  // The following is required to make unit tests work.
+  /* Environment setup. Do not modify the below code. */
+  if (typeof module !== 'undefined') {
+    module.exports = { Soldier, Viking, Saxon, War };
+  }
+  
