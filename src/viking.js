43ddba3a52074ce93d8c031ded4c6c803dbc953a
Andrés García <playsharp59@gmail.com>
2022-01-14 18:31:00 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,17 +1,85 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+    else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    }
+    else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+  addViking(numViking) {
+    this.vikingArmy.push(numViking);
+  }
 
+  addSaxon(numSaxon) {
+    this.saxonArmy.push(numSaxon);
+  }
 
+  vikingAttack() {
+    let randS = Math.floor(Math.random());
+    let randV = Math.floor(Math.random());
 
+    if (this.saxonArmy <= 0) {
+      this.saxonArmy.splice(randV, 1)
+    }
+  }
+
+  saxonAttack() {
+
+  }
+
+  showStatus() {
+
+  }
+}
+
+console.log();
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
