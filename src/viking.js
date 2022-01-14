0ab98d0c187fa838cfb8a15ea0b06728a19898bd
Anna Ma Porras <amporras85@gmail.com>
2022-01-14 17:01:36 +0100

    done except bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,57 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthValue, strengthValue) {
+    this.health = healthValue
+    this.strength = strengthValue
+
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(nameValue, healthValue, strengthValue) {
+    super(healthValue, strengthValue)
+    this.name = nameValue
+  }
+  receiveDamage(damage) {
+    this.health -= damage
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health -= damage
+
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
-class War {}
+class War { }
 
 
 
