dbf5488f054c0bbce84e08d8fd96f358bbe5f8ed
Guille Ávila <guille9797@hotmail.com>
2022-01-14 18:40:33 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -58,8 +58,59 @@ class Saxon extends Soldier {
 }
 
 
-// War
-class War { }
+class War {
+  constructor() {
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+
+  addViking(Viking) {
+    this.vikingArmy.push(Viking)
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon)
+  }
+
+  removeDead(army) {
+    if (army.length === 0) return
+    let indexOfDeadSoldier = 0
+    for (let i = 0; i < army.length; i++) {
+      if (army[i].health <= 0) {
+        indexOfDeadSoldier = i
+        break
+      }
+    }
+    army.splice(indexOfDeadSoldier, 1)
+  }
+
+  vikingAttack() {
+    const randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length))
+    const randomViking = Math.floor(Math.random() * (this.vikingArmy.length))
+
+    const result = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+
+    this.removeDead(this.saxonArmy)
+
+    return result
+  }
+
+  saxonAttack() {
+    if (!this.vikingArmy.length) return
+    const randomSaxon = Math.floor(Math.random() * (this.saxonArmy.length))
+    const randomViking = Math.floor(Math.random() * (this.vikingArmy.length))
+    console.log(randomViking, this.vikingArmy, this.vikingArmy[randomViking])
+    const result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
+
+    this.removeDead(this.vikingArmy)
+
+    return result
+  }
+
+
+
+}
+
 
 
 
