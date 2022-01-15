28947806564a8a13ec85bb37a109fb69b2193bc7
Doan Tran <doan7tran@gmail.com>
2022-01-15 01:03:59 +0100

    Small changes to showStatus function

--- src/viking.js
+++ src/viking.js
@@ -93,9 +93,9 @@ class War {
 
 // Show Status
 function showStatus() {
-  if (this.saxonArmy.length < 1 && this.vikingArmy.length > 1) {
+  if (this.saxonArmy.length < 1 && this.vikingArmy.length > 0) {
     return 'Vikings have won the war of the century!';
-  } else if (this.vikingArmy.length < 1 && this.saxonArmy.length > 1) {
+  } else if (this.vikingArmy.length < 1 && this.saxonArmy.length > 0) {
     return 'Saxons have fought for their lives and survived another day...';
   } else if (this.saxonArmy.length === 1 && this.vikingArmy.length === 1) {
     return 'Vikings and Saxons are still in the thick of battle.';
