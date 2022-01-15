baabe1852f179aa314f3c3acf720d4ac1394c232
Gerardo Regalado <gerardo.regalado24@gmail.com>
2022-01-14 19:30:39 -0600

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,109 @@
 // Soldier
-class Soldier {}
+class Soldier 
+{
+  constructor (health,strength) 
+  {
+    this.health=health
+    this.strength=strength
+  }
+  attack()
+  {
+    
+    return this.strength
+  }
+  receiveDamage(damage)
+  {
+    this.health -= damage
+  }
+    
+  
+}
 
-// Viking
-class Viking {}
+// Vikings
+class Viking extends Soldier
+{
+  constructor (name,health,strength)
+  {
+    super(health,strength)
+    this.name=name
+  }
+  attack()
+  {
+  
+    return this.strength
+  }
+  receiveDamage(damage)
+  {
+  this.health-=damage
+    if (this.health>0)
+    {
+      return `${this.name} has received ${damage} points of damage`
+    }
+    else 
+    {
+      return `${this.name} has died in act of combat`
+    }
+    
+  }
+  battleCry()
+  {
+    return `Odin Owns You All!`
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier 
+{
+  
+
+
+  receiveDamage(damage)
+  {
+    this.health-=damage
+    if (this.health>0)
+    {
+    return `A Saxon has received ${damage} points of damage`
+    }
+    else 
+    {
+    return `A Saxon has died in combat`
+    }
+  }
+    attack()
+    {
+    return this.strength
+    }
+  
+}
 
 // War
-class War {}
+class War extends Soldier
+{
+  constructor(vikingArmy,saxonArmy)
+  addViking()
+  {
+   
+  }
+  addSaxon()
+  {
+  
+  }
+  vikingAttack()
+  {
+
+  }
+  saxonAttack()
+  {
+
+  }
+  showSstatus()
+  {
+
+  }
+
+
+
+}
 
 
 
