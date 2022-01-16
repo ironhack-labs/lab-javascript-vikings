bb5a67fb9a9d19ca8b59cdff9921be03a29332fe
Zena Bos <zta.bos@gmail.com>
2022-01-16 14:32:04 +0100

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,61 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.damage = damage;
+    this.health -= this.damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${this.damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.damage = damage;
+    this.health -= this.damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${this.damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
 
-
+  addViking(viking) {}
+  addSaxon() {}
+  vikingAttack() {}
+  saxonAttak() {}
+  showStatus() {}
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
