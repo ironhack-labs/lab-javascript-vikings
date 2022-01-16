13d40ae72ed78849ca014542eda12f347eff7b91
Jorge Garzon <jgarzon.informatica@gmail.com>
2022-01-17 00:28:11 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -108,6 +108,16 @@ class War {
 
 
   }
+  showStatus() {
+    if (this.saxonArmy.length <= 0) {
+      return `Vikings have won the war of the century!`
+    } else if (this.vikingArmy <= 0) {
+      return `Saxons have fought for their lives and survived another day...`
+    } else {
+      return `Vikings and Saxons are still in the thick of battle.`
+    }
+
+  }
 }
 
 
