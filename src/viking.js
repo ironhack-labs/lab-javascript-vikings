8ad2781cf6c250a25ff88a6e0f8fffa0f26003ce
Leonardo <mr_kunkel@hotmail.com>
2022-01-16 22:20:21 -0600

    War Iteration

--- src/viking.js
+++ src/viking.js
@@ -50,10 +50,46 @@ class Saxon extends Soldier{
   }
 }
 
+const viking1 = new Viking("Leo", 1000, 100);
+const viking2 = new Viking("Pame", 1000, 100);
+const viking3 = new Viking("Edu", 1000, 100);
+const saxon1 = new Saxon("Leo", 1000, 100);
+const saxon2 = new Saxon("Pame", 1000, 100);
+const saxon3 = new Saxon("Edu", 1000, 100);
+
 // War
 class War {
+  vikingArmy = []
+  saxonArmy = []
+  constructor() {
+    this.addViking = function(warrior) {
+      this.vikingArmy.push(warrior)
+      console.log(this.vikingArmy);
+    }
+    this.addSaxon = function(warrior) {
+      this.saxonArmy.push(warrior)
+    }
+    this.vikingAttack = function() {}
+    this.saxonAttack = function() {}
+    this.showStatus = function() {}
+  }
+  
   
 }
+
+const guerra = new War()
+guerra.addViking(viking1)
+guerra.addViking(viking2)
+guerra.addViking(viking3)
+guerra.addSaxon(viking1)
+guerra.addSaxon(viking2)
+guerra.addSaxon(viking3)
+
+
+
+
+
+
 
 
 
