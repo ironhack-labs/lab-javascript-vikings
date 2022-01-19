39c3853c9b8e46903804b217975d4d6037f2b34b
Joana Carballo <joana16cruz@gmail.com>
2022-01-19 20:39:05 +0100

    solved lab without bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,85 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthArgument, strengthArgument) {
+    this.health = healthArgument;
+    this.strength = strengthArgument;
+  }
+  attack() {
+    
+    return this.strength;
+    
+  }
+  receiveDamage(damageArgument) {
+    // I need to remove the damage from health
+    this.health = this.health - damageArgument;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+  super(health, strength)
+  this.name = name;
+  }
+
+  receiveDamage(damageArgument) {
+    
+    this.health = this.health - damageArgument;
+    
+    if (this.health > 0) {
+      return `${this.name} has received ${damageArgument} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return 'Odin Owns You All!'
+  }
+
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    }
+  
+    receiveDamage(damageArgument) {
+      this.health = this.health - damageArgument;
+  
+      if (this.health > 0) {
+        return `A Saxon has received ${damageArgument} points of damage`;
+      } else {
+        return `A Saxon has died in combat`;
+      }
+}
+
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+/*  
+  vikingAttack() {
+    receiveDamage() {
+      Math.floor((Math.random() * ) + 1);
+    }
+  }*/
+}
+
+
 
 
 
