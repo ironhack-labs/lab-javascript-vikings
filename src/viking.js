1f50cf6b2ea909572f3326d0d4c58a1f655d36c3
Hector <h.marfen@gmail.com>
2022-01-19 16:48:01 +0100

    first trial

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,84 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+attack (){
+  return this.strength
+}
+receiveDamage (damage){
+this.health = this.health - damage
+} 
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+receiveDamage (damage){
+  this.health = this.health - damage
+    if(this.health > 0){
+    return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+battleCry(){
+  return "Odin Owns You All!"
+  }
+}
+
+
+/* receiveDamage(damage) {
+  this.health = this.health - damage;
+  if (this.health > 0) {
+    return `${this.name} has received ${damage} points of damage`;
+  } else {
+    return `${this.name} has died in act of combat`;
+  }
+}
+*/
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    this.health = this.health - damage
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor()
+  addvikking(){
+    
+    
+  }
+  addSaxon(saxon){
+
+  }
+  vikingAttack(){
+    //choose a random saxon
+    //equal the recieveDamage to the health of the saxon
+    //if saxon health is equal or under 0 then it should be eliminated
+    //if not dead return the result of recievedamage and not eliminate the saxon
+    
+  }
+  saxonAttack(){
+    //choose a random saxon
+    //equal the recieveDamage to the health of the viking
+    //if viking health is equal or under 0 then it should be eliminated
+    //if not dead return the result of recievedamage and not eliminate the viking
+  }
+  showStatus(){}
+}
 
 
 
