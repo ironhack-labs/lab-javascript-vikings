5ce9689837704d80f66a59bf3387c01a122c13c4
Bruno Mortella <bmortella@gmail.com>
2022-01-28 19:55:26 -0300

    Iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
 class Viking {}
@@ -9,8 +22,6 @@ class Saxon {}
 
 // War
 class War {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
