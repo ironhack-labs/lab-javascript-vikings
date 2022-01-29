7c841ef108c9b6fe55c2848ab172979cd2e18c7d
Camila Solér <msoler.camila@gmail.com>
2022-01-28 22:03:14 -0300

    iteration 1 feita

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+  attack() {
+    return this.strength;  
+  }
+
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage
+  }
+}
 
 // Viking
 class Viking {}
