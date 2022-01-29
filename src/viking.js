08c56c6a87d78f047f4fc37639026a3f66465bb7
Camila Solér <msoler.camila@gmail.com>
2022-01-29 15:35:54 -0300

    lab done including iteration 4 e 5

--- src/viking.js
+++ src/viking.js
@@ -92,11 +92,19 @@ class War {
     return saxonDamage;
   }
 
+  showStatus(){
+    if(this.saxonArmy.length <= 0){
+      return 'Vikings have won the war of the century!';
+    
+    }else if(this.vikingArmy.length <= 0){
+      return 'Saxons have fought for their lives and survived another day...';
 
-
+    }else{
+      return 'Vikings and Saxons are still in the thick of battle.';
+    }
+    return showStatus()
+  }
   
-  
-
 }
 
 
