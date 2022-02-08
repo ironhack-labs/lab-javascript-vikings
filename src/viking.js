5387cd25eb7663a6ee94945fd9cce1755a2bef70
breogann <fernandocostapradillo@gmail.com>
2022-02-08 22:15:40 +0100

    except bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,59 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name
+  }
+
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health  > 0) {
+      return `${this.name} has received ${damage} points of damage`
+
+    } else {
+      return `${this.name} has died in act of combat`
+  }
+} 
+
+  battleCry () {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  attack () {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return "A Saxon has died in combat"
+    }
+  }
+}
 
 // War
 class War {}
