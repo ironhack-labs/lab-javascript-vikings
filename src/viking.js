950ad4e6fb317744bd1b25ca7d83ae956d2a5b75
breogann <fernandocostapradillo@gmail.com>
2022-02-08 23:08:55 +0100

    halfway bonus

--- src/viking.js
+++ src/viking.js
@@ -56,7 +56,38 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+addViking(Viking) {
+  this.vikingArmy.push(Viking)
+}
+addSaxon(Saxon) {
+  this.saxonArmy.push(Saxon)
+}
+vikingAttack() {
+  this.saxonArmy[0].strength -= this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength)
+
+  if (this.saxonArmy[0].health <= 0) {
+    this.saxonArmy.pop(this.saxonArmy[0])
+  }
+
+}
+
+
+saxonAttack() {
+  Viking.strength -= Viking.receiveDamage(Saxon.strength)
+
+}
+showStatus() {
+
+}
+
+}
 
 
 
