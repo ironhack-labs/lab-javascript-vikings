207d3751683ea1319599754d77da5d9b05160f88
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-03-25 18:07:59 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,99 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+
+    this.health = health
+    this.strength = strength
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+
+}
+
+const soldier = new Soldier(100, 50)
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor(name, health, strength) {
+
+    super(health, strength)
+
+    this.name = name
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    }
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+
+}
+
+const viking = new Viking("olaf", 70, 80)
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    }
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`
+    }
+  }
+}
+
+const saxon = new Saxon(100, 40)
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+
+  addViking(viking) {
+    this.vikingArmy.push(viking)
+  }
+
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon)
+  }
+
+  vikingAttack() {
+    function vikingRandom(vikingArmy) {
+      let randomViking = Math.random()
+      return randomViking
+    }
+
+    function saxonRandom(saxonArmy) {
+      let randomSaxon = Math.random()
+      return randomSaxon
+    }
+
+  }
+
+  saxonAttack() {
+
+  }
+
+}
+
 
 
 
