700532f31315ccd96df3c0c43fb21d8104ca66fc
Melissa Osorio <motart2505@gmail.com>
2022-03-25 15:10:51 -0600

    Done till Bonus 4

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,87 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength){
+   this.health = health
+   this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health-= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage){
+    this.health-= damage
+    if(this.health>0){
+      return `${this.name} has received ${damage} points of damage`
+    }else{
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength){
+    super(health, strength)
+  }
+  receiveDamage(damage){
+    this.health-= damage
+    if(this.health>0){
+      return `A Saxon has received ${damage} points of damage`
+    }else{
+      return 'A Saxon has died in combat'
+    }
+  }
+}
 
-// War
-class War {}
+// War BONUS 4
+class War {
+  constructor(){  
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+  addViking(newViking){
+   this.vikingArmy.push(newViking);
+  }
+  addSaxon(newSaxon){
+    this.saxonArmy.push(newSaxon);
+   }
+  vikingAttack(){
+      let saxonAttacked = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+      let vikingAttacking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+      let battleResult=saxonAttacked.receiveDamage(vikingAttacking.strength);
+      
+      if(saxonAttacked.health<=0){
+        let deleteSaxon = this.saxonArmy.indexOf(saxonAttacked);
+         this.saxonArmy.splice(deleteSaxon);
+      } return battleResult;
+  }
+  saxonAttack(){
+    let saxonAttacking = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+    let vikingAttacked = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+    let battleResult=vikingAttacked.receiveDamage(saxonAttacking.strength);
+    
+    if(vikingAttacked.health<=0){
+      let deleteViking = this.vikingArmy.indexOf(vikingAttacked);
+       this.vikingArmy.splice(deleteViking);
+    } return battleResult;
+  }
+
+
+
+}
 
 
 
