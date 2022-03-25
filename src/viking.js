e3d8e3184787403c073f3001876c67ced98f2682
Julian Rivera <julianarevircoins@gmail.com>
2022-03-25 14:08:39 -0400

    updated the coditional

--- src/viking.js
+++ src/viking.js
@@ -25,7 +25,7 @@ class Viking extends Soldier {
   receiveDamage(damage) {
     this.health = this.health - damage;
 
-    if (this.health > 0) {
+    if (this.health >= 0) {
       return `${this.name} has received ${damage} points of damage`;
     } else {
       return `${this.name} has died in act of combat`;
@@ -41,7 +41,7 @@ class Saxon extends Soldier {
   receiveDamage(damage) {
     this.health = this.health - damage;
 
-    if (this.health > 0) {
+    if (this.health >= 0) {
       return `A Saxon has received ${damage} points of damage`;
     } else {
       return `A Saxon has died in combat`;
