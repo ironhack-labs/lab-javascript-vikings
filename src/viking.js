66e6f9ead37ccf8663bca8c092a610e4808fe3a9
David Garcia <metaphysicaless1489@gmail.com>
2022-03-29 20:17:44 -0700

    hope this works

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,85 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
 
-// Viking
-class Viking {}
+  attack() {
+    return this.strength
 
-// Saxon
-class Saxon {}
+  }
 
+  receiveDamage(damage) {
+    this.health -= damage ;
+  } 
+
+}
+
+
+
+// Viking 
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+  super(name, health, strength)
+  this.name = name;
+  this.health = health
+  this.strength = strength
+  }
+  
+  receiveDamage(damage) {
+    this.health -= damage; 
+    if(this.health >= 0) {
+      return `${this.name}has received ${this.damage} points of damage`;
+    } else {
+      return `${this.name}has died in act of combat`;
+    }
+  } 
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+
+}
+
+//Saxon
+  class Saxon extends Soldier {
+
+
+
+ receiveDamage (damage) {
+  this.health -= damage;
+  if(this.health >= 0) {
+    return `${this.name}has received ${this.damage} points of damage`;
+  } else {
+    return `${this.name}has died in act of combat`;
+  } 
+ }
+
+}
 // War
-class War {}
+class War {
+  addViking(
+    let newVikings = new Viking()
+    let newVikings = new Viking()
+    let newVikings = new Viking()
+    let newVikings = new Viking()
+    let newVikings = new Viking()
+  );
+  addSaxon(
+    let newSaxon = new Saxon()
+    let newSaxon = new Saxon()
+    let newSaxon = new Saxon()
+    let newSaxon = new Saxon()
+    let newSaxon = new Saxon()
+  );
+  
+  vikingAttack();
+  saxonAttack();
+  showStatus();
+  vikingArmy = []
+  saxonArmy = []
+}
 
 
 
