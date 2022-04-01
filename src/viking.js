368ca31d73940ccdbef8300beb4a1b4cc66b2530
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-01 14:27:23 +0200

    first commit

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,44 @@
 // Soldier
-class Soldier {}
+//ignore the attack method github after doing it once, for the viking you just extend it 
+class Soldier {
+constructor(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+
+attack(){
+  return this.strength;
+}
+
+receiveDamage(thedamage){
+  this.health -= damage;
+} 
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){ 
+  super(health,strength)
+  this.name = name;
+}
+  attack (){
+    return this.strength;
+  }
+
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+constructor(){
+
+super()
+
+}
+  attack(){
+    return this.strength;
+  }
+}
 
 // War
 class War {}
