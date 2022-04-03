ea5ad8170d44eac4f801d938dd7d5d1493f20353
Valentin Paul <valentin@vamptech.de>
2022-04-03 22:26:47 +0200

    solved lab vikings

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,71 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+    return this.strength;
+  }
+  receiveDamage (damage){
+   this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(vikingName, vikingHealth, vikingStrength) {
+    super(vikingHealth, vikingStrength);
+    this.vikingName = vikingName;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.vikingName} has received ${damage} points of damage`;
+    } else if (this.health <= 0) {
+      return `${this.vikingName} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health <= 0) {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+constructor(vikingArmy, saxonArmy){
+  this.vikingArmy = vikingArmy
+  this.saxonArmy = saxonArmy
+}
+addViking(Viking){
+  this.vikingArmy += Viking;
+}
+addSaxon(Saxon){
+this.saxonArmy += Saxon;
+}
+vikingAttack(){
+
+
+}
+saxonAttack(){
+
+}
+showStatus(){
+
+}
+}
 
 
 
