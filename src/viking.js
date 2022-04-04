e8087b6cc78b6d6fb0fb697aa539abbf0ebc0d3d
“Sina <johndoe@example.com>
2022-04-04 02:39:40 +0200

    solved questions 1 and 2

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+  constructor(health, strength){
+    this.health=health;
+    this.strength=strength;
+  }
+  
+  attack(){
+    return this.strength;
+  }
+  
+receiveDamage(thedamage){
+    
+      this.health= this.health - theDamage;
+      return "Soldiers never dies";
+      
+        
+      }
+  };
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier  {
+   
+    // TODO
+    constructor(name,health,strength){
+      super(health,strength);
+   this.name = name;
+      
+    }
+    
+    takenDamage(theDamage) {
+      this.health = this.health - theDamage;
+    if (this.health > 0 ) {
+      return `${this.name} has received ${theDamage} points of damage`;}
+     else {
+      return `${this.name} has died in act of combat`;
+      } 
+    }
+    battleCry() {
+      return "Odin Owns you All!";
+    }
+    }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    }
+  
+
 
 // War
 class War {}
