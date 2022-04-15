5d9d230204d82a86ef26d135b3ecf9ebb3917e81
Mariana Pontes <marianasevinatep@gmail.com>
2022-04-15 16:42:51 +0100

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,17 +1,88 @@
 // Soldier
-class Soldier {}
+//Iteration 1: Soldier: Modify the Soldier class and add 2 methods to it:
+//attack(), and receiveDamage().
+//class: should receive 2 arguments (health & strength) + should receive the health
+//property as its 1st argument + should receive the strength property as its 2nd
+// argument
 
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
+let soldier1 = new Soldier(10, 5);
+console.log(soldier1);
 // Viking
-class Viking {}
-
+//A Viking is a Soldier with an additional property, their name.
+//They also have a different receiveDamage() method and new method, battleCry().
+//Modify the Viking class, have it inherit from Soldier, re-implement the
+//receiveDamage() method for Viking, and add a new battleCry() method.
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return 'Odin Owns You All!';
+  }
+}
+const Viking1 = new Viking('Arnaldo', 50, 30);
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+  attack() {
+    return this.strength;
+  }
+}
 
 // War
-class War {}
-
-
-
+class War {
+  constructor() {}
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    if (Saxon.receiveDamage(Viking.strength) < 0) {
+      this.saxonArmy.pop(Saxon);
+    }
+    return receiveDamage(Saxon);
+  }
+  saxonAttack() {}
+}
+const newSaxon = new Saxon();
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
