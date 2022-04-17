60d3fabb0e4b2b9dbcc39f14a5d252b0596cc997
JoaoMorgado8 <joaoarmorgado@gmail.com>
2022-04-17 08:43:28 +0100

    kind of solved

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,74 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+    // This method is incompleted, just left it here so later I can check if my logic was going the right way :'( );
+    let randomSaxon = ''; // saxon picked randomlly;
+    Math.floor(Math.random() * this.saxonArmy.length);
+    let randomViking = '';
+    Math.floor(Math.random() * this.vikingArmy.length);
+    return (randomSaxon = randomViking.strength);
+  }
+}
 
-
+const war = new War();
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
