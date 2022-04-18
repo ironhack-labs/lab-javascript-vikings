ab3fb399620928ba03b9f83f46a47f4b51d432a2
unknown <rodrigozapanaflores@gmail.com>
2022-04-18 18:59:10 +0200

    falta el bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,58 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(healthParam, strengthParam) {
+    this.health = healthParam;
+    this.strength = strengthParam;
+  }
+  attack = () => {
+    return this.strength
+  }
+  receiveDamage = (theDamage) => {
+    this.health = this.health - theDamage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends soldier {
+  constructor(nameParam,healthParam,strengthParam){
+    super(healthParam , strengthParam);
+    this.nameParam = Viking
+  }
+  attack = () => {
+    return  this.strength
+  }
+  receiveDamage = (theDamage) => {
+    this.health = this.health - theDamage;
+
+    if(this.health > 0){
+      return`${this.nameParam} has received ${theDamage} points of damage`
+    }else{
+      return `${this.nameParam} has die in act of combat`
+    }
+  }
+  battleCry= () =>{
+    return `Odin owns your all`
+  }
+
+}
+  
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage = (theDamage) => {
+    this.health = this.health - theDamage;
+
+    if(this.health > 0){
+      return`A sajon has received ${theDamage} points of damage`
+    }else{
+      return `A sajon has died in combat`
+    }
+  }
+
+
+}
 
 // War
 class War {}
