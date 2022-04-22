c7a820bc8440bccdb16bf2abaefc5f7a7b1ef2da
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-22 20:15:00 +0200

    Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -33,7 +33,16 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon extends Soldier{}
+class Saxon extends Soldier{
+  receiveDamage (damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
