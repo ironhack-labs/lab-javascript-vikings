b25457e2f57f637a31cf4fb71c01493ac7dee405
“Mapy <mapy.villalobos@gmail.com>
2022-04-22 12:42:18 -0500

    done

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,83 @@
 // Soldier
-class Soldier {}
+class Soldier{
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name= name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    this.damage=damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${this.damage} points of damage`
+  } else {
+    return `${this.name} has died in act of combat`
+  }
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    this.damage=damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${this.damage} points of damage`
+  } else {
+    return `A Saxon has died in combat`
+  }
+  }
+
+}
+
 
 // War
-class War {}
+class War {
 
+  vikingArmy = []
+  saxonArmy = []
 
+  addViking(viking){
+  }
+  addSaxon(saxon){
+
+  }
+  vikingAttack(){
+  Saxon.damage = Viking.strength
+  return Saxon.receiveDamage
+  }
+  saxonAttack(){}
+  showStatus(){
+    if (this.saxonArmy <= 0 ){
+      return 'Vikings have won the war of the century!'
+    } else if (this.vikingArmy <= 0){
+      return 'Saxons have fought for their lives and survived another day...'
+    } else if (this.saxonArmy && this.vikingArmy >= 1) {
+      return 'Vikings and Saxons are still in the thick of battle.'
+    }
+  }
+
+  
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
