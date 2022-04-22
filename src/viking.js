c7aca80c4a28d83bb01a99e11dc6a1f3cde78296
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-04-22 23:12:29 +0200

    tried few bonus tasks and got correct, please comment out, if results in negative marking.

--- src/viking.js
+++ src/viking.js
@@ -45,7 +45,38 @@ class Saxon extends Soldier{
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+   
+   // let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+   // let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    
+  }
+
+  saxonAttack() { 
+ }
+  showStatus() {
+    if(this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
+    } else  if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else if (this.saxonArmy.length === 1 || this.vikingArmy.length === 1) {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
+
+}
+
 
 
 
