7dfc6afcf16cb7474b07f4532bfed79d3c78306b
morgansawerr <103467896+morgansawerr@users.noreply.github.com>
2022-04-23 09:07:25 -0500

    Update viking.js

--- src/viking.js
+++ src/viking.js
@@ -1,15 +1,68 @@
+const setupMethods = require("solc/wrapper");
+
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+    attack(){
+      return this.strength;
+    }
+
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name 
+  }
+
+  attack(){
+    return Viking.strength
+  }
+
+  receiveDamage(damage){
+    this.health -= damage;
+    if (Viking.health > 0){
+    console.log(`${this.name} has received ${this.damage} points of damage`)
+    } else {
+    console.log(`${this.name} has died in act of combat`)
+    }
+
+  battleCry(){
+    console.log("Odin Owns You All!")
+  }
+    
+
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  attack(){
+    return Saxon.strength
+  }
+
+  receiveDamage(damage){
+    this.health -= damage;
+    if (Saxon.health >= 0) {
+      console.log(`A Saxon has received ${this.damage} points of damage`)
+    } else {
+      console.log(`A Saxon has died in combat`)
+    }
+  }
+}
 
 // War
-class War {}
+class War {
 
+}
 
 
 // The following is required to make unit tests work.
