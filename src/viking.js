ab1e91bb9e0cc705eccfa046c952780d2891fbf2
Amanda Walker <amandajnewalker@gmail.com>
2022-04-23 18:04:05 -0400

    lab done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,63 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health, strength){
+this.health = health;
+this.strength =strength;
+}
+
+attack(){
+  return this.strength;
+}
+
+  receiveDamage(myDamage){
+  this.health -= myDamage;
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health,strength);
+    this.name = name;
+  }
+
+  receiveDamage(damageReceived) {
+    this.health -= damageReceived;
+    if (damageReceived >= this.health) {
+      return `${this.name} has died in act of combat`
+    }
+    return `${this.name} has received ${damageReceived} points of damage`
+  }
+  battleCry() {
+    return "Odin Owns You All"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receieveDamage(damageReceived) {
+    this.health -= damageReceived;
+    if (damageReceived >= this.health) {
+      return 'A saxoon has died in combat'
+    }
+    return `A Saxon has received ${damageReceived} points of damage`;
+  }
+}
 
-// War
-class War {}
+// BONUS War
+// class War {
+
+
+// }
+
+
+
+
+
+
+
+
 
 
 
