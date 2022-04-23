cb0f8a83849160a4259fd192e4312cdf11d2e69b
Azdhy Galvan <azdhy16@gmail.com>
2022-04-23 01:05:04 -0500

    Solved lab Azdhy

--- src/viking.js
+++ src/viking.js
@@ -85,12 +85,12 @@ class War {
   }
 
   showStatus() {
-    if ((this.saxonArmy.length = 0)) {
-      return 'Vikings have won the war of the century!';
-    } else if ((this.vikingArmy.length = 0)) {
-      return 'Saxons have fought for their lives and survived another day...';
-    } else if (this.saxonArmy.length === this.vikingArmy.length) {
-      return 'Vikings and Saxons are still in the thick of battle.';
+    if ((this.saxonArmy.length === 0)) {
+      return "Vikings have won the war of the century!";
+    } else if ((this.vikingArmy.length === 0)) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+      return "Vikings and Saxons are still in the thick of battle.";
     }
   }
 }
