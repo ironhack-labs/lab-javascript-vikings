18cb906ea17836c788ef0487f62b2e871f85617f
Evgeniia Riaguzova <aglaya_2004@mail.ru>
2022-04-23 09:47:40 +0200

    done, but dont know about return-statements

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,48 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.strength = strength;
+    this.health = health;
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health=this.health-damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength){
+    super(health, strength);
+    this.name=name;
+  }
+  receiveDamage(damage){
+    this.health=this.health-damage
+    if (this.health < 0) {
+      return `${this.name} has received ${this.damage} points of damage`
+    }
+      else {return `${this.name}has died in act of combat`}
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  receiveDamage(damage){
+    this.health=this.health-damage
+    if (this.health < 0) {
+      return "A Saxon has received ${this.damage} points of damage"
+    }
+      else {return "A Saxon has died in act of combat"}
+}
 
 // War
-class War {}
+ class War {}
 
 
 
