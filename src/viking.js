de24280cb3b8f9d79be423641f67375bc117952e
Carlos Perez <carlosa.ps@outlook.com>
2022-04-22 22:29:43 -0500

    Hecho, sin bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health,strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack () {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health -= theDamage 
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name,health,strength,){
+    super(health,strength);
+    this.name = name;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage 
+    if (this.health > 0){
+      return `${this.name} has received ${theDamage} points of damage`;
+    }else{
+      return `${this.name} has died in act of combat`
+    }
+  }
+
+  battleCry(){
+    return 'Odin Owns You All!';
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(health,strength){
+    super(health,strength)
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage 
+    if (this.health > 0){
+      return `A Saxon has received ${theDamage} points of damage`;
+    }else{
+      return `A Saxon has died in combat`;
+    }
+  }
+
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(vikingObject){
+    this.vikingArmy.push(vikingObject);
+  }
+
+  addSaxon(saxonObject){
+    this.saxonArmy.push(saxonObject);
+  }
+
+  vikingAttack(){}
+  
+  saxonAttack(){}
+
+  showStatus() {}
+
+} 
 
 
 
