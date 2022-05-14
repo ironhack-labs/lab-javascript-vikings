1799596847b3e94b6ac0fda1018cba7e911781fa
CrisArranz <c.arranz.arevalo@gmail.com>
2022-05-14 15:28:44 +0200

    First commit- all iterations without bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,73 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  attack() {
+    return super.attack();
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage)
+    return this.health > 0 ? `${this.name} has received ${damage} points of damage` : `${this.name} has died in act of combat`
+  }
+
+  battleCry(){
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  attack(){
+    return super.attack();
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+    return this.health > 0 ? `A Saxon has received ${damage} points of damage` : `A Saxon has died in combat`
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(){
+  }
+  addSaxon(){
+  }
+  vikingAttack(){
+
+  }
+  saxonAttack(){
+
+  }
+  showStatus(){
+
+  }
+
+}
 
 
 
