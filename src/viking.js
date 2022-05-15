8a47cdaf76af57d1ca002f3f034f7df333fdd1a3
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-05-15 13:07:03 +0200

    Iterations 0,1,2 done

--- src/viking.js
+++ src/viking.js
@@ -16,17 +16,29 @@ class Soldier {
 
 
 // Viking
+
 class Viking extends Soldier {
   constructor (name, health, strength) {
-    super (health, strength)
+    super (health, strength) 
     this.name = name
   }
 
   attack() {
-    
+    return this.strength
   }
 
-  receiveDamage() {}
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!"
+  }
 
 }
 
