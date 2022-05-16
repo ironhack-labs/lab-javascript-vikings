d29a6692eb59594546bd0e0ad1b686005323c22f
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-05-16 14:49:48 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,71 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+
+}
+
+
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name)
+    this.name = name
+    this.health = health
+    this.strength = strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`
+    }
+    return `${this.name} has received ${damage} points of damage`
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+}
+
+
+const viking1 = new Viking('Alnior', 10, 15)
+
+console.log(viking1.receiveDamage(10))
+console.log('Viking1:', viking1)
+
+
+
+
 
 // Saxon
-class Saxon {}
+
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage
+
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`
+    }
+    return `A Saxon has received ${damage} points of damage`
+  }
+
+}
+
 
 // War
 class War {}
