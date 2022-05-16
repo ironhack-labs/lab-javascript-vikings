3a2261ad0b65ee41c149edec80a404fe6e0422eb
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-05-16 14:31:22 +0200

    Iteration 3 Saxon done

--- src/viking.js
+++ src/viking.js
@@ -32,7 +32,16 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+      return "A Saxon has died in combat";
+    } else {
+      return `A Saxon has received ${damage} points of damage`;
+    }
+  }
+}
 
 // War
 class War {}
