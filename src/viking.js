0219341c8c24d147ad7c099001971afd49f04b4c
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-05-16 16:17:42 +0200

    done with bonus

--- src/viking.js
+++ src/viking.js
@@ -96,14 +96,80 @@ class War {
     }
   }
 
-  saxonOrVikingAttack() {
-    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+  saxonOrVikingAttack(attackerArmy, defenderArmy) {
+    const randomAttacker = Math.floor(Math.random() * attackerArmy.length);
+    const randomDefender = Math.floor(Math.random() * defenderArmy.length);
 
-    const saxonDamage = this.saxonArmy[randomSaxon].strength;
-    const vikingDamage = this.vikingArmy[randomViking].strength;
+    const attackerDamage = attackerArmy[randomAttacker].strength;
+
+    const result = defenderArmy[randomDefender].receiveDamage(attackerDamage);
+    if (defenderArmy[randomDefender].health <= 0) {
+      defenderArmy.splice(randomDefender, 1);
+    }
+    return result;
   }
 }
+
+// Vikings
+const vikings = [
+  new Viking('Harrold', 45, 50),
+  new Viking('Ragnar', 30, 50),
+  new Viking('Vikpus', 30, 50),
+  new Viking('Rognar', 30, 50),
+  new Viking('Harrick', 30, 50),
+  new Viking('Viggo', 30, 50),
+  new Viking('Harald', 30, 50),
+  new Viking('Astrid', 30, 50),
+  new Viking('Floki', 30, 50),
+  new Viking('Thor', 30, 50),
+  new Viking('Odin', 30, 50),
+  new Viking('Frigg', 30, 50)
+];
+
+// Saxons
+const saxons = [
+  new Saxon(20, 28),
+  new Saxon(30, 21),
+  new Saxon(40, 30),
+  new Saxon(20, 29),
+  new Saxon(25, 26),
+  new Saxon(35, 41),
+  new Saxon(34, 22),
+  new Saxon(32, 31),
+  new Saxon(45, 50)
+];
+
+const guerre1 = new War();
+
+for (const saxon of saxons) {
+  guerre1.addSaxon(saxon);
+}
+
+for (const viking of vikings) {
+  guerre1.addViking(viking);
+}
+
+function fights() {
+  const armies = [guerre1.vikingArmy, guerre1.saxonArmy];
+
+  while (guerre1.saxonArmy.length > 0) {
+    let randomNum1 = Math.floor(Math.random() * 2);
+    let randumNum2 = 1;
+
+    if (randomNum1 === 1) {
+      randumNum2 = 0;
+      console.log('Attaque des Saxons');
+    } else {
+      randumNum2 = 1;
+      console.log('Attaque des Vikings');
+    }
+    guerre1.saxonOrVikingAttack(armies[randomNum1], armies[randumNum2]);
+
+    console.log(guerre1.showStatus());
+  }
+}
+
+fights();
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
