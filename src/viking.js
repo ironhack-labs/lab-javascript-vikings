33f5b25f654bd8c4beceb6bfc0a22fc0daf12b9a
LuisEUM <luiseum95@gmail.com>
2022-05-18 00:57:43 +0200

    Iteartion 1, 2 and 3 almost Done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength
+  }
+  
+  receiveDamage(damage) {
+    this.health = this.health - damage 
+    return this.health
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength){
+    super (health, strength)
+    this.name = name; 
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage 
+    if(this.health <= 0 ){
+      return `${this.name} has died in act of combat`
+    } else if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    }
+    return this.health
+  }
+
+
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  // constructor (health, strength) {
+  //   super(health, strength)
+  // }
+
+  receiveDamage(){
+
+  }
+
+}
 
 // War
 class War {}
