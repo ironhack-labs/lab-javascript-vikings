562be61fe7fca5b338ce369bb24716ae3aa75a13
Andrea Galicia <qndreashinee@gmail.com>
2022-05-19 01:21:41 +0200

    hecho hasta el bonus

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,92 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health, strength) {
+this.health = health
+this.strength = strength
+}
+
+attack (){
+  return this.strength
+}
+
+receiveDamage(damage){
+  this.health -= damage
+
+}
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+    this.name = name
+    }
+
+    battleCry(){
+     return `Odin Owns You All!`
+    }
+
+    receiveDamage(damage){
+      super.receiveDamage(damage) 
+
+    if( this.health > 0 ){
+      return `${this.name} has received ${damage} points of damage`
+    }else{ 
+      return `${this.name} has died in act of combat`
+    }
+     
+    }
+    
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor(health, strength) {
+    super(health, strength)
+  }
+
+    receiveDamage(damage){
+      super.receiveDamage(damage)
+
+      if( this.health > 0 ){
+        return ` A Saxon has received ${damage} points of damage`
+      }else{ 
+        return ` A Saxon has died in act of combat`
+      }
+
+    }
+    
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy  = [];
+
+  addViking(viking){
+    this.vikingArmy.push(viking)
+  }
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+
+  }
+  vikingAttack(){
+//cuando un viking ataca un saxon recive daño
+//
+  }
+  saxonAttack(){
+
+  }
+  showStatus(){
+    
+  }
+
+
+
+
+}
 
 
 
