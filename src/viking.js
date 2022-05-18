560b18677637d4646f95d36e2cec367251722368
Almudena <almudena.vigara@gmail.com>
2022-05-18 12:18:59 +0200

    Bonus DONE

--- src/viking.js
+++ src/viking.js
@@ -5,7 +5,9 @@ class Soldier {
     this.strength = strength
   }
   attack(){
-    return parseFloat(`${this.strength}`)
+    //return parseFloat(`${this.strength}`)
+    return parseFloat(this.strength)
+
   }
   receiveDamage(damage) {
     this.health = this.health - damage;
@@ -28,11 +30,10 @@ class Viking extends Soldier {
     //should remove the received damage from the health property
     this.health = this.health - damage;
     if(this.health > 0){
-      return `${this.name} has received ${parseFloat(this.damage)} points of damage`
+      return `${this.name} has received ${damage} points of damage`
       } else {
       return `${this.name} has died in act of combat`
     }
-    //NO FUNCIONA:if the Viking is still alive, it should return "NAME has received DAMAGE points of damage"
   }
 
   battleCry(){
@@ -45,15 +46,20 @@ class Viking extends Soldier {
 
 // Saxon
 class Saxon extends Soldier {
+  
+  attack() {
+    return parseFloat(`${this.strength}`)
+  }
+
   receiveDamage(damage){
-    this.health = this.health - damage;
+    //this.health = this.health - damage; mi codigo primario
+    super.receiveDamage(damage)
     if(this.health > 0 ){
-      return `A Saxon has received ${parseFloat(this.damage)} points of damage`
+      return `A Saxon has received ${damage} points of damage`
       } else {
       return `A Saxon has died in combat`
     }
   }
-  //NO FUNCIONA:if the Saxon is still alive, it should return "A Saxon has received DAMAGE points of damage" 
 }
 
 ////////////////////////////////////////////
@@ -66,31 +72,39 @@ class War {
   }
 
   addViking(Viking){
+    this.vikingArmy.push(Viking)
     // should add the received Viking to the army (4 ms)
   };
   addSaxon(Saxon){
-    //should add the received Saxon to the army
+    this.saxonArmy.push(Saxon)
   };
   vikingAttack(){
-    //should make Saxon receiveDamage() equal to the strength of a Viking
-    //should return result of calling receiveDamage() of a Saxon with the strength of a Viking
+    const respond = this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+    //esta constante me guarda el mensaje recibido de la funcion receiveDamage
+    this.saxonArmy = this.saxonArmy.filter((saxon) => saxon.health > 0);
+    return respond;
   };
+
   saxonAttack(){
-    //should make a Viking receiveDamage() equal to the strength of a Saxon
-    //should return result of calling receiveDamage() of a Viking with the strength of a Saxon
+    const respond = this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+     //esta constante me guarda el mensaje recibido de la funcion receiveDamage
+    this.vikingArmy = this.vikingArmy.filter((viking) => viking.health > 0);
+    return respond;
   };
+
   showStatus(){
-    // ✕ should return "Vikings have won the war of the century!", if the Saxons array is empty
-    // ✕ should return "Saxons have fought for their lives and survived another day...", if the Vikings array is empty (1 ms)
-    // ✕ should return "Vikings and Saxons are still in the thick of battle.", if there are still both Vikings and Saxons
-  
+    if (this.saxonArmy.length === 0){
+      return "Vikings have won the war of the century!"
+    } else if (this.vikingArmy.length === 0) {
+       return "Saxons have fought for their lives and survived another day..."
+    } else {
+      return "Vikings and Saxons are still in the thick of battle."
+    }
+    
   };
 }
 
 ////////////////////////////////////////////
-
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
