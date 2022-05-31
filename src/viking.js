e2359b371e0b0abad503b63fcdf268f9b576a315
mareklaz <mareklaz@MacBook-Air-de-Marek.local>
2022-05-31 17:58:02 +0200

    new code added soldier-viking class

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage){
+    this.health = this.health - damage
+    if(this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry(){
+    return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
 class Saxon {}
