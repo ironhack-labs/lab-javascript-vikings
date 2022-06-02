0f49692e438f26baaac44c7970b49034b5a42e2b
violeta atkinson <violeta.atkinson@gmail.com>
2022-06-02 18:19:34 +0200

    lab done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,67 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage){
+     this.health -= damage 
+  }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+ constructor(name, health, strength){
+  super(name, strength, health);
+    this.name = name; 
+    this.health = health;
+ }
+ receiveDamage(thedamage){
+   this.health = this.health - thedamage; 
+    if (this.health > 0){
+      return(`${this.name} has received ${thedamage} points of damage`)
+    }
+    else {
+      return(`${this.name} has died in act of combat`)
+    }
+   }
+   battleCry() {
+     return "Odin Owns You All!"
+   }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(thedamage){
+    this.health = this.health - thedamage;
+    if(this.health > 0){
+      return(`A Saxon has received ${thedamage} points of damage`)
+    }
+    return "A Saxon has died in combat"
+  }
+  }
+
 
 // War
-class War {}
+class War {
+
+  addViking(Viking){}
+  
+  addSaxon(Saxon){}
+
+  vikingAttack(){}
+
+  saxonAttack(){}
+
+  showStatus(){}
+}
+
+
 
 
 
