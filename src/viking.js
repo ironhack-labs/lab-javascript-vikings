adb60d6c31c09c6f05bed7a6c813a9b950c2e69b
Natalia Pinzon <nanuta522@gmail.com>
2022-06-03 18:21:39 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,95 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (healthValue, strengthValue) {
+    this.health = healthValue
+    this.strength = strengthValue
+  }
+
+  attack () {
+    return this.strength
+  }
+
+  receiveDamage (damage) {
+    this.health -= damage 
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(nameValue, healthValue, strengthValue) {
+    super (healthValue, strengthValue)
+    this.name = nameValue
+  }
 
+  receiveDamage (damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    } 
+  }
+  battleCry() {
+    return 'Odin Owns You All!'
+  }
+}
+  
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage (damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return `A Saxon has died in combat`
+    } 
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+  
+
+  addViking (Viking) {
+    this.vikingArmy.push(Viking) 
+  }
+  
+  addSaxon (Saxon) {
+    this.saxonArmy.push(Saxon)
+  }
+
+  vikingAttack () {
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+    const randomSaxon = this.saxonArmy[Math.floor (Math.random() * this.saxonArmy.length)]
+
+   let receivedDamage = randomSaxon.receiveDamage(randomViking.strength)
+
+   if (randomSaxon.health <= 0) {
+     this.saxonArmy.splice(randomSaxon, 1)
+   }
+    return receivedDamage
+  }
+  
+  saxonAttack () {
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+
+    let receivedDamage = randomViking.receiveDamage(randomSaxon.strength)
+
+    if (randomViking.health <= 0) {
+      this.vikingArmy.splice(randomViking, 1)
+    }
+    return receivedDamage
+  }
+
+
+  showStatus () {}
+
+}
+
+
 
 
 
