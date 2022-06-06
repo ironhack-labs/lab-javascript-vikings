3d492a29d0b28c9236c9d2cdba15b74b67562ba2
Juan G Lopez <nahujg@gmail.com>
2022-06-06 17:11:02 +0200

    Terminado Iteration 3
    Terminado Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health= health
+    this.strength= strength
+  }
+  attack() {
+    return this.strength
+  }
+  receiveDamage(theDamage){
+    this.health = this.health - theDamage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength){
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(theDamage){
+    this.health = this.health - theDamage;
+    if (this.health > 0) {
+      return `${this.name} has received ${theDamage} points of damage`;
+    }else{
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(theDamage){
+    this.health = this.health - theDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamage} points of damage`;
+    }else{
+      return `A Saxon has died in combat`;
+    }
+  }
+
+}
 
 // War
 class War {}
