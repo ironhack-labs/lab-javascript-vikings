7bc2a834f078e3595e9bb7c028af01908a9fc98e
ElvanIpeker <eipekerdogan@hotmail.com>
2022-06-07 14:07:59 +0200

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,48 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = healththis.strength = strength
+  }
+
+  attack() {
+    return this.strength
+  }
+  receiveDamage(damage) {
+
+    this.health = this.health - damage
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage) {
+    super.receiveDamage(damage)
+    if (this.health > 0) {
+      return this.name + "has received" + damage + "points of damage"
+    }
+    else {
+      return this.name + "has died in act of combat"
+    }
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier { 
+  constructor(health,strength){
+    super(health,strength)
+  }
+
+
+
+}
 
 // War
-class War {}
+class War { }
 
 
 
