a23d67c665a31f665dd685785e78eefabc6e48b4
Alex Smith <axsmith2020@gmail.com>
2022-06-07 16:53:20 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,84 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health,strength){
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(damage){
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+  }
+  receiveDamage(damage){
+    this.health= this.health-damage
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage"
+    }
+    else{
+      return this.name + " has died in act of combat"
+    }
+  }
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  receiveDamage(damage){
+    this.health-=damage
+    if (this.health > 0) {
+      return `A Saxon has received ` + damage + ` points of damage`
+    }
+    else{
+      return "A Saxon has died in combat"
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+  
+
+
+  addViking(Vik){
+    this.vikingArmy.push(Vik)
+  }
+
+  addSaxon(Sax){
+    this.saxonArmy.push(Sax)
+  }
+  vikingAttack(){
+    let sax = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)]
+    sax.receiveDamage(this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)].strength)
+    if(sax.health <=0){
+      let index = this.saxonArmy.indexOf(sax)
+      this.saxonArmy.splice(index,1)
+    }
+  }
+  saxonAttack(){
+    let vik = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]
+    vik.receiveDamage(this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)].strength)
+    if(vik.health <=0){
+      let index = this.vikingArmy.indexOf(vik)
+      this.vikingArmy.splice(index,1)
+    }
+  }
+  showStatus(){}
+}
 
 
 
