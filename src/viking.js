8fc8a86877e1ad971e0a53c8f8aeb2504812926f
Meryl Kirchhoff <meryl.kirchhoff@gmx.de>
2022-06-08 14:02:04 +0200

    lab vikings version 1

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super(health, strength)
+      this.name = name
+    }
+    attack() {
+      return this.strength
+    }
+    receiveDamage(damage) {
+      this.health -= damage
+      if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`
+      } else {
+        return `${this.name} has died in act of combat`
+      }
+    }
+    battleCry() {
+      return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+      this.health -= damage
+      if(this.health > 0) {
+        return `A Saxon has received ${damage} points of damage`
+      } else {
+        return `A Saxon has died in combat`
+      }
+    }
+ 
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = []
+    this.saxonArmy = []
+  }
+
+  addViking() {}
+  addSaxon(){}
+  vikingAttack() {
+    let saxonFighter = this.saxonArmy[(Math.floor(Math.random() * this.saxonArmy.length))]
+    let vikingFighter = this.vikingArmy[(Math.floor(Math.random() * this.vikingArmy.length))]
+    saxonFighter.receiveDamage(vikingFighter.attack())
+    if(saxonFighter.health <= 0) {
+
+    }
+  }
+
+  saxonAttack() {
+  }
+  showStatus() {}
+  addViking(viking) {
+    this.vikingArmy.push(viking)
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon)
+  }
+}
 
 
 
