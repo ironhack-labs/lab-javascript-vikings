d9cf29414046adbc8fb0aac0f6052564e20ae75a
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-06-09 18:07:11 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,122 @@
 // Soldier
-class Soldier {}
+
+
+class Soldier {
+
+constructor(health,strength) {
+  this.health = health;
+  this.strength = strength;
+
+}
+
+
+attack() {
+return this.strength
+}
+
+receiveDamage(damage) {
+  
+  this.health -= damage;
+   
+}
+
+
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+   constructor(name, health, strength) {
+   super(health, strength);
+   this.name = name;
+   }
+
+  //  console.log(saxon)
+
+   attack() {
+   super.attack()
+   return this.strength
+   }
+   
+  receiveDamage(damage){
+
+    this.health -= damage;
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry() {
+
+  return 'Odin Owns You All!'
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    }
+
+  receiveDamage(damage) {
+
+    this.health -= damage;
+
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return "A Saxon has died in combat"
+    }
+
+  }
+
+  attack() {
+    super.attack()
+    return this.strength
+    }
+
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+
+  addViking(viking) {
+    this.vikingArmy.unshift(viking)
+
+  }
+
+  addSaxon(saxon) {
+
+    this.saxonArmy.unshift(saxon)
+
+  }
+
+
+  vikingAttack(){
+    // this.receiveDamage = 
+
+
+  }
+
+ saxonAttack() {
+
+
+ }
+
+
+ showStatus() {
+
+ }
+
+
+
+
+}
 
 
 
@@ -17,3 +125,9 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
+let viking = new Viking ('bob',100,10);
+let saxon = new Saxon (100,10);
+
+saxon.receiveDamage(viking.strength)
+console.log(saxon)
