f2db20c147e18f25228d4c079804ca61df86f502
Fernanda Guadarrama <fernanda10_star@hotmail.com>
2022-06-10 19:39:15 -0500

    Ya no se si amo tanto JS :clown_face:

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (salud, fuerza) {
+     this.health = salud;
+     this.strength = fuerza;
+    }
+     attack(){
+      return this.strength
+    } 
+     receiveDamage(damage){
+      this.health -= damage
+    }
+  }
 
-// Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(nombre, salud,fuerza){
+   super(salud, fuerza)
+   this.name= nombre
+  }
+  receiveDamage(damage){
+   this.health -= damage
+   if (this.health > 0){
+    return `${this.name} has received ${damage} points of damage`
+   }else{
+    return `${this.name} has died in act of combat`
+   }
+  }
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  //constructor(salud,fuerza){
+  //  super(nombre, salud, fuerza)
+//  }
+   receiveDamage(damage){
+    this.health -= damage
+   if (this.health > 0){
+    return `A Saxon has received ${damage} points of damage`
+   }else{
+    return `A Saxon has died in combat`
+   } 
+   }
+}
 
 // War
 class War {}
