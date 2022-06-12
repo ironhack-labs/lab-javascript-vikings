0867b325457bbf20d4de8abdefa0128c9f69b118
unknown <sandrapl.18@gmail.com>
2022-06-11 18:00:11 -0700

    Vikings finished

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,95 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack(){
+    return this.strength;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength){
+   super(health, strength);
+   this.name = name;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+
+    if(this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+    return `${this.name} has died in act of combat`;
+  }
+  battleCry(){
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    }
+  return `A Saxon has died in combat`;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+  this.vikingArmy = [];
+  this.saxonArmy = [];
 
+  }
+  addViking(Viking){
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon){
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack(){
+    let hitSaxon = Math.floor(Math.random()*this.saxonArmy.length);
+    let hitViking = Math.floor(Math.random()*this.vikingArmy.length);
+    let vikingStrength = this.vikingArmy[hitViking].strength;
+    let attackedSaxon = this.saxonArmy[hitSaxon];
+    let attack = attackedSaxon.receiveDamage(vikingStrength);
+    if (attackedSaxon.health <= 0){
+    this.saxonArmy.splice(hitSaxon, 1);
+    }
+    return attack;
+  }
+  saxonAttack(){
+    let hitSaxon = Math.floor(Math.random()*this.saxonArmy.length);
+    console.log(hitSaxon);
+    let hitViking = Math.floor(Math.random()*this.vikingArmy.length);
+    let saxonStrength = this.saxonArmy[hitSaxon].strength;
+    let attackedViking = this.vikingArmy[hitViking];
+    let attack = attackedViking.receiveDamage(saxonStrength);
+    if (attackedViking.health <= 0){
+      this.vikingArmy.splice(hitViking, 1);
+      }
+    return attack;
 
+  }
+  showStatus(){
+    if (this.saxonArmy.length === 0){
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0){
+      return "Saxons have fought for their lives and survived another day...";
+    } else {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
