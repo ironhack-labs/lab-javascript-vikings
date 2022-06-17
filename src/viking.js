57b8f5474ca175f9540a3a09c7fd8a2ac7ee03e6
Paul Tiry <tirypaul@gmail.com>
2022-06-17 15:52:17 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,104 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `${this.name} has received ${theDamage} points of damage`;
+    }
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamage} points of damage`;
+    }
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
 
+  addViking(vikingSoldier) {
+    this.vikingArmy.push(vikingSoldier);
+  }
 
+  addSaxon(saxonSoldier) {
+    this.saxonArmy.push(saxonSoldier);
+  }
+
+  vikingAttack() {
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const theAttack = this.saxonArmy[randomSaxon].receiveDamage(
+      this.vikingArmy[randomViking].attack()
+    );
+    this.saxonArmy.forEach((soldier, index) => {
+      if (soldier.health <= 0) {
+        this.saxonArmy.splice(index, 1);
+      }
+    });
+    return theAttack;
+  }
+
+  saxonAttack() {
+    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const theAttack = this.vikingArmy[randomViking].receiveDamage(
+      this.saxonArmy[randomSaxon].attack()
+    );
+    this.vikingArmy.forEach((soldier, index) => {
+      if (soldier.health <= 0) {
+        this.vikingArmy.splice(index, 1);
+      }
+    });
+    return theAttack;
+  }
+
+  showStatus() {
+    if (!this.saxonArmy[0]) return 'Vikings have won the war of the century!';
+    if (!this.vikingArmy[0])
+      return 'Saxons have fought for their lives and survived another day...';
+    return 'Vikings and Saxons are still in the thick of battle.';
+  }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
