93bbaa18dd2800cd182fa1f1266a703cc242b980
Matthieu Duperrey <matth.duperrey@gmail.com>
2022-06-17 15:27:14 +0200

    Viking done, Saxon in progress

--- src/viking.js
+++ src/viking.js
@@ -21,13 +21,38 @@ class Soldier {
 class Viking extends Soldier {
   constructor(name, health, strength) {
     super(health, strength)
+    this.name = name;
 
+  }
+  receiveDamage(damage){
+    this.health -= damage
+    if (this.health > 0){
+      return `${this.name} has received ${this.damage} points of damage`
+    } else if (this.health === 0) {
+      return `${this.name} has died in act of combat`
+    }
+
+    }
+
+    battleCry(){
+      return 'Odin own you all !'
+    }
+  }
+
+
+
+// Saxon
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage
+    if (this.health > 0) {
+      return `A Saxon has received ${this.damage} points of damage`
+    } else if (this.health === 0) {
+      return `A Saxon has died in act of combat`
+    }
   }
 
 }
-
-// Saxon
-class Saxon {}
 
 // War
 class War {}
