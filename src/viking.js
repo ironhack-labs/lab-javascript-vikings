165df7f5775bf4d386252eb34bb76b7aa415b785
miguelvalden <miguelvalden77@gmail.com>
2022-07-02 00:28:54 +0200

    Pass 3

--- src/viking.js
+++ src/viking.js
@@ -8,7 +8,7 @@ class Soldier {
   return this.strength
   }
   receiveDamage(damage){
-  this.health - damage
+  this.health -= damage
   }
 }
 
@@ -19,7 +19,7 @@ class Viking extends Soldier{
     this.name = name
   }
   receiveDamage(damage){
-    this.health - damage
+    this.health -= damage
     
     if(this.health <= 0){
       return `${this.name} has died in act of combat`
@@ -36,20 +36,20 @@ class Viking extends Soldier{
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage){
-    this.health - damage
+    this.health = this.health - damage
     if(this.health <= 0){
-      return `${this.name} has died in act of combat`
+      return `A Saxon has died in combat`
     } else {
-      return `Saxon has received ${damage} points of damage`
+      return `A Saxon has received ${damage} points of damage`
     }
     }
 }
 
 // War
 class War {
-  constructor(vikingArmy, saxonArmy){
-  this.vikingArmy = vikingArmy
-  this.saxonArmy = saxonArmy
+  constructor([], []){
+  this.vikingArmy = []
+  this.saxonArmy = []
   }
   addViking(viking){
   this.vikingArmy.push(viking)
@@ -59,20 +59,7 @@ class War {
   }
 
   vikingAttack(){
-  let saxonNum = this.saxonArmy.length
-  let randomNum = Math.random() * saxonNum
-  let floorNum = Math.floor(randomNum)
-  let randomSaxon = this.saxonArmy[floorNum]
 
-  let vikingNum = this.vikingArmy.length
-  let randomNum2 = Math.random() * vikingNum
-  let floorNum2 = Math.floor(randomNum2)
-  let randomViking = this.vikingArmy[floorNum2]
-
-  return randomSaxon.receiveDamage(randomViking.strength)
-  }
-
-  saxonAttack(){
     let saxonNum = this.saxonArmy.length
     let randomNum = Math.random() * saxonNum
     let floorNum = Math.floor(randomNum)
@@ -82,8 +69,26 @@ class War {
     let randomNum2 = Math.random() * vikingNum
     let floorNum2 = Math.floor(randomNum2)
     let randomViking = this.vikingArmy[floorNum2]
+
+    return randomSaxon.receiveDamage(randomViking.strength)
   
+
+  }
+
+  saxonAttack(){
+
+    let vikingNum = this.vikingArmy.length
+    let randomNum2 = Math.random() * vikingNum
+    let floorNum2 = Math.floor(randomNum2)
+    let randomViking = this.vikingArmy[floorNum2]
+
+    let saxonNum = this.saxonArmy.length
+    let randomNum = Math.random() * saxonNum
+    let floorNum = Math.floor(randomNum)
+    let randomSaxon = this.saxonArmy[floorNum]
+
     return randomViking.receiveDamage(randomSaxon.strength)
+  
   }
   showStatus(){
 
@@ -95,6 +100,12 @@ class War {
       return "Vikings and Saxons are still in the thick of battle."
     }
   }
+  removeSoldier(arrArmy, soldier){
+    let index = arrArmy.indexOf(soldier)
+    let newArmy = arrArmy.splice(index, 1)
+    arrArmy = newArmy
+    return arrArmy
+  }
 
 }
 
@@ -102,20 +113,41 @@ class War {
 let saxon = new Saxon(100, 20)
 let viking = new Viking("Pepe" ,110, 30)
 
-console.log(saxon)
-console.log(viking)
-console.log("Saxon strength", saxon.attack())
-console.log("Viking strength", viking.attack())
-console.log("Saxon has", saxon.health)
-console.log("Viking has", viking.health)
+// Test viking
+// console.log(viking)
+// console.log(viking.name)
+// console.log("Viking strength", viking.attack())
+// console.log("Viking has", viking.health)
+// console.log(viking.name)
+// console.log(viking.battleCry())
+//viking.receiveDamage(40)
+// console.log(viking.health)
 
-//
+// Test saxon
+// console.log(saxon)
+// console.log("Saxon strength", saxon.attack())
+// console.log("Viking has", saxon.health)
+//saxon.receiveDamage(40)
+// console.log(saxon.health)
+
+// Test war
 let war = new War([], [])
+console.log(war)
 war.addSaxon(saxon)
 war.addViking(viking)
 
-console.log(war.saxonAttack())
-console.log(war.vikingAttack())
+war.vikingAttack()
+war.vikingAttack()
+war.vikingAttack()
+war.vikingAttack()
+war.vikingAttack()
+war.vikingAttack()
+console.log(war)
+
+war.saxonAttack()
+war.saxonAttack()
+
+console.log(war.showStatus())
 
 //if(randomSaxon.health <= 0){
     // Seguir después aquí
