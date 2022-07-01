e03e259c1d48e4a6cee306b57a8471f7bdf633cb
Ivan Culebra <ivan.culebra.munera@gmail.com>
2022-07-01 17:37:00 +0200

    Iteration 3 completed

--- src/viking.js
+++ src/viking.js
@@ -32,7 +32,17 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health === 0) {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
