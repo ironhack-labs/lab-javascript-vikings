ede0a7ccd4d098f4586ee689226e467d18683243
Majid Boukachni <boukachni@gmail.com>
2022-07-10 15:46:06 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,62 @@
-// Soldier
-class Soldier {}
-
-// Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
-
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+    attack() {
+      return this.strength;
+    }
+    receiveDamage(damage){
+      this.health -= damage;
+    }
+  }
+  // Viking
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super(health, strength);
+      this.name = name;
+    }
+    battleCry() {
+      return 'Odin Owns You All!';
+    }
+    receiveDamage(damage) {
+      this.health -= damage;
+      if (this.health <= 0) {
+        return `${this.name} has died in act of combat`;
+      } else {
+        return `${this.name} has received ${damage} points of damage`;
+      }
+    }
+  }
+  
+  // Saxon
+  class Saxon extends Soldier {
+    attack() {
+      return this.strength;
+    }
+    receiveDamage(damage){
+      this.health -= damage;
+      if(this.health <= 0){
+        return "A Saxon has died in combat";
+      } else{
+        return `A Saxon has received ${damage} points of damage`;
+      }
+    } 
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 // War
 class War {}
+  
+
 
 
 
