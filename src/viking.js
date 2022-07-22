0be1aa139c3e2480b19d10aee85ae49605beca26
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-07-21 22:23:23 -0300

    criei Class Viking e Saxon

--- src/viking.js
+++ src/viking.js
@@ -9,18 +9,46 @@ class Soldier {
     return this.strength;
   }
 
-  receiveDamage(thedamage) {
-    this.health -= thedamage;
+  receiveDamage(attackDamage) {
+    this.health -= attackDamage;
   }
 }
 
 // Viking
-class Viking {
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(attackDamage) {
+    super.receiveDamage(attackDamage);
+    if (this.health > 0) {
+      return `${this.name} has received ${attackDamage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
 
 }
 
 // Saxon
-class Saxon { }
+class Saxon extends Soldier {
+  receiveDamage(attackDamage) {
+    super.receiveDamage(attackDamage);
+    if (this.health > 0) {
+      return `A Saxon has received ${attackDamage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
+
 
 // War
 class War { }
