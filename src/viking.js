7bea8539d4bfa644d5f644fdd3e4e65967dbf9ee
MacDantas <marcelodecarvalhodantas@hotmail.com>
2022-07-21 23:02:31 -0300

    Iteration 1 done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,33 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+}
 
 // War
 class War {}
