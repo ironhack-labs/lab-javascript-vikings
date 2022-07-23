5fc53c78cc6d783727c6be49b323211f780485d0
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-07-23 13:45:51 -0300

    finish Lab

--- src/viking.js
+++ src/viking.js
@@ -51,4 +51,90 @@ class Saxon extends Soldier {
 
 
 // War
-class War { }
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(vikingObject) {
+    this.vikingArmy.push(vikingObject);
+  }
+
+  addSaxon(saxonObject) {
+    this.saxonArmy.push(saxonObject);
+  }
+
+
+  armyAttack(army) {
+    const randomVikingIndex = Math.floor(
+      Math.random() * this.vikingArmy.length
+    );
+    const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+
+    const randomVikingSoldier = this.vikingArmy[randomVikingIndex];
+    const randomSaxonSoldier = this.saxonArmy[randomSaxonIndex];
+
+    if (army === "Viking") {
+      const attackResult = randomSaxonSoldier.receiveDamage(
+        randomVikingSoldier.strength
+      );
+
+      this.checkDead(randomSaxonSoldier);
+
+      return attackResult;
+    }
+
+    const attackResult = randomVikingSoldier.receiveDamage(
+      randomSaxonSoldier.strength
+    );
+
+    this.checkDead(randomVikingSoldier);
+
+    return attackResult;
+  }
+
+  checkDead(soldier) {
+    if (soldier.health <= 0) {
+      if (soldier instanceof Viking) {
+        const index = this.vikingArmy.indexOf(soldier);
+
+        if (index > -1) {
+          this.vikingArmy.splice(index, 1);
+        }
+      } else {
+        const index = this.saxonArmy.indexOf(soldier);
+
+        if (index > -1) {
+          this.saxonArmy.splice(index, 1);
+        }
+      }
+    }
+  }
+
+  vikingAttack() {
+    return this.armyAttack("Viking");
+  }
+
+  saxonAttack() {
+    return this.armyAttack("Saxon");
+  }
+
+  showStatus() {
+    if (!this.saxonArmy.length) {
+      return "Vikings have won the war of the century!";
+    }
+
+    if (!this.vikingArmy.length) {
+      return "Saxons have fought for their lives and survived another day...";
+    }
+
+    return "Vikings and Saxons are still in the thick of battle.";
+  }
+
+
+
+}
+
+
+
+
+
