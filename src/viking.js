c2c2283d40a0586914d8597157c3237942eeca1f
Romina Polo <rominalpolo@gmail.com>
2022-07-22 21:45:13 -0400

    first attempt at lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,57 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack = () => {
+        return this.strength
+    }
+    receiveDamage = (damage) => {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
 
+class Viking extends Soldier {
+    constructor (name,health,strength) {
+        super (health,strength)
+        this.name = name;
+        
+        if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`}
+        else {
+        return `${this.name} has died in act of combat`}
+}
+battleCry() {
+     return "Odin Owns You All!" }
+}
+}
+    
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+       this.health -= damage;
+       if (this.health <= 0) { 
+        return "A Saxon has died in combat"
+    }
+    else {
+        return `A Saxon has received ${damage} points`}
+}
+}
 
-// War
-class War {}
+// // War
+// class War {
+//     vikingArray = [];
+//     saxonArray = [];
+
+// addViking(viking)
+// {
+//     this.vikingArray.push(viking);
+// };
+
+// addSaxon(saxon)
+// {
+//     this.vikingArray.push(viking);
+// };
