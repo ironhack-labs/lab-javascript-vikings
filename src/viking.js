d0f6e9c6af5a8266e2fdfaa81665afd01806ab84
jdlms <97018563+jdlms@users.noreply.github.com>
2022-07-28 21:41:21 +0200

    first commit

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  strength() {
+   if (this.receiveDamage(i)) { 
+    return (`${this.name} has received `)
+  }
+}
 
 // Saxon
 class Saxon {}
