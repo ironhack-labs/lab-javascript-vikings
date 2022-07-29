f5eeb06f362345951c867c4a1963cc0c2e0262f1
jdlms <97018563+jdlms@users.noreply.github.com>
2022-07-29 21:35:08 +0200

    final friday commit

--- src/viking.js
+++ src/viking.js
@@ -70,20 +70,51 @@ class War {
   }
 
   vikingAttack() {
-    let rSaxon = this.random(saxonArmy);
-    let rViking = this.random(vikingArmy);
-    let damage = rViking.strength;
-    rSaxon.receiveDamage(strength);
+    let rViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let rSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let attack = this.vikingArmy[rViking].strength;
+    this.saxonArmy[rSaxon].receiveDamage(attack);
+    if (this.saxonArmy[rSaxon].health <= 0) {
+      //how to remove rSaxon?
+      return "A Saxon has died in combat";
+    }
+    // if (this.vikingArmy[rViking].health >= 1) {
+    //   return ``;
+    // }
+
+    // let damage = rViking.strength;
+    // let rSaxon = this.random(saxonArmy);
+    // rSaxon.receiveDamage(damage);
 
     // let oldHealth = rSaxon.health;
     // rSaxon.health = oldHealth -= rViking.strength;
   }
 
-  saxonAttach() {}
-
-  random(arr) {
-    return arr[Math.floor(Math.random() * this.arr.length)];
+  saxonAttack() {
+    let rViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let rSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let attack = this.saxonArmy[rSaxon].strength;
+    this.vikingArmy[rViking].receiveDamage(attack);
+    if (this.vikingArmy[rViking].health <= 0) {
+      //how to remove rViking?
+    } else {
+      return `${this.vikingArmy[rViking].name} has received ${this.saxonArmy[rSaxon].strength} points of damage`;
+    }
   }
 
-  showStatus() {}
+  //Trying to put the random function outside of the method and then calling it inside didn't work at all :)
+
+  // random(arr) {
+  //   return arr[Math.floor(Math.random() * arr.length)];
+  // }
+
+  showStatus() {
+    if (this.saxonArmy.length < 1) {
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length < 1) {
+      return "Saxons have fought for their lives and survived another day...";
+    } else {
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
 }
