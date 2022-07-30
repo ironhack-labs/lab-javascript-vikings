7ba20928a239cfdc8c8a7a23c80f32b0491af58f
João Serafim <serafim.joao@gmail.com>
2022-07-30 13:54:31 +0100

    receive Damage Partialy Done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.theDamage = theDamage;
+    this.health = this.health - theDamage;
+    if (theDamage > 0) {
+      return `${this.name} has received ${this.theDamage} points of damage", if the Viking is still alive`;
+    } else {
+      return `"${this.name} has died in act of combat", if the Viking dies`;
+    }
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    this.health = health;
+    this.strength = strength;
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(viking) {
+
+  }
+}
