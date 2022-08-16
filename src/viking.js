ab8bf78c80be9575902f969c95cddbc7cdbacf2e
Kenneth Elumba <kennethelumba@gmail.com>
2022-08-16 16:27:25 +0200

    iteration 3

--- src/viking.js
+++ src/viking.js
@@ -38,7 +38,18 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.health = this.health - damage
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        if (this.health === 0) {
+            return 'A Saxon has died in combat'
+        }
+    }
+}
 
 // War
 class War {}
