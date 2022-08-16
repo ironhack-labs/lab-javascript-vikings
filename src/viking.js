36648255b7d1a95dedf4a99c3303ce10bfdd6ec3
Kenneth Elumba <kennethelumba@gmail.com>
2022-08-16 16:16:37 +0200

    iteration 2

--- src/viking.js
+++ src/viking.js
@@ -1,10 +1,41 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {}
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+    }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    attack() {
+        super.attack();
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        
+        if (this.health === 0) {
+            return `${this.name} has died in act of combat`
+        } 
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+    }
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
 class Saxon {}
