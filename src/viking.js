058365075fc39a75f96b0ebb6fae9a615f0bd703
Kenneth Elumba <kennethelumba@gmail.com>
2022-08-17 17:52:40 +0200

    iteration 4

--- src/viking.js
+++ src/viking.js
@@ -52,4 +52,51 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    // create a constructor() with zero arguments
+    // inside the constructor() assign an empty array to the 
+    // vikingArmy and to the saxonArmy property
+    constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    }
+    addViking(Viking) {
+        // add the received Viking to the vikingArmy with push
+        this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+        // add the received Saxon to the saxonArmy with push
+        this.saxonArmy.push(Saxon);
+    }
+    vikingAttack() {
+        // should make a random Saxon receiveDamage() equal 
+        // to the strength of a viking 
+        // receiveDamage === this.strength
+        let randomSaxon = Math.random()
+        console.log(randomSaxon());
+
+        // should remove dead saxons from the army
+        // this.health <= 0
+
+        // should return result of calling receivedDamage() 
+        // of a Saxon with the strength of a Viking
+
+        
+
+        // remove dead saxons with (this.health <= 0)
+        //if (Saxon)
+
+    }
+    saxonAttack() {
+
+    }
+    showStatus() {
+        if (this.saxonArmy.length === []) {
+            return 'Vikings have won the war of the century!'
+        } else if (this.vikingArmy.length === []) {
+            return 'Saxons have fought for their lives and survived another day...'
+        } else {
+            return 'Vikings and Saxons are still in the thick of battle.'
+        }   
+    }
+}
