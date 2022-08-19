4d5961feab739cd727980484d0389301de91422b
Andrew <ayjtw7@gmail.com>
2022-08-19 17:54:28 -0400

    initial commit. Initializing soldier and viking objects

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,45 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        // should remove the received damage from the health property
+        this.health -= damage
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        // should return "NAME has received DAMAGE points of damage", if the Viking is still alive
+        this.health -= damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            // should return "NAME has died in act of combat", if the Viking dies
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All"
+    }
+}
 
 // Saxon
 class Saxon {}
