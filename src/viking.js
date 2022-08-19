9aee724fc9416f771b946313ac6dd3c1c40e825c
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-08-19 17:23:08 -0400

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,47 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if(this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`
+    } else if(this.health <= 0) {
+        return `${this.name} has died in act of combat`
+    }
+    }
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if(this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return 'A Saxon has died in combat'
+        }
+    }
+}
 
 // War
 class War {}
