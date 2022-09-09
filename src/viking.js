e15283208f6aba8c56b125fbf0191e4d1ece8e12
Joao Mingote <joaopedromingote@gmail.com>
2022-09-09 19:09:08 +0200

    Solved lab, missing the extras

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,65 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength,){
+    this.health = health;
+    this.strength = strength;
+    
+  };
+  attack(){
+    return (this.strength);
+  }
+
+  receiveDamage (theDamage) {
+   this.health -= theDamage
+  }
+}   
+  
+let theDamage = true;
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength,){
+  super (health, strength, name);  
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+ 
+  battleCry () {
+    return ("Odin Owns You All!")
+  }
+
+  receiveDamage (theDamage) {
+    if (this.health -= theDamage){
+      return (`${this.name} has received ${theDamage} points of damage`)
+    } else {
+      return (`${this.name} has died in act of combat`)
+    }
+   }
+
+
+}
+
+const viking1 = new Viking ('Joao')
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage (theDamage) {
+    if (this.health -= theDamage) {
+      return (`A Saxon has received ${theDamage} points of damage`) }
+        else {
+          return ("A Saxon has died in combat")
+      }
+  }
+}
 
 // War
 class War {}
+
+
+// TENHO 4 CLASSES if 
+/* (this.health -= theDamage){
+      return (`${this.name} has received ${theDamage} points of damage`)
+    } else {
+      return (`${this.name} has died in act of combat`)
+    } */
