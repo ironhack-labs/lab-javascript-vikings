3ea522deba64afb607a8ade7b57537dc8a27a8b7
julianBrandon <p0lterg3istt@gmail.com>
2022-09-10 12:57:46 +0200

    Add (Bonus) iteration 5, solve lab

--- src/viking.js
+++ src/viking.js
@@ -33,7 +33,6 @@ class Viking extends Soldier {
     battleCry(){
         return `Odin Owns You All!`;
     }
-
 }
 
 // Saxon
@@ -46,9 +45,7 @@ class Saxon extends Soldier {
         } else {
             return `A Saxon has died in combat`;
         }
-
     }
-
 }
 
 // War
@@ -84,12 +81,16 @@ class War {
             this.vikingArmy.pop(randomVikingVictim);
         }
         return attackResult;
-        
-
     }
 
     showStatus(){
-        // NEXT TO IMPLEMENT
-        
+        if (this.saxonArmy.length == 0){
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length == 0){
+            return "Saxons have fought for their lives and survived another day...";
+        }
+        else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
     }
 }
