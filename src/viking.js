1d4e17f9a138ef48881151ee7e2c0a8e3483b0cb
Marc Logan <marclogan90@gmail.com>
2022-09-11 19:19:12 +0200

    I did all iteration, however it sometimes gives me undefined. Could you give me feedback why?

--- src/viking.js
+++ src/viking.js
@@ -41,8 +41,8 @@ class Viking extends Soldier{
      
 }
 
-let Jason= new Viking('Jason',100,100);
-console.log(Jason.receiveDamage(50));
+let Thormin = new Viking('Thormin',100,100);
+console.log(Jason.receiveDamage(50)); // if I run this code it return Jason as undefined and I don't know why??
 console.log(Jason.battleCry());
 
 // Saxon
@@ -60,10 +60,10 @@ class Saxon extends Soldier {
     }
 }
 
-let newSaxon= new Saxon(100,100);
-let newSaxon2= new Saxon(80,100);
-let newSaxon3= new Saxon(60,100);
-console.log(newSaxon.receiveDamage(50));
-console.log(newSaxon2.receiveDamage(30));
+// let durten1 = new Saxon(100,100);
+// let durten2 = new Saxon(80,100);
+// let durten3 = new Saxon(60,100);
+// console.log(newSaxon.receiveDamage(50));
+// console.log(newSaxon2.receiveDamage(30));
 // War
 class War {}
