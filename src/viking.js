e500a85b513c6c1fa26b119152499a18e2522c69
Maxvidal17 <110576377+Maxvidal17@users.noreply.github.com>
2022-09-15 09:01:42 -0400

    Done except extra

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,58 @@
-// Soldier
-class Soldier {}
-
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage;
+  }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(name, health);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage;
+
+    if (this.health > 0) {
+      return "${this.name} has received ${theDamage} points of damage";
+    } else {
+      return "${this.name} has died in act of combat";
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health = this.health - theDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${theDamage} points of damage`;
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
-// War
 class War {}
+
+//constructor(vikingArmy) {}
+//
