cd29c4ca49046daea3433abdf7c163ee594802db
Yabel <yabelrr@gmail.com>
2022-09-16 17:29:28 +0200

    Iteration 3 (Saxon), done

--- src/viking.js
+++ src/viking.js
@@ -44,7 +44,23 @@ class Soldier {
   }
   
   // Saxon
-  class Saxon {}
+  class Saxon extends Soldier {
+
+    constructor(health, strength){
+        super(health, strength);
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health <= 0){
+            return `A Saxon has died in combat`;
+        } else {
+            return `A Saxon has received ${damage} points of damage`;
+
+        }
+    }
+  }
   
   // War
   class War {}
