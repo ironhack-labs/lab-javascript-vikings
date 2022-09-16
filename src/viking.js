26b0da0a8c9a1b683d828d66c62241fb1b7b5c57
Yabel <yabelrr@gmail.com>
2022-09-16 17:01:08 +0200

    Iteration 1, done

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,17 @@
 // Soldier
 class Soldier {
-    constructor(health, strength) {}
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
   }
   
   // Viking
