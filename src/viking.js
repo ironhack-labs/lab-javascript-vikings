d885e7d48ef6c8cc8668a27137a6c1a8b4ecdb3d
Jordi Martinez <jordiupc87@gmail.com>
2022-09-16 09:50:48 +0200

    All done + Bonus

--- src/viking.js
+++ src/viking.js
@@ -86,4 +86,12 @@ class War {
 
     return resultOfReceiveDamage;
   }
+
+  showStatus() {
+    if (this.saxonArmy.length <= 0)
+      return "Vikings have won the war of the century!";
+    if (this.vikingArmy.length <= 0)
+      return "Saxons have fought for their lives and survived another day...";
+    return "Vikings and Saxons are still in the thick of battle.";
+  }
 }
