dd369b93d5f2eb36c6e0016f397e6578a2b2596a
Yabel <yabelrr@gmail.com>
2022-09-16 17:21:48 +0200

    Iteration 2 (Viking), done

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,33 @@ class Soldier {
   }
   
   // Viking
-  class Viking {}
+  class Viking extends Soldier {
+
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health <= 0){
+            return `${this.name} has died in act of combat`;
+        } else {
+            return `${this.name} has received ${damage} points of damage`;
+
+        }
+    }
+
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+
+  }
   
   // Saxon
   class Saxon {}
