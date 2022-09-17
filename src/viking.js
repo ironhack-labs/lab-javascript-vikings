0db27b19672a37d537ffb82e93d1bd8b17edc899
Yabel <yabelrr@gmail.com>
2022-09-17 18:07:57 +0200

    Iteration 5 (Status), done

--- src/viking.js
+++ src/viking.js
@@ -105,6 +105,16 @@ class Soldier {
     }
 
     showStatus(){
-        
+        if (this.saxonArmy.length === 0){
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length === 0){
+            return "Saxons have fought for their lives and survived another day...";
+        } else if ((this.saxonArmy.length && this.vikingArmy.length) > 0){
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+
     }
   }
+
+
+  
