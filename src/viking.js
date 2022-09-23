42696a6db7d563fb18f037bec9d3a1a8b725e5d4
MoniIgnacio <moniignacio02@gmail.com>
2022-09-23 21:27:19 +0400

    add Class Soldier and SubClass Viking Done!

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,42 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength
+    }
+
+    attack = ()=>{
+        return this.strength
+    }
+
+    receiveDamage = (damage)=>{
+        this.health = (this.health - damage)
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health,strength);
+        this.name = name;
+    }
+    battleCry = ()=>{
+        return `Odin Owns You All!`
+        
+    }
+    receiveDamage = (damage)=>{
+        this.health = (this.health - damage);
+        let dead = 0
+
+        if(this.health === dead){
+            return `${this.name} has died in act of combat`
+        }else {
+            return `${this.name} has received ${this.damage} points of damage`
+        }
+    }
+
+}
 
 // Saxon
 class Saxon {}
