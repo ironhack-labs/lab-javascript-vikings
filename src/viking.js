ec7eca74b4be772e64e8eeed0d2fd922dfd34b0e
Arcadio REYES <arcadioreyesph@gmail.com>
2022-09-27 16:36:54 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,50 @@
 // Soldier
-class Soldier {}
+// iteration 0
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  atack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    console.log(this.health - receiveDamage);
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  atack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    console.log(this.health - receiveDamage);
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+  atack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    console.log(this.health - receiveDamage);
+  }
+}
 
 // War
 class War {}
