52b088ebd634ae82d5da285534f773d74119232b
Camila <agarcamila@gmail.com>
2022-10-10 13:44:34 +0100

    update

--- src/viking.js
+++ src/viking.js
@@ -14,8 +14,24 @@ class Soldier {
 };
 
 // Viking
-class Viking {}
-
+class Viking extends Soldier{
+    constructor(name, health, strength,) {
+        super(health, strength);
+    this.name = name;
+    }
+    receiveDamage(theDamage){
+    this.health += this.health - theDamage
+        if (this.health > 0) {
+    console.log(`${this.name} has received ${this.theDamage} points of damage`)
+    } else (this.health === 0){
+    console.log(`${this.name} has died in act of combat`) 
+    } 
+    }
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+    };
+    
 // Saxon
 class Saxon {}
 
