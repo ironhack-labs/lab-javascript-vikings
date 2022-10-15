8082131dfbe06e296509c2a1b010ab11a2f61e5a
Dapato2 <108306801+Dapato2@users.noreply.github.com>
2022-10-15 17:34:03 -0500

    [WDPT0922-DIEGO PERECTO] Solved Lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,73 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+
+        return this.strength
+    }
+    receiveDamage(dmg){
+
+        this.health = this.health -dmg      
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(name,health,strength){
+        super(health,strength)
+        this.name = name;
+    }
+    receiveDamage(dmg){
+        this.health = this.health -dmg 
+        if(this.health > 0){
+            return `${this.name} has received ${dmg} points of damage`;
+        }else{
+            return `${this.name} has died in act of combat`
+        }     
+    }
+    battleCry(){
+        return `Odin Owns You All!`
+    }
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health,strength){
+        super(health,strength)
+    }   
+    receiveDamage(dmg){
+        this.health = this.health -dmg 
+        if(this.health > 0){
+            return `A Saxon has received ${dmg} points of damage`;
+        }else{
+            return `A Saxon has died in combat`
+        }   
+}
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(vikObj){
+        this.vikingArmy.push(vikObj)
+    }
+    addSaxon(saxObj){
+        this.saxonArmy.push(saxObj)
+    }
+    vikingAttack(){
+        let saxObj = this.saxonArmy[Math.floor(Math.random * this.saxonArmy.length)];
+        let vikObj = this.vikingArmy[Math.floor(Math.random * this.vikingArmy.length)];
+        let attack = vikObj.receiveDamage(saxObj.attack());
+        if(saxObj.health <= 0){
+            this.vikingArmy.splice(vikObj, 1);
+          }
+        return attack;
+          }
+}
