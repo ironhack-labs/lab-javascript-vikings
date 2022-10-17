0e9e1b2bd0f57274173d82d51be77871db9345d0
David Hatch <hatchdavidv@gmail.com>
2022-10-17 12:54:47 -0500

    viking damage

--- src/viking.js
+++ src/viking.js
@@ -4,11 +4,29 @@ class Soldier {
         this.health = health;
         this.strength = strength;
     }
-    
+    attack(){
+       return this.strength; 
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+    }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage){
+        this.health -= damage; 
+        if (this.health>0){
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+}
 
 // Saxon
 class Saxon {}
