3515248ccce564251b2936d2e26e293100e4eb93
David Hatch <hatchdavidv@gmail.com>
2022-10-17 16:30:00 -0500

    saxon

--- src/viking.js
+++ src/viking.js
@@ -26,10 +26,25 @@ class Viking extends Soldier{
             return `${this.name} has died in act of combat`
         }
     }
+    battleCry(){
+        return `Odin Owns You All!`
+    }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength)
+    }
+    receiveDamage(damage){
+        this.health -= damage; 
+        if (this.health>0){
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
