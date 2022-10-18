db885b8311e50fe1163094f054adec96310f72e5
David Hatch <hatchdavidv@gmail.com>
2022-10-17 19:40:15 -0500

    attack changes

--- src/viking.js
+++ src/viking.js
@@ -59,19 +59,36 @@ class War {
         this.saxonArmy.push(saxon);
     }
     vikingAttack(){
+        const idSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        const idViking = Math.floor(Math.random() * this.vikingArmy.length);
+    
+        const saxonRandom = this.saxonArmy[idSaxon];
+        const vikingRandom = this.saxonArmy[idViking];
         
-        const random = Math.floor(Math.random() * this.saxonArmy.length);
-        this.saxonArmy.splice(random,1);
-        //Saxon.receiveDamage(Viking.strength);
+        const resultF = saxonRandom.receiveDamage(vikingRandom.strength)
+        
+        if (saxonRandom.health <0){
+            this.saxonArmy.splice(idSaxon,1);
+        }
+        return resultF
     }
     saxonAttack(){
-        const random = Math.floor(Math.random() * this.vikingArmy.length);
-        this.vikingArmy.splice(random,1);
+        const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        
+        const saxon = this.saxonArmy[randomSaxon];
+        const viking = this.saxonArmy[randomViking];
+        
+        const damagedViking = viking.receiveDamage(saxon.strenght)
+        
+        this.vikingArmy.splice(viking,1)
+
+        return damagedViking
     }
     showStatus(){
-        if (this.saxonArmy.length == 0){
-            return "Vikings have won the war of the century!";
-        } else if (this.vikingArmy ===0){
+        if (this.saxonArmy.length === 0){
+            return "Vikings have won the war of the century!"
+        } else if (this.vikingArmy.length ===0){
             return "Saxons have fought for their lives and survived another day..."
         } else {
             return "Vikings and Saxons are still in the thick of battle.";
