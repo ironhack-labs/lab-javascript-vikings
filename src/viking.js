45b69ca654b002f918f23214cc3f2f6176af2cff
MarMooolina <mar.molinacarlos@gmail.com>
2022-10-18 23:52:29 -0500

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,86 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health= health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+} 
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+        if (this.health > 0){
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage){
+        this.health -= damage;
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    }
+    
+    addViking(viking){
+    this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack(){
+        const idViking = Math.floor(Math.random()* this.vikingArmy.length);
+        const idSaxon = Math.floor(Math.random()* this.saxonArmy.length);
+
+        const saxonRandom = this.saxonArmy[idSaxon];
+        const vikingRandom = this.vikingArmy[idViking];
+
+        const finalResult = saxonRandom.receiveDamage(vikingRandom.strength)
+        if(saxonRandom.health < 0){
+            this.saxonArmy.splice(idSaxon, 1);
+        }
+        return finalResult;
+    }
+
+    saxonAttack(){}
+    
+    showStatus(){}
+}
