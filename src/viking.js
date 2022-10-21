01ef7627cf1b5228ff4736cd103d7856cce27b03
sijincodes <sijinmathews03@gmail.com>
2022-10-22 00:26:29 +0200

    updated

--- src/viking.js
+++ src/viking.js
@@ -62,30 +62,47 @@ class War {
     vikingAttack(){
        let  saxon=this.saxonArmy[Math.floor(Math.random()*this.addSaxon.length)]
         let viking=this.vikingArmy[Math.floor(Math.random()*this.addViking.length)]
-
+        let damage = saxon.receiveDamage(viking.strength)
 //this.saxonArmy.indexOf(saxon)
         if(saxon.health<=0)
             this.saxonArmy.splice(Math.floor(Math.random()*this.addSaxon.length),1)
 
-        return saxon.receiveDamage(viking.attack())
+        return damage //saxon.receiveDamage(viking.attack())
      
     }
     saxonAttack(){
         let  saxon=this.saxonArmy[Math.floor(Math.random()*this.addSaxon.length)]
         let viking=this.vikingArmy[Math.floor(Math.random()*this.addViking.length)]
+        let damage=viking.receiveDamage(saxon.strength)
         if(viking.health<=0)
             this.vikingArmy.splice(Math.floor(Math.random()*this.addViking.length),1)
 
-        return viking.receiveDamage(saxon.attack())
+        return damage //viking.receiveDamage(saxon.attack())
     }
     showStatus(){
-        if(this.saxonArmy.length == 0)
+        if(this.saxonArmy.length === 0)
           return "Vikings have won the war of the century!"
-        else if(this.vikingArmy.length == 0)
+        else if(this.vikingArmy.length === 0)
           return "Saxons have fought for their lives and survived another day..."
         else //if( this.saxonArmy.length >=1 && this.vikingArmy>=1)
           return "Vikings and Saxons are still in the thick of battle."
    
 
     }
+}
+const war = new War;
+
+war.addSaxon(new Saxon(100, 15));
+war.addSaxon(new Saxon(100, 10));
+war.addSaxon(new Saxon(100, 5));
+
+war.addViking(new Viking("Ragnar", 100, 20));
+war.addViking(new Viking("Rollo", 100, 15));
+war.addViking(new Viking("Thorsten", 100, 10));
+
+
+for (let i = 0; i < 20; i++) {
+  war.saxonAttack();
+  war.vikingAttack();
+  console.log(war.showStatus());
 }
