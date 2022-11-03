9c5f3855be9a7b579dccddf67462b78c36c3ef89
Philip Butland <Philip_Butland@yahoo.com>
2022-11-03 22:04:15 +0100

    Thats all I can do this evening. Will try and come back

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return this.name + " has received " + damage + " points of damage";
+    } else {
+      return this.name + " has died in act of combat";
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health > 0) {
+      return "A Saxon has received " + damage + " points of damage";
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(VikingObject) {
+    vikingArmy.push(VikingObject);
+  }
+
+  addSaxon(SaxonObject) {
+    SaxonArmy.push(SaxonObject);
+  }
+
+  vikingAttack() {
+    Saxon.receiveDamage(Viking.strength);
+    if (Saxon.health <= 0) {
+        // remove Saxon from army
+    }
+    return Saxon.receiveDamage(Viking.strength);
+  }
+
+  saxonAttack(){
+    Viking.receiveDamage(Saxon.strength);
+    if (Viking.health <= 0) {
+        // remove Viking from army
+    }
+    return Viking.receiveDamage(Saxon.strength);
+  }
+}
