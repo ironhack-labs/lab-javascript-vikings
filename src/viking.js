aabe77d886696cccc3332a89686d2ecd7286cbd2
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-11-05 10:50:19 +0100

    refactored the polymorphism

--- src/viking.js
+++ src/viking.js
@@ -22,7 +22,7 @@ class Viking extends Soldier {
     }
 
     receiveDamage(damage) {
-        this.health -= damage
+        super.receiveDamage(damage)
         if (this.health > 0) return `${this.name} has received ${damage} points of damage`
         return `${this.name} has died in act of combat`
     }
@@ -35,7 +35,7 @@ class Viking extends Soldier {
 // Saxon
 class Saxon extends Soldier {
     receiveDamage(damage) {
-        this.health -= damage;
+        super.receiveDamage(damage)
         if (this.health > 0) return `A Saxon has received ${damage} points of damage`
         return "A Saxon has died in combat";
     }
