38f1c47c5b364906f461889ada6463ee79326fb5
Carlos Sosa <csosa@simpletg.com>
2022-11-11 01:06:49 -0500

    iterations 1 - 5 done

--- src/viking.js
+++ src/viking.js
@@ -49,4 +49,49 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(theViking) {
+    this.vikingArmy.push(theViking);
+  }
+  addSaxon(theSaxon) {
+    this.saxonArmy.push(theSaxon);
+  }
+
+  vikingAttack() {
+    const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    const saxon = this.saxonArmy[saxonIndex];
+    const viking = this.vikingArmy[vikingIndex];
+
+    const attack = saxon.receiveDamage(viking.strength);
+
+    if (saxon.health <= 0) this.saxonArmy.splice(saxonIndex, 1);
+
+    return attack;
+  }
+  saxonAttack() {
+    const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    const saxon = this.saxonArmy[saxonIndex];
+    const viking = this.vikingArmy[vikingIndex];
+
+    const attack = viking.receiveDamage(saxon.strength);
+
+    if (viking.health <= 0) this.vikingArmy.splice(vikingIndex, 1);
+
+    return attack;
+  }
+  showStatus() {
+    if (!this.vikingArmy.length)
+      return `Saxons have fought for their lives and survived another day...`;
+    if (!this.saxonArmy.length)
+      return `Vikings have won the war of the century!`;
+    if (this.vikingArmy.length && this.saxonArmy.length)
+      return `Vikings and Saxons are still in the thick of battle.`;
+  }
+}
