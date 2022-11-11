c0a14bb772fec3fadef7dbeb61f7863e5bfb9ab0
Carlos Sosa <csosa@simpletg.com>
2022-11-11 00:25:41 -0500

    initial commit

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,52 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    return this.health > 0
+      ? `${this.name} has received ${theDamage} points of damage`
+      : `${this.name} has died in act of combat`;
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+
+  receiveDamage(theDamage) {
+    this.health -= theDamage;
+    return this.health > 0
+      ? `A Saxon has received ${theDamage} points of damage`
+      : `A Saxon has died in combat`;
+  }
+}
 
 // War
 class War {}
