08cdce4ea8ab05eb2b1762e5aa33e29c3bf9f095
unknown <roxan.m.alma18@gmail.com>
2022-11-18 18:19:57 +0100

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,78 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+constructor(health, strength){
+this.health = health;
+this.strength = strength;
+}
+
+attack(){
+    return this.strength;
+}
+receiveDamage(damage){
+    
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+       
+
+       super(name, health, strength);
+       this.name = name;
+       this.health = health;
+       this.strength = strength;
+    }
+
+    attack(){
+        return this.strength;
+    }
+    
+    receiveDamage(damage){
+       if(damage =0){
+        return `${this.name} has received `
+       }
+
+    }
+
+    battleCry(){
+        return `${this.name} Owns You All! `
+   
+    } 
+} 
+const odin = new Viking('Odin');
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+   
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(Viking){
+       
+
+       
+    } 
+
+    addSaxon(Saxon){
+  
+    }
+
+    vikingAttack(){
+        let dama = (Math.random(Saxon) * this.damage);
+        this.strength = dama;
+        }
+
+    saxonAttack(){}
+
+    showStatus(){}
+
+}
