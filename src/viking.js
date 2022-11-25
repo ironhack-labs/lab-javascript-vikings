200b5b4396bd16829bb74c1d5f9d6a0ac22e56dd
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@iMac-de-Ana.home>
2022-11-25 18:50:14 +0100

    completed iteration 3 and bonus iteration 4

--- src/viking.js
+++ src/viking.js
@@ -42,10 +42,56 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  // No volvemos a implementar el método attack porque lo heredamos tal cual del padre Soldier
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
 
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
 
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
 
+    vikingAttack() {
+        let saxonRandomIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        let randomSaxon =  this.saxonArmy[saxonRandomIndex];
+        let randomViking = this.vikingArmy[Math.floor((Math.random() * this.vikingArmy.length))];
+        
+        if(randomViking.strength >= randomSaxon.health) {
+            this.saxonArmy.splice(saxonRandomIndex,1);
+        }
+        
+        return randomSaxon.receiveDamage(randomViking.attack());
+    }
+
+    saxonAttack() {
+        let vikingRandomIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        let randomViking =  this.vikingArmy[vikingRandomIndex];
+        let randomSaxon = this.saxonArmy[Math.floor((Math.random() * this.saxonArmy.length))];
+        
+        if(randomSaxon.strength >= randomViking.health) {
+            this.vikingArmy.splice(vikingRandomIndex,1);
+        }
+        
+        return randomViking.receiveDamage(randomSaxon.attack());
+    }
+
+}
