da7ef68cb513e29f1101977e34e90b99a9ab022f
Sofía J <sofiajimglez@gmail.com>
2022-11-25 21:18:09 +0100

    Iteration 5 complete

--- src/viking.js
+++ src/viking.js
@@ -27,7 +27,7 @@ class Viking extends Soldier {
 
     receiveDamage(damage) {
         this.health = this.health - damage;
-        
+
         if (this.health > 0) {
             return `${this.name} has received ${damage} points of damage`;
         } else if (this.health <= 0) {
@@ -97,5 +97,13 @@ class War {
         return vikingHealth;
     }
 
-    showStatus() {}
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+            return 'Vikings have won the war of the century!';
+        } else if (this.vikingArmy.length === 0) {
+            return 'Saxons have fought for their lives and survived another day...';
+        } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+            return 'Vikings and Saxons are still in the thick of battle.';
+        }
+    }
 }
