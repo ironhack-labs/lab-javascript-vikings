2b47a94c48de17742bb397c049f08e39bca72377
Victor Lopez <victorlpz2301@gmail.com>
2022-11-25 21:35:53 +0100

    iteration3

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,47 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+     return this.strength
+    }
+
+    receiveDamage(damage){
+     this.health -= damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super (health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+        receiveDamage(damage) {
+            this.health -= damage
+           
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+}
+       battleCry(){
+        return `Odin Owns You All!`
+       }
+}
 
 // Saxon
-class Saxon {}
+class Saxon {
+    
+}
 
 // War
-class War {}
+class War {
+    
+}
