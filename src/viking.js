3b62c3a198da2bad0c9485e2f26665ff409203f5
Victor Lopez <victorlpz2301@gmail.com>
2022-11-26 19:51:47 +0100

    except war

--- src/viking.js
+++ src/viking.js
@@ -37,8 +37,20 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {
-    
+class Saxon extends Soldier {
+    constructor (health, strength) {
+        super(health, strength);
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+    if (this.health > 0) {
+        return `A Saxon has received ${damage} points of damage`
+    } else {
+        return `A Saxon has died in combat`
+    }
+}
 }
 
 // War
