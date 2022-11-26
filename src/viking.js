b866a33715dff4fec6195680904de2ff83347200
Alba Greciano <albagreciano@Alba-Greciano-2.local>
2022-11-26 12:44:31 +0100

    3 interpolacion

--- src/viking.js
+++ src/viking.js
@@ -36,7 +36,16 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
