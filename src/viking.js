6428e003abb4cef7249755c2e01a214588802186
Luis Escribano <johndoe@example.com>
2022-11-26 12:18:13 +0100

    iteration3

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,71 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor ( health, strength ) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack (){
+        return  this.strength 
+    }
 
-// Viking
-class Viking {}
+//     Soldado
+// Método receiveDamage ()
+// debe eliminar el daño recibido de la propiedad de salud
+    
+    receiveDamage ( damage ) {
+        // this.damage = damage
+        this.health = this.health - damage
+    }
+}
+
+class Viking extends Soldier {
+    constructor( name, health, strength ) {
+        super( health,strength )
+        this.name = name;
+       
+    }
+
+    attack (){
+        return  this.strength 
+    }
+
+
+    receiveDamage ( damage ) {
+        // this.damage = damage;
+        this.health = this.health - damage
+        if ( this.health > 0 ) {
+            return `${ this.name } has received ${ damage } points of damage`
+        } else {
+           return `${ this.name } has died in act of combat`
+        }
+    }
+    battleCry () {
+        return "Odin Owns You All!"
+    }
+}
+
+ 
+
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier { 
+    attack() {
+        return  super.attack()  
+    }
+    receiveDamage( damage ) {
+
+        this.health = this.health - damage
+        if ( this.health > 0) {
+            return `A Saxon has received ${ damage } points of damage`
+        } else {
+            return `A Saxon died in combat `
+        }
+    }
+   
+
+
+}
 
 // War
 class War {}
