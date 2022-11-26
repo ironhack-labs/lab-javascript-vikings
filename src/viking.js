d729f7923922588504addc7441440edf04c3915f
jogopin <jonn.g.pineda@gmail.com>
2022-11-26 17:41:28 +0100

    i had forgotten showStatus(), now its done

--- src/viking.js
+++ src/viking.js
@@ -97,5 +97,13 @@ class War {
 
         return attack;
     }
-    showStatus(){}
+    showStatus(){
+        if(this.saxonArmy.length === 0){
+            return `Vikings have won the war of the century!` 
+        }if(this.vikingArmy.length === 0){
+            return "Saxons have fought for their lives and survived another day..."
+        }else{
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
 }
