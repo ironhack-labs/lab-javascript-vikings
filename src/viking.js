103dfdf1dfd610718870487607f5a081cfe59b8b
physiodevapp <ficapp2.0@gmail.com>
2022-11-26 12:19:44 +0100

    feat: Iteration 2

--- src/viking.js
+++ src/viking.js
@@ -15,7 +15,25 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
 class Saxon {}
