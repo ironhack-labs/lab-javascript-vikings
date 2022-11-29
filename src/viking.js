99f30d76757cf05bdbb3de7221d98eab0369fa57
Victor Lopez <victorlpz2301@gmail.com>
2022-11-29 11:59:55 +0100

    finished

--- src/viking.js
+++ src/viking.js
@@ -55,5 +55,54 @@ class Saxon extends Soldier {
 
 // War
 class War {
+    constructor () {
+        this.vikingArmy = [];
+        this.saxonArmy = []
+    }
+
+    addViking(vikingObject) {
+        this.vikingArmy.push(vikingObject)
+    }
+
+    addSaxon(saxonObject) {
+        this.saxonArmy.push(saxonObject)
+    }
+
+    vikingAttack () {
+
+    let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+    let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+       
+    const saxonHealth = randomSaxon.receiveDamage(randomViking.strength) 
     
+    this.saxonArmy = this.saxonArmy.filter(saxon => saxon.health > 0)
+    
+    return saxonHealth
+    }
+
+    saxonAttack () {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+           
+        const vikingHealth = randomViking.receiveDamage(randomSaxon.strength) 
+        
+        this.vikingArmy = this.vikingArmy.filter(viking => viking.health > 0)
+        
+        return vikingHealth
+    }
+
+    showStatus () {
+       if(this.saxonArmy.length === 0) {
+       return `Vikings have won the war of the century!`
+
+       } else if (this.vikingArmy.length === 0) {
+        return `Saxons have fought for their lives and survived another day...`
+        
+       } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length  >=1)
+       return `Vikings and Saxons are still in the thick of battle.`
+    }
+
 }
+
+
+    
