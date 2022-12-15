434e2e0527e574fb1d56821a4a7240a01cc47bbd
Wilkins Morales <wilkinscom@gmail.com>
2022-12-15 22:25:54 +0200

    init changes

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,26 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health = health
+        this.strength = strength
+    }
+
+    attack(){
+        return this.strength
+     }
+
+    receiveDamage(damage){
+        this.health -= damage
+    }
+
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {}
 
 // War
-class War {}
+class War extends Soldier {}
