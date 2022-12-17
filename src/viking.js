15965a52dd0a4fc51345b4d24906a903460e069b
Wilkins Morales <wilkinscom@gmail.com>
2022-12-17 10:24:35 +0200

    Viking and Saxon class ok

--- src/viking.js
+++ src/viking.js
@@ -19,18 +19,40 @@ class Soldier {
 // Viking
 class Viking extends Soldier {
     constructor(name,health,strength){
-        super(health);
-        this.name = name
+        super(health)
         this.strength = strength
+        this.name = name
+        
+    }
+
+    receiveDamage(damage){
+        this.health -= damage
+        if (this.health <= 0) {
+            return `${this.name} has died in act of combat`
+        } else if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        }
     }
 
     battleCry(){
-
+        return `Odin Owns You All!`; 
     }
+    
 }
 
 // Saxon
-class Saxon extends Soldier {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health -= damage
+        if (this.health <= 0){
+            return `A Saxon has died in combat`
+        } else if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+    }
+}
 
 // War
-class War extends Soldier {}
+class War {
+    
+}
