775e677414c5ebd4ddb7c7a89d475057b2582fdf
Yeonhwa Park <mellamoyona@gmail.com>
2022-12-19 18:06:55 +0100

    done - Iteration 3, WIP - Bonus

--- src/viking.js
+++ src/viking.js
@@ -14,14 +14,85 @@ class Soldier {
         this.health -= damage
     }
 
-
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(name, health, strength) {
+        
+        super(health, strength)
+        this.name = name
+    }
+
+    receiveDamage(damage) {
+        
+        this.health -= damage
+
+        if( this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+        
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    receiveDamage(damage) {
+
+        this.health -= damage
+
+        if( this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+
+}
 
 // War
-class War {}
+
+class War {
+
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+
+    vikingAttack() {
+        Viking.attack
+        this.health -= Viking.strength
+        
+        return saxonArmy.receiveDamage(vikingArmy.strength)
+
+    }
+
+    saxonAttack() {
+        return Viking.receiveDamage(Saxon.strength)
+    }
+
+    /*
+    showStatus() {
+        if(this.SaxonArmy)
+    }
+    */
+
+}
+
