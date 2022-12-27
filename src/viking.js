8fd80314efe4c1ab61da1bf4d3691c0ea01e17fe
Wilkins Morales <wilkinscom@gmail.com>
2022-12-27 12:17:06 +0100

    4 issues to fix

--- src/viking.js
+++ src/viking.js
@@ -88,7 +88,15 @@ saxonAttack(){
 }
 
 showStatus(){
+    if (!this.saxonArmy.length) return 'Vikings have won the war of the century!';
     
+    /*if (this.saxonArmy.length > 0 && this.vikingArmy.length > 0) {
+        return 'Vikings and Saxons are still in the thick of battle.';
+    } else if (!this.saxonArmy.length) {
+        return 'Vikings have won the war of the century!';
+    } else if (!this.vikingArmy.length) {
+        return 'Saxons have fought for their lives and survived another day...';
+    }*/
 }
 
 }
