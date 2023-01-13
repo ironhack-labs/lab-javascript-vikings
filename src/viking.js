608009bc9ab617d9fc520adadad58af9661aaf65
Neilly28 <neilpilarca@gmail.com>
2023-01-13 20:54:43 +0100

    add refactored code

--- src/viking.js
+++ src/viking.js
@@ -51,35 +51,43 @@ class Saxon extends Soldier {
 class War {
   vikingArmy = [];
   saxonArmy = [];
+
   addViking(viking) {
     this.vikingArmy.push(viking);
   }
+
   addSaxon(saxon) {
     this.saxonArmy.push(saxon);
   }
 
+  randomSaxon() {
+    return Math.floor(Math.random() * this.saxonArmy.length);
+  }
+
+  randomViking() {
+    return Math.floor(Math.random() * this.vikingArmy.length);
+  }
+
   vikingAttack() {
-    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
-    let result = this.saxonArmy[randomSaxon].receiveDamage(
-      this.vikingArmy[randomViking].strength
+    let result = this.saxonArmy[this.randomSaxon()].receiveDamage(
+      this.vikingArmy[this.randomViking()].strength
     );
-    if (this.saxonArmy[randomSaxon].health <= 0) {
-      this.saxonArmy.splice(randomSaxon, 1);
+    if (this.saxonArmy[this.randomSaxon()].health <= 0) {
+      this.saxonArmy.splice(this.randomSaxon(), 1);
     }
     return result;
   }
+
   saxonAttack() {
-    const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
-    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
-    let result = this.vikingArmy[randomViking].receiveDamage(
-      this.saxonArmy[randomSaxon].strength
+    let result = this.vikingArmy[this.randomViking()].receiveDamage(
+      this.saxonArmy[this.randomSaxon()].strength
     );
-    if (this.vikingArmy[randomViking].health <= 0) {
-      this.vikingArmy.splice(randomViking, 1);
+    if (this.vikingArmy[this.randomViking()].health <= 0) {
+      this.vikingArmy.splice(this.randomViking(), 1);
     }
     return result;
   }
+
   showStatus() {
     if (this.saxonArmy.length === 0) {
       return "Vikings have won the war of the century!";
