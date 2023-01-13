b5cee57b3011dcfd3bb62d1cc2641fd832517328
Maximilien Hjortland <maximilienhjortland@icloud.com>
2023-01-13 18:23:09 +0100

    progress on 4 first iterations

--- src/viking.js
+++ src/viking.js
@@ -57,4 +57,66 @@ class Saxon extends Soldier {
 
 
 // War
-class War {}
+
+
+
+
+class War {
+constructor(vikingArmy = [], saxonArmy = []) {
+    this.vikingArmy = vikingArmy
+    this.saxonArmy = saxonArmy
+}
+
+addViking(Viking){
+    this.vikingArmy.push(Viking)
+}
+
+addSaxon(Saxon){
+    this.saxonArmy.push(Saxon)
+}
+saxonAttack(){
+    let attack = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].
+    receiveDamage(this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].strength
+    )
+    this.saxonArmy.forEach(element, i) >= {if (element, health <= 0) {
+        this.saxonArmy.splice(i, 1)
+    } }
+    return attack 
+}
+vikingAttack(){
+    let attack = this.vikingArmy[Math.floor(Math.random() * 
+    this.vikingArmy.length)].receiveDamage(this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].strength)
+    this.vikingArmy.forEach((element, i)) => {
+        if(element.health <= 0) {
+            this.vikingArmy.splice(i, 1)
+        }
+    }
+    return attack
+};
+      
+
+
+showStatus(){}
+}
+
+/* YUNUS
+saxonAttack() {
+        let attack = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].receiveDamage(this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].strength)
+        this.vikingArmy.forEach((element, i) => {
+            if(element.health <= 0) {
+                this.vikingArmy.splice(i, 1) 
+            }
+        });
+        return attack 
+        
+        vikingAttack() {
+        let attack = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].receiveDamage(this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)].strength)
+        this.saxonArmy.forEach((element, i) => {
+            if(element.health <= 0) {
+                this.saxonArmy.splice(i, 1) 
+            }
+        });
+        return attack
+*/
+
+        
