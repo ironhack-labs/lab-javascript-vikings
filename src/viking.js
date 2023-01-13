f224dd929cdbd0ef100b401786929de022e01e3c
Neilly <neilpilarca@gmail.com>
2023-01-13 16:13:39 +0100

    Solve lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,63 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(dmg) {
+    this.health -= dmg;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    //custom behavior below
+    this.name = name;
+  }
+  receiveDamage(dmg) {
+    this.health -= dmg;
+    if (this.health > 0) {
+      return `${this.name} has received ${dmg} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength);
+  }
+  receiveDamage(dmg) {
+    this.health -= dmg;
+    if (this.health > 0) {
+      return `A Saxon has received ${dmg} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {}
+  saxonAttack() {}
+  showStatus() {}
+}
