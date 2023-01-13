3ff028343dcdc6b2295453e751480ff4e791fb05
Alex <alex.isaguilera@hotmail.com>
2023-01-13 22:39:30 +0100

    Solved Lab

--- src/viking.js
+++ src/viking.js
@@ -70,7 +70,38 @@ class War {
     }
 
     vikingAttack(){
-        let attackedSaxon = saxonArmy[Math.floor(Math.random()*saxonArmy.length)]
-        let attackingViking = vikingArmy[Math.floor(Math.random()*vikingArmy.length)]
+        let attackedSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        let attackingViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]
+        
+        let result = attackedSaxon.receiveDamage(attackingViking.strength);
+
+        if (attackedSaxon.health <= 0){
+            this.saxonArmy.splice(attackedSaxon, 1);
+        }
+        return result;
+    }
+
+    saxonAttack(){
+        let attackingSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        let attackedViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)]
+
+        let result = attackedViking.receiveDamage(attackingSaxon.strength);
+
+        if(attackedViking.health <= 0){
+            this.vikingArmy.splice(attackedViking,1);
+        }
+        return result;
+    }
+
+    showStatus(){
+        if(this.saxonArmy == ""){
+            return `Vikings have won the war of the century!`;
+        }
+        else if(this.vikingArmy == ""){
+            return `Saxons have fought for their lives and survived another day...`;
+        }
+        else {
+            return `Vikings and Saxons are still in the thick of battle.`;
+        }
     }
 }
