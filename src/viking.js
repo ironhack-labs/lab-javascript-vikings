dda5261fd7f3d7ccbe0df04ed2b6f3478fabdfbf
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-13 16:20:46 +0100

    Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -37,7 +37,23 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength)
+    }
+attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else if (this.health <= 0) {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
