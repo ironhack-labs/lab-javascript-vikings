529a8c43cbbffc3d245f8d0b4c171b95d8b2662d
Paul Dombrowski <paul.dombr@googlemail.com>
2023-01-13 17:29:14 +0100

    work in progress

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,22 @@
-// Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+      this.health = health;
+      this.strength = strength;
+    }
+  
+    attack() {
+      return this.strength;
+    }
+  
+    receiveDamage(damage) {
+      this.health -= damage;
+    }
+  }
 
-// Viking
-class Viking {}
 
-// Saxon
-class Saxon {}
-
-// War
-class War {}
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super(health, strength);
+      this.name = name;
+    }
+} 
