2755f1f8f697b7db550ab29aa313d689a3947790
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-01-13 17:22:45 +0100

    Iteration 5 and finalize

--- src/viking.js
+++ src/viking.js
@@ -56,4 +56,50 @@ attack() {
 }
 
 // War
-class War {}
+class War {
+    vikingArmy = []
+    saxonArmy = []
+
+addViking (Viking) {
+ this.vikingArmy.push(Viking)
+}
+
+addSaxon (Saxon) {
+    this.saxonArmy.push(Saxon)
+}
+
+vikingAttack (){
+    const saxonIndex = [Math.floor(Math.random() * this.saxonArmy.length)]
+    const vikingIndex = [Math.floor(Math.random() * this.vikingArmy.length)]
+   /*  const fightingSaxon = this.saxonArmy[saxonIndex]
+    const fightingViking = this.vikingArmy[vikingIndex] */
+    const result = this.saxonArmy[saxonIndex].receiveDamage(this.vikingArmy[vikingIndex].strength)
+    if (this.saxonArmy[saxonIndex].health <= 0){
+        this.saxonArmy.pop(saxonIndex)
+    }
+    return result
+}
+
+saxonAttack () {
+    const vikingIndex = [Math.floor(Math.random() * this.vikingArmy.length)]
+    const saxonIndex = [Math.floor(Math.random() * this.saxonArmy.length)]
+    const result = this.vikingArmy[vikingIndex].receiveDamage(this.saxonArmy[saxonIndex].strength)
+    if (this.vikingArmy[vikingIndex].health <= 0){
+        this.vikingArmy.pop(vikingIndex)
+    }
+    return result
+}
+
+showStatus () {
+    if (this.saxonArmy.length === 0){
+        return "Vikings have won the war of the century!"
+    }
+    else if (this.vikingArmy.length === 0){
+        return "Saxons have fought for their lives and survived another day..."
+    }
+    else if (this.saxonArmy.length !== 0 && this.vikingArmy.length !== 0) {
+        return "Vikings and Saxons are still in the thick of battle."
+    }
+}
+}
+
