945f163ac5aa5027d093845447868eff8a5ca22d
Zergo12 <adzg96@gmail.com>
2023-01-13 20:06:16 -0600

    Primeros cambios

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+this.health = health
+this.strength = strength
+    }
+attack(){
+return (this.strength)
+}
+
+receiveDamage(theDamage){
+this.health -= theDamage
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+    }
+
+
+    receiveDamage(theDamage){
+    this.health -= theDamage
+
+    if (this.health === 0){
+        return (`${this.name} has died in act of combat`)
+    }
+
+    else {
+        return (`${this.name} has received ${theDamage} points of damage` )
+    }
+    
+    }
+    battleCry(){
+        return ("Odin Owns You All!")    
+    }
+    }
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+    super(health, strength)
+
+    }
+
+    receiveDamage(theDamage){
+     this.health -= theDamage   
+
+     if(this.health === 0){
+        return "A Saxon has died in combat"
+     }
+
+     else{
+        return `A Saxon has received ${theDamage} points of damage`
+     }
+    }
+}
+
+
+
 
 // War
-class War {}
+class War {
+    constructor(){
+    this.vikingArmy = []
+    this.saxonArmy = []
+    }
+    addViking(viking){
+        this.viking = viking
+        vikingArmy.push(this.viking)
+    }
+    addSaxon(saxon){
+        this.saxon = saxon
+    }
+    vikingAttack(){
+
+    }
+    saxonAttack(){
+
+    }
+    showStatus(){
+
+    }
+
+}
