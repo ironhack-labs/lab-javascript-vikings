c07e9b7fa83a0955d67aed48998c6a85ea1ba8f2
Eduardo Suarez <draco_008@hotmail.com>
2023-01-15 14:51:55 -0800

    Version 1.0 - Viking Lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
 // Soldier
-class Soldier {}
+class Soldier  {
+
+    constructor (health, strength){
+        this.health = health
+        this.strength = strength
+    }
+
+    attack (){
+        return this.strength
+    }
+
+    receiveDamage (damage){
+        this.health -= damage
+        
+    }
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor(name, health, strength, ){
+        super(health, strength);
+        this.name = name;
+    }
+
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+
+    receiveDamage(damage){
+        this.health -= damage
+
+        if(this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }else{
+            return `${this.name} has died in act of combat`
+        }
+        
+        
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+    constructor(health, strength){
+        super(health, strength);
+    }
+
+    receiveDamage(damage){
+        this.health -= damage
+
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }else{
+           return `A Saxon has died in combat`
+        }
+    } 
+}
 
 // War
-class War {}
+class War {
+    
+    vikingArmy = [];
+    saxonArmy = [];
+
+    addViking(Viking){
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack(){}
+    saxonAttack(){}
+    showStatus(){}
+}
+
+
+
+let soldado1 = new Soldier(100, 50);
+let ragnar = new Viking('ragnar', 0, 70);
+console.log(ragnar);
+console.log(soldado1);
+
+
