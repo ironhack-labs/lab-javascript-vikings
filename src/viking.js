190fc17c99bc40940a3b3e54b1d617fcff7e3aa2
Pedro Henrique <pedrohgbarbosa@hotmail.com>
2023-01-16 18:39:09 -0300

    solved file

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,69 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(Damage) {
+    this.health = this.health - Damage;
+
+    return;
+  }
+}
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(Damage) {
+    super.receiveDamage(Damage);
+    if (this.health > 0) {
+      return `${this.name} has received ${Damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(Damage) {
+    super.receiveDamage(Damage);
+    if (this.health > 0) {
+      return `A Saxon has received ${Damage} points of damage`;
+    } else {
+      return "A Saxon has died in combat";
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+
+  vikingAttack() {}
+
+  saxonAttack() {}
+
+  ShowStatus() {}
+}
