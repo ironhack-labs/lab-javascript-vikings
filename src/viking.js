028c72c952212d7ad8d192a57541baf4121c13fb
Paul Dombrowski <paul.dombr@googlemail.com>
2023-01-16 10:04:54 +0100

    done without bonus

--- src/viking.js
+++ src/viking.js
@@ -1,22 +1,56 @@
+// Soldier
+
 class Soldier {
     constructor(health, strength) {
-      this.health = health;
-      this.strength = strength;
+        this.health = health;
+        this.strength = strength; 
     }
-  
-    attack() {
-      return this.strength;
+
+    attack(){
+        return this.strength;
     }
-  
+
     receiveDamage(damage) {
-      this.health -= damage;
+        this.health -= damage;
     }
-  }
+}
+
+// Viking
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health>0){
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        else{
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return `Odin Owns You All!`;
+    }
+
+}
+
+// Saxon
+class Saxon extends Soldier{
 
 
-  class Viking extends Soldier {
-    constructor(name, health, strength) {
-      super(health, strength);
-      this.name = name;
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health>0){
+            return `A Saxon has received ${damage} points of damage`;
+        }
+        else{
+            return `A Saxon has died in combat`;
+        }
     }
-} 
+}
