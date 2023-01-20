7871bfccd1cd9a3fcf16a268c198b512008883ad
ElDav1d <dvivoes@yahoo.es>
2023-01-20 18:28:29 +0100

    Resolves Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -20,15 +20,24 @@ class Viking extends Soldier {
   receiveDamage = (damage) => {
     this.health = this.health - damage;
 
-    if (this.health) return `${this.name} has received ${damage} points of damage`;
+    if (this.health)
+      return `${this.name} has received ${damage} points of damage`;
 
     return `${this.name} has died in act of combat`;
   };
 
-   battleCry = () => "Odin Owns You All!";
+  battleCry = () => "Odin Owns You All!";
 }
 
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage = (damage) => {
+    this.health = this.health - damage;
 
+    if (this.health)
+      return `A Saxon has received ${damage} points of damage`;
+
+    return `A Saxon has died in combat`;
+  };
+}
 
 class War {}
