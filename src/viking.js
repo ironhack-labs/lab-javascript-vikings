8bba4c61e3aa85fd0c98c9e23b68f90bea70675c
miacsilva <miacsilvac@hotmail.com>
2023-01-20 18:16:25 +0000

    Final lab

--- src/viking.js
+++ src/viking.js
@@ -23,12 +23,10 @@ class Viking extends Soldier{
     }
     receiveDamage(theDamage){
         this.health -= theDamage;
-        if (this.health> 0){
-            return `${this.name} has received ${theDamage} points of damage`
-        }
-        else {
-            return `${this.name} has died in act of combat`
-        }
+        if (this.health> 0) return `${this.name} has received ${theDamage} points of damage`
+        
+        else return `${this.name} has died in act of combat`
+        
     } battleCry(){
         return "Odin Owns You All!"
     }
@@ -43,29 +41,43 @@ class Saxon extends Soldier {
     }
     receiveDamage(theDamage){
         this.health -= theDamage;
-        if (this.health> 0){
-            return `A Saxon has received ${theDamage} points of damage`
-        }
-        else {
-            return `A Saxon has died in combat`
-        }
+        if (this.health> 0) return `A Saxon has received ${theDamage} points of damage`
+        
+        else return `A Saxon has died in combat`
+        
 } }
 
 class War {
-    constructor()
+    vikingArmy = []
+    saxonArmy = []
+
+    addViking(viking){
+    this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        let viking = Math.floor(Math.random()* this.vikingArmy.length)
+        let saxon = Math.floor(Math.random()* this.saxonArmy.length)
+
+        let valor = this.saxonArmy[saxon].receiveDamage(this.vikingArmy[viking].strength)
+        
+        if (!(this.saxonArmy[saxon].health>0)){
+            return this.saxonArmy.splice(saxon,1)
+        } else return valor
+    }
+    saxonAttack(){}
+    showStatus(){}
 }
 
-/* class
+
+/* 
+ *vikA Saxon (chosen at random) has their receiveDamage() method called with the damage equal to the strength of a Viking (also chosen at random).
+// This should only perform a single attack and the Saxon doesn't get to attack back.
+
+should be a function
 should receive 0 arguments
-should assign an empty array to the vikingArmy property
-should assign an empty array to the saxonArmy property
-addViking() method
-should be a declared
-should receive 1 argument (a Viking object)
-should add the received Viking to the army
-shouldn't return anything
-addSaxon() method
-should be a declared
-should receive 1 argument (a Saxon object)
-should add the received Saxon to the army
-shouldn't return anything */
+should make a Saxon receiveDamage() equal to the strength of a Viking
+should remove dead Saxons from the army
+should return result of calling receiveDamage() of a Saxon with the strength of a Viking */
