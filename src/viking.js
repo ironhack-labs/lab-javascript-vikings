648e8ea16edf70401dbe1bda5e1ccf055b48c0c2
Joao <joaosacarneiro@Joaos-MacBook-Air.local>
2023-01-20 17:22:31 +0000

    done

--- src/viking.js
+++ src/viking.js
@@ -1,13 +1,82 @@
 // Soldier
 class Soldier {
-    ee;
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health -= damage
+    }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+    super(health, strength)
+    this.name = name
+    }
+    receiveDamage(damage){
+        this.health -= damage
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength){
+    super(health, strength)
+    }
+    receiveDamage(damage){
+        this.health -= damage
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        let randomViking = Math.floor(Math.random() * this.vikingArmy.length)
+        let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+        let attackResult = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength)
+        if(this.saxonArmy[randomSaxon].health <= 0){
+            this.saxonArmy.pop(randomSaxon)
+        }
+        return attackResult
+    }
+    saxonAttack(){
+        let randomViking = Math.floor(Math.random() * this.vikingArmy.length)
+        let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+        let attackResult = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength)
+        if(this.vikingArmy[randomViking].health <= 0){
+            this.vikingArmy.pop(randomViking)
+        }
+        return attackResult
+    }
+    showStatus(){
+
+    }
+}
