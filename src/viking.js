2700453184a9c738681fa31486f87003b02eb95a
Tiago Rato <tiagorato13@hotmail.com>
2023-01-20 15:19:18 +0000

    submit lab week1 day 5

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if(this.health > 0) return `${this.name} has received ${damage} points of damage`;
+        else return `${this.name} has died in act of combat`;
+    }   
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength) {
+        super(name, health, strength)
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+}
 
 // Saxon
 class Saxon {}
