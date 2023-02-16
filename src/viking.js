164b1b6a7570c4ebe7d83dcb6b664301052f47d8
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-02-16 18:25:58 +0100

    Added class Viking with new Functions and properties

--- src/viking.js
+++ src/viking.js
@@ -13,7 +13,20 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name,health,strength){
+        super(health,strength);
+        this.name = name;
+    }
+    receiveDamage(damage){
+         this.health = this.health - damage;
+         if(this.health > 1){
+            return `${this.name} has received ${damage} points of damage`
+         }else if(this.health < 1){
+            return `${this.name} has died in act of combat`
+         }
+    }
+}
 
 // Saxon
 class Saxon {}
