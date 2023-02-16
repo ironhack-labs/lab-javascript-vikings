11838d0a9dff8a064ab8afefc0772f9fe6446f49
RonnySk <ronny_sk@hotmail.com>
2023-02-16 22:22:11 +0100

    Finished Soldier and Viking

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+       this.health = health;
+       this.strength =  strength
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(theDamage) {
+        let vikingDamaged = this.health -= theDamage; 
+       return 
+    } 
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    
+    receiveDamage(theDamage) {
+        let vikingDamaged = this.health -= theDamage;
+        if(vikingDamaged === 0) {
+            return `${this.name} has died in act of combat`
+        } else {
+            return `${this.name} has received ${theDamage} points of damage`
+        }
+     }
+
+     battleCry() {
+        return "Odin Owns You All!"
+     }
+}
 
 // Saxon
 class Saxon {}
