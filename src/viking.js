acf2d1d3e55642d397301800373b247cc7a3d835
“Zuzana <zuzana@getyourguide.com>
2023-02-16 21:44:17 +0100

    war iteration

--- src/viking.js
+++ src/viking.js
@@ -35,4 +35,55 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor () {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+        }
+    addViking(Viking){
+        this.vikingArmy.push(Viking);
+    };
+    
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
+        };
+    
+    
+/* A Saxon (chosen at random) has their receiveDamage() method called with the damage equal to the strength of a Viking (also chosen at random). */ 
+
+    vikingAttack(){
+
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+        let fight = randomSaxon.receiveDamage(randomViking.strength);
+
+        if (randomSaxon.health <=0) {
+            this.saxonArmy.pop(randomSaxon);
+        }
+        return fight;
+    };
+
+    saxonAttack(){
+
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random()*this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random()*this.vikingArmy.length)];
+        let fight = randomViking.receiveDamage(randomSaxon.strength);
+
+        if (randomViking.health <=0) {
+            this.vikingArmy.pop(randomViking);
+        }
+        return fight;
+    };
+
+    showStatus(){
+        if (this.saxonArmy.length === 0) {
+            return `Vikings have won the war of the century!`
+        }
+        else if (this.vikingArmy.length === 0) {
+            return `Saxons have fought for their lives and survived another day...`
+        }
+        else {
+            return `Vikings and Saxons are still in the thick of battle.`;
+        }
+    }
+}
