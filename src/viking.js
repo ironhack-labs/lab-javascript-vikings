52672ab44f1361334c4e148f52b385785f12759e
olgam <olgamartyniuk88@gmail.com>
2023-02-17 17:27:17 +0100

    Basic part done, tried War but so far no luck

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,93 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.damage = damage;
+        this.health = this.health - this.damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        // is attack inherited automatically or do I have to code it?
+    }
+    
+    receiveDamage(damage) { // should be super. ?
+        this.damage = damage;
+        this.health = this.health - this.damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        else if (this.health <= 0) {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        this.damage = damage;
+        this.health -= this.damage;
+       
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        }
+        else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
+  
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = []; 
+    }
+    
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+
+    }
+
+    vikingAttack() {
+        Saxon.receiveDamage() = Viking.strength;
+
+        if (Saxon.health <= 0) {
+            this.saxonArmy.pop();
+        }
+
+    }
+
+    saxonAttack() {
+
+    }
+
+    showStatus() {
+
+    }
+
+}
