5397192c442f10eb4f1c9ccebbf866aaa5386bb1
Cedric Aim <cedric.aim@gmail.com>
2023-02-22 21:35:12 +0100

    iteration 1 et 2

--- src/viking.js
+++ src/viking.js
@@ -1,13 +1,32 @@
 // Soldier
 class Soldier {
-    constructor (health,strength) {
-      this.health = health;
-      this.strength= strength;  
-    }
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+    return `${this.name} has died in act of combat`;
+  }
+  battleCry() {}
+}
 
 // Saxon
 class Saxon {}
