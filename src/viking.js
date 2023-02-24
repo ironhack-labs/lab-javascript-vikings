beb9e916a2f3fb7db7fdfc7719e0f4064b984952
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-02-25 02:06:33 +0800

    finished almost all iterations

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,74 @@
-// Soldier
-class Soldier {}
 
-// Viking
-class Viking {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+}
 
-// Saxon
-class Saxon {}
 
-// War
-class War {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+
+        super(name, health, strength);
+
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health>0){
+            return `${this.name} has received ${damage} points of damage`;
+        }else if(this.health <= 0){
+            return `${this.name} has died in act of combat`;
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
+}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength);
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health>0){
+            return `A Saxon has received ${damage} points of damage`;
+        }else if(this.health <= 0){
+            return `A Saxon has died in combat`;
+        }
+    }
+}
+
+
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(obj){
+        this.vikingArmy.push(obj);
+    }
+    addSaxon(obj){
+        this.saxonArmy.push(obj);
+    }
+    vikingAttack(){
+        receiveDamage()
+    }
+    saxonAttack(){
+
+    }
+    showStatus(){
+
+    }
+}
+
+ 
