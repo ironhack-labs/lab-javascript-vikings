b460e42dfc9dc41eaf0c43046c93a1bd6c2fcebe
Agostina Romero <agostinaromontamatt@gmail.com>
2023-02-24 09:56:20 -0300

    'SolvedLab'

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,61 @@
 // Soldier
-class Soldier {}
-
+class Soldier {
+    constructor(health, strength) {
+        this.health= health;
+        this.strength= strength;
+        
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.damage = damage;
+        this.health -= this.damage;
+    }
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name= name;
+    }
+    receiveDamage(damage){
+        super.receiveDamage(damage)
+        if(this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        } 
+        return `${this.name} has died in act of combat`
+    
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage){
+        super.receiveDamage(damage);
+        if(this.health > 0){
+            return `A Saxon has received ${damage} points of damage`;
+        }
+          return 'A Saxon has died in combat'
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy= []
+        this.saxonArmy= []
+    }
+    addViking(Viking){
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+}
+
+let player1 = new Saxon("Joaquín", 26, 56);
+console.log(Saxon.receiveDamage())
