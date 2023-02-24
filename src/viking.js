ebd1592a11542da4a3b6eb5c1964a464175dc8b0
Chris Weber <johndoe@example.com>
2023-02-24 18:37:37 +0100

    let's start the war

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,54 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(thedamage) {
+    this.health -= thedamage;
+  }
+
+  battleCry() {
+    return "Odin Owns You All!";
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(thedamage) {
+    this.health -= thedamage;
+    if (this.health > 0) {
+      return `${this.name} has received ${thedamage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+}
+
+const bjørn = new Viking();
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(healt, strength) {
+    super(healt, strength);
+  }
+  receiveDamage(thedamage) {
+    this.health -= thedamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${thedamage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
 class War {}
