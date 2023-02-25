6942667415991ae3e4ac046f618b5d986786832e
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-02-25 10:27:29 +0800

    solved the lab incl bonus

--- src/viking.js
+++ src/viking.js
@@ -61,13 +61,38 @@ class War {
         this.saxonArmy.push(obj);
     }
     vikingAttack(){
-        receiveDamage()
+       const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+       const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+       const result = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
+       
+
+       if(this.saxonArmy[randomSaxon].health <= 0){
+             this.saxonArmy.splice(randomSaxon,1);
+       }
+        return result;
+       
+       
     }
     saxonAttack(){
+        const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
 
+        const result = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+
+        if(this.vikingArmy[randomViking].health <= 0){
+             this.vikingArmy.splice(randomViking,1);
+        }
+        return result;
     }
     showStatus(){
-
+        if(this.saxonArmy.length === 0){
+            return "Vikings have won the war of the century!";
+        }else if(this.vikingArmy.length === 0){
+            return "Saxons have fought for their lives and survived another day...";
+        }else if(this.saxonArmy.length > 0 && this.vikingArmy.length > 0){
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
     }
 }
 
