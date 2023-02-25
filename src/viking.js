75103afea29097a0223997598885b1d8c1e1056f
raqueldfrutos <raqueldfrutos@gmail.com>
2023-02-25 13:56:51 +0100

    All done except the Armies Attack Bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,64 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.strength = strength;
+    this.health = health;
+  }
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damageValue) {
+    this.health = this.health - damageValue;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+
+  receiveDamage(damageValue) {
+    this.health -= damageValue;
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    } else {
+      return `${this.name} has received ${damageValue} points of damage`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damageValue) {
+    this.health -= damageValue;
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    } else {
+      return `A Saxon has received ${damageValue} points of damage`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(vikingObj) {
+    this.vikingArmy.push(vikingObj);
+  }
+
+  addSaxon(saxonObj) {
+    this.saxonArmy.push(saxonObj);
+  }
+}
+
+
