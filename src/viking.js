6160c3fc3b7a426cc365030d15a173acb1a2b1f8
Javier Vivas <javier.vivas@icloud.com>
2023-02-25 15:35:26 +0100

    Terminado. Faltan solo Bonus

--- src/viking.js
+++ src/viking.js
@@ -5,15 +5,15 @@ class Soldier {
         this.health = health;
         this.strength = strength
     
-        }
+    }
     attack(){
        return this.strength
     }
 
     receiveDamage(damage){
         this.health -= damage
-        }   
-    };
+    }   
+};
   
 
     
@@ -25,21 +25,34 @@ class Viking extends Soldier {
     constructor(name,health,strength){
         super(health,strength)
         this.name = name
-
-
     }
     receiveDamage(damage){
-        this.health -= damage
+       this.health -= damage
+        if(this.health <=0){
+            return `${this.name} has died in act of combat` 
+        }else{
+            return `${this.name} has received ${damage} points of damage`
         }
-        if(health = 0){
-            return `${Viking.name} has died in act of combat`
-        }
-        
-        }
+    }
+    
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+};
     
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+
+    receiveDamage(damage){
+        this.health -= damage
+        if(this.health <=0){
+            return `A Saxon has died in combat` 
+        }else{
+            return `A Saxon has received ${damage} points of damage`
+        }   
+    }
+}
 
 // War
 class War {}
