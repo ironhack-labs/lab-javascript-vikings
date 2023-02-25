812575cd2dc5ff489537a199e5c7e4315b7f2d23
valyy151 <marin.valenta1@gmail.com>
2023-02-25 14:16:58 +0100

    Finished Project

--- src/viking.js
+++ src/viking.js
@@ -103,21 +103,32 @@ class War {
 		return attack;
 	}
 
-	// armiesAttack(typeOfAttack) {
-	// 	if (typeOfAttack === 'Viking') {
-	// 		randomSaxon.receiveDamage(randomViking.strength);
-	// 		if (randomSaxon.health <= 0) {
-	// 			this.saxonArmy.splice(0, 1);
-	// 		}
-	// 	} else if (typeOfAttack === 'Saxon') {
-	// 		randomViking.receiveDamage(randomSaxon.strength);
-	// 		if (randomViking.health <= 0) {
-	// 			this.vikingArmy.splice(0, 1);
-	// 		}
-	// 	}
-	// }
+	armiesAttack(typeOfAttack) {
+		let randomSaxon = this.randomSaxon();
+		let randomViking = this.randomViking();
 
-	showStatus() {}
+		if (typeOfAttack === 'Viking') {
+			randomSaxon.receiveDamage(randomViking.strength);
+			if (randomSaxon.health <= 0) {
+				this.saxonArmy.splice(randomSaxon, 1);
+			}
+		} else if (typeOfAttack === 'Saxon') {
+			randomViking.receiveDamage(randomSaxon.strength);
+			if (randomViking.health <= 0) {
+				this.vikingArmy.splice(randomViking, 1);
+			}
+		}
+	}
+
+	showStatus() {
+		if (this.saxonArmy.length === 0) {
+			return 'Vikings have won the war of the century!';
+		} else if (this.vikingArmy.length === 0) {
+			return 'Saxons have fought for their lives and survived another day...';
+		} else {
+			return 'Vikings and Saxons are still in the thick of battle.';
+		}
+	}
 }
 
 const bjorn = new Viking('Bjorn', 100, 25);
