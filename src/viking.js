12c54d3fa77a7fee493be163dd94d10d0ceed673
Nicolas_M <nicolas.meot@live.fr>
2023-02-25 10:57:34 +0100

    Bonus 1

--- src/viking.js
+++ src/viking.js
@@ -40,7 +40,42 @@ class Viking extends Soldier {
 new Viking('Bob', 10, 4)
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+    this.health -= damage;
+
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`
+    }
+    else{
+      return `A Saxon has died in combat`
+    }
+    }
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+  this.vikingArmy = [] ;
+  this.saxonArmy = [] ; 
+  }
+ addViking(viking){
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon){
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack(){
+    let randomSaxon = this.saxonArmy[Math.floor(this.vikingArmy.length * Math.random())]
+    let randomViking = this.vikingArmy[Math.floor(this.saxonArmy.length * Math.random())];
+    let result = randomSaxon.receiveDamage(randomViking.attack())
+    if (randomSaxon.health <= 0){ 
+      this.saxonArmy.splice(randomSaxon,1);
+    }
+    return result
+  }
+  saxonAttack(){}
+  showStatus(){}
+} 
+
+
