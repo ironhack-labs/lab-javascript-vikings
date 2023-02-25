6a87bb3508e1dd015459903861cfb93d489167c7
kobetoto <106164290+kobetoto@users.noreply.github.com>
2023-02-25 10:19:26 +0100

    iteration2

--- src/viking.js
+++ src/viking.js
@@ -14,7 +14,30 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(n, h, s) {
+    super(h, s)
+    this.name = n
+  };
+
+  receiveDamage(damage){
+    this.health -= damage;
+
+    if(this.health > 0){
+      return `${this.name} has received ${damage} points of damage`
+    }
+    else{
+      return `${this.name} has died in act of combat`
+    }
+    }
+
+    battleCry(){
+      return 'Odin Owns You All!';
+    }
+  };
+
+
+new Viking('Bob', 10, 4)
 
 // Saxon
 class Saxon {}
