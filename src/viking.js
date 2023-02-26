ce972c8feabace78e3275ba291e9bb8068a15a71
Hugo Silva <dasilvapapis@protonmail.com>
2023-02-26 00:59:09 +0000

    26 failures to fix

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,64 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+        this.damage;
+    }
+    attack() {
+        return this.strength; 
+        
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super (name, health, strength);
+        this.name = name;
+        this.health = health; 
+        this.strength = strength;
+    }
+    battleCry () {
+        return 'Odin Owns You All!';
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > damage) {
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }   
+    }
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health,strength) {
+        super (health, strength)
+        this.health = health;
+        this.strength = strength;
+    }
+    // attack() {
+    //    return this.strength;
+    // }
+    // No need to add this method due to the inheritance
+    receiveDamage (damage) {
+        this.health = this.health - damage;
+        if (this.health > damage) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
