58d0ad3792ee84746d4759f7229ad460586dde6e
SenemDEV <121942044+SenemDEV@users.noreply.github.com>
2023-02-26 13:22:20 +0100

    The first 3 iterations without the Bonus yet...

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,66 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health=health;
+        this.strength=strength;
+    }
+    attack() {
+      return this.strength;
+    }
+    receiveDamage(thedamage) {
+        this.health -= thedamage;
+         
+        }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  
+   constructor(name, health, strength){
+    super(health,strength);
+    this.name = name;
+   }
+
+   
+   receiveDamage(thedamage) {
+    this.health-=thedamage;
+    if (this.health > 0) {
+    
+       return `${this.name} has received ${this.thedamage} points of damage`
+    }
+    else if (this.health === 0){
+        return `${this.name} has died in act of combat`
+    } else {
+        this.health-=thedamage;
+    }
+
+   }
+   
+   battleCry() {
+    return "Odin Owns You All!"
+   }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(thedamage) {
+        this.health-=thedamage;
+        if (this.health > 0) {
+        
+           return `A Saxon has received ${this.thedamage} points of damage`
+        }
+        else if (this.health === 0){
+            return `A Saxon has died in combat`
+        }
+}
+}
 
 // War
 class War {}
+
+
+
+
+
