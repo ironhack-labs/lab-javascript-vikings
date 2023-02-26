25bc80ff93f7abd3cec1c6998e7dd65e18a4510f
TunaErkmen <98965068+TunaErkmen@users.noreply.github.com>
2023-02-26 15:35:26 +0100

    done until vikingAttack(), Bonus is almost finished.

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,90 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+       return this.strength;
+    }
+
+    receiveDamage(damage) {
+    this.health -= damage;
+    } 
+    }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name,health,strength){
+        super (health,strength);
+        this.name = name;
+    }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+        return `${this.name} has died in act of combat`;
+    } else if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`;
+    }
+  }
+    battleCry() {
+        return `Odin Owns You All!`;
+    }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super (health,strength);
+        this.name = 'Saxon';
+    }
+
+receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+			return `A ${this.name} has died in combat`;
+		} else if (this.health > 0) {
+			return `A ${this.name} has received ${damage} points of damage`;
+		}
+        }
+}
 
 // War
-class War {}
+class War {
+    constructor() {    
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+        
+    }
+ addSaxon(saxon) {
+ this.saxonArmy.push(saxon);
+}
+
+vikingAttack() {
+
+}
+
+saxonAttack() {
+
+}
+
+showStatus() {
+    this.saxonArmy = [];
+    this.vikingArmy = [];
+
+    if (this.saxonArmy.length === 0 ) {
+        return "Vikings have won the war of the century!"
+    } else if (this.vikingArmy.length === 0 ) {
+        return "Saxons have fought for their lives and survive another day...";
+      } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1){
+        return "Vikings and Saxons are still in the thick of battle.";
+      }
+}
+
+}
