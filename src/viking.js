121eabfbff2a445d68dac963a6937748741cdd6e
andrehcarvalho6 <andre.hcarvalho1@gmail.com>
2023-02-26 22:59:20 +0000

    done except for the bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,59 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    }
+    
+    attack() {
+   return this.strength;
+    }
+    
+    receiveDamage(damage) {
+    this.health -= damage;
+    }
+    }
+  // Viking
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+        }
+        
+        receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health > 0) {
+        return `${this.name} has received ${damage} points of damage`;
+        } else {
+        return `${this.name} has died in act of combat`;
+        }
+        }
+        
+        battleCry() {
+        return "Odin Owns You All!";
+         }
+        }
+  
+  // Iteration 3: Saxon
+    class Saxon extends Soldier {
+    receiveDamage(damage) {
+    super.receiveDamage(damage);
+    if (this.health > 0) {
+        return  `A Saxon has received ${damage} points of damage`; 
+      } else {
+    return "A Saxon has died in combat";
+    }
+    }
+    }
 
-// Viking
-class Viking {}
+  
+  // War
+  class War {
+    constructor() {
+        
+    }
+  }
 
-// Saxon
-class Saxon {}
 
-// War
-class War {}
+
+
