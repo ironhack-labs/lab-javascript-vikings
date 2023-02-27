cba2a49d64343199228895dda4064e7818a3a378
Jon <jon_ander_23@protonmail.com>
2023-02-27 15:44:21 +0100

    All done except Armies Attack section

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = function attack () {
+        return this.strength;
+    }
+    this.receiveDamage = function receiveDamage (damage) {
+        this.health -= damage;
+    }
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+    constructor (name, health, strength) {
+
+    super (name, health, strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.receiveDamage  = function receiveDamage (damage) {
+            this.health -= damage;
+            if (this.health > 0) {
+                return `${this.name} has received ${damage} points of damage`
+            }
+
+            else {
+                return `${this.name} has died in act of combat`
+            }
+        }
+        this.battleCry = function battleCry () {
+            return `Odin Owns You All!`;
+        }
+    }
+
+    
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    constructor (name, health, strength) {
+
+    super (name, health, strength);
+
+    this.receiveDamage  = function receiveDamage (damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+
+        else {
+            return `A Saxon has died in combat`
+        } 
+}
+    }
+}
 
 // War
-class War {}
+class War {
+ constructor () {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+    }
+    
+    addViking (viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon (saxon) {
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack () {
+
+        
+    }
+    }
+    
