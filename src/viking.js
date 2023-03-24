fb31b5838c0b88880321bd56bfdab5c357e52bff
Nadezda Samorokova <nadezdasamorokova@gmail.com>
2023-03-24 16:45:50 +0100

    Added Soldier, Viking and Saxon

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,63 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.health;
+    }
+
+    receiveDamage(damage) {
+        this.health =- damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health >= 1) {
+            return `${this.name} has received DAMAGE points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health >= 1) {
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
 class War {}
