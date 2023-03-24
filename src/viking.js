1a0afdc4d725d562290bcf6e05ed1253b72f2fae
Rafal Kolibaba <rafal.kolibaba@icloud.com>
2023-03-24 17:10:15 +0100

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,76 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super (health, strength)
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health = this.health - damage
+        if (this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else return `${this.name} has died in act of combat`
+    }
+    battleCry() {
+         return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon  extends Soldier {
+        receiveDamage(damage) {
+        this.health = this.health - damage
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else return `A Saxon has died in combat`
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){ 
+    this.vikingArmy =[]
+    this.saxonArmy = []
+}
+    addViking(Viking){
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack(){
+        const randomVikingNumber = Math.floor(Math.random() * this.vikingArmy.length)
+        const randomSaxonNumber = Math.floor(Math.random() * this.saxonArmy.length)
+        const randomViking = this.vikingArmy[randomVikingNumber]
+        const randomSaxon = this.saxonArmy[randomSaxonNumber]
+        const result = randomSaxon.receiveDamage(randomViking.attack())
+        
+        return result
+    }
+    saxonAttack(){
+        const randomVikingNumber = Math.floor(Math.random() * this.vikingArmy.length)
+        const randomSaxonNumber = Math.floor(Math.random() * this.saxonArmy.length)
+        const randomViking = this.vikingArmy[randomVikingNumber]
+        const randomSaxon = this.saxonArmy[randomSaxonNumber]
+        const result = randomViking.receiveDamage(randomSaxon.attack())
+        return result
+    }
+    showStatus(){}
+}
