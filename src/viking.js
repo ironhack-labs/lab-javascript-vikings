d695855f62cc41a44ffafab11564ba288a1e4a5e
Moriah Askenaizer <moriahaskenaizer@gmail.com>
2023-03-24 21:48:35 +0100

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,36 @@
 // Soldier
-class Soldier {}
+class Soldier{
+  constructor(health, strength){
+this.health= health;
+this.strength= strength;
+  }
+ attack(){
+  return this.strength
+ }
+receiveDamage(damage){
+this.health = this.health-damage
+}
 
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name,health, strength){
+    super(health,strength)
+    this.name = name
+}
+attack(){
+  return this.strength
+}
+receiveDamage(damage){
+this.health = this.health - damage
+}
+battleCry(){
+  return "Odin Owns You All!"
+}
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {}
 
 // War
 class War {}
