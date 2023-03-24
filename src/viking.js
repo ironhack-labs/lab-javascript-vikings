a15aae44c1cfb2be26a06a0afe17b34b1e6046ad
Kristof R <kristofrombauts@me.com>
2023-03-24 16:24:53 +0100

    Saxon done

--- src/viking.js
+++ src/viking.js
@@ -37,7 +37,17 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health -= damage
+
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+}
+}
 
 // War
 class War {}
