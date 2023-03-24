1211e686278b62a94ab3aa353c1c0486aeb00956
Fabian Hurtado Volkmann <fabian04berlin@outlook.de>
2023-03-24 14:36:01 +0100

    complete soldier and viking class

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,34 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  battleCry() {
+    return "Odin Owns You All!"
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+    if(this.health > 0){
+      return `${this.name} has received ${damage} points of damage`;
+    }
+    return `${this.name} has died in act of combat`;
+  }
+}
 
 // Saxon
 class Saxon {}
