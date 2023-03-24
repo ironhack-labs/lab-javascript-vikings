e75a923345d9c5ae610d4a7f03be85726bb12676
HeimLi <127536715+HeimLi@users.noreply.github.com>
2023-03-24 17:16:15 +0100

    debugged

--- src/viking.js
+++ src/viking.js
@@ -1,55 +1,74 @@
 // Soldier
 class Soldier {
     constructor(health, strength) {
-        this.health = health
-        this.strength = strength
-    
-    attack() {
-    console.log(this.health)}
-
-    receiveDamage(damage) {
-    this.health = this.health - damage}
-}
-
-// Viking
-class Viking extends Soldier {
-    constructor(name, health, strength) {
-        super (health, strength) 
-        this.name = name 
-
-    attack() {
-    console.log(this.health)}
-
-    receiveDamage(damage) {
-        this.healt = this.health - damage
-
-        if (this.health <= 0) {
-            console.log ("${this.name} has died in act of combat")
-        }
-        else {console.log("${this.name} has received ${"damage"} points of damage"}
+      this.health = health
+      this.strength = strength
     }
-
-    battleCry()
-    console.log("Odin Owns You All!");
-
-}
-// Saxon
-class Saxon extends Soldier {
-   
-    super(health, strength);
-
+  
     attack() {
-        console.log(this.strength)}
-
-    receiveDamage(damage)
-        this.health = this.health - damage}
-
-        if (this.health <= 0) {
-            console.log("A Saxon has died in combat")
-        }
-        else {console.log("A Saxon has received ${damage} points of damage"}
-
-}
-
-// War
-class War {}
+      return (this.strength)
+    }
+  
+    receiveDamage(damage) {
+      this.health = this.health - damage
+    }
+  }
+  
+  
+  // Viking
+  class Viking extends Soldier {
+    constructor(name, health, strength) {
+      super(health, strength)
+      this.name = name
+    }
+  
+    attack() {
+      return this.strength
+    }
+  
+    receiveDamage(damage) {
+      this.health = this.health - damage
+  
+      if (this.health >= 1) {
+        return `${this.name} has received ${damage} points of damage`
+      }
+      else {
+        return `${this.name} has died in act of combat`
+      }
+    }
+  
+      battleCry() {
+      return "Odin Owns You All!";
+    }
+  
+  }
+  // Saxon
+  class Saxon extends Soldier {
+    constructor(health, strength) {
+      super(health, strength);
+    }
+  
+    attack() {
+      return this.strength
+    }
+  
+    receiveDamage(damage) {
+      this.health = this.health - damage
+  
+      if (this.health >= 1) {
+        return `A Saxon has received ${damage} points of damage`
+      }
+      else {
+        return `A Saxon has died in combat`
+      }
+  
+    }
+  }
+  
+  // War
+  //class War { }
+  
+  
+  let viking = new Viking('Ragnar', 6, 9)
+  console.log(viking);
+  
