6a32d86bd5f75f0e88a932361cdf96f7a63faf30
Panthari Panthong <panthari.panthong@gmail.com>
2023-03-31 18:02:25 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name,health,strength) {
+        super(health, strength)
+        this.name = name
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+        return 'Odin Owns You All!'
+    }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage
+        if(this.health > 0){
+           return `A Saxon has received ${damage} points of damage`
+        }else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    vikingArmy = []
+    saxonArmy = []
+
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack(){
+
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * (this.saxonArmy.length-1))];
+        let saxonHealth = randomSaxon.health;
+
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * (this.vikingArmy.length-1))]
+        let vikingStrength = randomViking.strength
+
+
+        randomViking.receiveDamage(randomSaxon.attack())
+
+    }
+    // saxonAttack()
+    // showStatus()
+}
+
+
+// const garfield = new Viking('Harald', 150, 300);
+// console.log(garfield)
+
+const garfield = new War();
+console.log(garfield)
+
