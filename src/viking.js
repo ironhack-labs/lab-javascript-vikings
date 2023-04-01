85063c52ee6e3b46ad1cc9722bca04c6ac600ecc
Hector Riveros <89.hars@gmail.com>
2023-04-01 02:32:43 +0200

    progress

--- src/viking.js
+++ src/viking.js
@@ -1,23 +1,45 @@
 // Soldier
-class Soldier {health, strength}
+
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+    }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){ 
+        super(health, strength); // what happens if I pass anothe parameter in the super //
+        this.name = name;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0){
+            return `${this.name} has recived ${damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in the act of combat`
+        }
+    }
+    battleCry(){
+        return `Odin Owns You All!`
+    }
+}
+
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
-fdsafdasfdsafdsafdsafdsf
-fdsafdasfdsafdsafdsafdsfsf
-SharedArrayBufferds
-(dsaf
-    dsa
-    fdsa
-    f
-    dsaff
-    fasdf
-    ) => {
-    
-}
