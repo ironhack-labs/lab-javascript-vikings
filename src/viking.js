cb6d90a64c15cb6ac64512b37a915d88cd287a05
Hector Riveros <89.hars@gmail.com>
2023-04-03 08:29:41 +0200

    undone

--- src/viking.js
+++ src/viking.js
@@ -41,5 +41,17 @@ class Viking extends Soldier {
 // Saxon
 class Saxon {}
 
+class Saxon extends Soldier {
+    receiveDamage (damage) {
+        this.health = this.health - damage;
+        if(this.health > 0){
+            return `A saxon has received ${damage} points of damage`
+        }
+        else{
+            return (`A sxon has died in combat`)
+        }
+    }
+}
+
 // War
 class War {}
