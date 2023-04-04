eaed79954b17caa63b5138ffb4924b00a79ab96a
Bernard Cardoso <be1.0@hotmail.com>
2023-04-04 20:36:32 +0100

    1st Iteration done

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage){
+        damage - this.health;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    }
+}
 
 // Saxon
 class Saxon {}
