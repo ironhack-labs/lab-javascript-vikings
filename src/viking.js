8ad2c2f9a814728c9212d2d13277a11caa833517
Patricia <marie.lou.trips@gmail.com>
2023-04-04 22:36:59 +0200

    Iteration 1 done

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,27 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+    }
+
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+    };
+}
+
+
+// Viking
+class Viking {}
+
+// Saxon
+class Saxon {}
+
+// War
+class War {}
 
 // Viking
 class Viking {}
