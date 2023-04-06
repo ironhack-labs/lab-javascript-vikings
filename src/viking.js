418a299e6ed38e46a7407ced3dac4f9a53e57c5d
Bernard Cardoso <be1.0@hotmail.com>
2023-04-06 07:23:14 +0100

    War done!

--- src/viking.js
+++ src/viking.js
@@ -70,8 +70,8 @@ class War {
         return attackOfSaxon;
     }
     showStatus(){
-        if (this.saxonArmy = []){return "Vikings have won the war of the century!"}
-        else if (this.vikingArmy = []){return "Saxons have fought for their lives and survived another day..."}
+        if (this.saxonArmy.length = 0){return "Vikings have won the war of the century!"}
+        else if (this.vikingArmy.length = 0){return "Saxons have fought for their lives and survived another day..."}
         else {return "Vikings and Saxons are still in the thick of battle."}
     }
 }
