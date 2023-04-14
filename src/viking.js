82df83c6676cda472396a4d8d81a31e0357366f3
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-04-14 18:07:57 +0100

    lab doneeee

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,45 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health, strength){ 
+this.health = health; 
+this.strength= strength;
+}
+attack(){
+    return this.strength;
+}
+receiveDamage(damage){
+this.health-=damage;
+}
+}
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name; 
+
+    }receiveDamage(damage){
+        this.health -= damage;
+        if(this.health <= 0) return `${this.name} has died in act of combat`;
+        else return `${this.name} has received ${damage} points of damage`;
+    }battleCry(){
+        return "Odin Owns You All!"
+    }   
+    }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health <= 0) return `A Saxon has died in combat`;
+        else return `A Saxon has received ${damage} points of damage`
+    }
 
-// War
+}
+
+// should return "A Saxon has received DAMAGE points of damage", if the Saxon is still alive
+// should return "A Saxon has died in combat", if the Saxon dies
+
+// BONUS War
 class War {}
