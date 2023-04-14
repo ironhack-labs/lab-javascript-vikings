6bedf5e4b9c6f6d208b510a6c0a97d49df63a6a2
Robson Batista <robson_batista@icloud.com>
2023-04-14 15:58:10 +0100

    lab-javascript-vikings

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,46 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name = name;
+    
+
+    }
+
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if(this.health <= 0) return  `${this.name} has died in act of combat`;
+        return `${this.name} has received ${theDamage} points of damage`;
+    }
+
+    battleCry(){
+        return ("Odin Owns You All!");
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    receiveDamage(theDamage){
+        this.health -= theDamage;
+        if(this.health <= 0) return  ("A Saxon has died in combat");
+        return `A Saxon has received ${theDamage} points of damage`;
+    }}
 
 // War
 class War {}
