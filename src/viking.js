f4f0fcd64c256f6bbbc08a6ff8688af57f8a71b3
JLMarse <martinezserrano.joseluis@gmail.com>
2023-04-14 18:40:51 +0200

    Lorena y Jose Vikingos

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,88 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthValue, strengthValue) {
+        this.health = healthValue
+        this.strength = strengthValue
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+
+    }
+
+}
+
+
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(nameValue, healthValue, strengthValue) {
+        super(healthValue, strengthValue)
+        this.name = nameValue
+    }
+
+    //attack()
+
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health === 0) {
+            return `${this.name} has died in act of combat`
+        } else {
+            return `${this.name} has received ${damage} points of damage`
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        console.dir(this)
+        this.health -= damage
+
+        if (this.health === 0) {
+            return `A Saxon has died in combat`
+        } else {
+            return `A Saxon has received ${damage} points of damage`
+        }
+    }
+
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = []
+        this.saxonArmy = []
+
+    }
+
+
+    addViking(Viking) {
+
+        this.vikingArmy.push(Viking)
+    }
+
+
+
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack() {
+
+    }
+    //saxonAttack()
+    //showStatus()
+}
