a95051e2e13669e8923613239613b350003c424f
Philippe Miossec <pmiossec@gmail.com>
2023-05-12 17:13:37 +0200

    Soldier class

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage (damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
 class Viking {}
