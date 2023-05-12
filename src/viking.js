89b7b6f0780c974b384699dfb5221b2a6e081721
Philippe Miossec <pmiossec@gmail.com>
2023-05-12 17:22:37 +0200

    Viking and Saxon class

--- src/viking.js
+++ src/viking.js
@@ -15,10 +15,35 @@ class Soldier {
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength)  {
+        super(health, strength);
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+
+        return this.health > 0
+            ? `${this.name} has received ${damage} points of damage`
+            : `${this.name} has died in act of combat`;
+    }
+
+    battleCry = () => {
+        return "Odin Owns You All!";
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+
+        return this.health > 0
+            ? `A Saxon has received ${damage} points of damage`
+            : `A Saxon has died in combat`;
+    }    
+}
 
 // War
 class War {}
