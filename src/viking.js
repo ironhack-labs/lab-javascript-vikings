e6febffeb8c28473bdfbe1a70074105d28683a01
disaacjm <disaacjm@gmail.com>
2023-05-13 20:36:37 +0100

    almos done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,82 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+    }
+    
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, strength, health) {
+        super (strength, health)
+        this.name = name
+        }
+        receiveDamage(damage){
+            this.health -= damage
+            if (this.health > 0) {
+            return (`${this.name} has received ${damage} points of damage`);
+                        }else{
+             return (`${this.name} has died in act of combat`)
+            }
+        }
+        battleCry(){
+            return "Odin Owns You All!"
+    }    
+    }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier  {    
+    receiveDamage(damage){
+        this.health -= damage
+        if (this.health > 0) {
+        return (`A Saxon has received ${damage} points of damage`);
+                    }else{
+         return (`A Saxon has died in combat`)
+        }
+}
+}
+
 
 // War
-class War {}
+class War {
+    constructor(vikingArmy = [], saxonArmy = []){
+  this.vikingArmy = vikingArmy;
+    this.saxonArmy = saxonArmy;
+    }
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon)
+    }
+    vikingAttack() {
+    }
+    saxonAttack() {
+
+    }
+    showStatus() {
+        if (this.saxonArmy.length === 0) {
+          return 'Vikings have won the war of the century!';
+        } else if (this.vikingArmy.length === 0) {
+          return 'Saxons have fought for their lives and survived another day...';
+        } else {
+          return 'Vikings and Saxons are still in the thick of battle.';
+        }
+      }
+            
+        }
+ 
+
+
+addSaxon()
+vikingAttack()
+saxonAttack()
+showStatus()
