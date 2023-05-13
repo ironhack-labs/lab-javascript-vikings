a9b59ee3061dae7c7e9af68db33227fccda5cd7a
Gretil <gretil_15@hotmail.com>
2023-05-13 16:58:48 +0200

    All Iterations done, except refactor task

--- src/viking.js
+++ src/viking.js
@@ -32,7 +32,81 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+	attack() {
+		return this.strength;
+	}
+	receiveDamage(damage) {
+		this.health -= damage;
+		if (this.health > 0)
+			return `A Saxon has received ${damage} points of damage`;
+		else return `A Saxon has died in combat`;
+	}
+}
 
 // War
-class War {}
+
+class War {
+	vikingArmy = [];
+	saxonArmy = [];
+
+	addViking(viking) {
+		this.vikingArmy.push(viking);
+	}
+	addSaxon(saxon) {
+		this.saxonArmy.push(saxon);
+	}
+
+	// randomIndex {
+	// 	array[Math.floor(Math.random() * array.length)];
+	// }
+	// this didnt go as expected but i would like to ask wky
+
+	vikingAttack() {
+		/*
+      Viking = {
+        name: Greg,
+        health: 3,
+        strength: 4 }  
+      */
+		// battle
+
+		let result = this.saxonArmy[
+			Math.floor(Math.random() * this.saxonArmy.length)
+		].receiveDamage(
+			this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+				.strength
+		);
+
+		/*   saxonArmy = [
+      {health : 3
+        strength :4
+       }
+       */
+
+		this.saxonArmy = this.saxonArmy.filter((saxon) => saxon.health > 0);
+
+		return result;
+	}
+	saxonAttack() {
+		let result = this.vikingArmy[
+			Math.floor(Math.random() * this.vikingArmy.length)
+		].receiveDamage(
+			this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)].strength
+		);
+
+		this.vikingArmy = this.vikingArmy.filter((viking) => viking.health > 0);
+		// console.log(this.vikingArmy);
+
+		return result;
+	}
+	showStatus() {
+		if (this.saxonArmy.length === 0) {
+			return `Vikings have won the war of the century!`;
+		} else if (this.vikingArmy.length === 0) {
+			return `Saxons have fought for their lives and survived another day...`;
+		} else {
+			return `Vikings and Saxons are still in the thick of battle.`;
+		}
+	}
+}
