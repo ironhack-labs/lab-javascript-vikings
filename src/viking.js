86893aab6ad8327e9bc73d97ddc6c00f6f797f4a
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-13 17:37:37 +0200

    add bonus without generic method for vikingAttack() and saxonAttack()

--- src/viking.js
+++ src/viking.js
@@ -66,16 +66,60 @@ class War {
     }
 
     vikingAttack(){
-       
+       let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+       let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+
+       let saxonHealthLeft = this.saxonArmy[randomSaxon].receiveDamage(this.vikingArmy[randomViking].strength);
+
+       for (let i = 0; i < this.saxonArmy.length; i++) {
+        if (this.saxonArmy[i].health <= 0) {
+            this.saxonArmy.splice(i, 1);
+        }
+       }
+
+       return saxonHealthLeft;
+       /*
+       this.saxonArmy.forEach(function (saxonSoldier, index){
+        if (saxonSoldier.health <= 0) {
+            this.saxonArmy.splice(index, 1);
+        }
+       })
+       */
 
 
     }
 
     saxonAttack(){
+        let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+        let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
 
-    }
+        let vikingHealthLeft = this.vikingArmy[randomViking].receiveDamage(this.saxonArmy[randomSaxon].strength);
+        
+        for (let i = 0; i < this.vikingArmy.length; i++) {
+            if ( this.vikingArmy[i].health <= 0) {
+                this.vikingArmy.splice(i, 1);
+            }
+        }
+
+        return vikingHealthLeft;
+        /*
+        this.vikingArmy.forEach(function(vikingSoldier, index){
+            if (vikingSoldier.health <= 0) {
+                this.vikingArmy.splice(index, 1);
+            }
+        })
+        */
+        
+    }    
 
     showStatus(){
-
+        if (this.saxonArmy.length === 0) {
+            return `Vikings have won the war of the century!`;
+        } else if (!this.vikingArmy.length) {
+            return `Saxons have fought for their lives and survived another day...`;
+        } else {
+            return `Vikings and Saxons are still in the thick of battle.`;
+        }
     }
 }
+
