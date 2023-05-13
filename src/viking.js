0ec838f0326c7ce63f320d90a186fcfd76b4d878
Ola Bonati <olabonati@gmail.com>
2023-05-13 14:36:45 +0200

    viking lab done till bonus

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,81 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
 
+attack(){
+    return this.strength;
+}
+receiveDamage(damage){
+    this.health -= damage;
+   
+}
+}
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super ();
+        this.name = name;
+        this.health = health;
+        this.strength= strength;
+        
+    }
+
+receiveDamage(damage){
+            this.health -= damage;
+            if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        
+             } else if (this.health === 0) {
+        
+               return `${this.name} has died in act of combat`;
+        }
+    }
+    
+battleCry () {
+
+            return"Odin Owns You All!";
+                }
+
+    }    
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor (health, strength){
+    super();
+    this.health = health;
+    this.strength= strength;
+
+
+    }
+
+attack () {
+    return this.strength;
+
+}
+receiveDamage(damage){
+    this.health -= damage;
+    if (this.health >0) {
+        return `A Saxon has received ${damage} points of damage`;
+    } else if (this.health === 0){
+        return `A Saxon has died in combat`;
+    }
+}}
 
 // War
-class War {}
+class War {
+vikingArmy = [];
+saxonArmy = [];
+
+
+addViking(Viking) {
+    army.Viking
+
+}
+addSaxon() {
+
+}}
+
+let Soldier1 = new Soldier ()
