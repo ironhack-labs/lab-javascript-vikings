fa386bf78a649ed7e6eddd547ad8d67b08257215
Eirik Klebo <eiriktobias@gmail.com>
2023-05-15 00:45:29 +0200

    done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,42 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  receiveDamage(damage) {
+    return `${this.name} has received ${damage} points of damage`;
+    this.health -= 1;
+    
+  }
+
+  dies(died) {
+    return `${this.name} has died in act of combat`;
+  }
+
+  battleCry() {
+    return "Odin Owns You All";
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    return `"A Saxon has received ${damage} points of damage`;
+  }
+}
 
 // War
 class War {}
