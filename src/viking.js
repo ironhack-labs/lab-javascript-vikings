36b45abd25bd28ceeb14b57528dabb37240e836b
Mary Sue Meissner <marysue.meissner@outlook.com>
2023-05-14 15:09:26 +0400

    Preparation for War between the Vikings and the Saxons

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,118 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage){
+        this.health -= damage;
+   
+}
+};
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super('name');
+        this.name = name; 
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength
+    }
+
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+            }
+            else {
+            return `${this.name} has died in act of combat`;
+             }
+      }; 
+    
+
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+    
+};
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super();
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+          return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+            }
+            else {
+            return `A Saxon has died in combat`;
+             }
+      }; 
+
+};
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.saxonArmy = [];
+        this.vikingArmy = [];
+    };
+
+    addViking(Soldier) {
+        this.vikingArmy.push(Soldier);
+    };
+
+    addSaxon(Soldier) {
+        this.saxonArmy.push(Soldier);
+    };
+    
+    vikingAttack () {
+        vikingAttack.forEach(function(element, index){
+
+        });
+    }
+
+    saxonAttack () {
+        saxonAttack.forEach(function(element, index){
+
+        });
+    }
+
+    showStatus () {
+        showStatus.forEach(function(element, index){
+
+        });
+    }
+}
+
+    
+
+
+
+    
+
+
+
+
+
