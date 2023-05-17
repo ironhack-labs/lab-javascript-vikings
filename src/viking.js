6fb212a37b70d7e1ee24aec80081167cea22ec78
Juan Cruz <j.cruzb@outlook.com>
2023-05-18 00:03:46 +0200

    ejercicio completado

--- src/viking.js
+++ src/viking.js
@@ -45,4 +45,39 @@ class Saxon  extends Soldier{
 }
 
 // War
-class War {}
+class War {
+    constructor( ){
+        this.vikingArmy = [];
+        this.saxonArmy=[];
+    }
+    addViking(vikings){
+        this.vikingArmy.push(vikings);
+
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+    vikingAttack(){
+        this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+        if(this.saxonArmy[0].health<=0){
+            this.saxonArmy.pop();
+        }
+        return `A Saxon has died in combat`;
+    }
+    saxonAttack(){
+        this.vikingArmy[0].receiveDamage(this.saxonArmy[0].strength);
+        const vikingDie = this.vikingArmy[0]
+        if(this.vikingArmy[0].health<=0){
+            this.vikingArmy.pop();
+        }
+        return `${vikingDie.name} has received ${this.saxonArmy[0].strength} points of damage`
+    }
+    showStatus(){
+        if(this.saxonArmy.length===0){
+            return "Vikings have won the war of the century!"
+        }else if(this.vikingArmy.length===0){
+            return "Saxons have fought for their lives and survived another day..."
+        }
+        return "Vikings and Saxons are still in the thick of battle."
+    }
+}
