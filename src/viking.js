d6662241a8aa46cb70aea719ba10b1105b4b0fe7
Agathe Lurquin <agathe.lurquin@essca.eu>
2023-07-10 13:12:14 +0200

    Soldier, Viking, Saxon done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,63 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack(){
+    return this.strength
+  }
+
+  receiveDamage(damage){
+    this.health -= damage;
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength){
+    super(health, strength);
+    this.name = name;
+  }
+
+  attack(){
+    super.attack();
+    return this.strength;
+  }
+
+  receiveDamage(damage){
+    super.receiveDamage(damage);
+
+    if(this.health){
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
+    }
+
+  }
+
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage){
+    super.receiveDamage(damage);
+    if(this.health){
+      return `A Saxon has received ${damage} points of damage`
+    } else {
+      return `A Saxon has died in combat`
+    }
+
+  }
+}
+
+
+
 
 // War
 class War {}
