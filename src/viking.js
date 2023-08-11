349fbe5d1df7a4c127d35e7bfb9ac2c1d6c6a513
Da-vinia <daviniatosco@gmail.com>
2023-08-11 17:59:44 +0200

    lab solved

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,77 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+       this.health = this.health - damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+
+        super(health, strength)
+        this.name = name;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return   "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    // constructor() {
+
+    //     super()
+    // }
+
+ 
+        receiveDamage(damage) {
+            this.health = this.health - damage;
+            if(this.health > 0) {
+                return `A Saxon has received ${damage} points of damage`;
+            } else  {
+                return `A Saxon has died in combat`;
+            }
+    }
+
+}
+
+// Bonus
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = vikingArmy;
+        this.saxonArmy = saxonArmy;
+    }
+
+    addViking(Viking) {
+        this.vikingArmy
+    }
+
+
+
+}
