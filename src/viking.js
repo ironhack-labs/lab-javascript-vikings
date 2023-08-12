b3682eb2483cf61a4ff56bc07b5a3baca2e361a1
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-12 11:00:56 +0200

    wip

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,18 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) /*arguments*/ {
+        this.health = health; /*property with its  1 argument*/
+        this.strength = strength; /*property with its  2 argument*/
+    }
 
+    attack() { /*attack method-its a function with no arguments-return the streng property of the soldier */
+        return this.strength;
+    }
+
+    receiveDamage(damage) { /*function-recieve one argument (damage) - substracts the recieved damage from health1 */
+        this.health -= damage;
+    }
+}
 // Viking
 class Viking {}
 
