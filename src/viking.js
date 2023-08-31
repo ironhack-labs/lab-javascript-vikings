6ecc5494a8e93908f8b90a2e6afed17560301528
chloe <marin.ducarme@gmail.com>
2023-08-31 22:24:30 +0200

    first changes

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,50 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength; }
+
+        attack() {
+            return this.strength;
+        }
+        receiveDamage(damage) {
+            this.health -= damage;
+        }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+      this.name = name;
+      this.health = health;
+      this.strength = strength; }
+    
+      receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health >= 1 ) {
+            return `${name} has received ${damage} points of damage`;
+        } else {
+            return `${name} has died in act of combat`;
+        }
+      }
+      
+      battleCry() {
+        return "Odin Owns You All!";
+      }
+    }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+    receiveDamage(damage) {
+      this.health -= damage;
+      if (this.health >= 1 ) {
+        return `A Saxon has received ${damage} points of damage`;
+      } else {
+        return "A Saxon has died in combat";
+      }
+    }
+}
 
 // War
 class War {}
