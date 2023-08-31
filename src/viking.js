93792b5722fcc14394f2c12b558c5b4c2cbdee2f
Ian IJmkers <ianymkers@gmail.com>
2023-08-31 21:49:19 +0200

    The class really helped with constructing class and the components within the constructor. Soldier iteration was quite simple as it reminded me a lot of the destiny example i used as a practice in class. Instructions were quite straightforward. I used online resources when I came across errors like a deprecating variable. Implementing functions in this lab made functions a lot clearer. String interpolation was quite simple. I also added console.logs and the new soldier to see what my initial values were. Then I proceeded to Vikings whereby the extender was something I read up on during the class because I wanted to initially use it for my class practice. this was also quite straightforward. attack method was quite simple too. I was proud of the receiveDamage part because it was probably the first time I didnt have to recite anything from my materials or online resources.

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,43 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+      this.health = health;
+      this.strength = strength;  
+    }
+  attack(){
+      return this.strength; 
+    }
+  receiveDamage(damage) {
+    this.health -= damage;
+    }
+  }
+  const soldier = new Soldier (200, 60);
+  console.log(`The initial health of soldier: ${soldier.health}`);
+  console.log(`The initial strength of solider: ${soldier.strength}`);
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super(name, health, strength)
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+attack () {
+  return this.strength;
+  }
+receiveDamage(damage) {
+  this.health -= damage;
+
+  if (this.health > 0) {
+  return `${this.name} has received ${damage} points of damage`;
+  }
+
+  else {
+  return `${this.name} has died in act of combat`;
+  }
+  }
+}
 
 // Saxon
 class Saxon {}
