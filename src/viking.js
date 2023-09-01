3af38b2d50b9d5028066d8c77a0e0178e63edabc
zsaritek <zsaritek@gmail.com>
2023-09-01 16:45:22 +0200

    WD-PT-RMT-AUGUST2023-VIKINGS-ZEYNEP SARITEKE(module1-week3-day2)SOLVED LAB

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,85 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return `Odin Owns You All!`
+    }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(the_damage) {
+        this.health -= the_damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${the_damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+
+
+    }
+
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+
+    }
+
+    vikingAttack() {
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        let viking = this.vikingArmy[randomVikingIndex];
+
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        let saxon = this.saxonArmy[randomSaxonIndex];
+        let message = saxon.receiveDamage(viking.attack());
+        if (saxon.health <= 0) {
+            this.saxonArmy.splice(randomSaxonIndex, 1);
+        }
+        return message;
+    }
+
+    saxonAttack() {
+        const randomIndex = Math.floor(Math.random() * saxonArmy.length);
+    }
+}
