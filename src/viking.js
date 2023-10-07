03b6d018097268423b77ce22e2054cc8ed66157b
Victor Junquera <victorjunquera@eventalista.es>
2023-10-07 09:00:19 +0200

    lab done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,103 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        this.health = health
+        this.strength = strength
+    }
+    
+    attack() {
+        return this.strength
+    }
+    
+    receiveDamage(damage) {
+        this.health -= damage
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength) {
+        super (health, strength)
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+        if (this.health > 0) {
+            return (`${this.name} has received ${damage} points of damage`)
+        } else {
+            return (`${this.name} has died in act of combat`)
+        }
+    }
+
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+    receiveDamage (damage) {
+        this.health -= damage
+        if (this.health > 0) {
+            return (`A Saxon has received ${damage} points of damage`)
+        } else  {
+            return (`A Saxon has died in combat`)
+        }
+    }
+
+}
 
 // War
-class War {}
+class War {
+    constructor () {
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+
+    addViking (viking) {
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon (saxon) {
+        this.saxonArmy.push(saxon)
+    }
+
+    attack(attacker, defender) {
+        const randomAttackerIndex = Math.floor(Math.random() * attacker.length);
+        const randomDefenderIndex = Math.floor(Math.random() * defender.length);
+        const randomAttacker = attacker[randomAttackerIndex];
+        const randomDefender = defender[randomDefenderIndex];
+    
+        const damage = randomAttacker.attack();
+        const result = randomDefender.receiveDamage(damage);
+    
+        if (randomDefender.health <= 0) {
+          defender.splice(randomDefenderIndex, 1);
+        }
+    
+        return result;
+      }
+    
+      vikingAttack() {
+        return this.attack(this.vikingArmy, this.saxonArmy);
+      }
+    
+      saxonAttack() {
+        return this.attack(this.saxonArmy, this.vikingArmy);
+      }
+    
+      showStatus() {
+        if (this.saxonArmy.length === 0) {
+          return 'Vikings have won the war of the century!';
+
+        } else if (this.vikingArmy.length === 0) {
+          return 'Saxons have fought for their lives and survived another day...';
+          
+        } else {
+          return 'Vikings and Saxons are still in the thick of battle.';
+        }
+      }
+    
+}
