97df35fd0b5643347e63782cd0ac30709655cb7a
Pabloluis9 <140593315+Pabloluis9@users.noreply.github.com>
2023-10-15 18:40:57 +0200

    Modificado

--- src/viking.js
+++ src/viking.js
@@ -110,9 +110,9 @@ saxonAttack() {
 }
 
 showStatus() {
-    if (this.saxonArmy.isEmpty()) {
+    if (this.saxonArmy.length === 0) {
         return "Vikings have won the war of the century!";
-    } else if (this.vikingArmy.isEmpty()) {
+    } else if (this.vikingArmy.length === 0) {
         return "Saxons have fought for their lives and survived another day...";
     } else {
         return "Vikings and Saxons are still in the thick of battle.";
