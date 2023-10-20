23446ccb3c33e721b3ef13043e1df624cea9b133
Matt Miller <just.matt.miller@gmail.com>
2023-10-20 20:14:31 +0200

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,79 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+    }
+}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `"${this.name} has received ${this.damage} points of damage"`;
+        } else {
+            return `"${this.name} has died in an act of combat"`;
+        }
+    }
+    battleCry() {
+        return ("Odin Owns You All!")
+    }
+}
 
-// Viking
-class Viking {}
+class Saxon extends Soldier { 
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return (`A Saxon has received ${this.damage} points of damage`);
+        } else {
+            return ("A Saxon has died in combat");
+        }
+    }
+}
 
-// Saxon
-class Saxon {}
+class War { 
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = []; 
+    }
+    addViking(viking) {
+        this.vikingArmy += viking;
+    }
+    addSaxon(saxon) {
+        this.saxonArmy += saxon;
+    }
+    vikingAttack() {
+        Saxon.receiveDamage(damage = Viking.strength);
+        if (Saxon.health < 1) {
+            this.saxonArmy -= saxon;}
+    }
+    saxonAttack() {
+        Viking.receiveDamage(damage = Saxon.strength);
+        if (Viking.health < 1) {
+            this.vikingArmy -= viking;}
+    }
+    showStatus(){
+        if (this.saxonArmy = []) {
+            return ("Vikings have won the war of the century!");}
+        else {(this.vikingArmy = []) 
+            return ("Saxons have fought for their lives and survived another day...");}   
 
-// War
-class War {}
+    }
+
+    }
+//}
+//receiveDamage(){
+
+//}
+//battleCry(){
+
+//}
