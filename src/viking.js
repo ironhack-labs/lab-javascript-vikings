04c2f7a16fa238aeec276d371d868109b3b4545b
NISHA PATIL <prashantpatil@PRASHANTs-Air.home>
2023-10-20 21:33:25 +0200

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,40 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  } attack(){
+    return this.strength;
+  }
+  receiveDamage(damage){
+    this.health -= damage;
+  }   
+}
+   
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super('abc', health, strength);
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+
+  }
+  attack(){
+    return this.strength;
+  }
+}
+const Odin = new Viking;
+Odin.battleCry();
+
+battleCry() 
+    console.log ("Odin Owns You All!");
+
+
+
+
 
 // Saxon
 class Saxon {}
