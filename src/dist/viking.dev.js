ed47cefd70f999c2f3a9a349c93dd0e0ab1e0a1c
Your Name <you@example.com>
2023-10-23 09:59:16 +0200

    done

---
+++ src/dist/viking.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// Soldier
+var Soldier = function Soldier() {
+  _classCallCheck(this, Soldier);
+}; // Viking
+
+
+var Viking = function Viking() {
+  _classCallCheck(this, Viking);
+}; // Saxon
+
+
+var Saxon = function Saxon() {
+  _classCallCheck(this, Saxon);
+}; // War
+
+
+var War = function War() {
+  _classCallCheck(this, War);
+};
