8332a99505d4a501520c637b8e8b7b3c02c7bb68
Eduardo Botelho <eduardo11224b@gmail.com>
2023-12-01 19:05:21 +0000

    created saxon class

--- src/viking.js
+++ src/viking.js
@@ -35,7 +35,15 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    }
+    return `A Saxon has received ${damage} points of damage`;
+  }
+}
 
 // War
 class War {}
