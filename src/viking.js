974d6defe70b8d0f3531856648aae2e13c72807b
Nadine Rangosch <nrangosch@gmail.com>
2023-12-01 17:28:59 +0100

    tested game, modified receiveDamage, showStatus and attack functions, added some console logs for a playable game

--- src/viking.js
+++ src/viking.js
@@ -18,15 +18,20 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
-  receiveDamage(damage){
-    super.receiveDamage(damage)
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
     if (this.health > 0) {
+        // console for trial game
+      console.log(`${this.name} has received ${damage} points of damage`);
       return `${this.name} has received ${damage} points of damage`;
     } else {
+      console.log(`${this.name} has died in act of combat`);
       return `${this.name} has died in act of combat`;
     }
   }
   battleCry() {
+    //console for trial game
+    console.log("Odin Owns You All!");
     return "Odin Owns You All!";
   }
 }
@@ -34,10 +39,13 @@ class Viking extends Soldier {
 // Saxon
 class Saxon extends Soldier {
   receiveDamage(damage) {
-    super.receiveDamage(damage)
+    super.receiveDamage(damage);
     if (this.health > 0) {
+        // console for trial game
+      console.log(`A Saxon has received ${damage} points of damage`);
       return `A Saxon has received ${damage} points of damage`;
     } else {
+      console.log(`A Saxon has died in combat`);
       return `A Saxon has died in combat`;
     }
   }
@@ -48,46 +56,54 @@ class War {
   addViking(vikingObj) {
     this.vikingArmy.push(vikingObj);
   }
+
   addSaxon(saxonObj) {
     this.saxonArmy.push(saxonObj);
   }
+
   vikingAttack() {
-    const randomSaxon =
-      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
-    const randomViking =
-      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
-      if(randomSaxon.health<=randomViking.strength){
-        let index = this.saxonArmy.indexOf(randomSaxon)
-        this.saxonArmy.splice(index, 1)
-      }
-
-    
-
-    return randomSaxon.receiveDamage(randomViking.strength);
+    if (this.vikingArmy.length === 0) {
+      this.showStatus();
+      return;
+    }
+    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let attackResult = this.saxonArmy[randomSaxon].receiveDamage(
+      this.vikingArmy[randomViking].attack()
+    );
+    if (this.saxonArmy[randomSaxon].health <= 0) {
+      let index = this.saxonArmy[randomSaxon];
+      this.saxonArmy.splice(index, 1);
+    }
+    return attackResult;
   }
+
   saxonAttack() {
-    const randomSaxon =
-      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
-    const randomViking =
-      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
-      if(randomViking.health<=randomSaxon.strength){
-        let index = this.vikingArmy.indexOf(randomViking)
-        this.vikingArmy.splice(index, 1)
-      }
-
-    
-
-    return randomViking.receiveDamage(randomSaxon.strength);
+    if (this.saxonArmy.length === 0) {
+      this.showStatus();
+      return;
+    }
+    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    let randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    let attackResult = this.vikingArmy[randomViking].receiveDamage(
+      this.saxonArmy[randomSaxon].attack()
+    );
+    if (this.vikingArmy[randomViking].health <= 0) {
+      let index = this.vikingArmy[randomViking];
+      this.vikingArmy.splice(index, 1);
+    }
+    return attackResult;
   }
+
   showStatus() {
-    if (this.saxonArmy.length ===0){
-        return "Vikings have won the war of the century!"
+    if (this.vikingArmy.length !== 0 && this.saxonArmy.length !== 0) {
+        return "Vikings and Saxons are still in the thick of battle.";
+      }
+    else if (this.saxonArmy.length === 0) {
+      return "Vikings have won the war of the century!";
     }
-    else if(this.vikingArmy.length === 0){
-        return "Saxons have fought for their lives and survived another day..."
-    }
-    else{
-        return "Vikings and Saxons are still in the thick of battle."
+    else if (this.vikingArmy.length === 0) {
+      return "Saxons have fought for their lives and survived another day...";
     }
   }
   constructor() {
@@ -95,3 +111,72 @@ class War {
     this.saxonArmy = [];
   }
 }
+
+// // Trial Game
+// // Creating a war
+// const battle = new War();
+
+// // Creating and add soldiers to the armies
+// function generate5Vikings() {
+//   const viking1 = new Viking("Ragnar", 100, 150);
+//   battle.addViking(viking1);
+//   const viking2 = new Viking("Floki", 80, 130);
+//   battle.addViking(viking2);
+//   const viking3 = new Viking("Astrid", 100, 100);
+//   battle.addViking(viking3);
+//   const viking4 = new Viking("Lagertha", 120, 120);
+//   battle.addViking(viking4);
+//   const viking5 = new Viking("Björn", 120, 140);
+//   battle.addViking(viking5);
+// }
+
+// function generate6Saxons() {
+//   const saxon1 = new Saxon(100, 70);
+//   battle.addSaxon(saxon1);
+//   const saxon2 = new Saxon(80, 70);
+//   battle.addSaxon(saxon2);
+//   const saxon3 = new Saxon(100, 70);
+//   battle.addSaxon(saxon3);
+//   const saxon4 = new Saxon(100, 70);
+//   battle.addSaxon(saxon4);
+//   const saxon5 = new Saxon(100, 70);
+//   battle.addSaxon(saxon5);
+//   const saxon6 = new Saxon(100, 70);
+//   battle.addSaxon(saxon6);
+// }
+// generate5Vikings();
+// generate6Saxons();
+
+// console.log(battle.vikingArmy);
+// console.log(battle.saxonArmy);
+
+// // Attacks
+// battle.saxonAttack();
+
+// battle.vikingArmy[0].battleCry();
+// battle.vikingAttack();
+// battle.showStatus();
+// battle.saxonAttack();
+// battle.showStatus();
+
+// battle.vikingArmy[1].battleCry();
+// battle.vikingAttack();
+// battle.saxonAttack();
+
+// battle.showStatus();
+// battle.vikingAttack();
+// battle.showStatus();
+// battle.saxonAttack();
+
+// battle.showStatus();
+// battle.vikingAttack();
+// battle.showStatus();
+// battle.saxonAttack();
+
+// battle.showStatus();
+// battle.vikingAttack();
+// battle.saxonAttack();
+// battle.vikingAttack();
+// battle.saxonAttack();
+// console.log(battle.showStatus());
+
