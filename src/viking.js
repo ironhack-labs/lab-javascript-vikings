502ced7642f0b9c0dd281bdd599c8030a0c3f84e
Christian Doeller <mail@christiandoeller.de>
2023-12-02 10:48:12 +0100

    started bonus 1

--- src/viking.js
+++ src/viking.js
@@ -34,20 +34,62 @@ class Viking extends Soldier {
 
 // Saxon
 class Saxon extends Soldier {
-// no constructor because inherits perfectly
-  receiveDamage(damage){
+  // no constructor because inherits perfectly
+  receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
-        return `A Saxon has received ${damage} points of damage`;
-      } else {
-        return `A Saxon has died in combat`;
-      }
+      return `A Saxon has received ${damage} points of damage`;
+    } else {
+      return `A Saxon has died in combat`;
+    }
   }
 }
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(viking) {
+    this.vikingArmy.push(viking);
+  }
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon);
+  }
+  vikingAttack() {
+    // choose random soldiers
+    let saxonSoldier =
+      this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    let vikingSoldier =
+      this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    // saxon receives damage
+    let result = saxonSoldier.receiveDamage(vikingSoldier.strength);
+    // remove dead saxons
+    for (let i = 0; i < this.saxonArmy.length; i++){
+        if (this.saxonArmy[i].health<0){
+            this.saxonArmy.splice(i,1);
+        }
+    }
+    return result;
+  }
+  saxonAttack() {
+        // choose random soldiers
+        let saxonSoldier =
+        this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+      let vikingSoldier =
+        this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+      // viking receives damage
+      let result = vikingSoldier.receiveDamage(saxonSoldier.strength);
+      // remove dead vikings **** DOES NOT WORK ****
+      for (let i = 0; i < this.vikingArmy.length; i++){
+          if (this.vikingArmy[i].health < 0){
+              this.vikingArmy.splice(i,1);
+          }
+      }
+      return result;
+  }
 
-/*
+  showStatus() {}
 
-*/
+}
