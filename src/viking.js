698b54a1523fb695eee2d37515fd1aace978fb17
Jesús Sánchez <jsxexu38@gmail.com>
2023-12-18 12:35:04 +0100

    feat: iteration 4 & 5

--- src/viking.js
+++ src/viking.js
@@ -69,11 +69,38 @@ addSaxon(saxon) {
 }
 
 vikingAttack() {
-    const viking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
-    const saxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
-
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
     
+    let result = undefined
+    result = randomSaxon.receiveDamage(randomViking.strength)
+
+    if (randomSaxon.health <= 0) {
+        this.saxonArmy.splice(randomSaxon, 1)
+    }
+    return result
 }
-saxonAttack() {}
-showStatus() {}
+
+saxonAttack() {
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)]
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)]
+
+    let result = undefined
+    result = randomViking.receiveDamage(randomSaxon.strength)
+
+    if (randomViking.health <= 0) {
+        this.vikingArmy.splice(randomViking, 1)
+    }
+    return result
+}
+
+showStatus() {
+    if (this.saxonArmy.length === 0) {
+        return "Vikings have won the war of the century!"
+    } else if (this.vikingArmy.length === 0) {
+        return "Saxons have fought for their lives and survived another day..."
+    } else {
+        return "Vikings and Saxons are still in the thick of battle."
+    }
+}
 }
