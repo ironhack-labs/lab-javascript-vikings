032a519ef3a57e56f60835145f22250c5155c271
Madhushree B <boylemadhushree@gmail.com>
2024-01-07 00:24:22 +0100

    iteration 5

--- src/viking.js
+++ src/viking.js
@@ -46,12 +46,55 @@ class Saxon extends Soldier {
         }
 }
 
-// War
-class War () {
-    
-    addViking(){} 
-    addSaxon() 
-    vikingAttack()
-    saxonAttack()
-    showStatus()
+class War {
+  constructor () {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+
+  }
+    addViking(viking){
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack() {
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+        let saxonStatus = randomSaxon.receiveDamage(randomViking.strength);
+
+        if(saxonStatus === 'A Saxon has died in combat') {
+            this.saxonArmy.pop();
+        }
+        return saxonStatus;
+    }
+    saxonAttack() {
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+        let vikingStatus = randomViking.receiveDamage(randomSaxon.strength);
+
+        if(vikingStatus === `${randomViking.name} has died in act of combat`) {
+            this.vikingArmy.pop();
+        }
+        return vikingStatus;
+    }
+
+    showStatus() {
+        // let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        // let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+
+        if(this.saxonArmy.length === 0){
+            return ("Vikings have won the war of the century!")
+        } else if (this.vikingArmy.length === 0) {
+            return ("Saxons have fought for their lives and survived another day...")
+        } else {
+            return ("Vikings and Saxons are still in the thick of battle.")
+        }       
+    }
 }
+
+
+
+
