e180adcd05271502879d217b2d1314f015600dfa
Vituin <124085647+Vituin@users.noreply.github.com>
2024-01-22 21:43:12 +0100

    No solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(healthValue, strengthValue) {
+        this.health = healthValue
+        this.strength = strengthValue
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+    }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name
+    }
+}
+
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
