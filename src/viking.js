3f849d7a7c874cde6ac17c501bd2536bb0fb0110
Abraham Agusti <abraham.agusti@gmail.com>
2024-01-25 21:30:39 +0100

    Iteration 1, iteration 2 50%

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,51 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health=health;
+	    this.strength=strength;        
+    }
+
+    attack(){
+        return this.strength
+    }
+
+    receiveDamage(thedamage){
+        this.health -= thedamage
+    }
+
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super (health, strength)
+        this.name = name;
+    }
+    
+    receiveDamage(thedamage){
+        this.health -= thedamage
+
+        if (this.health >0 ){
+            return '${this.name} has received ${thedamage} points of damage'
+        }
+        else {
+            return  '${this.name} has died in act of combat'
+        }
+
+    }
+
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+    
+} 
+
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
+
