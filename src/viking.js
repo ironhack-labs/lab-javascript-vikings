4da033f50809e43bbb4411b979fdc1a0a7024529
Arnau Ramos <arnauramos21@gmail.com>
2024-01-25 21:00:23 +0100

    iteration1 done

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,16 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor (health, strength) {
+        this.health = health,
+        this.strength = strength
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage) {
+        this.health -= damage
+    }
+}
 
 // Viking
 class Viking {}
