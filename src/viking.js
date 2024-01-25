b9bff26daee625d7253499a57cd19e20d6bdf498
guzegui <guzmanegui@gmail.com>
2024-01-25 10:12:30 +0100

    Remove debug comments and commands from Iteration 3

--- src/viking.js
+++ src/viking.js
@@ -40,16 +40,12 @@ class Saxon extends Soldier {
   receiveDamage(damage) {
     super.receiveDamage(damage);
     if (this.health > 0) {
-      console.log(this.constructor.name);
       return `A ${this.constructor.name} has received ${damage} points of damage`;
     } else {
       return `A ${this.constructor.name} has died in combat`;
     }
   }
 }
-
-const testSaxon = new Saxon(50, 50);
-console.log(testSaxon.receiveDamage(10));
 
 // War
 class War {}
