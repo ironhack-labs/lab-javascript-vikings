2b4ab2e0f7ccaa56a12d99980fa080759593bbb7
Arnau Ramos <arnauramos21@gmail.com>
2024-01-26 17:55:22 +0100

    did BONUS: Iteration 5

--- src/viking.js
+++ src/viking.js
@@ -94,4 +94,18 @@ class War {
 
         return receivedDamage;
     }
+
+    //Show Status
+
+    showStatus() {
+        if (this.saxonArmy.length == 0) {
+            return "Vikings have won the war of the century!"
+        } else if (this.vikingArmy.length == 0) {
+            return "Saxons have fought for their lives and survived another day..."
+        } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
 }
+
+
