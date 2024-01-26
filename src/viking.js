ce6456d815fcef58da7f4d27ca24f173d090a2dd
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-01-26 23:03:22 +0100

    vikings lab exercise

--- src/viking.js
+++ src/viking.js
@@ -49,4 +49,61 @@ class Saxon extends Soldier {
 
 
 // War
-class War {}
+class War {
+    constructor () {
+        this.vikingArmy = [],
+        this.saxonArmy = []
+    }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking)
+    }
+
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon) 
+    }
+
+    vikingAttack() {
+        //select random saxon and viking
+        const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        
+        // Calculate damage on saxon
+        const receivedDamage = randomSaxon.receiveDamage(randomViking.attack());
+
+        // Remove dead Saxons from the army
+        if (randomSaxon.health <= 0) {
+            this.saxonArmy.splice(Math.floor(Math.random() * this.saxonArmy.length), 1);
+        }
+
+        // Return received damage of the Saxon
+        return receivedDamage;
+    }
+    saxonAttack() {
+        
+        const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        
+        
+        const receivedDamage = randomViking.receiveDamage(randomSaxon.attack());
+
+        
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice(Math.floor(Math.random() * this.vikingArmy.length), 1);
+        }
+
+        return receivedDamage;
+    }
+
+    //Show Status
+
+    showStatus() {
+        if (this.saxonArmy.length == 0) {
+            return "Vikings have won the war of the century!"
+        } else if (this.vikingArmy.length == 0) {
+            return "Saxons have fought for their lives and survived another day..."
+        } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1) {
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
+}
