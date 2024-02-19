b04ae0ff4db2d58202fcedf08b8bf1e265bb10bc
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-19 11:50:43 +0100

    viking class done

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,41 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack = () => {
+    return this.strength;
+  };
+  receiveDamage = (damage) => {
+    this.health = this.health - damage;
+  };
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength);
+    this.name = name;
+  }
+  battleCry = () => {
+    return "Odin Owns You All!";
+  };
+  receiveDamage = (damage) => {
+    this.health = this.health - damage;
+  };
+  receiveDamage = (damage) => {
+    this.health = this.health - damage;
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    } else {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+  };
+}
 
 // Saxon
-class Saxon {}
 
+class Saxon extends Soldier {}
 // War
 class War {}
