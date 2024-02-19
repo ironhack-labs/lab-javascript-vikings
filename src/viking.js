c1b7ad922783a574ea0531b9ab3202f74a7dd4db
Hemyulin <daniel.budiansky@protonmail.com>
2024-02-19 11:52:36 +0100

    saxon class done

--- src/viking.js
+++ src/viking.js
@@ -36,6 +36,15 @@ class Viking extends Soldier {
 
 // Saxon
 
-class Saxon extends Soldier {}
+class Saxon extends Soldier {
+  receiveDamage = (damage) => {
+    this.health = this.health - damage;
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    } else {
+      return `A Saxon has received ${damage} points of damage`;
+    }
+  };
+}
 // War
 class War {}
