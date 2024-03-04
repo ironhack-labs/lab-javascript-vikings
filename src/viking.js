ef41fd1b054a1a0a155e8afc28075ca0c66631c6
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-03-04 12:36:41 +0100

    1st Commit 04.03.2024

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,57 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    ReceiveDamage(damage) {
+        this.health -= damage;
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+
+    ReceiveDamage(damage) {
+        super.ReceiveDamage(damage);
+        if (this.health <= 0) {
+            return `${this.name} has dies in act of combat`;
+        } else {
+            return `${this.name} has received DAMAGE points of damage`;
+        }
+    }
+    battleCry() {
+        return `Odin Owns You All!`;
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+    ReceiveDamage(damage) {
+        super.ReceiveDamage(damage);
+        if (this.health <= 0) {
+            return `A Saxon has died in combat`;
+        } else {
+            return `A Saxon has received ${damage} points of damage`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(health, strength) { }
+}
+
+
