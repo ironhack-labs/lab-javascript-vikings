e4d5517b37647214f3bac442968ddc485da02ec9
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-03-05 18:47:27 +0100

    fixed. 05.03.2024

--- src/viking.js
+++ src/viking.js
@@ -51,48 +51,82 @@ class Saxon extends Soldier {
     }
 }
 
+
 // War
 class War {
     constructor() {
         this.vikingArmy = [];
         this.saxonArmy = [];
     }
-    addViking(Viking) {
-        this.vikingArmy.push(Viking);
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
     }
-    addSaxon(Saxon) {
-        this.saxonArmy.push(Saxon);
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
     }
+
     vikingAttack() {
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomSaxon = this.saxonArmy[randomSaxonIndex];
 
-    const randomSaxon = Math.floor(Math.random(this.saxonArmy.length));
-    const damage =
-    receiveDamage() 
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomViking = this.vikingArmy[randomVikingIndex];
+        const damageDealt = randomViking.attack();
 
+        const message = randomSaxon.receiveDamage(damageDealt);
+
+        this.saxonArmy.forEach((saxon, index) => {
+            if (saxon.health <= 0) {
+                this.saxonArmy.splice(index, 1);
+            }
+        });
+
+        return message;
     }
 
-// should make Saxon receiveDamage() equal to the strength of a Viking
-// should remove dead saxons from the army
-// should return result of calling receiveDamage() of a Saxon with the strength of a Viking
+    saxonAttack() {
+        if (this.saxonArmy.length === 0) {
+            return "Saxons have no one to attack!";
+        }
 
-    saxonAttack() { }
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomViking = this.vikingArmy[randomVikingIndex];
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomSaxon = this.saxonArmy[randomSaxonIndex];
+        const damageDealt = randomSaxon.attack();
 
-// should make a Viking receiveDamage() equal to the strength of a Saxon
-// should remove dead vikings from the army
-// should return result of calling receiveDamage() of a Viking with the strength of a Saxon
+        const message = randomViking.receiveDamage(damageDealt);
 
-    showStatus() { }
+        if (randomViking.health <= 0) {
+            this.vikingArmy.splice(randomVikingIndex, 1);
+        }
 
-// should return "Vikings have won the war of the century!", if the Saxons array is empty
-// should return "Saxons have fought for their lives and survived another day...", if the Vikings array is empty
-// should return "Vikings and Saxons are still in the thick of battle.", if there are still both Vikings and Saxons
+        return message;
+    }
 
+    showStatus() {
+        let vikingCount = 0;
+        let saxonCount = 0;
+
+        this.vikingArmy.forEach(viking => vikingCount++);
+        this.saxonArmy.forEach(saxon => saxonCount++);
+
+        if (saxonCount === 0) {
+            return "Vikings have won the war of the century!";
+        } else if (vikingCount === 0) {
+            return "Saxons have fought for their lives and survived another day...";
+        } else {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+    }
 }
 
 
-const viking01 = new Viking(`Viking 01`,100,20);
-// name, health, strength
-const saxon01 = new Saxon(80,15);
-// health, strength
+const viking1 = new Viking("VIKING 01", 100, 20);
+const viking2 = new Viking("VIKING 02", 120, 25);
+const saxon1 = new Saxon(80, 10);
+const saxon2 = new Saxon(120, 20);
 
 const war = new War();
