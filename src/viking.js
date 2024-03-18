b9022d51f226a3551d05a80a489fc4a256342602
Francisco Coutinho <francisco.coutinho.so@gmail.com>
2024-03-18 23:18:15 +0000

    FT_PT_032024_FranciscoCoutinho

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,92 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }  
+attack (){
+    return this.strength
+}
+receiveDamage(damage){
+    this.health -= damage;
+    
+}
+
+};
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name,health, strength) {
+        super(name);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+    
+};
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+    constructor(health, strength) {
+        super( strength);
+        this.health = health;
+        this.strength = strength;
+
+    }
+    attack () {
+        return this.strength 
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        if(this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        }else {
+            return `A Saxon has died in combat`;
+        }
+    }
+};
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+    vikingsAttack() {
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomSaxon = this.saxonArmy[randomSaxonIndex];
+       
+
+    }
+    saxonsAttack() {
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomViking = this.vikingArmy[randomVikingIndex];
+
+    }
+    showStatus() {
+
+    }
+
+    
+};
+
